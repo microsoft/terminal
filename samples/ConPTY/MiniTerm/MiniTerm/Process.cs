637c57473e923e5185c79c0b7d72beacf894be62
Will Fuqua <wafuqua@gmail.com>
2018-09-20 22:13:37 +0700

    add c# files
    - Move from rather ad-hoc, error-prone resource management to IDisposable, which should give us a bit more enforcement.
    - Optimistically remove "buggy" from readme because the known bugs are now fixed! The main source of bugs was the incorrect InitializeProcThreadAttributeList usage.
    - Handle ctrl-c by forwarding it to the PseudoConsole
    - Handle terminal close when the window close button is used
    - Use .NET's CopyTo in the CopyPipeToOutput, it's much simpler code and seems more robust than the ReadFile/WriteFile approach
    - Minor refactor to split native APIs to multiple files

---
+++ samples/ConPTY/MiniTerm/MiniTerm/Process.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using static MiniTerm.Native.ProcessApi;
+
+namespace MiniTerm
+{
+    /// <summary>
+    /// Support for starting and configuring processes.
+    /// </summary>
+    /// <remarks>
+    /// Possible to replace with managed code? The key is being able to provide the PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE attribute
+    /// </remarks>
+    static class Process
+    {
+        /// <summary>
+        /// Start and configure a process. The return value should be considered opaque, and eventually disposed.
+        /// </summary>
+        internal static ProcessResources Start(IntPtr hPC, string command, IntPtr attributes)
+        {
+            var startupInfo = ConfigureProcessThread(hPC, attributes);
+            var processInfo = RunProcess(ref startupInfo, "cmd.exe");
+            return new ProcessResources(startupInfo, processInfo);
+        }
+
+        private static STARTUPINFOEX ConfigureProcessThread(IntPtr hPC, IntPtr attributes)
+        {
+            var lpSize = IntPtr.Zero;
+            var success = InitializeProcThreadAttributeList(
+                lpAttributeList: IntPtr.Zero,
+                dwAttributeCount: 1,
+                dwFlags: 0,
+                lpSize: ref lpSize
+            );
+            if (success || lpSize == IntPtr.Zero) // we're not expecting `success` here, we just want to get the calculated lpSize
+            {
+                throw new InvalidOperationException("Could not calculate the number of bytes for the attribute list. " + Marshal.GetLastWin32Error());
+            }
+
+            var startupInfo = new STARTUPINFOEX();
+            startupInfo.StartupInfo.cb = Marshal.SizeOf<STARTUPINFOEX>();
+            startupInfo.lpAttributeList = Marshal.AllocHGlobal(lpSize);
+
+            success = InitializeProcThreadAttributeList(
+                lpAttributeList: startupInfo.lpAttributeList,
+                dwAttributeCount: 1,
+                dwFlags: 0,
+                lpSize: ref lpSize
+            );
+            if (!success)
+            {
+                throw new InvalidOperationException("Could not set up attribute list. " + Marshal.GetLastWin32Error());
+            }
+
+            success = UpdateProcThreadAttribute(
+                lpAttributeList: startupInfo.lpAttributeList,
+                dwFlags: 0,
+                attribute: attributes,
+                lpValue: hPC,
+                cbSize: (IntPtr)IntPtr.Size,
+                lpPreviousValue: IntPtr.Zero,
+                lpReturnSize: IntPtr.Zero
+            );
+            if (!success)
+            {
+                throw new InvalidOperationException("Could not set pseudoconsole thread attribute. " + Marshal.GetLastWin32Error());
+            }
+
+            return startupInfo;
+        }
+
+        private static PROCESS_INFORMATION RunProcess(ref STARTUPINFOEX sInfoEx, string commandLine)
+        {
+            int securityAttributeSize = Marshal.SizeOf<SECURITY_ATTRIBUTES>();
+            var pSec = new SECURITY_ATTRIBUTES { nLength = securityAttributeSize };
+            var tSec = new SECURITY_ATTRIBUTES { nLength = securityAttributeSize };
+            var success = CreateProcess(
+                lpApplicationName: null,
+                lpCommandLine: commandLine,
+                lpProcessAttributes: ref pSec,
+                lpThreadAttributes: ref tSec,
+                bInheritHandles: false,
+                dwCreationFlags: EXTENDED_STARTUPINFO_PRESENT,
+                lpEnvironment: IntPtr.Zero,
+                lpCurrentDirectory: null,
+                lpStartupInfo: ref sInfoEx,
+                lpProcessInformation: out PROCESS_INFORMATION pInfo
+            );
+            if (!success)
+            {
+                throw new InvalidOperationException("Could not create process. " + Marshal.GetLastWin32Error());
+            }
+
+            return pInfo;
+        }
+
+        private static void CleanUp(STARTUPINFOEX startupInfo, PROCESS_INFORMATION processInfo)
+        {
+            // Free the attribute list
+            if (startupInfo.lpAttributeList != IntPtr.Zero)
+            {
+                DeleteProcThreadAttributeList(startupInfo.lpAttributeList);
+                Marshal.FreeHGlobal(startupInfo.lpAttributeList);
+            }
+
+            // Close process and thread handles
+            if (processInfo.hProcess != IntPtr.Zero)
+            {
+                CloseHandle(processInfo.hProcess);
+            }
+            if (processInfo.hThread != IntPtr.Zero)
+            {
+                CloseHandle(processInfo.hThread);
+            }
+        }
+
+        internal sealed class ProcessResources : IDisposable
+        {
+            public ProcessResources(STARTUPINFOEX startupInfo, PROCESS_INFORMATION processInfo)
+            {
+                StartupInfo = startupInfo;
+                ProcessInfo = processInfo;
+            }
+
+            STARTUPINFOEX StartupInfo { get; }
+            PROCESS_INFORMATION ProcessInfo { get; }
+
+            public void Dispose()
+            {
+                CleanUp(StartupInfo, ProcessInfo);
+            }
+        }
+    }
+}
---
+++ samples/ConPTY/MiniTerm/MiniTerm/PseudoConsolePipe.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using static MiniTerm.Native.PseudoConsoleApi;
+
+namespace MiniTerm
+{
+    /// <summary>
+    /// A pipe used to talk to the pseudoconsole, as described in:
+    /// https://docs.microsoft.com/en-us/windows/console/creating-a-pseudoconsole-session
+    /// </summary>
+    /// <remarks>
+    /// We'll have two instances of this class, one for input and one for output.
+    /// </remarks>
+    internal sealed class PseudoConsolePipe : IDisposable
+    {
+        public SafeFileHandle ReadSide;
+        public SafeFileHandle WriteSide;
+
+        public PseudoConsolePipe()
+        {
+            if (!CreatePipe(out ReadSide, out WriteSide, IntPtr.Zero, 0))
+            {
+                throw new InvalidOperationException("failed to create pipe");
+            }
+        }
+
+        public void Dispose()
+        {
+            ReadSide.Dispose();
+            WriteSide.Dispose();
+        }
+    }
+}
