9efc61b1b96163931b95f01e162fc46cf3f2bb2a
Heiko Voigt <hvoigt@hvoigt.net>
2019-05-13 16:15:22 +0200

    make copying of files windows localization agnostic
    On a german Windows when building I get the following error:
    
    (D = Datei, V = Verzeichnis)? Ist das Ziel ...\Terminal\x64\Debug\TerminalSettings.pdb ein Dateiname
    oder ein Verzeichnisname
    (D = Datei, V = Verzeichnis)? f
    
    The trick with piping 'f' for file into stdin does not work here, since
    in german file is called 'Datei'. Due to the fact that the UI is
    translated a 'd' is expected.
    
    Lets use '*' at the end of the target filename which is a hack to trick
    'xcopy' into assuming it is a filename her a target is a folder, if the
    target does not exist.

--- tools/openterm.cmd
+++ tools/openterm.cmd
@@ -18,16 +18,16 @@ set _last_cascadia_build=%OPENCON%\%PLATFORM%\%_LAST_BUILD_CONF%
 set copy_dir=OpenConsole\%_r%
 set cascadia_copy_dir=OpenConsole\%_r%\Cascadia
 
-(echo f | xcopy /Y %_last_build%\OpenConsole.exe %TEMP%\%copy_dir%\OpenConsole.exe) > nul
-(echo f | xcopy /Y %_last_build%\OpenConsole.exe %TEMP%\%cascadia_copy_dir%\conhost.exe) > nul
-(echo f | xcopy /Y %_last_build%\VtPipeTerm.exe %TEMP%\%copy_dir%\VtPipeTerm.exe) > nul
-(echo f | xcopy /Y %_last_build%\Nihilist.exe %TEMP%\%copy_dir%\Nihilist.exe) > nul
-(echo f | xcopy /Y %_last_build%\console.dll %TEMP%\%copy_dir%\console.dll) > nul
-(echo f | xcopy /Y %_last_cascadia_build%\CascadiaWin32.exe %TEMP%\%cascadia_copy_dir%\CascadiaWin32.exe) > nul
-(echo f | xcopy /Y %_last_cascadia_build%\TerminalConnection.dll %TEMP%\%cascadia_copy_dir%\TerminalConnection.dll) > nul
-(echo f | xcopy /Y %_last_cascadia_build%\TerminalControl.dll %TEMP%\%cascadia_copy_dir%\TerminalControl.dll) > nul
-(echo f | xcopy /Y %_last_cascadia_build%\TerminalSettings.dll %TEMP%\%cascadia_copy_dir%\TerminalSettings.dll) > nul
-(echo f | xcopy /Y %_last_cascadia_build%\TerminalApp.dll %TEMP%\%cascadia_copy_dir%\TerminalApp.dll) > nul
+(xcopy /Y %_last_build%\OpenConsole.exe %TEMP%\%copy_dir%\OpenConsole.exe*) > nul
+(xcopy /Y %_last_build%\OpenConsole.exe %TEMP%\%cascadia_copy_dir%\conhost.exe*) > nul
+(xcopy /Y %_last_build%\VtPipeTerm.exe %TEMP%\%copy_dir%\VtPipeTerm.exe*) > nul
+(xcopy /Y %_last_build%\Nihilist.exe %TEMP%\%copy_dir%\Nihilist.exe*) > nul
+(xcopy /Y %_last_build%\console.dll %TEMP%\%copy_dir%\console.dll*) > nul
+(xcopy /Y %_last_cascadia_build%\CascadiaWin32.exe %TEMP%\%cascadia_copy_dir%\CascadiaWin32.exe*) > nul
+(xcopy /Y %_last_cascadia_build%\TerminalConnection.dll %TEMP%\%cascadia_copy_dir%\TerminalConnection.dll*) > nul
+(xcopy /Y %_last_cascadia_build%\TerminalControl.dll %TEMP%\%cascadia_copy_dir%\TerminalControl.dll*) > nul
+(xcopy /Y %_last_cascadia_build%\TerminalSettings.dll %TEMP%\%cascadia_copy_dir%\TerminalSettings.dll*) > nul
+(xcopy /Y %_last_cascadia_build%\TerminalApp.dll %TEMP%\%cascadia_copy_dir%\TerminalApp.dll*) > nul
 
 echo Launching %TEMP%\%cascadia_copy_dir%\CascadiaWin32.exe...
 start %TEMP%\%cascadia_copy_dir%\CascadiaWin32.exe %*
