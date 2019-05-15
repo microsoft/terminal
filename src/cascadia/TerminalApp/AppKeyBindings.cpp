3eaabfac86f54ef2be4cb6279a97590b0b3b38af
Mike Griese <migrie@microsoft.com>
2019-05-15 17:16:43 -0500

    * use a constexpr wstring_view * flip the names, actions map around

--- src/cascadia/TerminalApp/AppKeyBindings.cpp
+++ src/cascadia/TerminalApp/AppKeyBindings.cpp
@@ -3,86 +3,87 @@
 
 #include "pch.h"
 #include "AppKeyBindings.h"
+#include "KeyChordSerialization.h"
 
 using namespace winrt::Microsoft::Terminal;
 using namespace winrt::TerminalApp;
 using namespace winrt::Windows::Data::Json;
 
-static const wchar_t* const KEYS_KEY{ L"keys" };
-static const wchar_t* const COMMAND_KEY{ L"command" };
+static constexpr std::wstring_view KEYS_KEY{ L"keys" };
+static constexpr std::wstring_view COMMAND_KEY{ L"command" };
 
-static const wchar_t* const COPYTEXT_KEY{ L"copy" };
-static const wchar_t* const PASTETEXT_KEY{ L"paste" };
-static const wchar_t* const NEWTAB_KEY{ L"newTab" };
-static const wchar_t* const NEWTABWITHPROFILE0_KEY{ L"newTabProfile0" };
-static const wchar_t* const NEWTABWITHPROFILE1_KEY{ L"newTabProfile1" };
-static const wchar_t* const NEWTABWITHPROFILE2_KEY{ L"newTabProfile2" };
-static const wchar_t* const NEWTABWITHPROFILE3_KEY{ L"newTabProfile3" };
-static const wchar_t* const NEWTABWITHPROFILE4_KEY{ L"newTabProfile4" };
-static const wchar_t* const NEWTABWITHPROFILE5_KEY{ L"newTabProfile5" };
-static const wchar_t* const NEWTABWITHPROFILE6_KEY{ L"newTabProfile6" };
-static const wchar_t* const NEWTABWITHPROFILE7_KEY{ L"newTabProfile7" };
-static const wchar_t* const NEWTABWITHPROFILE8_KEY{ L"newTabProfile8" };
-static const wchar_t* const NEWTABWITHPROFILE9_KEY{ L"newTabProfile9" };
-static const wchar_t* const NEWWINDOW_KEY{ L"newWindow" };
-static const wchar_t* const CLOSEWINDOW_KEY{ L"closeWindow" };
-static const wchar_t* const CLOSETAB_KEY{ L"closeTab" };
-static const wchar_t* const SWITCHTOTAB_KEY{ L"switchToTab" };
-static const wchar_t* const NEXTTAB_KEY{ L"nextTab" };
-static const wchar_t* const PREVTAB_KEY{ L"prevTab" };
-static const wchar_t* const INCREASEFONTSIZE_KEY{ L"increaseFontSize" };
-static const wchar_t* const DECREASEFONTSIZE_KEY{ L"decreaseFontSize" };
-static const wchar_t* const SCROLLUP_KEY{ L"scrollUp" };
-static const wchar_t* const SCROLLDOWN_KEY{ L"scrollDown" };
-static const wchar_t* const SCROLLUPPAGE_KEY{ L"scrollUpPage" };
-static const wchar_t* const SCROLLDOWNPAGE_KEY{ L"scrollDownPage" };
-static const wchar_t* const SWITCHTOTAB0_KEY{ L"switchToTab0" };
-static const wchar_t* const SWITCHTOTAB1_KEY{ L"switchToTab1" };
-static const wchar_t* const SWITCHTOTAB2_KEY{ L"switchToTab2" };
-static const wchar_t* const SWITCHTOTAB3_KEY{ L"switchToTab3" };
-static const wchar_t* const SWITCHTOTAB4_KEY{ L"switchToTab4" };
-static const wchar_t* const SWITCHTOTAB5_KEY{ L"switchToTab5" };
-static const wchar_t* const SWITCHTOTAB6_KEY{ L"switchToTab6" };
-static const wchar_t* const SWITCHTOTAB7_KEY{ L"switchToTab7" };
-static const wchar_t* const SWITCHTOTAB8_KEY{ L"switchToTab8" };
-static const wchar_t* const SWITCHTOTAB9_KEY{ L"switchToTab9" };
-static const wchar_t* const OPENSETTINGS_KEY{ L"openSettings" };
+static constexpr std::wstring_view COPYTEXT_KEY{ L"copy" };
+static constexpr std::wstring_view PASTETEXT_KEY{ L"paste" };
+static constexpr std::wstring_view NEWTAB_KEY{ L"newTab" };
+static constexpr std::wstring_view NEWTABWITHPROFILE0_KEY{ L"newTabProfile0" };
+static constexpr std::wstring_view NEWTABWITHPROFILE1_KEY{ L"newTabProfile1" };
+static constexpr std::wstring_view NEWTABWITHPROFILE2_KEY{ L"newTabProfile2" };
+static constexpr std::wstring_view NEWTABWITHPROFILE3_KEY{ L"newTabProfile3" };
+static constexpr std::wstring_view NEWTABWITHPROFILE4_KEY{ L"newTabProfile4" };
+static constexpr std::wstring_view NEWTABWITHPROFILE5_KEY{ L"newTabProfile5" };
+static constexpr std::wstring_view NEWTABWITHPROFILE6_KEY{ L"newTabProfile6" };
+static constexpr std::wstring_view NEWTABWITHPROFILE7_KEY{ L"newTabProfile7" };
+static constexpr std::wstring_view NEWTABWITHPROFILE8_KEY{ L"newTabProfile8" };
+static constexpr std::wstring_view NEWTABWITHPROFILE9_KEY{ L"newTabProfile9" };
+static constexpr std::wstring_view NEWWINDOW_KEY{ L"newWindow" };
+static constexpr std::wstring_view CLOSEWINDOW_KEY{ L"closeWindow" };
+static constexpr std::wstring_view CLOSETAB_KEY{ L"closeTab" };
+static constexpr std::wstring_view SWITCHTOTAB_KEY{ L"switchToTab" };
+static constexpr std::wstring_view NEXTTAB_KEY{ L"nextTab" };
+static constexpr std::wstring_view PREVTAB_KEY{ L"prevTab" };
+static constexpr std::wstring_view INCREASEFONTSIZE_KEY{ L"increaseFontSize" };
+static constexpr std::wstring_view DECREASEFONTSIZE_KEY{ L"decreaseFontSize" };
+static constexpr std::wstring_view SCROLLUP_KEY{ L"scrollUp" };
+static constexpr std::wstring_view SCROLLDOWN_KEY{ L"scrollDown" };
+static constexpr std::wstring_view SCROLLUPPAGE_KEY{ L"scrollUpPage" };
+static constexpr std::wstring_view SCROLLDOWNPAGE_KEY{ L"scrollDownPage" };
+static constexpr std::wstring_view SWITCHTOTAB0_KEY{ L"switchToTab0" };
+static constexpr std::wstring_view SWITCHTOTAB1_KEY{ L"switchToTab1" };
+static constexpr std::wstring_view SWITCHTOTAB2_KEY{ L"switchToTab2" };
+static constexpr std::wstring_view SWITCHTOTAB3_KEY{ L"switchToTab3" };
+static constexpr std::wstring_view SWITCHTOTAB4_KEY{ L"switchToTab4" };
+static constexpr std::wstring_view SWITCHTOTAB5_KEY{ L"switchToTab5" };
+static constexpr std::wstring_view SWITCHTOTAB6_KEY{ L"switchToTab6" };
+static constexpr std::wstring_view SWITCHTOTAB7_KEY{ L"switchToTab7" };
+static constexpr std::wstring_view SWITCHTOTAB8_KEY{ L"switchToTab8" };
+static constexpr std::wstring_view SWITCHTOTAB9_KEY{ L"switchToTab9" };
+static constexpr std::wstring_view OPENSETTINGS_KEY{ L"openSettings" };
 
-static const std::map<ShortcutAction, std::wstring_view> commandNames {
-    { ShortcutAction::CopyText, COPYTEXT_KEY },
-    { ShortcutAction::PasteText, PASTETEXT_KEY },
-    { ShortcutAction::NewTab, NEWTAB_KEY },
-    { ShortcutAction::NewTabProfile0, NEWTABWITHPROFILE0_KEY },
-    { ShortcutAction::NewTabProfile1, NEWTABWITHPROFILE1_KEY },
-    { ShortcutAction::NewTabProfile2, NEWTABWITHPROFILE2_KEY },
-    { ShortcutAction::NewTabProfile3, NEWTABWITHPROFILE3_KEY },
-    { ShortcutAction::NewTabProfile4, NEWTABWITHPROFILE4_KEY },
-    { ShortcutAction::NewTabProfile5, NEWTABWITHPROFILE5_KEY },
-    { ShortcutAction::NewTabProfile6, NEWTABWITHPROFILE6_KEY },
-    { ShortcutAction::NewTabProfile7, NEWTABWITHPROFILE7_KEY },
-    { ShortcutAction::NewTabProfile8, NEWTABWITHPROFILE8_KEY },
-    { ShortcutAction::NewTabProfile9, NEWTABWITHPROFILE9_KEY },
-    { ShortcutAction::NewWindow, NEWWINDOW_KEY },
-    { ShortcutAction::CloseWindow, CLOSEWINDOW_KEY },
-    { ShortcutAction::CloseTab, CLOSETAB_KEY },
-    { ShortcutAction::NextTab, NEXTTAB_KEY },
-    { ShortcutAction::PrevTab, PREVTAB_KEY },
-    { ShortcutAction::IncreaseFontSize, INCREASEFONTSIZE_KEY },
-    { ShortcutAction::DecreaseFontSize, DECREASEFONTSIZE_KEY },
-    { ShortcutAction::ScrollUp, SCROLLUP_KEY },
-    { ShortcutAction::ScrollDown, SCROLLDOWN_KEY },
-    { ShortcutAction::ScrollUpPage, SCROLLUPPAGE_KEY },
-    { ShortcutAction::ScrollDownPage, SCROLLDOWNPAGE_KEY },
-    { ShortcutAction::SwitchToTab0, SWITCHTOTAB0_KEY },
-    { ShortcutAction::SwitchToTab1, SWITCHTOTAB1_KEY },
-    { ShortcutAction::SwitchToTab2, SWITCHTOTAB2_KEY },
-    { ShortcutAction::SwitchToTab3, SWITCHTOTAB3_KEY },
-    { ShortcutAction::SwitchToTab4, SWITCHTOTAB4_KEY },
-    { ShortcutAction::SwitchToTab5, SWITCHTOTAB5_KEY },
-    { ShortcutAction::SwitchToTab6, SWITCHTOTAB6_KEY },
-    { ShortcutAction::SwitchToTab7, SWITCHTOTAB7_KEY },
-    { ShortcutAction::SwitchToTab8, SWITCHTOTAB8_KEY },
-    { ShortcutAction::SwitchToTab9, SWITCHTOTAB9_KEY },
+static const std::map<std::wstring_view, ShortcutAction> commandNames {
+    { COPYTEXT_KEY, ShortcutAction::CopyText },
+    { PASTETEXT_KEY, ShortcutAction::PasteText },
+    { NEWTAB_KEY, ShortcutAction::NewTab },
+    { NEWTABWITHPROFILE0_KEY, ShortcutAction::NewTabProfile0 },
+    { NEWTABWITHPROFILE1_KEY, ShortcutAction::NewTabProfile1 },
+    { NEWTABWITHPROFILE2_KEY, ShortcutAction::NewTabProfile2 },
+    { NEWTABWITHPROFILE3_KEY, ShortcutAction::NewTabProfile3 },
+    { NEWTABWITHPROFILE4_KEY, ShortcutAction::NewTabProfile4 },
+    { NEWTABWITHPROFILE5_KEY, ShortcutAction::NewTabProfile5 },
+    { NEWTABWITHPROFILE6_KEY, ShortcutAction::NewTabProfile6 },
+    { NEWTABWITHPROFILE7_KEY, ShortcutAction::NewTabProfile7 },
+    { NEWTABWITHPROFILE8_KEY, ShortcutAction::NewTabProfile8 },
+    { NEWTABWITHPROFILE9_KEY, ShortcutAction::NewTabProfile9 },
+    { NEWWINDOW_KEY, ShortcutAction::NewWindow },
+    { CLOSEWINDOW_KEY, ShortcutAction::CloseWindow },
+    { CLOSETAB_KEY, ShortcutAction::CloseTab },
+    { NEXTTAB_KEY, ShortcutAction::NextTab },
+    { PREVTAB_KEY, ShortcutAction::PrevTab },
+    { INCREASEFONTSIZE_KEY, ShortcutAction::IncreaseFontSize },
+    { DECREASEFONTSIZE_KEY, ShortcutAction::DecreaseFontSize },
+    { SCROLLUP_KEY, ShortcutAction::ScrollUp },
+    { SCROLLDOWN_KEY, ShortcutAction::ScrollDown },
+    { SCROLLUPPAGE_KEY, ShortcutAction::ScrollUpPage },
+    { SCROLLDOWNPAGE_KEY, ShortcutAction::ScrollDownPage },
+    { SWITCHTOTAB0_KEY, ShortcutAction::SwitchToTab0 },
+    { SWITCHTOTAB1_KEY, ShortcutAction::SwitchToTab1 },
+    { SWITCHTOTAB2_KEY, ShortcutAction::SwitchToTab2 },
+    { SWITCHTOTAB3_KEY, ShortcutAction::SwitchToTab3 },
+    { SWITCHTOTAB4_KEY, ShortcutAction::SwitchToTab4 },
+    { SWITCHTOTAB5_KEY, ShortcutAction::SwitchToTab5 },
+    { SWITCHTOTAB6_KEY, ShortcutAction::SwitchToTab6 },
+    { SWITCHTOTAB7_KEY, ShortcutAction::SwitchToTab7 },
+    { SWITCHTOTAB8_KEY, ShortcutAction::SwitchToTab8 },
+    { SWITCHTOTAB9_KEY, ShortcutAction::SwitchToTab9 },
 };
 
 namespace winrt::TerminalApp::implementation
@@ -265,24 +266,18 @@ namespace winrt::TerminalApp::implementation
                     }
                     const auto keyChordString = keys.GetAt(0).GetString();
                     ShortcutAction action;
-                    bool parsedSuccessfully = false;
+
                     // Try matching the command to one we have
-                    for (const auto& pair : commandNames)
+                    if (commandNames.find(commandString) != commandNames.end())
                     {
-                        if (pair.second == commandString)
-                        {
-                            action = pair.first;
-                            parsedSuccessfully = true;
-                            break;
-                        }
+                        action = commandNames.at(commandString);
                     }
-                    if (!parsedSuccessfully)
+                    else
                     {
                         continue;
                     }
+
                     // Try parsing the chord
-                    Settings::KeyChord chord{ false, false, false, 0 };
-                    parsedSuccessfully = false;
                     try
                     {
                         chord = Settings::KeyChord::FromString(keyChordString);
@@ -291,10 +286,6 @@ namespace winrt::TerminalApp::implementation
                     catch (...)
                     {
                         continue;
-                    }
-                    if (parsedSuccessfully)
-                    {
-                        newBindings.SetKeyBinding(action, chord);
                     }
                 }
             }
@@ -309,9 +300,9 @@ namespace winrt::TerminalApp::implementation
     // - bindingsArray: The JsonArray to insert the object into.
     // - chord: The KeyChord to serailize and place in the json array
     // - actionName: the name of the ShortcutAction to use with this KeyChord
-    void _AddShortcutToJsonArray(JsonArray bindingsArray,
-                                 const Settings::KeyChord& chord,
-                                 const std::wstring_view& actionName)
+    static void _AddShortcutToJsonArray(const JsonArray& bindingsArray,
+                                        const Settings::KeyChord& chord,
+                                        const std::wstring_view& actionName)
     {
         const auto keyString = chord.ToString();
         if (keyString == L"")
@@ -342,8 +333,8 @@ namespace winrt::TerminalApp::implementation
         // it has a binding.
         for (auto& actionName : commandNames)
         {
-            const auto searchedForAction = actionName.first;
-            const auto searchedForName = actionName.second;
+            const auto searchedForName = actionName.first;
+            const auto searchedForAction = actionName.second;
             for (const auto& kv : _keyShortcuts)
             {
                 const auto chord = kv.first;
