e9a3d16286e8cfebd487be5665753cd5852e2126
Tim Heuer <tim@timheuer.com>
2019-05-22 13:01:33 -0700

    Adding auto-UI shortcuts to menu based on keymappings (#924)
    * Adding vsconfig file for VS2019 help to prompt for missing components requried.
    
    * Adding a keybinding for launching the settings.  Suggested fix for #683
    
    * Modified to comma per PR feedback
    
    * Implements 791 for profile and settings shortcuts (most frequent and have shortcuts)
    
    * Quick change for consistency (missed in first checkin due to using ENUM) on using 'Ctrl' instead of 'Control'
    
    * Adding UI shortcut generation to new keybinding mappings.  Resolving #791
    
    * Making a few changes on reviewer feedback for shortcut UI.
    
    * Additional reviewer feedback on variable name change (not a member var)

--- src/cascadia/TerminalApp/App.cpp
+++ src/cascadia/TerminalApp/App.cpp
@@ -215,10 +215,25 @@ namespace winrt::TerminalApp::implementation
     void App::_CreateNewTabFlyout()
     {
         auto newTabFlyout = Controls::MenuFlyout{};
+        auto keyBindings = _settings->GetKeybindings();
+
         for (int profileIndex = 0; profileIndex < _settings->GetProfiles().size(); profileIndex++)
         {
             const auto& profile = _settings->GetProfiles()[profileIndex];
             auto profileMenuItem = Controls::MenuFlyoutItem{};
+
+            // add the keyboard shortcuts for the first 9 profiles
+            if (profileIndex < 9)
+            {
+                // enum value for ShortcutAction::NewTabProfileX; 0==NewTabProfile0
+                auto profileKeyChord = keyBindings.GetKeyBinding(static_cast<ShortcutAction>(profileIndex + static_cast<int>(ShortcutAction::NewTabProfile0)));
+                
+                // make sure we find one to display
+                if (profileKeyChord)
+                {
+                    _SetAcceleratorForMenuItem(profileMenuItem, profileKeyChord);
+                }                
+            }
 
             auto profileName = profile.GetName();
             winrt::hstring hName{ profileName };
@@ -253,6 +268,12 @@ namespace winrt::TerminalApp::implementation
 
             settingsItem.Click({ this, &App::_SettingsButtonOnClick });
             newTabFlyout.Items().Append(settingsItem);
+
+            auto settingsKeyChord = keyBindings.GetKeyBinding(ShortcutAction::OpenSettings);
+            if (settingsKeyChord)
+            {
+                _SetAcceleratorForMenuItem(settingsItem, settingsKeyChord);
+            }
 
             // Create the feedback button.
             auto feedbackFlyout = Controls::MenuFlyoutItem{};
@@ -896,6 +917,40 @@ namespace winrt::TerminalApp::implementation
         else
         {
             return { nullptr };
+        }
+    }
+
+
+    // Method Description:
+    // - Takes a MenuFlyoutItem and a corresponding KeyChord value and creates the accelerator for UI display.
+    //   Takes into account a special case for an error condition for a comma
+    // Arguments:
+    // - MenuFlyoutItem that will be displayed, and a KeyChord to map an accelerator
+    void App::_SetAcceleratorForMenuItem(Windows::UI::Xaml::Controls::MenuFlyoutItem& menuItem, const winrt::Microsoft::Terminal::Settings::KeyChord& keyChord)
+    {
+        // work around https://github.com/microsoft/microsoft-ui-xaml/issues/708 in case of VK_OEM_COMMA
+        if (keyChord.Vkey() != VK_OEM_COMMA)
+        {
+            // use the XAML shortcut to give us the automatic capabilities
+            auto menuShortcut = Windows::UI::Xaml::Input::KeyboardAccelerator{};
+
+            // TODO: Modify this when https://github.com/microsoft/terminal/issues/877 is resolved
+            menuShortcut.Key(static_cast<Windows::System::VirtualKey>(keyChord.Vkey()));
+
+            // inspect the modifiers from the KeyChord and set the flags int he XAML value
+            auto modifiers = AppKeyBindings::ConvertVKModifiers(keyChord.Modifiers());
+
+            // add the modifiers to the shortcut
+            menuShortcut.Modifiers(modifiers);
+
+            // add to the menu
+            menuItem.KeyboardAccelerators().Append(menuShortcut);
+        }
+        else // we've got a comma, so need to just use the alternate method
+        {
+            // extract the modifier and key to a nice format
+            auto overrideString = AppKeyBindings::FormatOverrideShortcutText(keyChord.Modifiers());
+            menuItem.KeyboardAcceleratorTextOverride(overrideString + L" ,");
         }
     }
 
