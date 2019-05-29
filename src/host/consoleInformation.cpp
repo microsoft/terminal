2883a6f3b4a6429cad798a65fb4c64e16ecb2a31
Flo56958 <gutsche.florian@gmail.com>
2019-05-29 09:03:45 +0200

    Update consoleInformation.cpp

--- src/host/consoleInformation.cpp
+++ src/host/consoleInformation.cpp
@@ -270,7 +270,7 @@ void CONSOLE_INFORMATION::SetTitle(const std::wstring_view newTitle)
 
 // Method Description:
 // - Set the console title's prefix, and trigger a renderer update of the title.
-//      This is the part of the title shuch as "Mark", "Select", or "Scroll"
+//      This is the part of the title such as "Mark", "Select", or "Scroll"
 // Arguments:
 // - newTitlePrefix: The new value to use for the title prefix
 // Return Value:
