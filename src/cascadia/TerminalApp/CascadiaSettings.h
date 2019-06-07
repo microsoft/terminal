31b614d5b2d6906311bebcf4d43c7b449c03e9f6
Jeremy Banks <JBanks@users.noreply.github.com>
2019-06-07 17:12:32 -0400

    Code to add WSLProfiles. (#1050)
    * Code to add WSLProfiles.
    
    * Updates recomended by miniksa
    
    * Corrections from Mauve
    
    * More updates from miniska (clarified WaitForSingleObject errors, and moved the try block to the calling function)
    
    * Added THROW_LAST_ERROR for WAIT_FAILED instead of passing an unhandled exception.
    
    * Migrate STL dependancies to LibraryIncludes.h
    
    * Renamed function to provide more clarity
    
    * Set WSL starting directory.
    
    * Default Linux icon and brackets on new lines.
    
    * Added system path so we don't rely on execution from the PATH environment variable.  Removed incorrect error useage.  Removed variable that was not required.
    
    * Remove default directory setting.

--- src/cascadia/TerminalApp/CascadiaSettings.h
+++ src/cascadia/TerminalApp/CascadiaSettings.h
@@ -70,6 +70,7 @@ private:
     static std::optional<std::string> _LoadAsUnpackagedApp();
     static bool _isPowerShellCoreInstalledInPath(const std::wstring_view programFileEnv, std::filesystem::path& cmdline);
     static bool _isPowerShellCoreInstalled(std::filesystem::path& cmdline);
+    static void _AppendWslProfiles(std::vector<TerminalApp::Profile>& profileStorage);
     static std::wstring ExpandEnvironmentVariableString(std::wstring_view source);
     static Profile _CreateDefaultProfile(const std::wstring_view name);
 };
