df0f9fe5a0f1918a3f48b3bbafb740cab226a880
Jeremy Banks <JBanks@users.noreply.github.com>
2019-06-10 16:13:32 -0400

    Update src/cascadia/TerminalApp/CascadiaSettings.cpp
    Removed Spacing
    
    Co-Authored-By: Dustin L. Howett (MSFT) <duhowett@microsoft.com>

--- src/cascadia/TerminalApp/CascadiaSettings.cpp
+++ src/cascadia/TerminalApp/CascadiaSettings.cpp
@@ -537,7 +537,7 @@ void CascadiaSettings::_AppendWslProfiles(std::vector<TerminalApp::Profile>& pro
             std::getline(wlinestream, distName, L'\r');
             size_t firstChar = distName.find_first_of(L"( ");
             // Some localizations don't have a space between the name and "(Default)"
-                //https://github.com/microsoft/terminal/issues/1168#issuecomment-500187109
+            // https://github.com/microsoft/terminal/issues/1168#issuecomment-500187109
             if (firstChar < distName.size())
             {
                 distName.resize(firstChar);
