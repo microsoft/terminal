122f0de382542de14d79babad92699d916057783
Dustin L. Howett (MSFT) <duhowett@microsoft.com>
2019-07-09 14:47:30 -0700

    Move most of TerminalApp's runtime Xaml to a .xaml file and class (#1885)

--- src/cascadia/TerminalApp/App.h
+++ src/cascadia/TerminalApp/App.h
@@ -26,7 +26,6 @@ namespace winrt::TerminalApp::implementation
         App();
 
         Windows::UI::Xaml::UIElement GetRoot() noexcept;
-        Windows::UI::Xaml::UIElement GetTabs() noexcept;
 
         // Gets the current dragglable area in the non client region of the top level window
         Windows::UI::Xaml::Controls::Border GetDragBar() noexcept;
@@ -52,7 +51,7 @@ namespace winrt::TerminalApp::implementation
         // ALSO: If you add any UIElements as roots here, make sure they're
         // updated in _ApplyTheme. The two roots currently are _root and _tabRow
         // (which is a root when the tabs are in the titlebar.)
-        Windows::UI::Xaml::Controls::Grid _root{ nullptr };
+        Windows::UI::Xaml::Controls::Control _root{ nullptr };
         Microsoft::UI::Xaml::Controls::TabView _tabView{ nullptr };
         Windows::UI::Xaml::Controls::Grid _tabRow{ nullptr };
         Windows::UI::Xaml::Controls::Grid _tabContent{ nullptr };
--- src/cascadia/TerminalApp/MinMaxCloseControl.h
+++ src/cascadia/TerminalApp/MinMaxCloseControl.h
@@ -13,16 +13,19 @@ namespace winrt::TerminalApp::implementation
 {
     struct MinMaxCloseControl : MinMaxCloseControlT<MinMaxCloseControl>
     {
-        MinMaxCloseControl(uint64_t hWnd);
+        MinMaxCloseControl();
 
         void Minimize_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e);
         void Maximize_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e);
         void Close_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e);
         void DragBar_DoubleTapped(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::Input::DoubleTappedRoutedEventArgs const& e);
 
+        uint64_t ParentWindowHandle() const;
+        void ParentWindowHandle(uint64_t handle);
+
     private:
         void _OnMaximize(byte flag);
-        HWND _window = nullptr;
+        HWND _window{ nullptr }; // non-owning handle; should not be freed in the dtor.
     };
 }
 
