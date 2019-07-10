31b27407ac905e04deacae4755ba515d2b5ef92f
Michael Niksa <miniksa@microsoft.com>
2019-07-10 19:38:26 +0000

    Merged PR 3490896: [Git2Git] Merged PR 3487659: [Git2Git] Merged PR 3487638: inbox: merge github changes up to 122f0de3
    [Git2Git] Merged PR 3487659: [Git2Git] Merged PR 3487638: inbox: merge github changes up to 122f0de3
    
    [Git2Git] Merged PR 3487638: inbox: merge github changes up to 122f0de3
    
    Related work items: #22702360, #22702370, #22702376 Retrieved from https://microsoft.visualstudio.com OpenConsole Dart inbox 311175250fedd9824b7a83e26c4e823c9b196c39
    
    Related work items: #22702360, #22702370, #22702376 Retrieved from https://microsoft.visualstudio.com os OS official/rs_onecore_dep_uxp aa5182a2b1dceefb61e09aad392aac0a56970d80
    
    Related work items: #22702360, #22702370, #22702376

--- src/host/ft_host/API_BufferTests.cpp
+++ src/host/ft_host/API_BufferTests.cpp
@@ -14,11 +14,6 @@ class BufferTests
 {
     BEGIN_TEST_CLASS(BufferTests)
         TEST_CLASS_PROPERTY(L"IsolationLevel", L"Method")
-        TEST_CLASS_PROPERTY(L"BinaryUnderTest", L"conhost.exe")
-        TEST_CLASS_PROPERTY(L"ArtifactUnderTest", L"wincon.h")
-        TEST_CLASS_PROPERTY(L"ArtifactUnderTest", L"winconp.h")
-        TEST_CLASS_PROPERTY(L"ArtifactUnderTest", L"conmsgl1.h")
-        TEST_CLASS_PROPERTY(L"ArtifactUnderTest", L"conmsgl2.h")
     END_TEST_CLASS()
 
     TEST_METHOD(TestSetConsoleActiveScreenBufferInvalid);
