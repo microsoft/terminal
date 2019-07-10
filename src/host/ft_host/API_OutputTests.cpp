3e5bb994781810b41858aa169a3f78421f1c02be
Dustin L. Howett (MSFT) <duhowett@microsoft.com>
2019-07-10 12:40:51 -0700

    inbox: reflect incoming changes up to uxp aa5182a2 (#1916)

--- src/host/ft_host/API_OutputTests.cpp
+++ src/host/ft_host/API_OutputTests.cpp
@@ -18,12 +18,6 @@ class OutputTests
 {
     BEGIN_TEST_CLASS(OutputTests)
         TEST_CLASS_PROPERTY(L"IsolationLevel", L"Method")
-        TEST_CLASS_PROPERTY(L"BinaryUnderTest", L"conhost.exe")
-        TEST_CLASS_PROPERTY(L"ArtifactUnderTest", L"wincon.h")
-        TEST_CLASS_PROPERTY(L"ArtifactUnderTest", L"winconp.h")
-        TEST_CLASS_PROPERTY(L"ArtifactUnderTest", L"wincontypes.h")
-        TEST_CLASS_PROPERTY(L"ArtifactUnderTest", L"conmsgl1.h")
-        TEST_CLASS_PROPERTY(L"ArtifactUnderTest", L"conmsgl2.h")
     END_TEST_CLASS()
 
     TEST_CLASS_SETUP(TestSetup);
