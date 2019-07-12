120e6157c35ea76556419033707b730220cb730d
Dustin L. Howett (MSFT) <duhowett@microsoft.com>
2019-07-12 15:21:45 -0700

    Fix the WAP packaging project (#1900)
    * Fix the WAP packaging project
    
    This commits fixes the centennial package by:
    * Forcing XBF (XAML binary format) files to be embedded in project
      PRI files.
    * Moving package content generation to before PRI generation
    * Collecting all of the package's PRI files to merge into resources.pri
    * Fixing the hardcoded resource paths to reflect the new reality.
    
    It also includes a magic value that fixes the bug where the project is
    autodetected as a Mixed (CLR + Native) project.
    
    Fixes #1816.

--- src/cascadia/TerminalApp/TerminalPage.cpp
+++ src/cascadia/TerminalApp/TerminalPage.cpp
@@ -13,10 +13,7 @@ namespace winrt::TerminalApp::implementation
 {
     TerminalPage::TerminalPage()
     {
-        // The generated code will by default attempt to load from ms-appx://TerminalApp/TerminalPage.xaml.
-        // We'll force it to load from the root of the appx instead.
-        const winrt::Windows::Foundation::Uri resourceLocator{ L"ms-appx:///TerminalPage.xaml" };
-        winrt::Windows::UI::Xaml::Application::LoadComponent(*this, resourceLocator, winrt::Windows::UI::Xaml::Controls::Primitives::ComponentResourceLocation::Nested);
+        InitializeComponent();
     }
 
     // Method Description:
