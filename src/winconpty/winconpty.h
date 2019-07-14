80c481e1847a43b771ead5f51cb0587ade4b3556
Dustin L. Howett <duhowett@microsoft.com>
2019-07-13 19:09:43 -0700

    conpty: add winconpty (conpty.dll)!

---
+++ src/winconpty/winconpty.h
@@ -0,0 +1,33 @@
+#include "precomp.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _PseudoConsole
+{
+    HANDLE hSignal;
+    HANDLE hPtyReference;
+    HANDLE hConPtyProcess;
+} PseudoConsole;
+
+// Signals
+// These are not defined publicly, but are used for controlling the conpty via
+//      the signal pipe.
+#define PTY_SIGNAL_RESIZE_WINDOW (8u)
+
+// Implementations of the various PseudoConsole functions.
+HRESULT _CreatePseudoConsole(const HANDLE hToken,
+                             const COORD size,
+                             const HANDLE hInput,
+                             const HANDLE hOutput,
+                             const DWORD dwFlags,
+                             _Inout_ PseudoConsole* pPty);
+
+HRESULT _ResizePseudoConsole(_In_ const PseudoConsole* const pPty, _In_ const COORD size);
+void _ClosePseudoConsoleMembers(_In_ PseudoConsole* pPty);
+VOID _ClosePseudoConsole(_In_ PseudoConsole* pPty);
+
+#ifdef __cplusplus
+}
+#endif
