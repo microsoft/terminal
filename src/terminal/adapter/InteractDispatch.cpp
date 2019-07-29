de65659330ba8fe7e6baa8bebb532915e96874fb
Mike Griese <migrie@microsoft.com>
2019-07-29 15:35:59 -0500

    Don't trigger a frame due to circling when in the middle of a resize operation
      This fixes #1795, and shined quite a bit of light on the whole conpty resize process.

--- src/terminal/adapter/InteractDispatch.cpp
+++ src/terminal/adapter/InteractDispatch.cpp
@@ -114,6 +114,10 @@ bool InteractDispatch::WindowManipulation(const DispatchTypes::WindowManipulatio
         }
         break;
     case DispatchTypes::WindowManipulationType::ResizeWindowInCharacters:
+        // TODO:GH#1765 This should BeginResize and EndResize, like the signal
+        // pipe does. Either that, or we should introduce a better
+        // `ResizeConpty` function to the ConGetSet interface, that specifically
+        // handles a conpty resize.
         if (cParams == 2)
         {
             fSuccess = DispatchCommon::s_ResizeWindow(*_pConApi, rgusParams[1], rgusParams[0]);
