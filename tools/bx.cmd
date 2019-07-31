9d2c3ec2dd06f13b438bdf287e81695728b5b316
Mike Griese <migrie@microsoft.com>
2019-07-31 08:13:17 -0500

    Cleanup for PR

--- tools/bx.cmd
+++ tools/bx.cmd
@@ -1,37 +1,7 @@
 @echo off
 
-@rem TODO Find a way to only rebuild the metaproj if the sln changed
+rem bx - Build only the project in this directory without cleaning it first.
+rem This is another script to help Microsoft developers feel at home working on
+rem the terminal project.
 
-setlocal
-rem set MSBuildEmitSolution=1
-rem "%msbuild%" %OPENCON%\OpenConsole.sln /t:ValidateSolutionConfiguration /m > NUL
-rem set MSBuildEmitSolution=0
-
-set _METAPROJ=%OPENCON%\OpenConsole.sln.metaproj
-
-set _BX_SCRIPT=powershell bx.ps1
-
-rem @echo on
-FOR /F "tokens=* USEBACKQ" %%F IN (`%_BX_SCRIPT% 2^> NUL`) DO (
-    echo %%F
-    set _OUTPUT=%%F
-)
-if (%_OUTPUT%) == () (
-    echo Could not find a .vcxproj file in this directory.
-    echo `bx.cmd` only works in directories with a vcxproj file.
-    echo Please navigate to directory with a project file in it, or try `bz` to build the entire solution.
-    goto :eof
-)
-
-rem echo Found %_OUTPUT%
-set PROJECT_NAME=%_OUTPUT%
-
-set MSBUILD_COMMAND="%msbuild%" %OPENCON%\Openconsole.sln /t:%PROJECT_NAME% /m /p:Configuration=Debug /p:Platform=%ARCH%
-
-echo Building only %PROJECT_NAME% Using commandline:
-echo %MSBUILD_COMMAND%
-
-%MSBUILD_COMMAND%
-
-endlocal
-:eof
+call bcz exclusive no_clean %*
--- tools/bx.ps1
+++ tools/bx.ps1
@@ -1,37 +1,28 @@
+# This is a helper script to figure out which target corresponds to the project
+# in this directory. Parses the solution's .metaproj file looking for the
+# project file in this directory, to be able to get the project's name.
+
 $projects = Get-Childitem -Path .\ -Filter *.vcxproj -File
 if ($projects.length -eq 0)
 {
     exit -1
 }
 $projectPath = $projects.FullName
-# exit
-# param([String]$projectPath)
 
-# $projectPath = "c:\Users\migrie\dev\private\OpenConsole\src\cascadia\WindowsTerminal\WindowsTerminal.vcxproj"
-# $projectPath = $1
 $msBuildCondition = "'%(ProjectReference.Identity)' == '$projectPath.metaproj'"
-# $msBuildCondition = "'%(ProjectReference.Identity)' == '" $projectPath ".metaproj'"
-# Write-Host $msBuildCondition
 
+# Parse the solution's metaproj file.
 [xml]$Metaproj = Get-Content "$env:OPENCON\OpenConsole.sln.metaproj"
 
-$thing = $Metaproj.Project.PropertyGroup.MSBuildRuntimeVersion
-# Write-Host $thing
-
 $targets = $Metaproj.Project.Target
-# Write-Host $targets
 
-# # run selected tests
-# foreach ($t in $targets)
-# {
-#     # Write-Host $t.Name
-# }
-
+# Filter to project targets that match out metaproj file.
+# For Conhost\Server, this will match:
+#   [Conhost\Server, Conhost\Server:Clean, Conhost\Server:Rebuild, Conhost\Server:Publish]
 $matchingTargets = $targets | Where-Object { $_.MSBuild.Condition -eq $msBuildCondition }
-# Write-Host $matchingTargets.Name
-# Write-Host $matchingTargets.MsBuild.Targets
 
+# Further filter to the targets that dont have a suffix (like ":Clean")
 $matchingTargets = $matchingTargets | Where-Object { $hasProperty = $_.MsBuild.PSobject.Properties.name -match "Targets" ; return -Not $hasProperty }
-# $matchingTargets = $matchingTargets | Where-Object { $hasProperty = $_.Targets -eq $null }
+
 Write-Host $matchingTargets.Name
 exit 0
