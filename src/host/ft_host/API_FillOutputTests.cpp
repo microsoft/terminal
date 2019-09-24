e59f7d8b03b5a03552f0bcc417d141fe65d030f4
Carlos Zamora <cazamor@microsoft.com>
2019-09-24 12:47:22 -0700

    added testing

--- src/host/ft_host/API_FillOutputTests.cpp
+++ src/host/ft_host/API_FillOutputTests.cpp
@@ -58,4 +58,140 @@ class FillOutputTests
                                                                 &charsWritten));
         VERIFY_ARE_EQUAL(1u, charsWritten);
     }
+
+    TEST_METHOD(UnsetWrap)
+    {
+        // WARNING: If this test suddenly decides to start failing,
+        // this is because the wrap registry key is not set.
+        // TODO GH #2859: Get/Set Registry Key for Wrap
+
+        HANDLE hConsole = GetStdOutputHandle();
+        DWORD charsWritten = 0;
+
+        CONSOLE_SCREEN_BUFFER_INFOEX sbiex = { 0 };
+        sbiex.cbSize = sizeof(sbiex);
+        GetConsoleScreenBufferInfoEx(hConsole, &sbiex);
+
+        const auto ogConsoleWidth = sbiex.dwSize.X;
+
+        std::wstring input = L"";
+        for (SHORT i = 0; i < ogConsoleWidth + 2; i++)
+        {
+            input.append(L"a");
+        }
+
+        // Write until a wrap occurs
+        VERIFY_WIN32_BOOL_SUCCEEDED(WriteConsoleW(hConsole,
+                                                  input.data(),
+                                                  gsl::narrow_cast<DWORD>(input.size()),
+                                                  &charsWritten,
+                                                  nullptr));
+
+        // Verify wrap occurred
+        std::unique_ptr<wchar_t[]> bufferText = std::make_unique<wchar_t[]>(ogConsoleWidth);
+        DWORD readSize = 0;
+        VERIFY_WIN32_BOOL_SUCCEEDED(ReadConsoleOutputCharacterW(hConsole,
+                                                                bufferText.get(),
+                                                                ogConsoleWidth,
+                                                                { 0, 0 },
+                                                                &readSize));
+
+        for (DWORD i = 0; i < readSize; i++)
+        {
+            VERIFY_ARE_EQUAL(bufferText[i], L'a');
+        }
+
+        bufferText = std::make_unique<wchar_t[]>(2);
+        VERIFY_WIN32_BOOL_SUCCEEDED(ReadConsoleOutputCharacterW(hConsole,
+                                                                bufferText.get(),
+                                                                2,
+                                                                { 0, 1 },
+                                                                &readSize));
+
+        VERIFY_ARE_EQUAL(2u, readSize);
+        for (DWORD i = 0; i < readSize; i++)
+        {
+            VERIFY_ARE_EQUAL(bufferText[i], L'a');
+        }
+
+        // Fill Console Line with 'b's
+        VERIFY_WIN32_BOOL_SUCCEEDED(FillConsoleOutputCharacterW(hConsole,
+                                                                L'b',
+                                                                ogConsoleWidth,
+                                                                { 2, 0 },
+                                                                &charsWritten));
+
+        // Verify first line is full of 'a's then 'b's
+        bufferText = std::make_unique<wchar_t[]>(ogConsoleWidth);
+        VERIFY_WIN32_BOOL_SUCCEEDED(ReadConsoleOutputCharacterW(hConsole,
+                                                                bufferText.get(),
+                                                                ogConsoleWidth,
+                                                                { 0, 0 },
+                                                                &readSize));
+
+        for (DWORD i = 0; i < readSize; i++)
+        {
+            if (i < 2)
+            {
+                VERIFY_ARE_EQUAL(bufferText[i], L'a');
+            }
+            else
+            {
+                VERIFY_ARE_EQUAL(bufferText[i], L'b');
+            }
+        }
+
+        // Verify second line is still has 'a's that wrapped over
+        bufferText = std::make_unique<wchar_t[]>(2);
+        VERIFY_WIN32_BOOL_SUCCEEDED(ReadConsoleOutputCharacterW(hConsole,
+                                                                bufferText.get(),
+                                                                static_cast<SHORT>(2),
+                                                                { 0, 0 },
+                                                                &readSize));
+
+        VERIFY_ARE_EQUAL(2u, readSize);
+        for (DWORD i = 0; i < readSize; i++)
+        {
+            VERIFY_ARE_EQUAL(bufferText[i], L'a');
+        }
+
+        // Resize to be smaller by 2
+        sbiex.srWindow.Right -= 2;
+        sbiex.dwSize.X -= 2;
+        SetConsoleScreenBufferInfoEx(hConsole, &sbiex);
+
+        // Verify first line is full of 'a's then 'b's
+        bufferText = std::make_unique<wchar_t[]>(ogConsoleWidth - static_cast<SHORT>(2));
+        VERIFY_WIN32_BOOL_SUCCEEDED(ReadConsoleOutputCharacterW(hConsole,
+                                                                bufferText.get(),
+                                                                ogConsoleWidth - static_cast<SHORT>(2),
+                                                                { 0, 0 },
+                                                                &readSize));
+
+        for (DWORD i = 0; i < readSize; i++)
+        {
+            if (i < 2)
+            {
+                VERIFY_ARE_EQUAL(bufferText[i], L'a');
+            }
+            else
+            {
+                VERIFY_ARE_EQUAL(bufferText[i], L'b');
+            }
+        }
+
+        // Verify second line is still has 'a's ('b's didn't wrap over)
+        bufferText = std::make_unique<wchar_t[]>(static_cast<SHORT>(2));
+        VERIFY_WIN32_BOOL_SUCCEEDED(ReadConsoleOutputCharacterW(hConsole,
+                                                                bufferText.get(),
+                                                                static_cast<SHORT>(2),
+                                                                { 0, 0 },
+                                                                &readSize));
+
+        VERIFY_ARE_EQUAL(2u, readSize);
+        for (DWORD i = 0; i < readSize; i++)
+        {
+            VERIFY_ARE_EQUAL(bufferText[i], L'a');
+        }
+    }
 };
