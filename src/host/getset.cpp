0ce08aff32573e1125b9b38f339058b01952b3e5
Mike Griese <migrie@microsoft.com>
2019-10-01 12:42:33 -0500

    do not allow CUU and CUD to move "across" margins. (#2996)
    If we're moving the cursor up, its vertical movement should be stopped
    at the top margin. It should not magically jump up to the bottom margin.
    Similarly, this applies to moving down and the bottom margin.
    Furthermore, this constraint should always apply, not just when the
    start position is within BOTH margins
    
    Fixes #2929.

--- src/host/getset.cpp
+++ src/host/getset.cpp
@@ -1430,18 +1430,44 @@ void DoSrvPrivateAllowCursorBlinking(SCREEN_INFORMATION& screenInfo, const bool 
     // Make sure the cursor doesn't move outside the viewport.
     screenInfo.GetViewport().Clamp(clampedPos);
 
-    // Make sure the cursor stays inside the margins, but only if it started there
-    if (screenInfo.AreMarginsSet() && screenInfo.IsCursorInMargins(cursor.GetPosition()))
+    // Make sure the cursor stays inside the margins
+    if (screenInfo.AreMarginsSet())
     {
-        try
+        const auto margins = screenInfo.GetAbsoluteScrollMargins().ToInclusive();
+
+        const auto cursorY = cursor.GetPosition().Y;
+
+        const auto lo = margins.Top;
+        const auto hi = margins.Bottom;
+
+        // See microsoft/terminal#2929 - If the cursor is _below_ the top
+        // margin, it should stay below the top margin. If it's _above_ the
+        // bottom, it should stay above the bottom. Cursor movements that stay
+        // outside the margins shouldn't necessarily be affected. For example,
+        // moving up while below the bottom margin shouldn't just jump straight
+        // to the bottom margin. See
+        // ScreenBufferTests::CursorUpDownOutsideMargins for a test of that
+        // behavior.
+        const bool cursorBelowTop = cursorY >= lo;
+        const bool cursorAboveBottom = cursorY <= hi;
+
+        if (cursorBelowTop)
         {
-            const auto margins = screenInfo.GetAbsoluteScrollMargins().ToInclusive();
-            const auto v = clampedPos.Y;
-            const auto lo = margins.Top;
-            const auto hi = margins.Bottom;
-            clampedPos.Y = std::clamp(v, lo, hi);
+            try
+            {
+                clampedPos.Y = std::max(clampedPos.Y, lo);
+            }
+            CATCH_RETURN();
         }
-        CATCH_RETURN();
+
+        if (cursorAboveBottom)
+        {
+            try
+            {
+                clampedPos.Y = std::min(clampedPos.Y, hi);
+            }
+            CATCH_RETURN();
+        }
     }
     cursor.SetPosition(clampedPos);
 
