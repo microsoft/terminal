53c81a08f88fc3b34f7a492d3da46b3a3651f80d
Mike Griese <migrie@microsoft.com>
2019-10-04 10:47:39 -0500

    Return to ground when we flush the last char (#2823)
    ## Summary of the Pull Request
    The InputStateMachineEngine was incorrectly not returning to the ground state after flushing the last sequence. That means that something like alt+backspace would leave us in the Escape state, not the ground state. This makes sure we return to ground.
    
    Additionally removes the "Parser.UnitTests-common.vcxproj" file, which was originally used for a theoretical time when we only open-sourced the parser. It's unnecessary now, and we can get rid of it.
    
    Also includes a small patch to bcz.cmd, to make sure bx works with projects with a space in their name.
    
    <!-- Please review the items on the PR checklist before submitting-->
    ## PR Checklist
    * [x] Closes #2746
    * [x] I work here
    * [x] Tests added/passed
    * [n/a] Requires documentation to be updated
    
    
    <hr>
    
    * Return to ground when we flush the last char
    
      The InputStateMachineEngine was incorrectly not returning to the ground state
      after flushing the last sequence. That means that something like alt+backspace
      would leave us in the Escape state, not the ground state. This makes sure we
      return to ground.
    
      Fixes #2746.
    
      Additionally removes the "Parser.UnitTests-common.vcxproj" file, which was
      originally used for a theoretical time when we only open-sourced the parser.
      It's unnecessary now, and we can get rid of it.
    
      Also includes a small patch to bcz.cmd, to make sure bx works with projects
      with a space in their name.
    
    * Update src/terminal/parser/stateMachine.cpp
    
    Co-Authored-By: Dustin L. Howett (MSFT) <duhowett@microsoft.com>
    
    * add the comment @miniksa wanted

--- src/terminal/parser/stateMachine.cpp
+++ src/terminal/parser/stateMachine.cpp
@@ -1399,6 +1399,25 @@ void StateMachine::ProcessString(const wchar_t* const rgwch, const size_t cch)
     }
     else if (s_fProcessIndividually)
     {
+        // One of the "weird things" in VT input is the case of something like
+        // <kbd>alt+[</kbd>. In VT, that's encoded as `\x1b[`. However, that's
+        // also the start of a CSI, and could be the start of a longer sequence,
+        // there's no way to know for sure. For an <kbd>alt+[</kbd> keypress,
+        // the parser originally would just sit in the `CsiEntry` state after
+        // processing it, which would pollute the following keypress (e.g.
+        // <kbd>alt+[</kbd>, <kbd>A</kbd> would be processed like `\x1b[A`,
+        // which is _wrong_).
+        //
+        // Fortunately, for VT input, each keystroke comes in as an individual
+        // write operation. So, if at the end of processing a string for the
+        // InputEngine, we find that we're not in the Ground state, that implies
+        // that we've processed some input, but not dispatched it yet. This
+        // block at the end of `ProcessString` will then re-process the
+        // undispatched string, but it will ensure that it dispatches on the
+        // last character of the string. For our previous `\x1b[` scenario, that
+        // means we'll make sure to call `_ActionEscDispatch('[')`., which will
+        // properly decode the string as <kbd>alt+[</kbd>.
+
         if (_pEngine->FlushAtEndOfString())
         {
             // Reset our state, and put all but the last char in again.
@@ -1413,25 +1432,31 @@ void StateMachine::ProcessString(const wchar_t* const rgwch, const size_t cch)
             switch (_state)
             {
             case VTStates::Ground:
-                return _ActionExecute(*pwch);
+                _ActionExecute(*pwch);
+                break;
             case VTStates::Escape:
             case VTStates::EscapeIntermediate:
-                return _ActionEscDispatch(*pwch);
+                _ActionEscDispatch(*pwch);
+                break;
             case VTStates::CsiEntry:
             case VTStates::CsiIntermediate:
             case VTStates::CsiIgnore:
             case VTStates::CsiParam:
-                return _ActionCsiDispatch(*pwch);
+                _ActionCsiDispatch(*pwch);
+                break;
             case VTStates::OscParam:
             case VTStates::OscString:
             case VTStates::OscTermination:
-                return _ActionOscDispatch(*pwch);
+                _ActionOscDispatch(*pwch);
+                break;
             case VTStates::Ss3Entry:
             case VTStates::Ss3Param:
-                return _ActionSs3Dispatch(*pwch);
-            default:
-                return;
+                _ActionSs3Dispatch(*pwch);
+                break;
             }
+            // microsoft/terminal#2746: Make sure to return to the ground state
+            // after dispatching the characters
+            _EnterGround();
         }
     }
 }
