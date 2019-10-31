7ce59bafbb00f66b297a5c386ffc9134a6cb5003
Mike Griese <migrie@microsoft.com>
2019-10-31 09:52:43 -0500

    Revert a bad sln change, and clean out dead code

--- src/cascadia/TerminalApp/ActionAndArgs.cpp
+++ src/cascadia/TerminalApp/ActionAndArgs.cpp
@@ -4,20 +4,4 @@
 
 namespace winrt::TerminalApp::implementation
 {
-    // TerminalApp::IActionArgs ActionAndArgs::Args()
-    // {
-    //     throw hresult_not_implemented();
-    // }
-    // void ActionAndArgs::Args(TerminalApp::IActionArgs const& value)
-    // {
-    //     throw hresult_not_implemented();
-    // }
-    // TerminalApp::ShortcutAction ActionAndArgs::Action()
-    // {
-    //     throw hresult_not_implemented();
-    // }
-    // void ActionAndArgs::Action(TerminalApp::ShortcutAction const& value)
-    // {
-    //     throw hresult_not_implemented();
-    // }
 }
--- src/cascadia/TerminalApp/ActionAndArgs.h
+++ src/cascadia/TerminalApp/ActionAndArgs.h
@@ -7,12 +7,6 @@ namespace winrt::TerminalApp::implementation
     struct ActionAndArgs : public ActionAndArgsT<ActionAndArgs>
     {
         ActionAndArgs() = default;
-
-        // TerminalApp::IActionArgs Args();
-        // void Args(TerminalApp::IActionArgs const& value);
-        // TerminalApp::ShortcutAction Action();
-        // void Action(TerminalApp::ShortcutAction const& value);
-
         GETSET_PROPERTY(TerminalApp::ShortcutAction, Action, TerminalApp::ShortcutAction::Invalid);
         GETSET_PROPERTY(IActionArgs, Args, nullptr);
     };
@@ -22,5 +16,3 @@ namespace winrt::TerminalApp::factory_implementation
 {
     BASIC_FACTORY(ActionAndArgs);
 }
-
-// BASIC_FACTORY(ActionAndArgs);
