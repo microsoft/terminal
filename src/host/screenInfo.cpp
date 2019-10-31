d1312b552b5bfc2e05d1b45c36ad3ca37a3e8003
skyline75489 <skyline75489@outlook.com>
2019-10-31 09:19:10 +0800

    Replace some macros with constexpr

--- src/host/screenInfo.cpp
+++ src/host/screenInfo.cpp
@@ -2040,7 +2040,7 @@ const SCREEN_INFORMATION& SCREEN_INFORMATION::GetMainBuffer() const
                                                          WindowSize,
                                                          GetAttributes(),
                                                          *GetPopupAttributes(),
-                                                         CURSOR_SMALL_SIZE,
+                                                         Cursor::CURSOR_SMALL_SIZE,
                                                          ppsiNewScreenBuffer);
     if (NT_SUCCESS(Status))
     {
