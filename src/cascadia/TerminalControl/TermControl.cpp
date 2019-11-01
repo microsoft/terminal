1cdd41492ffc9bec1c6b948eb09164ffd75d1186
Kaiyu Wang <kawa@microsoft.com>
2019-11-01 14:47:45 -0700

    do not add SearchBox to TermControl for now

--- src/cascadia/TerminalControl/TermControl.cpp
+++ src/cascadia/TerminalControl/TermControl.cpp
@@ -131,12 +131,6 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
         _connection.TerminalDisconnected([=]() {
             _connectionClosedHandlers();
         });
-
-        SearchBoxControl searchBox;
-        searchBox.Visibility(Visibility::Visible);
-        searchBox.HorizontalAlignment(HorizontalAlignment::Right);
-
-        container.Children().Append(searchBox);
     }
 
     // Method Description:
