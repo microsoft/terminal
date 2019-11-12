d6cab12fe40951cdc2e740803f9fcf2ebea92f06
skyline75489 <skyline75489@outlook.com>
2019-11-12 13:47:59 +0800

    Throttle scroll position update

--- src/cascadia/TerminalControl/TermControl.cpp
+++ src/cascadia/TerminalControl/TermControl.cpp
@@ -54,6 +54,7 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
         _settings{ settings },
         _closing{ false },
         _lastScrollOffset{ std::nullopt },
+        _lastScrollTime{ std::nullopt },
         _autoScrollVelocity{ 0 },
         _autoScrollingPointerPoint{ std::nullopt },
         _autoScrollTimer{},
@@ -1410,6 +1411,23 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
         {
             return;
         }
+
+        const auto timeNow = std::chrono::high_resolution_clock::now();
+
+        if (_lastScrollTime.has_value())
+        {
+            // Limit update operation to frame rate
+            // See also: Microsoft::Console::Render::RenderThread::s_FrameLimitMilliseconds
+            static constexpr long long s_ScrollRateLimitMilliseconds = 8;
+            const long long deltaTimeInMilliSec = std::chrono::duration_cast<std::chrono::milliseconds>(timeNow - _lastScrollTime.value()).count();
+            if (deltaTimeInMilliSec < s_ScrollRateLimitMilliseconds)
+            {
+                _lastScrollTime = std::nullopt;
+                return;
+            }
+        }
+
+        _lastScrollTime = timeNow;
 
         // Update our scrollbar
         _scrollBar.Dispatcher().RunAsync(CoreDispatcherPriority::Low, [=]() {
