c5542ccd0d98a97bc7c247e44b7e1b18420281c5
Michael Kitzan <mkitzan45@gmail.com>
2019-12-04 10:14:25 -0800

    Weak captures in Tab and TerminalPage lambdas

--- src/cascadia/TerminalApp/Tab.h
+++ src/cascadia/TerminalApp/Tab.h
@@ -5,10 +5,14 @@
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
 #include "Pane.h"
 
-class Tab
+// TODO: make Tab a WinRT type
+class Tab : public std::enable_shared_from_this<Tab>
 {
 public:
     Tab(const GUID& profile, const winrt::Microsoft::Terminal::TerminalControl::TermControl& control);
+
+    // Called after construction to setup events with weak_ptr
+    void BindEventHandlers(const winrt::Microsoft::Terminal::TerminalControl::TermControl& control) noexcept;
 
     winrt::Microsoft::UI::Xaml::Controls::TabViewItem GetTabViewItem();
     winrt::Windows::UI::Xaml::UIElement GetRootElement();
