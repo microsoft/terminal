402b7ff0e055a56baa5e9b4ee127b9ed75658191
Michael Niksa <miniksa@microsoft.com>
2019-12-09 11:07:08 -0800

    Create Telnet connection type and default loopback profile for… (#3858)
    For our Universal terminal for development purposes, we will use telnet to escape the universal application container and empower developers to debug/diagnose issues with their own machine on loopback to the already-elevated telnet context.

--- src/cascadia/TerminalApp/AppLogic.cpp
+++ src/cascadia/TerminalApp/AppLogic.cpp
@@ -129,9 +129,24 @@ namespace winrt::TerminalApp::implementation
         _root = winrt::make_self<TerminalPage>();
     }
 
+    // Method Decscription:
+    // - Called around the codebase to discover if this is a UWP where we need to turn off specific settings.
+    // Arguments:
+    // - <none> - reports internal state
+    // Return Value:
+    // - True if UWP, false otherwise.
+    bool AppLogic::IsUwp() const noexcept
+    {
+        return _isUwp;
+    }
+
     // Method Description:
     // - Called by UWP context invoker to let us know that we may have to change some of our behaviors
     //   for being a UWP
+    // Arguments:
+    // - <none> (sets to UWP = true, one way change)
+    // Return Value:
+    // - <none>
     void AppLogic::RunAsUwp()
     {
         _isUwp = true;
@@ -438,7 +453,7 @@ namespace winrt::TerminalApp::implementation
 
         try
         {
-            auto newSettings = CascadiaSettings::LoadAll();
+            auto newSettings = _isUwp ? CascadiaSettings::LoadUniversal() : CascadiaSettings::LoadAll();
             _settings = std::move(newSettings);
             const auto& warnings = _settings->GetWarnings();
             hr = warnings.size() == 0 ? S_OK : S_FALSE;
--- src/cascadia/TerminalApp/TerminalPage.cpp
+++ src/cascadia/TerminalApp/TerminalPage.cpp
@@ -13,6 +13,7 @@
 #include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
 
 #include "AzureCloudShellGenerator.h" // For AzureConnectionType
+#include "TelnetGenerator.h" // For TelnetConnectionType
 #include "TabRowControl.h"
 
 using namespace winrt;
@@ -340,34 +341,39 @@ namespace winrt::TerminalApp::implementation
 
         // add static items
         {
-            // Create the settings button.
-            auto settingsItem = WUX::Controls::MenuFlyoutItem{};
-            settingsItem.Text(RS_(L"SettingsMenuItem"));
+            const auto isUwp = ::winrt::Windows::UI::Xaml::Application::Current().as<::winrt::TerminalApp::App>().Logic().IsUwp();
 
-            WUX::Controls::SymbolIcon ico{};
-            ico.Symbol(WUX::Controls::Symbol::Setting);
-            settingsItem.Icon(ico);
-
-            settingsItem.Click({ this, &TerminalPage::_SettingsButtonOnClick });
-            newTabFlyout.Items().Append(settingsItem);
-
-            auto settingsKeyChord = keyBindings.GetKeyBindingForAction(ShortcutAction::OpenSettings);
-            if (settingsKeyChord)
+            if (!isUwp)
             {
-                _SetAcceleratorForMenuItem(settingsItem, settingsKeyChord);
+                // Create the settings button.
+                auto settingsItem = WUX::Controls::MenuFlyoutItem{};
+                settingsItem.Text(RS_(L"SettingsMenuItem"));
+
+                WUX::Controls::SymbolIcon ico{};
+                ico.Symbol(WUX::Controls::Symbol::Setting);
+                settingsItem.Icon(ico);
+
+                settingsItem.Click({ this, &TerminalPage::_SettingsButtonOnClick });
+                newTabFlyout.Items().Append(settingsItem);
+
+                auto settingsKeyChord = keyBindings.GetKeyBindingForAction(ShortcutAction::OpenSettings);
+                if (settingsKeyChord)
+                {
+                    _SetAcceleratorForMenuItem(settingsItem, settingsKeyChord);
+                }
+
+                // Create the feedback button.
+                auto feedbackFlyout = WUX::Controls::MenuFlyoutItem{};
+                feedbackFlyout.Text(RS_(L"FeedbackMenuItem"));
+
+                WUX::Controls::FontIcon feedbackIcon{};
+                feedbackIcon.Glyph(L"\xE939");
+                feedbackIcon.FontFamily(Media::FontFamily{ L"Segoe MDL2 Assets" });
+                feedbackFlyout.Icon(feedbackIcon);
+
+                feedbackFlyout.Click({ this, &TerminalPage::_FeedbackButtonOnClick });
+                newTabFlyout.Items().Append(feedbackFlyout);
             }
-
-            // Create the feedback button.
-            auto feedbackFlyout = WUX::Controls::MenuFlyoutItem{};
-            feedbackFlyout.Text(RS_(L"FeedbackMenuItem"));
-
-            WUX::Controls::FontIcon feedbackIcon{};
-            feedbackIcon.Glyph(L"\xE939");
-            feedbackIcon.FontFamily(Media::FontFamily{ L"Segoe MDL2 Assets" });
-            feedbackFlyout.Icon(feedbackIcon);
-
-            feedbackFlyout.Click({ this, &TerminalPage::_FeedbackButtonOnClick });
-            newTabFlyout.Items().Append(feedbackFlyout);
 
             // Create the about button.
             auto aboutFlyout = WUX::Controls::MenuFlyoutItem{};
@@ -525,6 +531,12 @@ namespace winrt::TerminalApp::implementation
         {
             connection = TerminalConnection::AzureConnection(settings.InitialRows(),
                                                              settings.InitialCols());
+        }
+
+        else if (profile->HasConnectionType() &&
+                 profile->GetConnectionType() == TelnetConnectionType)
+        {
+            connection = TerminalConnection::TelnetConnection(settings.Commandline());
         }
 
         else
