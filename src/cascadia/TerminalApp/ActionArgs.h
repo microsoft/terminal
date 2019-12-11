microsoft/terminal

commit dd1c7b3e525b5f4b03642e10602e1154e36d8a9a

Add support for new panes with specifc profiles and other settings overrides (#3825)
## Summary of the Pull Request


This enables the user to set a number of extra settings in the `NewTab` and `SplitPane` `ShortcutAction`s, that enable customizing how a new terminal is created at runtime. The following four properties were added:
* `profile`
* `commandline`
* `tabTitle`
* `startingDirectory`

`profile` can be used with either a GUID or the name of a profile, and the action will launch that profile instead of the default.

`commandline`, `tabTitle`, and `startingDirectory` can all be used to override the profile's values of those settings. This will be more useful for #607.

With this PR, you can make bindings like the following:

```json

{ "keys": ["ctrl+a"], "command": { "action": "splitPane", "split": "vertical" } },
{ "keys": ["ctrl+b"], "command": { "action": "splitPane", "split": "vertical", "profile": "{6239a42c-1111-49a3-80bd-e8fdd045185c}" } },
{ "keys": ["ctrl+c"], "command": { "action": "splitPane", "split": "vertical", "profile": "profile1" } },
{ "keys": ["ctrl+d"], "command": { "action": "splitPane", "split": "vertical", "profile": "profile2" } },
{ "keys": ["ctrl+e"], "command": { "action": "splitPane", "split": "horizontal", "commandline": "foo.exe" } },
{ "keys": ["ctrl+f"], "command": { "action": "splitPane", "split": "horizontal", "profile": "profile1", "commandline": "foo.exe" } },
{ "keys": ["ctrl+g"], "command": { "action": "newTab" } },
{ "keys": ["ctrl+h"], "command": { "action": "newTab", "startingDirectory": "c:\\foo" } },
{ "keys": ["ctrl+i"], "command": { "action": "newTab", "profile": "profile2", "startingDirectory": "c:\\foo" } },
{ "keys": ["ctrl+j"], "command": { "action": "newTab", "tabTitle": "bar" } },
{ "keys": ["ctrl+k"], "command": { "action": "newTab", "profile": "profile2", "tabTitle": "bar" } },

src/cascadia/TerminalApp/ActionArgs.h
@@ -6,6 +6,7 @@
 // HEY YOU: When adding ActionArgs types, make sure to add the corresponding
 //          *.g.cpp to ActionArgs.cpp!
 #include "ActionEventArgs.g.h"
+#include "NewTerminalArgs.g.h"
 #include "CopyTextArgs.g.h"
 #include "NewTabArgs.g.h"
 #include "SwitchToTabArgs.g.h"
@@ -32,6 +33,63 @@ namespace winrt::TerminalApp::implementation
             _ActionArgs{ args } {};
         GETSET_PROPERTY(IActionArgs, ActionArgs, nullptr);
         GETSET_PROPERTY(bool, Handled, false);
+    };
+
+    struct NewTerminalArgs : public NewTerminalArgsT<NewTerminalArgs>
+    {
+        NewTerminalArgs() = default;
+        GETSET_PROPERTY(winrt::hstring, Commandline, L"");
+        GETSET_PROPERTY(winrt::hstring, StartingDirectory, L"");
+        GETSET_PROPERTY(winrt::hstring, TabTitle, L"");
+        GETSET_PROPERTY(Windows::Foundation::IReference<int32_t>, ProfileIndex, nullptr);
+        GETSET_PROPERTY(winrt::hstring, Profile, L"");
+
+        static constexpr std::string_view CommandlineKey{ "commandline" };
+        static constexpr std::string_view StartingDirectoryKey{ "startingDirectory" };
+        static constexpr std::string_view TabTitleKey{ "tabTitle" };
+        static constexpr std::string_view ProfileIndexKey{ "index" };
+        static constexpr std::string_view ProfileKey{ "profile" };
+
+    public:
+        bool Equals(const IActionArgs& other)
+        {
+            auto otherAsUs = other.try_as<NewTerminalArgs>();
+            if (otherAsUs)
+            {
+                return otherAsUs->_Commandline == _Commandline &&
+                       otherAsUs->_StartingDirectory == _StartingDirectory &&
+                       otherAsUs->_TabTitle == _TabTitle &&
+                       otherAsUs->_ProfileIndex == _ProfileIndex &&
+                       otherAsUs->_Profile == _Profile;
+            }
+            return false;
+        };
+        static winrt::TerminalApp::NewTerminalArgs FromJson(const Json::Value& json)
+        {
+            // LOAD BEARING: Not using make_self here _will_ break you in the future!
+            auto args = winrt::make_self<NewTerminalArgs>();
+            if (auto commandline{ json[JsonKey(CommandlineKey)] })
+            {
+                args->_Commandline = winrt::to_hstring(commandline.asString());
+            }
+            if (auto startingDirectory{ json[JsonKey(StartingDirectoryKey)] })
+            {
+                args->_StartingDirectory = winrt::to_hstring(startingDirectory.asString());
+            }
+            if (auto tabTitle{ json[JsonKey(TabTitleKey)] })
+            {
+                args->_TabTitle = winrt::to_hstring(tabTitle.asString());
+            }
+            if (auto index{ json[JsonKey(ProfileIndexKey)] })
+            {
+                args->_ProfileIndex = index.asInt();
+            }
+            if (auto profile{ json[JsonKey(ProfileKey)] })
+            {
+                args->_Profile = winrt::to_hstring(profile.asString());
+            }
+            return *args;
+        }
     };
 
     struct CopyTextArgs : public CopyTextArgsT<CopyTextArgs>
@@ -66,9 +124,7 @@ namespace winrt::TerminalApp::implementation
     struct NewTabArgs : public NewTabArgsT<NewTabArgs>
     {
         NewTabArgs() = default;
-        GETSET_PROPERTY(Windows::Foundation::IReference<int32_t>, ProfileIndex, nullptr);
-
-        static constexpr std::string_view ProfileIndexKey{ "index" };
+        GETSET_PROPERTY(winrt::TerminalApp::NewTerminalArgs, TerminalArgs, nullptr);
 
     public:
         bool Equals(const IActionArgs& other)
@@ -76,7 +132,7 @@ namespace winrt::TerminalApp::implementation
             auto otherAsUs = other.try_as<NewTabArgs>();
             if (otherAsUs)
             {
-                return otherAsUs->_ProfileIndex == _ProfileIndex;
+                return otherAsUs->_TerminalArgs == _TerminalArgs;
             }
             return false;
         };
@@ -84,10 +140,7 @@ namespace winrt::TerminalApp::implementation
         {
             // LOAD BEARING: Not using make_self here _will_ break you in the future!
             auto args = winrt::make_self<NewTabArgs>();
-            if (auto profileIndex{ json[JsonKey(ProfileIndexKey)] })
-            {
-                args->_ProfileIndex = profileIndex.asInt();
-            }
+            args->_TerminalArgs = NewTerminalArgs::FromJson(json);
             return *args;
         }
     };
@@ -265,6 +318,7 @@ namespace winrt::TerminalApp::implementation
     {
         SplitPaneArgs() = default;
         GETSET_PROPERTY(winrt::TerminalApp::SplitState, SplitStyle, winrt::TerminalApp::SplitState::None);
+        GETSET_PROPERTY(winrt::TerminalApp::NewTerminalArgs, TerminalArgs, nullptr);
 
         static constexpr std::string_view SplitKey{ "split" };
 
@@ -274,7 +328,8 @@ namespace winrt::TerminalApp::implementation
             auto otherAsUs = other.try_as<SplitPaneArgs>();
             if (otherAsUs)
             {
-                return otherAsUs->_SplitStyle == _SplitStyle;
+                return otherAsUs->_SplitStyle == _SplitStyle &&
+                       otherAsUs->_TerminalArgs == _TerminalArgs;
             }
             return false;
         };
@@ -282,6 +337,7 @@ namespace winrt::TerminalApp::implementation
         {
             // LOAD BEARING: Not using make_self here _will_ break you in the future!
             auto args = winrt::make_self<SplitPaneArgs>();
+            args->_TerminalArgs = NewTerminalArgs::FromJson(json);
             if (auto jsonStyle{ json[JsonKey(SplitKey)] })
             {
                 args->_SplitStyle = ParseSplitState(jsonStyle.asString());
@@ -294,4 +350,5 @@ namespace winrt::TerminalApp::implementation
 namespace winrt::TerminalApp::factory_implementation
 {
     BASIC_FACTORY(ActionEventArgs);
+    BASIC_FACTORY(NewTerminalArgs);
 }

src/cascadia/TerminalApp/ActionArgs.idl
@@ -30,6 +30,17 @@ namespace TerminalApp
         Horizontal = 2
     };
 
+    [default_interface] runtimeclass NewTerminalArgs {
+        NewTerminalArgs();
+        String Commandline;
+        String StartingDirectory;
+        String TabTitle;
+        String Profile; // Either a GUID or a profile's name if the GUID isn't a match
+        // ProfileIndex can be null (for "use the default"), so this needs to be
+        // a IReference, so it's nullable
+        Windows.Foundation.IReference<Int32> ProfileIndex { get; };
+    };
+
     [default_interface] runtimeclass ActionEventArgs : IActionEventArgs
     {
         ActionEventArgs(IActionArgs args);
@@ -42,9 +53,7 @@ namespace TerminalApp
 
     [default_interface] runtimeclass NewTabArgs : IActionArgs
     {
-        // ProfileIndex can be null (for "use the default"), so this needs to be
-        // a IReference, so it's nullable
-        Windows.Foundation.IReference<Int32> ProfileIndex { get; };
+        NewTerminalArgs TerminalArgs { get; };
     };
 
     [default_interface] runtimeclass SwitchToTabArgs : IActionArgs
@@ -70,5 +79,6 @@ namespace TerminalApp
     [default_interface] runtimeclass SplitPaneArgs : IActionArgs
     {
         SplitState SplitStyle { get; };
+        NewTerminalArgs TerminalArgs { get; };
     };
 }

src/cascadia/LocalTests_TerminalApp/KeyBindingsTests.cpp
@@ -6,6 +6,7 @@
 #include "../TerminalApp/ColorScheme.h"
 #include "../TerminalApp/CascadiaSettings.h"
 #include "JsonTestClass.h"
+#include "TestUtils.h"
 
 using namespace Microsoft::Console;
 using namespace TerminalApp;
@@ -46,42 +47,6 @@ namespace TerminalAppLocalTests
             InitializeJsonReader();
             return true;
         }
-
-        // Function Description:
-        // - This is a helper to retrieve the ActionAndArgs from the keybindings
-        //   for a given chord.
-        // Arguments:
-        // - bindings: The AppKeyBindings to lookup the ActionAndArgs from.
-        // - kc: The key chord to look up the bound ActionAndArgs for.
-        // Return Value:
-        // - The ActionAndArgs bound to the given key, or nullptr if nothing is bound to it.
-        static const ActionAndArgs GetActionAndArgs(const implementation::AppKeyBindings& bindings,
-                                                    const KeyChord& kc)
-        {
-            std::wstring buffer{ L"" };
-            if (WI_IsFlagSet(kc.Modifiers(), KeyModifiers::Ctrl))
-            {
-                buffer += L"Ctrl+";
-            }
-            if (WI_IsFlagSet(kc.Modifiers(), KeyModifiers::Shift))
-            {
-                buffer += L"Shift+";
-            }
-            if (WI_IsFlagSet(kc.Modifiers(), KeyModifiers::Alt))
-            {
-                buffer += L"Alt+";
-            }
-            buffer += static_cast<wchar_t>(MapVirtualKeyW(kc.Vkey(), MAPVK_VK_TO_CHAR));
-            Log::Comment(NoThrowString().Format(L"Looking for key:%s", buffer.c_str()));
-
-            const auto keyIter = bindings._keyShortcuts.find(kc);
-            VERIFY_IS_TRUE(keyIter != bindings._keyShortcuts.end(), L"Expected to find an action bound to the given KeyChord");
-            if (keyIter != bindings._keyShortcuts.end())
-            {
-                return keyIter->second;
-            }
-            return nullptr;
-        };
     };
 
     void KeyBindingsTests::ManyKeysSameAction()
@@ -233,7 +198,7 @@ namespace TerminalAppLocalTests
             Log::Comment(NoThrowString().Format(
                 L"Verify that `copy` without args parses as Copy(TrimWhitespace=true)"));
             KeyChord kc{ true, false, false, static_cast<int32_t>('C') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             const auto& realArgs = actionAndArgs.Args().try_as<CopyTextArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
             // Verify the args have the expected value
@@ -244,7 +209,7 @@ namespace TerminalAppLocalTests
             Log::Comment(NoThrowString().Format(
                 L"Verify that `copyTextWithoutNewlines` parses as Copy(TrimWhitespace=false)"));
             KeyChord kc{ false, true, false, static_cast<int32_t>('C') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             const auto& realArgs = actionAndArgs.Args().try_as<CopyTextArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
             // Verify the args have the expected value
@@ -255,7 +220,7 @@ namespace TerminalAppLocalTests
             Log::Comment(NoThrowString().Format(
                 L"Verify that `copy` with args parses them correctly"));
             KeyChord kc{ true, false, true, static_cast<int32_t>('C') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             const auto& realArgs = actionAndArgs.Args().try_as<CopyTextArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
             // Verify the args have the expected value
@@ -266,7 +231,7 @@ namespace TerminalAppLocalTests
             Log::Comment(NoThrowString().Format(
                 L"Verify that `copy` with args parses them correctly"));
             KeyChord kc{ false, true, true, static_cast<int32_t>('C') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             const auto& realArgs = actionAndArgs.Args().try_as<CopyTextArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
             // Verify the args have the expected value
@@ -277,68 +242,73 @@ namespace TerminalAppLocalTests
             Log::Comment(NoThrowString().Format(
                 L"Verify that `newTab` without args parses as NewTab(Index=null)"));
             KeyChord kc{ true, false, false, static_cast<int32_t>('T') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::NewTab, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<NewTabArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
             // Verify the args have the expected value
-            VERIFY_IS_NULL(realArgs.ProfileIndex());
+            VERIFY_IS_NOT_NULL(realArgs.TerminalArgs());
+            VERIFY_IS_NULL(realArgs.TerminalArgs().ProfileIndex());
         }
         {
             Log::Comment(NoThrowString().Format(
                 L"Verify that `newTab` parses args correctly"));
             KeyChord kc{ true, false, true, static_cast<int32_t>('T') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::NewTab, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<NewTabArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
             // Verify the args have the expected value
-            VERIFY_IS_NOT_NULL(realArgs.ProfileIndex());
-            VERIFY_ARE_EQUAL(0, realArgs.ProfileIndex().Value());
+            VERIFY_IS_NOT_NULL(realArgs.TerminalArgs());
+            VERIFY_IS_NOT_NULL(realArgs.TerminalArgs().ProfileIndex());
+            VERIFY_ARE_EQUAL(0, realArgs.TerminalArgs().ProfileIndex().Value());
         }
         {
             Log::Comment(NoThrowString().Format(
                 L"Verify that `newTabProfile0` parses as NewTab(Index=0)"));
             KeyChord kc{ false, true, true, static_cast<int32_t>('T') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::NewTabProfile0, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<NewTabArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
             // Verify the args have the expected value
-            VERIFY_IS_NOT_NULL(realArgs.ProfileIndex());
-            VERIFY_ARE_EQUAL(0, realArgs.ProfileIndex().Value());
+            VERIFY_IS_NOT_NULL(realArgs.TerminalArgs());
+            VERIFY_IS_NOT_NULL(realArgs.TerminalArgs().ProfileIndex());
+            VERIFY_ARE_EQUAL(0, realArgs.TerminalArgs().ProfileIndex().Value());
         }
         {
             Log::Comment(NoThrowString().Format(
                 L"Verify that `newTab` with an index greater than the legacy "
                 L"args afforded parses correctly"));
             KeyChord kc{ true, false, true, static_cast<int32_t>('Y') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::NewTab, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<NewTabArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
             // Verify the args have the expected value
-            VERIFY_IS_NOT_NULL(realArgs.ProfileIndex());
-            VERIFY_ARE_EQUAL(11, realArgs.ProfileIndex().Value());
+            VERIFY_IS_NOT_NULL(realArgs.TerminalArgs());
+            VERIFY_IS_NOT_NULL(realArgs.TerminalArgs().ProfileIndex());
+            VERIFY_ARE_EQUAL(11, realArgs.TerminalArgs().ProfileIndex().Value());
         }
         {
             Log::Comment(NoThrowString().Format(
                 L"Verify that `newTabProfile8` parses as NewTab(Index=8)"));
             KeyChord kc{ false, true, true, static_cast<int32_t>('Y') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::NewTabProfile8, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<NewTabArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
             // Verify the args have the expected value
-            VERIFY_IS_NOT_NULL(realArgs.ProfileIndex());
-            VERIFY_ARE_EQUAL(8, realArgs.ProfileIndex().Value());
+            VERIFY_IS_NOT_NULL(realArgs.TerminalArgs());
+            VERIFY_IS_NOT_NULL(realArgs.TerminalArgs().ProfileIndex());
+            VERIFY_ARE_EQUAL(8, realArgs.TerminalArgs().ProfileIndex().Value());
         }
 
         {
             Log::Comment(NoThrowString().Format(
                 L"Verify that `copy` ignores args it doesn't understand"));
             KeyChord kc{ true, false, true, static_cast<int32_t>('B') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::CopyText, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<CopyTextArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
@@ -350,7 +320,7 @@ namespace TerminalAppLocalTests
             Log::Comment(NoThrowString().Format(
                 L"Verify that `copy` null as it's `args` parses as the default option"));
             KeyChord kc{ true, false, true, static_cast<int32_t>('B') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::CopyText, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<CopyTextArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
@@ -362,7 +332,7 @@ namespace TerminalAppLocalTests
             Log::Comment(NoThrowString().Format(
                 L"Verify that `increaseFontSize` without args parses as AdjustFontSize(Delta=1)"));
             KeyChord kc{ true, false, false, static_cast<int32_t>('F') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::IncreaseFontSize, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<AdjustFontSizeArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
@@ -374,7 +344,7 @@ namespace TerminalAppLocalTests
             Log::Comment(NoThrowString().Format(
                 L"Verify that `decreaseFontSize` without args parses as AdjustFontSize(Delta=-1)"));
             KeyChord kc{ true, false, false, static_cast<int32_t>('G') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::DecreaseFontSize, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<AdjustFontSizeArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
@@ -405,7 +375,7 @@ namespace TerminalAppLocalTests
 
         {
             KeyChord kc{ true, false, false, static_cast<int32_t>('A') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::SplitVertical, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<SplitPaneArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
@@ -414,7 +384,7 @@ namespace TerminalAppLocalTests
         }
         {
             KeyChord kc{ true, false, false, static_cast<int32_t>('B') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::SplitHorizontal, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<SplitPaneArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
@@ -423,7 +393,7 @@ namespace TerminalAppLocalTests
         }
         {
             KeyChord kc{ true, false, false, static_cast<int32_t>('C') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::SplitPane, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<SplitPaneArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
@@ -432,7 +402,7 @@ namespace TerminalAppLocalTests
         }
         {
             KeyChord kc{ true, false, false, static_cast<int32_t>('D') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::SplitPane, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<SplitPaneArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
@@ -441,7 +411,7 @@ namespace TerminalAppLocalTests
         }
         {
             KeyChord kc{ true, false, false, static_cast<int32_t>('E') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::SplitPane, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<SplitPaneArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
@@ -450,7 +420,7 @@ namespace TerminalAppLocalTests
         }
         {
             KeyChord kc{ true, false, false, static_cast<int32_t>('F') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::SplitPane, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<SplitPaneArgs>();
             VERIFY_IS_NOT_NULL(realArgs);
@@ -459,7 +429,7 @@ namespace TerminalAppLocalTests
         }
         {
             KeyChord kc{ true, false, false, static_cast<int32_t>('G') };
-            auto actionAndArgs = GetActionAndArgs(*appKeyBindings, kc);
+            auto actionAndArgs = TestUtils::GetActionAndArgs(*appKeyBindings, kc);
             VERIFY_ARE_EQUAL(ShortcutAction::SplitPane, actionAndArgs.Action());
             const auto& realArgs = actionAndArgs.Args().try_as<SplitPaneArgs>();
             VERIFY_IS_NOT_NULL(realArgs);

commit bb60db76fbd381b7b054f1919f1aa4097a226956

Re-add the keychords to the new tab flyout (#3903)
## Summary of the Pull Request

On this month's episode of "Mike accidentally breaks this": 
  Mike forgets that `==` is defined on a pair of winrt objects as "do these point to the _SAME_ object", not "do they have the same values". 

This just slipped right through code review.

## References

Broken in #3825

## PR Checklist
* [x] Closes #3896
* [x] I work here
* [ ] Tests added/passed
* [n/a] Requires documentation to be updated

## Detailed Description of the Pull Request / Additional comments

## Validation Steps Performed

Manually checked that these are still there

src/cascadia/TerminalApp/ActionArgs.h
@@ -51,18 +51,13 @@ namespace winrt::TerminalApp::implementation
         static constexpr std::string_view ProfileKey{ "profile" };
 
     public:
-        bool Equals(const IActionArgs& other)
+        bool Equals(const winrt::TerminalApp::NewTerminalArgs& other)
         {
-            auto otherAsUs = other.try_as<NewTerminalArgs>();
-            if (otherAsUs)
-            {
-                return otherAsUs->_Commandline == _Commandline &&
-                       otherAsUs->_StartingDirectory == _StartingDirectory &&
-                       otherAsUs->_TabTitle == _TabTitle &&
-                       otherAsUs->_ProfileIndex == _ProfileIndex &&
-                       otherAsUs->_Profile == _Profile;
-            }
-            return false;
+            return other.Commandline() == _Commandline &&
+                   other.StartingDirectory() == _StartingDirectory &&
+                   other.TabTitle() == _TabTitle &&
+                   other.ProfileIndex() == _ProfileIndex &&
+                   other.Profile() == _Profile;
         };
         static winrt::TerminalApp::NewTerminalArgs FromJson(const Json::Value& json)
         {
@@ -132,7 +127,7 @@ namespace winrt::TerminalApp::implementation
             auto otherAsUs = other.try_as<NewTabArgs>();
             if (otherAsUs)
             {
-                return otherAsUs->_TerminalArgs == _TerminalArgs;
+                return otherAsUs->_TerminalArgs.Equals(_TerminalArgs);
             }
             return false;
         };

src/cascadia/TerminalApp/ActionArgs.idl
@@ -39,6 +39,7 @@ namespace TerminalApp
         // ProfileIndex can be null (for "use the default"), so this needs to be
         // a IReference, so it's nullable
         Windows.Foundation.IReference<Int32> ProfileIndex { get; };
+        Boolean Equals(NewTerminalArgs other);
     };
 
     [default_interface] runtimeclass ActionEventArgs : IActionEventArgs