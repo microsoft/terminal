92d7559d65c9a680284c3662cd0208092e5bcf81
Michael Kitzan <mkitzan45@gmail.com>
2019-12-12 15:02:01 -0800

    Added test file for TerminalApi
    Only tests SetColorTableEntry right now

---
+++ src/cascadia/UnitTests_TerminalCore/TerminalApiTest.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include "precomp.h"
+#include <WexTestClass.h>
+
+#include "../cascadia/TerminalCore/Terminal.hpp"
+#include "MockTermSettings.h"
+#include "../renderer/inc/DummyRenderTarget.hpp"
+#include "consoletaeftemplates.hpp"
+
+using namespace winrt::Microsoft::Terminal::Settings;
+using namespace Microsoft::Terminal::Core;
+
+namespace TerminalCoreUnitTests
+{
+#define WCS(x) WCSHELPER(x)
+#define WCSHELPER(x) L#x
+
+    class TerminalApiTest
+    {
+        TEST_CLASS(TerminalApiTest);
+
+        TEST_METHOD(SetColorTableEntry)
+        {
+            Terminal term;
+            DummyRenderTarget emptyRT;
+            term.Create({ 100, 100 }, 0, emptyRT);
+
+            auto settings = winrt::make<MockTermSettings>(100, 100, 100);
+            term.UpdateSettings(settings);
+
+            VERIFY_IS_TRUE(term.SetColorTableEntry(0, 100));
+            VERIFY_IS_TRUE(term.SetColorTableEntry(128, 100));
+            VERIFY_IS_TRUE(term.SetColorTableEntry(255, 100));
+         
+            VERIFY_IS_FALSE(term.SetColorTableEntry(256, 100));
+            VERIFY_IS_FALSE(term.SetColorTableEntry(512, 100));
+        }
+    };
+}
