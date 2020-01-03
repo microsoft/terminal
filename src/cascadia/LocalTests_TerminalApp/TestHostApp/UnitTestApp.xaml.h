9c47d8b7c811864744adc906b5009cc83a23f88d
Mike Griese <migrie@microsoft.com>
2020-01-03 08:53:32 -0600

    so this is a copypasta of the TAEF example on how to do this. We'll play more with it in the next commit. THIS REQUIRES x86 BUILDS

---
+++ src/cascadia/LocalTests_TerminalApp/TestHostApp/UnitTestApp.xaml.h
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All Rights Reserved.
+//-----------------------------------------------------------
+#pragma once
+
+#include "UnitTestApp.g.h"
+
+namespace TestHostApp
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    ref class App sealed
+    {
+    protected:
+        virtual void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs ^ e) override;
+
+        internal :
+            App();
+    };
+}
