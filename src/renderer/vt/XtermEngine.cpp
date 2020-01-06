59e719404e57b38353e37ce9afbed8c875e35366
Mike Griese <migrie@microsoft.com>
2020-01-06 08:01:45 -0600

    @j4james mentioned this in #3093, but the Terminal still needs to implement cursor hiding, so this isn't really helpful by itself

--- src/renderer/vt/XtermEngine.cpp
+++ src/renderer/vt/XtermEngine.cpp
@@ -107,7 +107,7 @@ XtermEngine::XtermEngine(_In_ wil::unique_hfile hPipe,
         // by prepending a cursor off.
         if (_lastCursorIsVisible)
         {
-            _buffer.insert(0, "\x1b[25l");
+            _buffer.insert(0, "\x1b[?25l");
             _lastCursorIsVisible = false;
         }
         // If the cursor was NOT previously visible, then that's fine! we don't
