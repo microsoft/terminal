8a5ea3c9b365a3aaa1e8527ce817cafca18d371a
Dustin L. Howett <duhowett@microsoft.com>
2020-01-15 16:06:49 -0800

    even more hax for the generator

--- src/cascadia/TerminalApp/PowershellCoreProfileGenerator.cpp
+++ src/cascadia/TerminalApp/PowershellCoreProfileGenerator.cpp
@@ -10,18 +10,37 @@
 #include "Utils.h"
 #include "DefaultProfileUtils.h"
 
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.Management.Deployment.h>
+
 #include <appmodel.h>
 #include <shlobj.h>
 
+static constexpr std::wstring_view POWERSHELL_PFN{ L"Microsoft.PowerShell_8wekyb3d8bbwe" };
+static constexpr std::wstring_view POWERSHELL_PREVIEW_PFN{ L"Microsoft.PowerShellPreview_8wekyb3d8bbwe" };
+static constexpr std::wstring_view PWSH_EXE{ L"pwsh.exe" };
+static constexpr std::wstring_view POWERSHELL_ICON{ L"ms-appx:///ProfileIcons/pwsh.ico" };
+static constexpr std::wstring_view POWERSHELL_PREVIEW_ICON{ L"ms-appx:///ProfileIcons/pwsh-preview.ico" };
+
 namespace
 {
+    enum PowerShellFlags
+    {
+        // these flags are used as a sort key, so they encode some native ordering
+        Preview = 1 << 0, // preview version
+        WOW = 1 << 1, // non-native (Windows-on-Windows)
+        Store = 1 << 2, // distributed via the store
+        Scoop = 1 << 3, // installed via Scoop
+        Dotnet = 1 << 4, // installed as a dotnet global tool
+    };
+    DEFINE_ENUM_FLAG_OPERATORS(PowerShellFlags);
+
     struct PowerShellInstance
     {
-        int majorVersion;
-        bool preview;
-        bool nativeArchitecture;
-        bool store;
+        int majorVersion; // 0 = we don't know
+        PowerShellFlags flags;
         std::filesystem::path executablePath;
+        std::wstring iconPath; // not a std::filesystem::path because it could be a URI
 
         // Total sort order:
         // 6-preview (wow) < 6-preview (native) < 7-preview (native) < 6 (wow) < 6 (native) < 7 (native)
@@ -31,17 +50,9 @@ namespace
             {
                 return majorVersion < second.majorVersion;
             }
-            if (preview != second.preview)
+            if (flags != second.flags)
             {
-                return preview; // preview is less than GA
-            }
-            if (nativeArchitecture != second.nativeArchitecture)
-            {
-                return !nativeArchitecture; // non-native architecture is less than native architecture.
-            }
-            if (store != second.store)
-            {
-                return !store; // non-store is less than store.
+                return flags > second.flags; // flags are inverted (preview is worse than GA, WOW is worse than native, store is worse than traditional)
             }
             return executablePath < second.executablePath; // fall back to path sorting
         }
@@ -58,14 +69,35 @@ static void _collectTraditionalLayoutPowerShellInstancesInDirectory(std::wstring
         for (auto& versionedDir : std::filesystem::directory_iterator(root))
         {
             auto versionedPath = versionedDir.path();
-            auto executable = versionedPath / L"pwsh.exe";
+            auto executable = versionedPath / PWSH_EXE;
             if (std::filesystem::exists(executable))
             {
                 auto preview = versionedPath.filename().wstring().find(L"-preview") != std::wstring::npos;
-                out.emplace_back(PowerShellInstance{ std::stoi(versionedPath.filename()), preview, true, false, executable });
+                PowerShellFlags flags = preview ? PowerShellFlags::Preview : static_cast<PowerShellFlags>(0);
+                out.emplace_back(PowerShellInstance{ std::stoi(versionedPath.filename()),
+                                                     flags,
+                                                     executable });
             }
         }
     }
+}
+
+static winrt::Windows::ApplicationModel::Package _getStorePackage(const std::wstring_view packageFamilyName)
+try
+{
+    winrt::Windows::Management::Deployment::PackageManager packageManager;
+    auto foundPackages = packageManager.FindPackagesForUser(L"", packageFamilyName);
+    auto iterator = foundPackages.First();
+    if (!iterator.HasCurrent())
+    {
+        return nullptr;
+    }
+    return iterator.Current();
+}
+catch (...)
+{
+    LOG_CAUGHT_EXCEPTION();
+    return nullptr;
 }
 
 static void _collectStorePowerShellInstances(std::vector<PowerShellInstance>& out)
@@ -82,16 +114,41 @@ static void _collectStorePowerShellInstances(std::vector<PowerShellInstance>& ou
 
     if (std::filesystem::exists(appExecAliasPath))
     {
-        auto previewPath = appExecAliasPath / L"Microsoft.PowerShellPreview_8wekyb3d8bbwe";
-        auto gaPath = appExecAliasPath / L"Microsoft.PowerShell_8wekyb3d8bbwe";
+        auto previewPath = appExecAliasPath / POWERSHELL_PREVIEW_PFN;
+        auto gaPath = appExecAliasPath / POWERSHELL_PFN;
         if (std::filesystem::exists(previewPath))
         {
-                out.emplace_back(PowerShellInstance{ 10, true, true, true, previewPath / L"pwsh.exe" });
+            auto previewPackage = _getStorePackage(POWERSHELL_PREVIEW_PFN);
+            if (previewPackage)
+            {
+                out.emplace_back(PowerShellInstance{
+                    static_cast<int>(previewPackage.Id().Version().Major),
+                    PowerShellFlags::Store | PowerShellFlags::Preview,
+                    previewPath / PWSH_EXE });
+            }
         }
         if (std::filesystem::exists(gaPath))
         {
-                out.emplace_back(PowerShellInstance{ 10, false, true, true, gaPath / L"pwsh.exe" });
+            auto gaPackage = _getStorePackage(POWERSHELL_PFN);
+            if (gaPackage)
+            {
+                out.emplace_back(PowerShellInstance{
+                    gaPackage.Id().Version().Major,
+                    PowerShellFlags::Store,
+                    gaPath / PWSH_EXE,
+                });
+            }
         }
+    }
+}
+
+static void _collectPwshExeInDirectory(const std::wstring_view directory, const PowerShellFlags flags, std::vector<PowerShellInstance>& out)
+{
+    std::filesystem::path root{ wil::ExpandEnvironmentStringsW<std::wstring>(directory.data()) };
+    auto pwshPath = root / PWSH_EXE;
+    if (std::filesystem::exists(pwshPath))
+    {
+        out.emplace_back(PowerShellInstance{ 0, flags, pwshPath });
     }
 }
 
@@ -102,10 +159,12 @@ static std::vector<PowerShellInstance> _enumeratePowerShellInstances()
     for (auto& v : versions)
     {
         // If they came from ProgramFiles(x86), they're not the native architecture.
-        v.nativeArchitecture = false;
+        WI_SetFlag(v.flags, PowerShellFlags::WOW);
     }
     _collectTraditionalLayoutPowerShellInstancesInDirectory(L"%ProgramFiles%\\PowerShell", versions);
     _collectStorePowerShellInstances(versions);
+    _collectPwshExeInDirectory(L"%USERPROFILE%\\.dotnet\\tools", PowerShellFlags::Dotnet, versions);
+    _collectPwshExeInDirectory(L"%USERPROFILE%\\scoop\\shims", PowerShellFlags::Scoop, versions);
     std::sort(versions.rbegin(), versions.rend()); // sort in reverse (best first)
     return versions;
 }
@@ -128,7 +187,6 @@ std::vector<TerminalApp::Profile> PowershellCoreProfileGenerator::GenerateProfil
 {
     std::vector<TerminalApp::Profile> profiles;
 
-    std::filesystem::path psCoreCmdline;
     auto psInstances = _enumeratePowerShellInstances();
     for (const auto& psI : psInstances)
     {
@@ -137,16 +195,27 @@ std::vector<TerminalApp::Profile> PowershellCoreProfileGenerator::GenerateProfil
         {
             name += L" Core";
         }
-        name += L" " + std::to_wstring(psI.majorVersion);
-        if (psI.preview)
+        if (psI.majorVersion != 0)
+        {
+            name += L" " + std::to_wstring(psI.majorVersion);
+        }
+        if (WI_IsFlagSet(psI.flags, PowerShellFlags::Preview))
         {
             name += L"-preview";
         }
-        if (psI.store)
+        if (WI_IsFlagSet(psI.flags, PowerShellFlags::Store))
         {
             name += L" (Store)";
         }
-        if (!psI.nativeArchitecture)
+        if (WI_IsFlagSet(psI.flags, PowerShellFlags::Dotnet))
+        {
+            name += L" (.NET Global)";
+        }
+        if (WI_IsFlagSet(psI.flags, PowerShellFlags::Scoop))
+        {
+            name += L" (Scoop)";
+        }
+        if (WI_IsFlagSet(psI.flags, PowerShellFlags::WOW))
         {
             name += L" (x86)";
         }
@@ -156,7 +225,7 @@ std::vector<TerminalApp::Profile> PowershellCoreProfileGenerator::GenerateProfil
         profile.SetStartingDirectory(DEFAULT_STARTING_DIRECTORY);
         profile.SetColorScheme({ L"Campbell" });
 
-        //profile.SetIconPath((psI.executablePath / L"assets" / (psI.preview ? L"Powershell_av_colors.ico" : L"Powershell_black.ico")).wstring());
+        profile.SetIconPath(WI_IsFlagSet(psI.flags, PowerShellFlags::Preview) ? POWERSHELL_PREVIEW_ICON : POWERSHELL_ICON);
         profiles.emplace_back(std::move(profile));
     }
 
