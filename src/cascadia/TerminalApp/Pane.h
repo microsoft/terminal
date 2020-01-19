2162ebfa1dfdc5b2d41b8990b4e55f1c994258a2
Josh Soref <jsoref@users.noreply.github.com>
2020-01-19 02:02:46 -0500

    spelling: themselves

--- src/cascadia/TerminalApp/Pane.h
+++ src/cascadia/TerminalApp/Pane.h
@@ -191,7 +191,7 @@ private:
         std::unique_ptr<LayoutSizeNode> secondChild;
 
         // These two fields hold next possible snapped values of firstChild and
-        // secondChild. Although that could be calculated from these fields themself,
+        // secondChild. Although that could be calculated from these fields themselves,
         // it would be wasteful as we have to know these values more often than for
         // simple increment. Hence we cache that here.
         std::unique_ptr<LayoutSizeNode> nextFirstChild;
