7a2518a85048345fe1933b955dd8a51c2ef28252
Josh Soref <jsoref@users.noreply.github.com>
2020-01-18 21:49:42 -0500

    spelling: demonstration

--- src/renderer/dx/CustomTextLayout.cpp
+++ src/renderer/dx/CustomTextLayout.cpp
@@ -232,7 +232,7 @@ CustomTextLayout::CustomTextLayout(gsl::not_null<IDWriteFactory1*> const factory
         // Shapes a single run of text into glyphs.
         // Alternately, you could iteratively interleave shaping and line
         // breaking to reduce the number glyphs held onto at once. It's simpler
-        // for this demostration to just do shaping and line breaking as two
+        // for this demonstration to just do shaping and line breaking as two
         // separate processes, but realize that this does have the consequence that
         // certain advanced fonts containing line specific features (like Gabriola)
         // will shape as if the line is not broken.
