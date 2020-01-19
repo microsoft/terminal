16e2b6f15be8c2a2d3b4f0473647475ba8cf83e5
Josh Soref <jsoref@users.noreply.github.com>
2020-01-19 01:11:43 -0500

    spelling: visible

--- src/cascadia/TerminalControl/TSFInputControl.cpp
+++ src/cascadia/TerminalControl/TSFInputControl.cpp
@@ -193,7 +193,7 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
 
     // Method Description:
     // - Handler for CompositionStarted event by CoreEditContext responsible
-    //   for making internal TSFInputControl controls visisble.
+    //   for making internal TSFInputControl controls visible.
     // Arguments:
     // - sender: CoreTextEditContext sending the request. Not used in method.
     // - args: CoreTextCompositionStartedEventArgs. Not used in method.
@@ -207,7 +207,7 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
 
     // Method Description:
     // - Handler for CompositionCompleted event by CoreEditContext responsible
-    //   for making internal TSFInputControl controls visisble.
+    //   for making internal TSFInputControl controls visible.
     // Arguments:
     // - sender: CoreTextEditContext sending the request. Not used in method.
     // - args: CoreTextCompositionCompletedEventArgs. Not used in method.
