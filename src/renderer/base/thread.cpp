1664025c44846d9e83fe1fcfe4c5c9b2d6607400
Greg Depoire--Ferrer <greg.depoire@gmail.com>
2020-02-24 16:09:38 +0100

    Fix `NotifyPaint` called before `_fWaiting.store(false)`

--- src/renderer/base/thread.cpp
+++ src/renderer/base/thread.cpp
@@ -164,7 +164,7 @@ DWORD WINAPI RenderThread::_ThreadProc()
         if (!_fNextFrameRequested.exchange(false))
         {
             // <--
-            // If `NotifyPaint` is called at this point, then it will will not
+            // If `NotifyPaint` is called at this point, then it will not
             // set the event because `_fWaiting` is not `true` yet so we have
             // to check again below.
 
@@ -177,7 +177,25 @@ DWORD WINAPI RenderThread::_ThreadProc()
                 WaitForSingleObject(_hEvent, INFINITE);
             }
 
+            // <--
+            // If `NotifyPaint` is called at this point, then it _will_ set
+            // the event because `_fWaiting` is `true`, but we're not waiting
+            // anymore!
+            // This can probably happen quite often: imagine a scenario where
+            // we are waiting, and the terminal calls `NotifyPaint` twice
+            // very quickly.
+            // In that case, both calls might end up calling `SetEvent`. The
+            // first one will resume this thread and the second one will
+            // `SetEvent` the event. So the next time we wait, the event will
+            // already be set and we won't actually wait.
+            // Because it can happen often, and because rendering is an
+            // expensive operation, we should reset the event to not render
+            // again if nothing changed.
+
             _fWaiting.store(false);
+
+            // see comment above
+            ResetEvent(_hEvent);
         }
 
         ResetEvent(_hPaintCompletedEvent);
