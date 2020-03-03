136817e3378693b8d132fcb3cb50aa21fe8c2b45
Michael Niksa <miniksa@microsoft.com>
2020-03-03 11:14:18 -0800

    Initial tests.

---
+++ src/til/ut_til/BitmapTests.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include "precomp.h"
+#include "WexTestClass.h"
+
+using namespace WEX::Common;
+using namespace WEX::Logging;
+using namespace WEX::TestExecution;
+
+class BitmapTests
+{
+    TEST_CLASS(BitmapTests);
+
+    TEST_METHOD(Construct)
+    {
+        
+    }
+};
