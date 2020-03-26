5bcf0fc3de2b40097a5fa775b3d3b8f96968be86
Dustin L. Howett (MSFT) <duhowett@microsoft.com>
2020-03-26 16:25:11 -0700

    Rework TermControl's initialization (#5051)
    This commit rewrites a large swath of TermControl's initialization code.
    
    * `TermControl` now _always_ has a `_terminal`; it will never be null
    * Event registration for `_terminal` and any other available-at-init
      fixtures has been moved into the constructor.
    * Event handlers how more uniformly check `_closing` if they interact
      with the _terminal.
    * Swap chain attachment has been cleaned up and no longer uses a
      coroutine when it's spawned from the UI thread.
       * We have to register the renderer's swapchain change notification
         handler after we set the swap chain, otherwise it'll call us back
         when it initializes itself.
    * `InitializeTerminal` now happens under the `_terminal`'s write lock
       * Certain things that InitializeTerminal were calling themselves
         attempted to take the lock. They no longer do so.
    * TermControlAutomationPeer cannot take the read lock, because setting
      the scrollbar's `Maximum` during `InitializeTerminal` will trigger
      vivification of the automation peer tree; if it attempts to take the
      lock it will deadlock during initialization.
    * `BlinkCursor` was renamed to `CursorTimerTick` because it's the "Tick"
      handler for the "CursorTimer".
    * `DragDropHandler` was converted into a coroutine instead of just
      _calling_ a coroutine.
    
    Caveats:
    
    Terminal may not have a `_buffer` until InitializeTerminal happens.
    There's a nasty coupling between RenderTarget and TextBuffer that means
    that we need to have a renderer before we have a buffer.
    
    There's a second nasty coupling between RenderThread and Renderer: we
    can't create a RenderThread during construction because it needs to be
    given a renderer, and we can't create a Renderer during construction
    because it needs a RenderThread. We don't want to kick off a thread
    during construction.
    
    Testing:
    
    I wailed on this by opening and closing and resizing terminals and panes
    and tabs, up to a hundred open tabs and one tab with 51 panes. I set one
    tab to update the title as fast as it possibly could and tested
    teardown, zoom, resize, mouse movement, etc. while this was all
    happening.
    
    Closes #4613.

--- src/cascadia/TerminalControl/TermControl.h
+++ src/cascadia/TerminalControl/TermControl.h
@@ -77,7 +77,8 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
         void AdjustFontSize(int fontSizeDelta);
         void ResetFontSize();
 
-        winrt::fire_and_forget SwapChainChanged();
+        winrt::fire_and_forget RenderEngineSwapChainChanged();
+        void _AttachDxgiSwapChainToXaml(IDXGISwapChain1* swapChain);
 
         void CreateSearchBoxControl();
 
@@ -183,15 +184,13 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
         void _ScrollbarChangeHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& e);
         void _GotFocusHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
         void _LostFocusHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
-        void _DragDropHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::DragEventArgs const& e);
+        winrt::fire_and_forget _DragDropHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::DragEventArgs const e);
         void _DragOverHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::DragEventArgs const& e);
-        winrt::fire_and_forget _DoDragDrop(Windows::UI::Xaml::DragEventArgs const e);
 
-        void _BlinkCursor(Windows::Foundation::IInspectable const& sender, Windows::Foundation::IInspectable const& e);
+        void _CursorTimerTick(Windows::Foundation::IInspectable const& sender, Windows::Foundation::IInspectable const& e);
         void _SetEndSelectionPointAtCursor(Windows::Foundation::Point const& cursorPosition);
         void _SendInputToConnection(const std::wstring& wstr);
         void _SendPastedTextToConnection(const std::wstring& wstr);
-        winrt::fire_and_forget _SwapChainRoutine();
         void _SwapChainSizeChanged(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::SizeChangedEventArgs const& e);
         void _SwapChainScaleChanged(Windows::UI::Xaml::Controls::SwapChainPanel const& sender, Windows::Foundation::IInspectable const& args);
         void _DoResize(const double newWidth, const double newHeight);
