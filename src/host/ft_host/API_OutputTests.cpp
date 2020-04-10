51f6cce895362f37269dcaaba19819cba285f6c2
Leon Liang <lelian@microsoft.com>
2020-04-10 14:07:53 -0700

    remove this

--- src/host/ft_host/API_OutputTests.cpp
+++ src/host/ft_host/API_OutputTests.cpp
@@ -46,8 +46,6 @@ class OutputTests
     TEST_METHOD(WriteBackspaceTest);
 
     TEST_METHOD(WinPtyWrite);
-
-    TEST_METHOD(WriteTwoColumnCharacterToCheckOverflow);
 };
 
 bool OutputTests::TestSetup()
@@ -1063,43 +1061,4 @@ void OutputTests::WinPtyWrite()
         VERIFY_FAIL(L"Unknown test type.");
         break;
     }
-}
-
-void OutputTests::WriteTwoColumnCharacterToCheckOverflow()
-{
-    // Get output buffer information.
-    const auto consoleOutputHandle = GetStdOutputHandle();
-    SetConsoleActiveScreenBuffer(consoleOutputHandle);
-
-    CONSOLE_SCREEN_BUFFER_INFOEX sbiex{ 0 };
-    sbiex.cbSize = sizeof(sbiex);
-
-    VERIFY_WIN32_BOOL_SUCCEEDED(GetConsoleScreenBufferInfoEx(consoleOutputHandle, &sbiex));
-    const auto bufferSize = sbiex.dwSize;
-
-    // Establish a writing region that is the width of the buffer and just one row tall.
-    const SMALL_RECT region{ 0, 0, bufferSize.X - 1, 0 };
-    const COORD regionDimensions{ region.Right - region.Left + 1, region.Bottom - region.Top + 1 };
-    const auto regionSize = regionDimensions.X * regionDimensions.Y;
-    const COORD regionOrigin{ 0, 0 };
-
-    CHAR_INFO testValue;
-    testValue.Attributes = 0x3e;
-    testValue.Char.UnicodeChar = L'A';
-
-    std::vector<CHAR_INFO> buffer(regionSize, testValue);
-
-    // Place a double width character with a lead bit in the last column of the buffer.
-    CHAR_INFO lastColumnValue;
-    lastColumnValue.Attributes = 0x13e;
-    lastColumnValue.Char.UnicodeChar = L'我';
-    buffer[regionSize - 1] = lastColumnValue;
-
-    // Call the API and confirm results.
-    SMALL_RECT affected = region;
-    VERIFY_WIN32_BOOL_SUCCEEDED(WriteConsoleOutputW(consoleOutputHandle, buffer.data(), regionDimensions, regionOrigin, &affected));
-    VERIFY_ARE_EQUAL(region, affected);
-
-    // Move the cursor to the last column of the buffer
-    VERIFY_WIN32_BOOL_SUCCEEDED(SetConsoleCursorPosition(consoleOutputHandle, { bufferSize.X - 1, 0 }));
 }
