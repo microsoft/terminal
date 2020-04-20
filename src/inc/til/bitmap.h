2b1baa5289141698dfff111324bf8e0b66c93ae5
Mike Griese <migrie@microsoft.com>
2020-04-20 15:30:09 -0500

    maybe I should let it finish building before I commit
    (cherry picked from commit 98cdbeca349acb55c5b5aef1d96206a8fa7b62e4)

--- src/inc/til/bitmap.h
+++ src/inc/til/bitmap.h
@@ -343,7 +343,7 @@ namespace til // Terminal Implementation Library. Also: "Today I Learned"
 
         bool one() const
         {
-            _bits.count() == 1;
+            return _bits.count() == 1;
         }
 
         constexpr bool any() const noexcept
