ca228a818d077c1d60c17e219a05df40eeaf4cc4
Leon Liang <lelian@microsoft.com>
2020-04-23 13:00:49 -0700

    comment to keep sane

--- tools/GenerateCommitHashHeader.ps1
+++ tools/GenerateCommitHashHeader.ps1
@@ -1,3 +1,5 @@
+# This script gets the current git commit hash and places it in a header file stamped to a wstring_view
+
 Write-Output "constexpr std::wstring_view CurrentCommitHash{ L`"" | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -NoNewline
 git rev-parse HEAD  | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -Append -NoNewline
 Write-Output "`" };" | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -Append -NoNewline
