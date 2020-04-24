41ec532c8458f8aa3c08fc003f05e3ac01ae450c
Leon Liang <lelian@microsoft.com>
2020-04-24 11:26:45 -0700

    some error handling

--- tools/GenerateCommitHashHeader.ps1
+++ tools/GenerateCommitHashHeader.ps1
@@ -1,5 +1,13 @@
 # This script gets the current git commit hash and places it in a header file stamped to a wstring_view
 
 Write-Output "constexpr std::wstring_view CurrentCommitHash{ L`"" | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -NoNewline
-git rev-parse HEAD  | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -Append -NoNewline
+
+$hash = git rev-parse HEAD
+if ($LASTEXITCODE)
+{
+  # If for some reason the git command fails, we'll just return master.
+  $hash = "master"
+}
+
+$hash | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -Append -NoNewline
 Write-Output "`" };" | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -Append -NoNewline
