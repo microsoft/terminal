1c08c53113b75524a5c16ff21ac22028ec36f4c4
Leon Liang <lelian@microsoft.com>
2020-04-24 12:25:55 -0700

    some better error handling:

--- tools/GenerateCommitHashHeader.ps1
+++ tools/GenerateCommitHashHeader.ps1
@@ -1,13 +1,32 @@
 # This script gets the current git commit hash and places it in a header file stamped to a wstring_view
 
-Write-Output "constexpr std::wstring_view CurrentCommitHash{ L`"" | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -NoNewline
+Write-Output "constexpr std::wstring_view CurrentCommitHash{ L`"" | Out-File -FilePath "test.txt" -Encoding ASCII -NoNewline
 
-$hash = git rev-parse HEAD
-if ($LASTEXITCODE)
+$latestMasterHash = (git ls-remote https://github.com/microsoft/terminal.git --heads master).Split()[0]
+
+$hash = "temp"
+$branchName = git branch --show-current
+if ($LASTEXITCODE -or $branchName -eq "")
 {
-  # If for some reason the git command fails, we'll just return master.
-  $hash = "master"
+    # If for whatever reason we're not in our git repo or we're in detached HEAD,
+    # just use the remote master's latest commit.
+    $hash = $latestMasterHash
+}
+else
+{
+    # So now we should try to check that this branch even exists in remote
+    # If it does, we'll return the latest commit of this branch in remote
+    # Otherwise, we'll return remote master's latest commit.
+    $currentBranchHash = git ls-remote https://github.com/microsoft/terminal.git --heads $branchName
+    if ($LASTEXITCODE -or $currentBranchHash -eq "")
+    {
+        $hash = $latestMasterHash
+    }
+    else 
+    {
+        $hash = $currentBranchHash.Split()[0]
+    }
 }
 
-$hash | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -Append -NoNewline
-Write-Output "`" };" | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -Append -NoNewline
+$hash | Out-File -FilePath "test.txt" -Encoding ASCII -Append -NoNewline
+Write-Output "`" };" | Out-File -FilePath "test.txt" -Encoding ASCII -Append -NoNewline
