c6d336327893985f0a2361a813073a8d19fa77cb
Leon Liang <lelian@microsoft.com>
2020-04-28 12:57:46 -0700

    put dis in a var

--- tools/GenerateCommitHashHeader.ps1
+++ tools/GenerateCommitHashHeader.ps1
@@ -1,7 +1,9 @@
 # This script gets the current git commit hash and places it in a header file stamped to a wstring_view
 # If we're unable to retrieve the hash, we'll return the hard coded string "master" instead.
 
-Write-Output "constexpr std::wstring_view CurrentCommitHash{ L`"" | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -NoNewline
+$filePath = "Generated Files\CurrentCommitHash.h"
+
+Write-Output "constexpr std::wstring_view CurrentCommitHash{ L`"" | Out-File -FilePath $filePath -Encoding ASCII -NoNewline
 
 # Let's see if we're on a build agent that can give us the commit hash through this predefined variable.
 $hash = $env:Build_SourceVersion
@@ -15,5 +17,5 @@ if (-not $hash)
     }
 }
 
-$hash | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -Append -NoNewline
-Write-Output "`" };" | Out-File -FilePath "Generated Files\CurrentCommitHash.h" -Encoding ASCII -Append -NoNewline
+$hash | Out-File -FilePath $filePath -Encoding ASCII -Append -NoNewline
+Write-Output "`" };" | Out-File -FilePath $filePath -Encoding ASCII -Append -NoNewline
