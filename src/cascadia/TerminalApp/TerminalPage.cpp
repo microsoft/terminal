microsoft/terminal

commit 624a553d23c6c1150bc6c4fe17b60960205649fc

Add support for setting a tab's color at runtime w/ context menu (#3789)
This commit introduces a context menu for Tab and a new item,
"Color...", which will display a color picker.

A flyout menu, containing a custom flyout, is attached to each tab. The
flyout displays a palette of 16 preset colors and includes a color
picker. When the user selects or clears color, an event is fired, which
is intercepted by the tab to which the flyout belongs.

The changing of the color is achieved by putting the selected color in
the resource dictionary of the tab, using well-defined dictionary keys
(e.g. TabViewItemHeaderBackground). Afterwards the visual state of the
tab is toggled, so that the color change is visible immediately.

Custom-colored tabs will be desaturated (somewhat) by alpha blending
them with the tab bar background.

The flyout menu also contains a 'Close' flyout item.

## Validation Steps Performed
I've validated the behavior manually: start the program via the start
menu. Right click on the tab -> Choose a tab color.

The color flyout is going to be shown. Click a color swatch or click
'Select a custom color' to use the color picker. Use the 'Clear the
current color' to remove the custom color.

Closes #2994. References #3327.

src/cascadia/TerminalApp/TerminalPage.cpp
@@ -16,6 +16,7 @@
 #include "AzureCloudShellGenerator.h" // For AzureConnectionType
 #include "TelnetGenerator.h" // For TelnetConnectionType
 #include "TabRowControl.h"
+#include "ColorHelper.h"
 #include "DebugTapConnection.h"
 
 using namespace winrt;
@@ -872,7 +873,7 @@ namespace winrt::TerminalApp::implementation
         term.PasteFromClipboard({ this, &TerminalPage::_PasteFromClipboardHandler });
 
         // Bind Tab events to the TermControl and the Tab's Pane
-        hostingTab.BindEventHandlers(term);
+        hostingTab.Initialize(term);
 
         // Don't capture a strong ref to the tab. If the tab is removed as this
         // is called, we don't really care anymore about handling the event.
@@ -888,6 +889,30 @@ namespace winrt::TerminalApp::implementation
                 page->_UpdateTitle(*tab);
             }
         });
+
+        // react on color changed events
+        hostingTab.ColorSelected([weakTab{ hostingTab.get_weak() }, weakThis{ get_weak() }](auto&& color) {
+            auto page{ weakThis.get() };
+            auto tab{ weakTab.get() };
+
+            if (page && tab && tab->IsFocused())
+            {
+                page->_SetNonClientAreaColors(color);
+            }
+        });
+
+        hostingTab.ColorCleared([weakTab{ hostingTab.get_weak() }, weakThis{ get_weak() }]() {
+            auto page{ weakThis.get() };
+            auto tab{ weakTab.get() };
+
+            if (page && tab && tab->IsFocused())
+            {
+                page->_ClearNonClientAreaColors();
+            }
+        });
+
+        // remove any colors left by other colored tabs
+        _ClearNewTabButtonColor();
     }
 
     // Method Description:
@@ -1474,6 +1499,10 @@ namespace winrt::TerminalApp::implementation
             _RemoveTabViewItem(sender.as<MUX::Controls::TabViewItem>());
             eventArgs.Handled(true);
         }
+        else if (eventArgs.GetCurrentPoint(*this).Properties().IsRightButtonPressed())
+        {
+            eventArgs.Handled(true);
+        }
     }
 
     void TerminalPage::_UpdatedSelectedTab(const int32_t index)
@@ -1495,7 +1524,20 @@ namespace winrt::TerminalApp::implementation
                 _tabContent.Children().Append(tab->GetRootElement());
 
                 tab->SetFocused(true);
+
+                // Raise an event that our title changed
                 _titleChangeHandlers(*this, Title());
+
+                // Raise an event that our titlebar color changed
+                std::optional<Windows::UI::Color> color = tab->GetTabColor();
+                if (color.has_value())
+                {
+                    _SetNonClientAreaColors(color.value());
+                }
+                else
+                {
+                    _ClearNonClientAreaColors();
+                }
             }
             CATCH_LOG();
         }
@@ -1788,6 +1830,153 @@ namespace winrt::TerminalApp::implementation
         winrt::com_ptr<Tab> tabImpl;
         tabImpl.copy_from(winrt::get_self<Tab>(tab));
         return tabImpl;
+    }
+
+    // Method Description:
+    // - Sets the tab split button color when a new tab color is selected
+    // Arguments:
+    // - color: The color of the newly selected tab, used to properly calculate
+    //          the foreground color of the split button (to match the font
+    //          color of the tab)
+    // - accentColor: the actual color we are going to use to paint the tab row and
+    //                split button, so that there is some contrast between the tab
+    //                and the non-client are behind it
+    // Return Value:
+    // - <none>
+    void TerminalPage::_SetNewTabButtonColor(const Windows::UI::Color& color, const Windows::UI::Color& accentColor)
+    {
+        // TODO GH#3327: Look at what to do with the tab button when we have XAML theming
+        bool IsBrightColor = ColorHelper::IsBrightColor(color);
+        bool isLightAccentColor = ColorHelper::IsBrightColor(accentColor);
+        winrt::Windows::UI::Color pressedColor{};
+        winrt::Windows::UI::Color hoverColor{};
+        winrt::Windows::UI::Color foregroundColor{};
+        const float hoverColorAdjustment = 5.f;
+        const float pressedColorAdjustment = 7.f;
+
+        if (IsBrightColor)
+        {
+            foregroundColor = winrt::Windows::UI::Colors::Black();
+        }
+        else
+        {
+            foregroundColor = winrt::Windows::UI::Colors::White();
+        }
+
+        if (isLightAccentColor)
+        {
+            hoverColor = ColorHelper::Darken(accentColor, hoverColorAdjustment);
+            pressedColor = ColorHelper::Darken(accentColor, pressedColorAdjustment);
+        }
+        else
+        {
+            hoverColor = ColorHelper::Lighten(accentColor, hoverColorAdjustment);
+            pressedColor = ColorHelper::Lighten(accentColor, pressedColorAdjustment);
+        }
+
+        Media::SolidColorBrush backgroundBrush{ accentColor };
+        Media::SolidColorBrush backgroundHoverBrush{ hoverColor };
+        Media::SolidColorBrush backgroundPressedBrush{ pressedColor };
+        Media::SolidColorBrush foregroundBrush{ foregroundColor };
+
+        _newTabButton.Resources().Insert(winrt::box_value(L"SplitButtonBackground"), backgroundBrush);
+        _newTabButton.Resources().Insert(winrt::box_value(L"SplitButtonBackgroundPointerOver"), backgroundHoverBrush);
+        _newTabButton.Resources().Insert(winrt::box_value(L"SplitButtonBackgroundPressed"), backgroundPressedBrush);
+
+        _newTabButton.Resources().Insert(winrt::box_value(L"SplitButtonForeground"), foregroundBrush);
+        _newTabButton.Resources().Insert(winrt::box_value(L"SplitButtonForegroundPointerOver"), foregroundBrush);
+        _newTabButton.Resources().Insert(winrt::box_value(L"SplitButtonForegroundPressed"), foregroundBrush);
+
+        _newTabButton.Background(backgroundBrush);
+        _newTabButton.Foreground(foregroundBrush);
+    }
+
+    // Method Description:
+    // - Clears the tab split button color to a system color
+    //   (or white if none is found) when the tab's color is cleared
+    // - Clears the tab row color to a system color
+    //   (or white if none is found) when the tab's color is cleared
+    // Arguments:
+    // - <none>
+    // Return Value:
+    // - <none>
+    void TerminalPage::_ClearNewTabButtonColor()
+    {
+        // TODO GH#3327: Look at what to do with the tab button when we have XAML theming
+        winrt::hstring keys[] = {
+            L"SplitButtonBackground",
+            L"SplitButtonBackgroundPointerOver",
+            L"SplitButtonBackgroundPressed",
+            L"SplitButtonForeground",
+            L"SplitButtonForegroundPointerOver",
+            L"SplitButtonForegroundPressed"
+        };
+
+        // simply clear any of the colors in the split button's dict
+        for (auto keyString : keys)
+        {
+            auto key = winrt::box_value(keyString);
+            if (_newTabButton.Resources().HasKey(key))
+            {
+                _newTabButton.Resources().Remove(key);
+            }
+        }
+
+        const auto res = Application::Current().Resources();
+
+        const auto defaultBackgroundKey = winrt::box_value(L"TabViewItemHeaderBackground");
+        const auto defaultForegroundKey = winrt::box_value(L"SystemControlForegroundBaseHighBrush");
+        winrt::Windows::UI::Xaml::Media::SolidColorBrush backgroundBrush;
+        winrt::Windows::UI::Xaml::Media::SolidColorBrush foregroundBrush;
+
+        if (res.HasKey(defaultBackgroundKey))
+        {
+            winrt::Windows::Foundation::IInspectable obj = res.Lookup(defaultBackgroundKey);
+            backgroundBrush = obj.try_as<winrt::Windows::UI::Xaml::Media::SolidColorBrush>();
+        }
+        else
+        {
+            backgroundBrush = winrt::Windows::UI::Xaml::Media::SolidColorBrush{ winrt::Windows::UI::Colors::Black() };
+        }
+
+        if (res.HasKey(defaultForegroundKey))
+        {
+            winrt::Windows::Foundation::IInspectable obj = res.Lookup(defaultForegroundKey);
+            foregroundBrush = obj.try_as<winrt::Windows::UI::Xaml::Media::SolidColorBrush>();
+        }
+        else
+        {
+            foregroundBrush = winrt::Windows::UI::Xaml::Media::SolidColorBrush{ winrt::Windows::UI::Colors::White() };
+        }
+
+        _newTabButton.Background(backgroundBrush);
+        _newTabButton.Foreground(foregroundBrush);
+    }
+
+    // Method Description:
+    // - Sets the tab split button color when a new tab color is selected
+    // - This method could also set the color of the title bar and tab row
+    // in the future
+    // Arguments:
+    // - selectedTabColor: The color of the newly selected tab
+    // Return Value:
+    // - <none>
+    void TerminalPage::_SetNonClientAreaColors(const Windows::UI::Color& /*selectedTabColor*/)
+    {
+        // TODO GH#3327: Look at what to do with the NC area when we have XAML theming
+    }
+
+    // Method Description:
+    // - Clears the tab split button color when the tab's color is cleared
+    // - This method could also clear the color of the title bar and tab row
+    // in the future
+    // Arguments:
+    // - <none>
+    // Return Value:
+    // - <none>
+    void TerminalPage::_ClearNonClientAreaColors()
+    {
+        // TODO GH#3327: Look at what to do with the NC area when we have XAML theming
     }
 
     // -------------------------------- WinRT Events ---------------------------------

src/cascadia/LocalTests_TerminalApp/TabTests.cpp
@@ -17,6 +17,7 @@ using namespace winrt::TerminalApp;
 using namespace WEX::Logging;
 using namespace WEX::TestExecution;
 using namespace WEX::Common;
+using namespace winrt::Windows::ApplicationModel::DataTransfer;
 
 namespace TerminalAppLocalTests
 {

src/cascadia/LocalTests_TerminalApp/pch.h
@@ -39,6 +39,8 @@ Author(s):
 #include "../../types/inc/utils.hpp"
 #include "../../inc/DefaultSettings.h"
 
+#include <winrt/Windows.ApplicationModel.Resources.Core.h>
+#include "winrt/Windows.UI.Xaml.Markup.h"
 #include <winrt/Windows.system.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>

src/cascadia/TerminalApp/ColorHelper.cpp
@@ -1,0 +1,269 @@
+#include "pch.h"
+#include "ColorHelper.h"
+#include <limits>
+
+using namespace winrt::TerminalApp;
+
+// Method Description:
+// Determines whether or not a given color is light
+// Arguments:
+// - color: this color is going to be examined whether it
+// is light or not
+// Return Value:
+// - true of light, false if dark
+bool ColorHelper::IsBrightColor(const winrt::Windows::UI::Color& color)
+{
+    // http://www.w3.org/TR/AERT#color-contrast
+    auto brightness = (color.R * 299 + color.G * 587 + color.B * 114) / 1000.f;
+    return brightness > 128.f;
+}
+
+// Method Description:
+// Converts a rgb color to an hsl one
+// Arguments:
+// - color: the rgb color, which is going to be converted
+// Return Value:
+// - a hsl color with the following ranges
+//      - H: [0.f -360.f]
+//      - L: [0.f - 1.f] (rounded to the third decimal place)
+//      - S: [0.f - 1.f] (rounded to the third decimal place)
+HSL ColorHelper::RgbToHsl(const winrt::Windows::UI::Color& color)
+{
+    // https://www.rapidtables.com/convert/color/rgb-to-hsl.html
+    auto epsilon = std::numeric_limits<float>::epsilon();
+    auto r = color.R / 255.f;
+    auto g = color.G / 255.f;
+    auto b = color.B / 255.f;
+
+    auto max = std::max(r, std::max(g, b));
+    auto min = std::min(r, std::min(g, b));
+
+    auto delta = max - min;
+
+    auto h = 0.f;
+    auto s = 0.f;
+    auto l = (max + min) / 2;
+
+    if (delta < epsilon || max < epsilon) /* delta == 0 || max == 0*/
+    {
+        l = std::roundf(l * 1000) / 1000;
+        return HSL{ h, s, l };
+    }
+
+    s = l > 0.5 ? delta / (2 - max - min) : delta / (max + min);
+
+    if (max - r < epsilon) // max == r
+    {
+        h = (g - b) / delta + (g < b ? 6 : 0);
+    }
+    else if (max - g < epsilon) // max == g
+    {
+        h = (b - r) / delta + 2;
+    }
+    else if (max - b < epsilon) // max == b
+    {
+        h = (r - g) / delta + 4;
+    }
+
+    // three decimal places after the comma ought
+    // to be enough for everybody - Bill Gates, 1981
+    float finalH = std::roundf(h * 60);
+    float finalS = std::roundf(s * 1000) / 1000;
+    float finalL = std::roundf(l * 1000) / 1000;
+
+    return HSL{ finalH, finalS, finalL };
+}
+
+// Method Description:
+// Converts a hsl color to rgb one
+// Arguments:
+// - color: the hsl color, which is going to be converted
+// Return Value:
+// - the rgb color (r,g,b - [0, 255] range)
+winrt::Windows::UI::Color ColorHelper::HslToRgb(const HSL& color)
+{
+    auto epsilon = std::numeric_limits<float>::epsilon();
+
+    auto h = (color.H - 1.f > epsilon) ? color.H / 360.f : color.H;
+    auto s = (color.S - 1.f > epsilon) ? color.S / 100.f : color.S;
+    auto l = (color.L - 1.f > epsilon) ? color.L / 100.f : color.L;
+
+    auto r = l;
+    auto g = l;
+    auto b = l;
+
+    if (s > epsilon)
+    {
+        auto q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        auto p = 2 * l - q;
+        r = HueToRgb(p, q, h + 1.f / 3.f);
+        g = HueToRgb(p, q, h);
+        b = HueToRgb(p, q, h - 1.f / 3.f);
+    }
+
+    auto finalR = static_cast<uint8_t>(std::roundf(r * 255));
+    auto finalG = static_cast<uint8_t>(std::roundf(g * 255));
+    auto finalB = static_cast<uint8_t>(std::roundf(b * 255));
+    uint8_t finalA = 255; //opaque
+
+    return winrt::Windows::UI::ColorHelper::FromArgb(finalA, finalR, finalG, finalB);
+}
+
+float ColorHelper::HueToRgb(float p, float q, float t)
+{
+    auto epsilon = std::numeric_limits<float>::epsilon();
+
+    if (t < 0)
+        t += 1;
+    if (t > 1)
+        t -= 1;
+    if (t - (1.f / 6.f) < epsilon)
+        return p + (q - p) * 6 * t;
+    if (t - .5f < epsilon)
+        return q;
+    if (t - 2.f / 3.f < epsilon)
+        return p + (q - p) * (2.f / 3.f - t) * 6;
+    return p;
+}
+
+// Method Description:
+// Lightens a color by a given amount
+// Arguments:
+// - color: the color which is going to be lightened
+// - amount: the lighten amount (0-100)
+// Return Value:
+// - the lightened color in RGB format
+winrt::Windows::UI::Color ColorHelper::Lighten(const winrt::Windows::UI::Color& color, float amount /* = 10.f*/)
+{
+    auto hsl = RgbToHsl(color);
+    hsl.L += amount / 100;
+    hsl.L = std::clamp(hsl.L, 0.f, 1.f);
+    return HslToRgb(hsl);
+}
+
+// Method Description:
+// Darkens a color by a given amount
+// Arguments:
+// - color: the color which is going to be darkened
+// - amount: the darken amount (0-100)
+// Return Value:
+// - the darkened color in RGB format
+winrt::Windows::UI::Color ColorHelper::Darken(const winrt::Windows::UI::Color& color, float amount /* = 10.f*/)
+{
+    auto hsl = RgbToHsl(color);
+    hsl.L -= amount / 100;
+    hsl.L = std::clamp(hsl.L, 0.f, 1.f);
+    return HslToRgb(hsl);
+}
+
+// Method Description:
+// Gets an accent color to a given color. Basically, generates
+// 16 shades of the color and finds the first which has a good
+// contrast according to http://www.w3.org/TR/2008/REC-WCAG20-20081211/#contrast-ratiodef (WCAG Version 2)
+// Readability ratio of 3.5 seems to look quite nicely
+// Arguments:
+// - color: the color for which we need an accent
+// Return Value:
+// - the accent color in RGB format
+winrt::Windows::UI::Color ColorHelper::GetAccentColor(const winrt::Windows::UI::Color& color)
+{
+    auto accentColor = RgbToHsl(color);
+
+    if (accentColor.S < 0.15)
+    {
+        accentColor.S = 0.15f;
+    }
+
+    constexpr auto shadeCount = 16;
+    constexpr auto shadeStep = 1.f / shadeCount;
+    auto shades = std::map<float, HSL>();
+    for (auto i = 0; i < 15; i++)
+    {
+        auto shade = HSL{ accentColor.H, accentColor.S, i * shadeStep };
+        auto contrast = GetReadability(shade, accentColor);
+        shades.insert(std::make_pair(contrast, shade));
+    }
+
+    // 3f is quite nice if the whole non-client area is painted
+    constexpr auto readability = 1.75f;
+    for (auto shade : shades)
+    {
+        if (shade.first >= readability)
+        {
+            return HslToRgb(shade.second);
+        }
+    }
+    return HslToRgb(shades.end()->second);
+}
+
+// Method Description:
+// Gets the readability of two colors according to
+// http://www.w3.org/TR/2008/REC-WCAG20-20081211/#contrast-ratiodef (WCAG Version 2)
+// Arguments:
+// - firstColor: the first color for the readability check (hsl)
+// - secondColor: the second color for the readability check (hsl)
+// Return Value:
+// - the readability of the colors according to (WCAG Version 2)
+float ColorHelper::GetReadability(const HSL& first, const HSL& second)
+{
+    return GetReadability(HslToRgb(first), HslToRgb(second));
+}
+
+// Method Description:
+// Gets the readability of two colors according to
+// http://www.w3.org/TR/2008/REC-WCAG20-20081211/#contrast-ratiodef (WCAG Version 2)
+// Arguments:
+// - firstColor: the first color for the readability check (rgb)
+// - secondColor: the second color for the readability check (rgb)
+// Return Value:
+// - the readability of the colors according to (WCAG Version 2)
+float ColorHelper::GetReadability(const winrt::Windows::UI::Color& first, const winrt::Windows::UI::Color& second)
+{
+    auto l1 = GetLuminance(first);
+    auto l2 = GetLuminance(second);
+
+    return (std::max(l1, l2) + 0.05f) / std::min(l1, l2) + 0.05f;
+}
+
+// Method Description:
+// Calculates the luminance of a given color according to
+// http://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
+// Arguments:
+// - color: its luminance is going to be calculated
+// Return Value:
+// - the luminance of the color
+float ColorHelper::GetLuminance(const winrt::Windows::UI::Color& color)
+{
+    auto epsilon = std::numeric_limits<float>::epsilon();
+    float R, G, B;
+    auto RsRGB = color.R / 255.f;
+    auto GsRGB = color.G / 255.f;
+    auto BsRGB = color.B / 255.f;
+
+    if (RsRGB - 0.03928f <= epsilon)
+    {
+        R = RsRGB / 12.92f;
+    }
+    else
+    {
+        R = std::pow(((RsRGB + 0.055f) / 1.055f), 2.4f);
+    }
+    if (GsRGB - 0.03928f <= epsilon)
+    {
+        G = GsRGB / 12.92f;
+    }
+    else
+    {
+        G = std::pow(((GsRGB + 0.055f) / 1.055f), 2.4f);
+    }
+    if (BsRGB - 0.03928f <= epsilon)
+    {
+        B = BsRGB / 12.92f;
+    }
+    else
+    {
+        B = std::pow(((BsRGB + 0.055f) / 1.055f), 2.4f);
+    }
+    float luminance = (0.2126f * R) + (0.7152f * G) + (0.0722f * B);
+    return std::roundf(luminance * 10000) / 10000.f;
+}

src/cascadia/TerminalApp/ColorHelper.h
@@ -1,0 +1,32 @@
+#pragma once
+#include "pch.h"
+
+#include <winrt/windows.ui.core.h>
+
+namespace winrt::TerminalApp
+{
+    class HSL
+    {
+    public:
+        float H;
+        float S;
+        float L;
+    };
+
+    class ColorHelper
+    {
+    public:
+        static bool IsBrightColor(const Windows::UI::Color& color);
+        static HSL RgbToHsl(const Windows::UI::Color& color);
+        static Windows::UI::Color HslToRgb(const HSL& color);
+        static Windows::UI::Color Lighten(const Windows::UI::Color& color, float amount = 10.f);
+        static Windows::UI::Color Darken(const Windows::UI::Color& color, float amount = 10.f);
+        static Windows::UI::Color GetAccentColor(const Windows::UI::Color& color);
+        static float GetLuminance(const Windows::UI::Color& color);
+        static float GetReadability(const Windows::UI::Color& first, const Windows::UI::Color& second);
+        static float GetReadability(const HSL& first, const HSL& second);
+
+    private:
+        static float HueToRgb(float p, float q, float t);
+    };
+}

src/cascadia/TerminalApp/ColorPickupFlyout.cpp
@@ -1,0 +1,103 @@
+#include "pch.h"
+#include "ColorPickupFlyout.h"
+#include "ColorPickupFlyout.g.cpp"
+#include "winrt/Windows.UI.Xaml.Media.h"
+#include "winrt/Windows.UI.Xaml.Shapes.h"
+#include "winrt/Windows.UI.Xaml.Interop.h"
+#include <LibraryResources.h>
+
+namespace winrt::TerminalApp::implementation
+{
+    // Method Description:
+    // - Default constructor, localizes the buttons and hooks
+    // up the event fired by the custom color picker, so that
+    // the tab color is set on the fly when selecting a non-preset color
+    // Arguments:
+    // - <none>
+    ColorPickupFlyout::ColorPickupFlyout()
+    {
+        InitializeComponent();
+
+        OkButton().Content(winrt::box_value(RS_(L"Ok")));
+        CustomColorButton().Content(winrt::box_value(RS_(L"TabColorCustomButton/Content")));
+        ClearColorButton().Content(winrt::box_value(RS_(L"TabColorClearButton/Content")));
+    }
+
+    // Method Description:
+    // - Handler of the click event for the preset color swatches.
+    // Reads the color from the clicked rectangle and fires an event
+    // with the selected color. After that hides the flyout
+    // Arguments:
+    // - sender: the rectangle that got clicked
+    // Return Value:
+    // - <none>
+    void ColorPickupFlyout::ColorButton_Click(IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const&)
+    {
+        auto button{ sender.as<Windows::UI::Xaml::Controls::Button>() };
+        auto rectangle{ button.Content().as<Windows::UI::Xaml::Shapes::Rectangle>() };
+        auto rectClr{ rectangle.Fill().as<Windows::UI::Xaml::Media::SolidColorBrush>() };
+        _ColorSelectedHandlers(rectClr.Color());
+        Hide();
+    }
+
+    // Method Description:
+    // - Handler of the clear color button. Clears the current
+    // color of the tab, if any. Hides the flyout after that
+    // Arguments:
+    // - <none>
+    // Return Value:
+    // - <none>
+    void ColorPickupFlyout::ClearColorButton_Click(IInspectable const&, Windows::UI::Xaml::RoutedEventArgs const&)
+    {
+        _ColorClearedHandlers();
+        Hide();
+    }
+
+    // Method Description:
+    // - Handler of the select custom color button. Expands or collapses the flyout
+    // to show the color picker. In order to accomplish this a FlyoutPresenterStyle is used,
+    // in which a Style is embedded, containing the desired width
+    // Arguments:
+    // - <none>
+    // Return Value:
+    // - <none>
+    void ColorPickupFlyout::ShowColorPickerButton_Click(Windows::Foundation::IInspectable const&, Windows::UI::Xaml::RoutedEventArgs const&)
+    {
+        auto targetType = this->FlyoutPresenterStyle().TargetType();
+        auto s = Windows::UI::Xaml::Style{};
+        s.TargetType(targetType);
+        auto visibility = customColorPanel().Visibility();
+        if (visibility == winrt::Windows::UI::Xaml::Visibility::Collapsed)
+        {
+            customColorPanel().Visibility(winrt::Windows::UI::Xaml::Visibility::Visible);
+            auto setter = Windows::UI::Xaml::Setter(Windows::UI::Xaml::FrameworkElement::MinWidthProperty(), winrt::box_value(540));
+            s.Setters().Append(setter);
+        }
+        else
+        {
+            customColorPanel().Visibility(winrt::Windows::UI::Xaml::Visibility::Collapsed);
+            auto setter = Windows::UI::Xaml::Setter(Windows::UI::Xaml::FrameworkElement::MinWidthProperty(), winrt::box_value(0));
+            s.Setters().Append(setter);
+        }
+        this->FlyoutPresenterStyle(s);
+    }
+
+    // Method Description:
+    // - Handles the color selection of the color pickup. Gets
+    // the currently selected color and fires an event with it
+    // Arguments:
+    // - <none>
+    // Return Value:
+    // - <none>
+    void ColorPickupFlyout::CustomColorButton_Click(Windows::Foundation::IInspectable const&, Windows::UI::Xaml::RoutedEventArgs const&)
+    {
+        auto color = customColorPicker().Color();
+        _ColorSelectedHandlers(color);
+        Hide();
+    }
+
+    void ColorPickupFlyout::ColorPicker_ColorChanged(const Windows::UI::Xaml::Controls::ColorPicker&, const Windows::UI::Xaml::Controls::ColorChangedEventArgs& args)
+    {
+        _ColorSelectedHandlers(args.NewColor());
+    }
+}

src/cascadia/TerminalApp/ColorPickupFlyout.h
@@ -1,0 +1,27 @@
+#pragma once
+#include "ColorPickupFlyout.g.h"
+#include "../cascadia/inc/cppwinrt_utils.h"
+
+namespace winrt::TerminalApp::implementation
+{
+    struct ColorPickupFlyout : ColorPickupFlyoutT<ColorPickupFlyout>
+    {
+        ColorPickupFlyout();
+
+        void ColorButton_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void ShowColorPickerButton_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void CustomColorButton_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void ClearColorButton_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void ColorPicker_ColorChanged(const Windows::UI::Xaml::Controls::ColorPicker&, const Windows::UI::Xaml::Controls::ColorChangedEventArgs& args);
+
+        WINRT_CALLBACK(ColorCleared, TerminalApp::ColorClearedArgs);
+        WINRT_CALLBACK(ColorSelected, TerminalApp::ColorSelectedArgs);
+    };
+}
+
+namespace winrt::TerminalApp::factory_implementation
+{
+    struct ColorPickupFlyout : ColorPickupFlyoutT<ColorPickupFlyout, implementation::ColorPickupFlyout>
+    {
+    };
+}

src/cascadia/TerminalApp/ColorPickupFlyout.idl
@@ -1,0 +1,12 @@
+namespace TerminalApp
+{
+    delegate void ColorSelectedArgs(Windows.UI.Color color);
+    delegate void ColorClearedArgs();
+    
+    [default_interface] runtimeclass ColorPickupFlyout : Windows.UI.Xaml.Controls.Flyout
+    {
+        ColorPickupFlyout();
+        event ColorSelectedArgs ColorSelected;
+        event ColorClearedArgs ColorCleared;
+    }
+}

commit 7a251d85f716f47792a030170b5f97f617f32f86

Don't set the new tab button colors for now (#5749)
This is actually related to another issue we have, #3917. I think if the system is set to "Dark" theme, but the app is set to light theme, then the brush lookup in `_ClearNewTabButtonColor` still returns to us the dark theme brushes.

Fortunately, since we're not actually setting the color of the new tab button anymore, we can just remove the call to that method now, and loop back on it later.

## References
* regressed in #3789 
* related to #3917

## PR Checklist
* [x] Closes #5741

src/cascadia/TerminalApp/TerminalPage.cpp
@@ -913,8 +913,12 @@ namespace winrt::TerminalApp::implementation
             }
         });
 
+        // TODO GH#3327: Once we support colorizing the NewTab button based on
+        // the color of the tab, we'll want to make sure to call
+        // _ClearNewTabButtonColor here, to reset it to the default (for the
+        // newly created tab).
         // remove any colors left by other colored tabs
-        _ClearNewTabButtonColor();
+        // _ClearNewTabButtonColor();
     }
 
     // Method Description:
@@ -1931,6 +1935,11 @@ namespace winrt::TerminalApp::implementation
         winrt::Windows::UI::Xaml::Media::SolidColorBrush backgroundBrush;
         winrt::Windows::UI::Xaml::Media::SolidColorBrush foregroundBrush;
 
+        // TODO: Related to GH#3917 - I think if the system is set to "Dark"
+        // theme, but the app is set to light theme, then this lookup still
+        // returns to us the dark theme brushes. There's gotta be a way to get
+        // the right brushes...
+        // See also GH#5741
         if (res.HasKey(defaultBackgroundKey))
         {
             winrt::Windows::Foundation::IInspectable obj = res.Lookup(defaultBackgroundKey);