ae7087ca0b91e577f3bb127eee918cde6b81943d
Dustin L. Howett <duhowett@microsoft.com>
2020-05-15 21:51:54 -0700

    TEMP REVERT EMJ FIXES

--- src/types/CodepointWidthDetector.cpp
+++ src/types/CodepointWidthDetector.cpp
@@ -38,10 +38,6 @@ namespace
         // 0x2666 0x2710,
         // 0x270E 0x2765 0x1f000 - 0x1f02b except 0x1f004 0x1f594
         //
-        // GH #5822 - Removed glyphs that appear inside of CP437 (https://en.wikipedia.org/wiki/Code_page_437)
-        // and WGL4 (https://en.wikipedia.org/wiki/Windows_Glyph_List_4) since they've been narrow since the
-        // beginning of time and changing it to wide would only cause destruction.
-        //
         // *** Codepoint ranges marked with "OVR" have their given width from EastAsianWidth.txt overridden.
         UnicodeRange{ 0xa1, 0xa1, CodepointWidth::Ambiguous },
         UnicodeRange{ 0xa4, 0xa4, CodepointWidth::Ambiguous },
@@ -183,6 +179,7 @@ namespace
         UnicodeRange{ 0x2592, 0x2595, CodepointWidth::Ambiguous },
         UnicodeRange{ 0x25a0, 0x25a1, CodepointWidth::Ambiguous },
         UnicodeRange{ 0x25a3, 0x25a9, CodepointWidth::Ambiguous },
+        UnicodeRange{ 0x25aa, 0x25ab, CodepointWidth::Wide }, // OVR
         UnicodeRange{ 0x25b2, 0x25b3, CodepointWidth::Ambiguous },
         UnicodeRange{ 0x25b6, 0x25b7, CodepointWidth::Ambiguous },
         UnicodeRange{ 0x25bc, 0x25bd, CodepointWidth::Ambiguous },
@@ -192,7 +189,7 @@ namespace
         UnicodeRange{ 0x25ce, 0x25d1, CodepointWidth::Ambiguous },
         UnicodeRange{ 0x25e2, 0x25e5, CodepointWidth::Ambiguous },
         UnicodeRange{ 0x25ef, 0x25ef, CodepointWidth::Ambiguous },
-        UnicodeRange{ 0x25fd, 0x25fe, CodepointWidth::Wide },
+        UnicodeRange{ 0x25fb, 0x25fe, CodepointWidth::Wide }, // OVR 5fb-5fc
         UnicodeRange{ 0x2600, 0x2604, CodepointWidth::Wide }, // OVR
         UnicodeRange{ 0x2605, 0x2606, CodepointWidth::Ambiguous },
         UnicodeRange{ 0x2609, 0x2609, CodepointWidth::Ambiguous },
@@ -207,9 +204,13 @@ namespace
         UnicodeRange{ 0x2626, 0x2626, CodepointWidth::Wide }, // OVR
         UnicodeRange{ 0x262a, 0x262a, CodepointWidth::Wide }, // OVR
         UnicodeRange{ 0x262e, 0x262f, CodepointWidth::Wide }, // OVR
-        UnicodeRange{ 0x2638, 0x2639, CodepointWidth::Wide }, // OVR
+        UnicodeRange{ 0x2638, 0x263a, CodepointWidth::Wide }, // OVR
+        UnicodeRange{ 0x2640, 0x2640, CodepointWidth::Wide }, // OVR
+        UnicodeRange{ 0x2642, 0x2642, CodepointWidth::Wide }, // OVR
         UnicodeRange{ 0x2648, 0x2653, CodepointWidth::Wide },
-        UnicodeRange{ 0x265f, 0x265f, CodepointWidth::Wide }, // OVR
+        UnicodeRange{ 0x265f, 0x2660, CodepointWidth::Wide }, // OVR
+        UnicodeRange{ 0x2663, 0x2663, CodepointWidth::Wide }, // OVR
+        UnicodeRange{ 0x2665, 0x2666, CodepointWidth::Wide }, // OVR
         UnicodeRange{ 0x2668, 0x2668, CodepointWidth::Wide }, // OVR
         UnicodeRange{ 0x2669, 0x266a, CodepointWidth::Ambiguous },
         UnicodeRange{ 0x266c, 0x266d, CodepointWidth::Ambiguous },
