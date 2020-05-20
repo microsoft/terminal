24964329bb0055f0cccbb31e52b27cccea890f0c
Mike Griese <migrie@microsoft.com>
2020-05-20 12:03:32 -0500

    Okay, actually find the tests this time

--- build/scripts/Run-Tests.ps1
+++ build/scripts/Run-Tests.ps1
@@ -2,12 +2,13 @@
 Param(
     [Parameter(Mandatory=$true, Position=0)][string]$MatchPattern,
     [Parameter(Mandatory=$true, Position=1)][string]$Platform,
-    [Parameter(Mandatory=$true, Position=2)][string]$Configuration
+    [Parameter(Mandatory=$true, Position=2)][string]$Configuration,
+    [Parameter(Mandatory=$false, Position=3)][string]$SearchPath=".\bin\$Platform\$Configuration"
 )
 
-$testdlls = Get-ChildItem -Path ".\bin\$Platform\$Configuration" -Recurse -Filter $MatchPattern
-
-&".\bin\$Platform\$Configuration\te.exe" $testdlls.FullName
+$testdlls = Get-ChildItem -Path $SearchPath -Recurse -Filter $MatchPattern
+$testdlls
+# &".\bin\$Platform\$Configuration\te.exe" $testdlls.FullName
 
 if ($lastexitcode -Ne 0) { Exit $lastexitcode }
 
