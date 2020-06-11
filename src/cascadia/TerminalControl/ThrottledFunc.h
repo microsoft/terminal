b8314dd9d55ac0e0244a814995565af674fee46f
Greg Depoire--Ferrer <greg.depoire@gmail.com>
2020-06-11 14:33:18 +0200

    Also delay the initial scroll update

--- src/cascadia/TerminalControl/ThrottledFunc.h
+++ src/cascadia/TerminalControl/ThrottledFunc.h
@@ -26,9 +26,9 @@ class ThrottledFunc : public std::enable_shared_from_this<ThrottledFunc<T>>
 public:
     using Func = std::function<void(T arg)>;
 
-    ThrottledFunc(Func func, winrt::Windows::Foundation::TimeSpan minimumDelay) :
+    ThrottledFunc(Func func, winrt::Windows::Foundation::TimeSpan delay) :
         _func{ func },
-        _minimumDelay{ minimumDelay }
+        _delay{ delay }
     {
     }
 
@@ -51,19 +51,10 @@ public:
 
         auto weakThis = this->weak_from_this();
 
-        const auto waitTime = GetNextWaitTime();
-        if (waitTime.has_value())
-        {
-            co_await winrt::resume_after(waitTime.value());
-        }
+        co_await winrt::resume_after(_delay);
 
         if (auto that{ weakThis.lock() })
         {
-            // No need to lock because there cannot be another thread that is
-            // reading/writing to the value because `_nextArg` has a value so
-            // other calls to `Run` early return.
-            that->_lastRun = std::chrono::high_resolution_clock::now();
-
             auto arg = that->_nextArg.Take();
             if (arg.has_value())
             {
@@ -83,52 +74,7 @@ public:
     }
 
 private:
-    // Method Description:
-    // - Computes the time that the code should wait before doing the next action,
-    //   in order to throttle actions with the specified throttle time. If the code
-    //   should not wait, then an empty value is returned.
-    // Arguments:
-    // - <none>
-    // Return Value:
-    // - the time to wait before the next action or an empty value for no wait
-    std::optional<winrt::Windows::Foundation::TimeSpan> GetNextWaitTime()
-    {
-        if (!_lastRun.has_value())
-        {
-            return { std::nullopt };
-        }
-        else
-        {
-            const auto timeNow = std::chrono::high_resolution_clock::now();
-            const auto sinceLastAction = timeNow - _lastRun.value();
-            if (sinceLastAction > _minimumDelay)
-            {
-                // Fortunately, time will not go backwards, so if we have
-                // waited enough to start a new action, we will still have
-                // waited enough to start a new action later (unless we
-                // do an action in which case the `_lastAction` field will be
-                // set).
-                _lastRun = { std::nullopt };
-
-                return { std::nullopt };
-            }
-            else
-            {
-                auto waitTime = std::chrono::duration_cast<winrt::Windows::Foundation::TimeSpan>(_minimumDelay - sinceLastAction);
-                if (waitTime.count() > 0)
-                {
-                    return { waitTime };
-                }
-                else
-                {
-                    return { std::nullopt };
-                }
-            }
-        }
-    }
-
     Func _func;
-    winrt::Windows::Foundation::TimeSpan _minimumDelay;
-    std::optional<std::chrono::high_resolution_clock::time_point> _lastRun;
+    winrt::Windows::Foundation::TimeSpan _delay;
     ThreadSafeOptional<T> _nextArg;
 };
