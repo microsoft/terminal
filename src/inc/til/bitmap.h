37f33f66ccdea54948f15701825faf9576472cca
Greg Depoire--Ferrer <greg.depoire@gmail.com>
2020-06-13 20:45:29 +0200

    fix Static Analysis

--- src/inc/til/bitmap.h
+++ src/inc/til/bitmap.h
@@ -398,7 +398,11 @@ namespace til // Terminal Implementation Library. Also: "Today I Learned"
 
             const auto bitShift = delta_y * _sz.width();
 
+#pragma warning(push)
+            // we can't depend on GSL here (some libraries use BLOCK_GSL), so we use static_cast for explicit narrowing
+#pragma warning(disable : 26472)
             const auto newBits = static_cast<size_t>(std::abs(bitShift));
+#pragma warning(pop)
             const bool isLeftShift = bitShift > 0;
 
             if (newBits >= _bits.size())
