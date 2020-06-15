0cf9f0b7a2e97d0285d6684d56dab088a9b03e59
Dustin Howett <duhowett@microsoft.com>
2020-06-15 14:42:59 -0700

    Remove the WinTelnetEngine
    Nobody was using it.

--- src/host/VtIo.cpp
+++ src/host/VtIo.cpp
@@ -7,7 +7,6 @@
 
 #include "../renderer/vt/XtermEngine.hpp"
 #include "../renderer/vt/Xterm256Engine.hpp"
-#include "../renderer/vt/WinTelnetEngine.hpp"
 
 #include "../renderer/base/renderer.hpp"
 #include "../types/inc/utils.hpp"
@@ -50,10 +49,6 @@ VtIo::VtIo() :
     else if (VtMode == XTERM_STRING)
     {
         ioMode = VtIoMode::XTERM;
-    }
-    else if (VtMode == WIN_TELNET_STRING)
-    {
-        ioMode = VtIoMode::WIN_TELNET;
     }
     else if (VtMode == XTERM_ASCII_STRING)
     {
@@ -177,12 +172,6 @@ VtIo::VtIo() :
                                                                  initialViewport,
                                                                  gci.Get16ColorTable(),
                                                                  true);
-                break;
-            case VtIoMode::WIN_TELNET:
-                _pVtRenderEngine = std::make_unique<WinTelnetEngine>(std::move(_hOutput),
-                                                                     gci,
-                                                                     initialViewport,
-                                                                     gci.Get16ColorTable());
                 break;
             default:
                 return E_FAIL;
