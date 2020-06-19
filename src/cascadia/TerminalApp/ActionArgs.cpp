10e91a823d90509c5b4fc2fa83ed29c01c21051d
Mike Griese <migrie@microsoft.com>
2020-06-19 16:55:41 -0500

    strings for every possible action, and args value

--- src/cascadia/TerminalApp/ActionArgs.cpp
+++ src/cascadia/TerminalApp/ActionArgs.cpp
@@ -22,42 +22,179 @@ namespace winrt::TerminalApp::implementation
 {
     winrt::hstring NewTerminalArgs::GenerateName()
     {
-        return L"NewTerminalArgs";
+        std::wstringstream ss;
+
+        if (!_Profile.empty())
+        {
+            ss << fmt::format(L"profile: {}, ", _Profile);
+        }
+        else if (_ProfileIndex)
+        {
+            ss << fmt::format(L"profile index: {}, ", _ProfileIndex.Value());
+        }
+
+        if (!_Commandline.empty())
+        {
+            ss << fmt::format(L"commandline: {}, ", _Commandline);
+        }
+
+        if (!_StartingDirectory.empty())
+        {
+            ss << fmt::format(L"directory: {}, ", _StartingDirectory);
+        }
+
+        if (!_TabTitle.empty())
+        {
+            ss << fmt::format(L"title: {}, ", _TabTitle);
+        }
+        auto s = ss.str();
+        if (s.empty())
+        {
+            return L"";
+        }
+
+        // Chop off the last ", "
+        return winrt::hstring{ s.substr(0, s.size() - 2) };
     }
+
     winrt::hstring CopyTextArgs::GenerateName()
     {
         if (_SingleLine)
         {
-            return L"Copy text as single line";
+            return RS_(L"CopyTextAsSingleLineCommandKey");
         }
-        return L"Copy Text";
+        return RS_(L"CopyTextCommandKey");
     }
+
     winrt::hstring NewTabArgs::GenerateName()
     {
+        winrt::hstring newTerminalArgsStr;
+        if (_TerminalArgs)
+        {
+            newTerminalArgsStr = _TerminalArgs.GenerateName();
+        }
+        if (newTerminalArgsStr.empty())
+        {
+            return RS_(L"NewTabCommandKey");
+        }
+        else
+        {
+            return winrt::hstring{ fmt::format(L"{}, {}", RS_(L"NewTabCommandKey"), newTerminalArgsStr) };
+        }
         return L"NewTabArgs";
     }
+
     winrt::hstring SwitchToTabArgs::GenerateName()
     {
-        return L"SwitchToTabArgs";
+        return winrt::hstring{ fmt::format(L"{}, index:{}", RS_(L"SwitchToTabCommandKey"), _TabIndex) };
     }
+
     winrt::hstring ResizePaneArgs::GenerateName()
     {
-        return L"ResizePaneArgs";
+        winrt::hstring directionString;
+        switch (_Direction)
+        {
+        case Direction::Left:
+            directionString = RS_(L"DirectionLeft");
+            break;
+        case Direction::Right:
+            directionString = RS_(L"DirectionRight");
+            break;
+        case Direction::Up:
+            directionString = RS_(L"DirectionUp");
+            break;
+        case Direction::Down:
+            directionString = RS_(L"DirectionDown");
+            break;
+        }
+        return winrt::hstring{ fmt::format(RS_(L"ResizePaneWithArgCommandKey").c_str(), directionString) };
     }
+
     winrt::hstring MoveFocusArgs::GenerateName()
     {
-        return L"MoveFocusArgs";
+        winrt::hstring directionString;
+        switch (_Direction)
+        {
+        case Direction::Left:
+            directionString = RS_(L"DirectionLeft");
+            break;
+        case Direction::Right:
+            directionString = RS_(L"DirectionRight");
+            break;
+        case Direction::Up:
+            directionString = RS_(L"DirectionUp");
+            break;
+        case Direction::Down:
+            directionString = RS_(L"DirectionDown");
+            break;
+        }
+        return winrt::hstring{ fmt::format(RS_(L"MoveFocusWithArgCommandKey").c_str(), directionString) };
     }
+
     winrt::hstring AdjustFontSizeArgs::GenerateName()
     {
-        return L"AdjustFontSizeArgs";
+        if (_Delta < 0)
+        {
+            return _Delta == -1 ? winrt::hstring{ fmt::format(RS_(L"DecreaseFontSizeCommandKey").c_str()) } :
+                                  winrt::hstring{ fmt::format(RS_(L"DecreaseFontSizeWithAmountCommandKey").c_str(), -_Delta) };
+        }
+        else
+        {
+            return _Delta == 1 ? winrt::hstring{ fmt::format(RS_(L"IncreaseFontSizeCommandKey").c_str()) } :
+                                 winrt::hstring{ fmt::format(RS_(L"IncreaseFontSizeWithAmountCommandKey").c_str(), _Delta) };
+        }
     }
+
     winrt::hstring SplitPaneArgs::GenerateName()
     {
-        return L"SplitPaneArgs";
+        std::wstringstream ss;
+        if (_SplitMode == SplitType::Duplicate)
+        {
+            ss << RS_(L"DuplicatePaneCommandKey").c_str();
+        }
+        else
+        {
+            ss << RS_(L"SplitPaneCommandKey").c_str();
+        }
+        ss << L", ";
+
+        switch (_SplitStyle)
+        {
+        case SplitState::Vertical:
+            ss << L"direction: Vertical, ";
+            break;
+        case SplitState::Horizontal:
+            ss << L"direction: Horizontal, ";
+            break;
+        }
+
+        winrt::hstring newTerminalArgsStr;
+        if (_TerminalArgs)
+        {
+            newTerminalArgsStr = _TerminalArgs.GenerateName();
+        }
+
+        if (_SplitMode != SplitType::Duplicate && !newTerminalArgsStr.empty())
+        {
+            ss << newTerminalArgsStr.c_str();
+            ss << L", ";
+        }
+
+        // Chop off the last ", "
+        auto s = ss.str();
+        return winrt::hstring{ s.substr(0, s.size() - 2) };
     }
+
     winrt::hstring OpenSettingsArgs::GenerateName()
     {
-        return L"OpenSettingsArgs";
+        switch (_Target)
+        {
+        case SettingsTarget::DefaultsFile:
+            return RS_(L"OpenDefaultSettingsCommandKey");
+        case SettingsTarget::AllFiles:
+            return RS_(L"OpenBothSettingsFilesCommandKey");
+        default:
+            return RS_(L"OpenSettingsCommandKey");
+        }
     }
 }
