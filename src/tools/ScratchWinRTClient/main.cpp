d56137876e3d7cac81e5fd13d4b12a1909537843
Mike Griese <migrie@microsoft.com>
2020-07-10 15:23:58 -0500

    curious, I can get the IClosable, but not the ScratchInterface?

--- src/tools/ScratchWinRTClient/main.cpp
+++ src/tools/ScratchWinRTClient/main.cpp
@@ -4,19 +4,27 @@
 using namespace winrt;
 using namespace Windows::Foundation;
 
-static constexpr GUID server_clsid // DAA16D7F-EF66-4FC9-B6F2-3E5B6D924576
-    {
-        0xdaa16d7f,
-        0xef66,
-        0x4fc9,
-        { 0xb6, 0xf2, 0x3e, 0x5b, 0x6d, 0x92, 0x45, 0x76 }
-    };
+// DAA16D7F-EF66-4FC9-B6F2-3E5B6D924576
+static constexpr GUID MyStringable_clsid{
+    0xdaa16d7f,
+    0xef66,
+    0x4fc9,
+    { 0xb6, 0xf2, 0x3e, 0x5b, 0x6d, 0x92, 0x45, 0x76 }
+};
+
+// EAA16D7F-EF66-4FC9-B6F2-3E5B6D924576
+static constexpr GUID ScratchStringable_clsid{
+    0xeaa16d7f,
+    0xef66,
+    0x4fc9,
+    { 0xb6, 0xf2, 0x3e, 0x5b, 0x6d, 0x92, 0x45, 0x76 }
+};
 
 void actualApp()
 {
     {
         printf("Trying to directly create a IStringable...\n");
-        auto server = create_instance<IStringable>(server_clsid, CLSCTX_LOCAL_SERVER);
+        auto server = create_instance<IStringable>(MyStringable_clsid, CLSCTX_LOCAL_SERVER);
         if (server)
         {
             printf("%ls\n", server.ToString().c_str());
@@ -37,7 +45,7 @@ void actualApp()
     }
     {
         printf("Trying to directly create a IScratchInterface...\n");
-        auto server = create_instance<winrt::ScratchWinRTServer::IScratchInterface>(server_clsid, CLSCTX_LOCAL_SERVER);
+        auto server = create_instance<winrt::ScratchWinRTServer::IScratchInterface>(MyStringable_clsid, CLSCTX_LOCAL_SERVER);
         if (server)
         {
             printf("Found scratch interface\n");
@@ -50,18 +58,86 @@ void actualApp()
     }
 }
 
+void closeApp()
+{
+    printf("closeApp\n");
+    {
+        printf("Trying to directly create a IStringable...\n");
+        auto server = create_instance<IStringable>(ScratchStringable_clsid, CLSCTX_LOCAL_SERVER);
+        if (server)
+        {
+            printf("%ls\n", server.ToString().c_str());
+            if (auto asScratch = server.try_as<winrt::ScratchWinRTServer::IScratchInterface>())
+            {
+                printf("Found scratch interface\n");
+                printf("%ls\n", asScratch.DoTheThing().c_str());
+            }
+            else
+            {
+                printf("Could not get the IScratchInterface from the IStringable\n");
+            }
+
+            if (auto asClosable = server.try_as<IClosable>())
+            {
+                asClosable.Close();
+                printf("Closed!!!!!!\n");
+            }
+            else
+            {
+                printf("Could not get the IClosable from the IStringable\n");
+            }
+        }
+        else
+        {
+            printf("Could not get the IStringable directly\n");
+        }
+    }
+    {
+        printf("Trying to directly create a IClosable...\n");
+        auto server = create_instance<IClosable>(ScratchStringable_clsid, CLSCTX_LOCAL_SERVER);
+        if (server)
+        {
+            server.Close();
+            printf("Closed?\n");
+            if (auto asScratch = server.try_as<ScratchWinRTServer::IScratchInterface>())
+            {
+                printf("Found scratch interface\n");
+                printf("%ls\n", asScratch.DoTheThing().c_str());
+            }
+            else
+            {
+                printf("Could not get the IScratchInterface from the IClosable\n");
+            }
+        }
+        else
+        {
+            printf("Could not get the IClosable directly\n");
+        }
+    }
+}
+
 int main()
 {
+    init_apartment();
     try
     {
-        init_apartment();
         actualApp();
-
-        puts("Press Enter me when you're done.");
-        getchar();
     }
     catch (hresult_error const& e)
     {
-        printf("Error: %ls", e.message().c_str());
+        printf("Error: %ls\n", e.message().c_str());
     }
+
+    printf("------------------\n");
+    try
+    {
+        closeApp();
+    }
+    catch (hresult_error const& e)
+    {
+        printf("Error: %ls\n", e.message().c_str());
+    }
+
+    puts("Press Enter me when you're done.");
+    getchar();
 }
--- src/tools/ScratchWinRTServer/main.cpp
+++ src/tools/ScratchWinRTServer/main.cpp
@@ -3,7 +3,7 @@
 using namespace winrt;
 using namespace Windows::Foundation;
 
-struct ScratchStringable : implements<ScratchStringable, IStringable, winrt::ScratchWinRTServer::IScratchInterface>
+struct ScratchStringable : implements<ScratchStringable, IStringable, IClosable, winrt::ScratchWinRTServer::IScratchInterface>
 {
     hstring ToString()
     {
@@ -13,34 +13,27 @@ struct ScratchStringable : implements<ScratchStringable, IStringable, winrt::Scr
     {
         return L"Zhu Li! Do the thing!";
     }
+    void Close() { printf("Closed ScratchStringable\n"); }
 };
-struct Stringable : implements<Stringable, IStringable>
+struct MyStringable : implements<MyStringable, IStringable>
 {
     hstring ToString()
     {
-        return L"Hello from server, Stringable";
+        return L"Hello from server, MyStringable";
     }
 };
 
-struct Factory : implements<Factory, IClassFactory>
+struct MyStringableFactory : implements<MyStringableFactory, IClassFactory>
 {
-    HRESULT __stdcall CreateInstance(
-        IUnknown* outer,
-        GUID const& iid,
-        void** result) noexcept final
+    HRESULT __stdcall CreateInstance(IUnknown* outer, GUID const& iid, void** result) noexcept final
     {
         *result = nullptr;
-
         if (outer)
         {
             return CLASS_E_NOAGGREGATION;
         }
-
-        // return make<Stringable>().as(iid, result);
-        return make<ScratchStringable>().as(iid, result);
-        // auto f = winrt::make_self<winrt::ScratchWinRTServer::ScratchClass>();
-        // return f.as(iid, result);
-        //return make<winrt::ScratchWinRTServer::ScratchClass>().as(iid, result);
+        printf("Created MyStringable\n");
+        return make<MyStringable>().as(iid, result);
     }
 
     HRESULT __stdcall LockServer(BOOL) noexcept final
@@ -49,28 +42,62 @@ struct Factory : implements<Factory, IClassFactory>
     }
 };
 
-static constexpr GUID server_clsid // DAA16D7F-EF66-4FC9-B6F2-3E5B6D924576
+struct ScratchStringableFactory : implements<ScratchStringableFactory, IClassFactory>
+{
+    HRESULT __stdcall CreateInstance(IUnknown* outer, GUID const& iid, void** result) noexcept final
     {
-        0xdaa16d7f,
-        0xef66,
-        0x4fc9,
-        { 0xb6, 0xf2, 0x3e, 0x5b, 0x6d, 0x92, 0x45, 0x76 }
-    };
+        *result = nullptr;
+        if (outer)
+        {
+            return CLASS_E_NOAGGREGATION;
+        }
+        printf("Created ScratchStringable\n");
+        return make<ScratchStringable>().as(iid, result);
+    }
+
+    HRESULT __stdcall LockServer(BOOL) noexcept final
+    {
+        return S_OK;
+    }
+};
+
+// DAA16D7F-EF66-4FC9-B6F2-3E5B6D924576
+static constexpr GUID MyStringable_clsid{
+    0xdaa16d7f,
+    0xef66,
+    0x4fc9,
+    { 0xb6, 0xf2, 0x3e, 0x5b, 0x6d, 0x92, 0x45, 0x76 }
+};
+
+// EAA16D7F-EF66-4FC9-B6F2-3E5B6D924576
+static constexpr GUID ScratchStringable_clsid{
+    0xeaa16d7f,
+    0xef66,
+    0x4fc9,
+    { 0xb6, 0xf2, 0x3e, 0x5b, 0x6d, 0x92, 0x45, 0x76 }
+};
 
 int main()
 {
     init_apartment();
 
-    DWORD registration{};
+    DWORD registrationMyStringable{};
+    check_hresult(CoRegisterClassObject(MyStringable_clsid,
+                                        make<MyStringableFactory>().get(),
+                                        CLSCTX_LOCAL_SERVER,
+                                        REGCLS_MULTIPLEUSE,
+                                        &registrationMyStringable));
 
-    check_hresult(CoRegisterClassObject(
-        server_clsid,
-        make<Factory>().get(),
-        CLSCTX_LOCAL_SERVER,
-        REGCLS_MULTIPLEUSE,
-        &registration));
+    printf("%d\n", registrationMyStringable);
 
-    printf("%d\n", registration);
+    DWORD registrationScratchStringable{};
+    check_hresult(CoRegisterClassObject(ScratchStringable_clsid,
+                                        make<ScratchStringableFactory>().get(),
+                                        CLSCTX_LOCAL_SERVER,
+                                        REGCLS_MULTIPLEUSE,
+                                        &registrationMyStringable));
+
+    printf("%d\n", registrationScratchStringable);
     puts("Press Enter me when you're done serving.");
     getchar();
 }
