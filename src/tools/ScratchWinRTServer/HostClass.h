a511ab0bc78b31beca49d23d93020301179933eb
Mike Griese <migrie@microsoft.com>
2020-07-15 16:28:35 -0500

    Revert "I was thinking of making the HostClass a COM class by default, but this is horrible. Then, the client wouldn't be able to use it to get the winrt type. We want to use the winrt type in Lists in the client. So this won't work"
    This reverts commit 501c47adb24d90a34c1900714075200a43fccd53.

--- src/tools/ScratchWinRTServer/HostClass.h
+++ src/tools/ScratchWinRTServer/HostClass.h
@@ -1,30 +1,30 @@
 #pragma once
 
-// #include "HostClass.g.h"
+#include "HostClass.g.h"
 #include "IMyComInterface.h"
 
-// namespace winrt::ScratchWinRTServer::implementation
-// {
-struct HostClass : winrt::implements<HostClass, IMyComInterface>
+namespace winrt::ScratchWinRTServer::implementation
 {
-    HostClass(const winrt::guid& g);
-    ~HostClass();
-    void DoTheThing();
+    struct HostClass : HostClassT<HostClass, IMyComInterface>
+    {
+        HostClass(const winrt::guid& g);
+        ~HostClass();
+        void DoTheThing();
 
-    int DoCount();
-    winrt::guid Id();
+        int DoCount();
+        winrt::guid Id();
 
-    HRESULT __stdcall Call() override;
+        HRESULT __stdcall Call() override;
 
-private:
-    int _DoCount{ 0 };
-    winrt::guid _id;
-};
-// }
+    private:
+        int _DoCount{ 0 };
+        winrt::guid _id;
+    };
+}
 
-// namespace winrt::ScratchWinRTServer::factory_implementation
-// {
-//     struct HostClass : HostClassT<HostClass, implementation::HostClass>
-//     {
-//     };
-// }
+namespace winrt::ScratchWinRTServer::factory_implementation
+{
+    struct HostClass : HostClassT<HostClass, implementation::HostClass>
+    {
+    };
+}
