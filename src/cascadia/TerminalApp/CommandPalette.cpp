e8d017c79e22e2c9827c16b6b6f0a3db27b8da4a
Leon Liang <lelian@microsoft.com>
2020-07-17 11:12:36 -0700

    whoops didn't mean for that one line to go in heheheh

--- src/cascadia/TerminalApp/CommandPalette.cpp
+++ src/cascadia/TerminalApp/CommandPalette.cpp
@@ -417,7 +417,7 @@ namespace winrt::TerminalApp::implementation
             {
                 if (orderByTabIndex)
                 {
-                    return false;
+                    return _compareTabIndex(command, other.command);
                 }
                 return !_compareCommandNames(command, other.command);
             }
