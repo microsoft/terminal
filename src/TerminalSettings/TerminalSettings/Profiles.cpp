ac9174fcca46a81845bc794a7e4bc86964454071
Vineeth Thomas Alex <vithoma@microsoft.com>
2020-07-27 11:31:01 -0700

    Added more UI changes

--- src/TerminalSettings/TerminalSettings/Profiles.cpp
+++ src/TerminalSettings/TerminalSettings/Profiles.cpp
@@ -44,4 +44,44 @@ namespace winrt::SettingsControl::implementation
     {
 
     }
+
+    void Profiles::cursorColorPickerConfirmColor_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        //cursorColorPickerButton.Flyout.Hide();
+    }
+
+    void Profiles::cursorColorPickerCancelColor_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        //cursorColorPickerButton.Flyout.Hide();
+    }
+
+    void Profiles::foregroundColorPickerConfirmColor_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        //foregroundColorPickerButton.Flyout.Hide();
+    }
+
+    void Profiles::foregroundColorPickerCancelColor_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        //foregroundColorPickerButton.Flyout.Hide();
+    }
+
+    void Profiles::backgroundColorPickerConfirmColor_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        //backgroundColorPickerButton.Flyout.Hide();
+    }
+
+    void Profiles::backgroundColorPickerCancelColor_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        //backgroundColorPickerButton.Flyout.Hide();
+    }
+
+    void Profiles::selectionBackgroundColorPickerConfirmColor_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        //selectionBackgroundColorPickerButton.Flyout.Hide();
+    }
+
+    void Profiles::selectionBackgroundColorPickerCancelColor_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        //selectionBackgroundColorPickerButton.Flyout.Hide();
+    }
 }
--- src/TerminalSettings/TerminalSettings/Profiles.h
+++ src/TerminalSettings/TerminalSettings/Profiles.h
@@ -15,6 +15,14 @@ namespace winrt::SettingsControl::implementation
         void Name(winrt::hstring const& value);
 
         void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void cursorColorPickerConfirmColor_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void cursorColorPickerCancelColor_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void foregroundColorPickerConfirmColor_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void foregroundColorPickerCancelColor_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void backgroundColorPickerConfirmColor_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void backgroundColorPickerCancelColor_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void selectionBackgroundColorPickerConfirmColor_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void selectionBackgroundColorPickerCancelColor_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
 
     private:
         winrt::hstring m_name;
