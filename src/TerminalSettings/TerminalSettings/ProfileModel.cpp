45ab020f69fc185294c8f7c5f6f0fde7f4ef15bb
Carlos Zamora <carlos.zamora@microsoft.com>
2020-07-29 14:07:41 -0400

    Add databinding to profiles

---
+++ src/TerminalSettings/TerminalSettings/ProfileModel.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "ObjectModel/Profile.h"
+#include "ObjectModel.ProfileModel.g.cpp"
+#include "ProfileModel.h"
+
+namespace winrt::ObjectModel::implementation
+{
+    ProfileModel::ProfileModel()
+    {
+        m_Profile = winrt::make<ObjectModel::implementation::Profile>();
+    }
+    ObjectModel::Profile ProfileModel::Profile()
+    {
+        return m_Profile;
+    }
+}
