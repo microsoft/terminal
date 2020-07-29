eae0474f4b96fb73591c664cc07ef1365d05c596
John Grandle <jograndl@microsoft.com>
2020-07-29 15:18:25 -0700

    Hook the model up

--- src/TerminalSettings/TerminalSettings/ColorSchemes.cpp
+++ src/TerminalSettings/TerminalSettings/ColorSchemes.cpp
@@ -33,4 +33,72 @@ namespace winrt::SettingsControl::implementation
     {
         m_colorSchemeModel.ColorScheme().Background(event.NewColor());
     }
+    void ColorSchemes::Foreground_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().Foreground(event.NewColor());
+    }
+    void ColorSchemes::Black_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().Black(event.NewColor());
+    }
+    void ColorSchemes::BrightBlack_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().BrightBlack(event.NewColor());
+    }
+    void ColorSchemes::Blue_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().Blue(event.NewColor());
+    }
+    void ColorSchemes::BrightBlue_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().BrightBlue(event.NewColor());
+    }
+    void ColorSchemes::Cyan_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().Cyan(event.NewColor());
+    }
+    void ColorSchemes::BrightCyan_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().BrightCyan(event.NewColor());
+    }
+    void ColorSchemes::Green_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().Green(event.NewColor());
+    }
+    void ColorSchemes::BrightGreen_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().BrightGreen(event.NewColor());
+    }
+    void ColorSchemes::Purple_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().Purple(event.NewColor());
+    }
+    void ColorSchemes::BrightPurple_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().BrightPurple(event.NewColor());
+    }
+    void ColorSchemes::Red_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().Red(event.NewColor());
+    }
+    void ColorSchemes::BrightRed_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().BrightRed(event.NewColor());
+    }
+    void ColorSchemes::White_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().White(event.NewColor());
+    }
+    void ColorSchemes::BrightWhite_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().BrightWhite(event.NewColor());
+    }
+    void ColorSchemes::Yellow_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().Yellow(event.NewColor());
+    }
+    void ColorSchemes::BrightYellow_ColorChanged(ColorPicker const&, ColorChangedEventArgs const& event)
+    {
+        m_colorSchemeModel.ColorScheme().BrightYellow(event.NewColor());
+    }
 }
--- src/TerminalSettings/TerminalSettings/ColorSchemes.h
+++ src/TerminalSettings/TerminalSettings/ColorSchemes.h
@@ -11,9 +11,25 @@ namespace winrt::SettingsControl::implementation
         ObjectModel::ColorSchemeModel ColorSchemeModel();
 
         void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
-        void Background_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const& picker, Windows::UI::Xaml::Controls::ColorChangedEventArgs const& event);
+        void Background_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void Foreground_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void Black_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void BrightBlack_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void Blue_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void BrightBlue_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void Cyan_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void BrightCyan_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void Green_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void BrightGreen_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void Purple_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void BrightPurple_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void Red_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void BrightRed_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void White_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void BrightWhite_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void Yellow_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
+        void BrightYellow_ColorChanged(Windows::UI::Xaml::Controls::ColorPicker const&, Windows::UI::Xaml::Controls::ColorChangedEventArgs const&);
 
-        void BackgroundHexValue_Changed(Windows::Foundation::IInspectable const&, Windows::UI::Xaml::Controls::TextChangedEventArgs const& event);
     private:
         ObjectModel::ColorSchemeModel m_colorSchemeModel{ nullptr };
     };
