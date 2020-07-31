2909429ad14e713396dce18d300f6c65bc47eaf2
James Holderness <j4_james@hotmail.com>
2020-07-31 21:40:04 +0100

    Extend the graphics adapter test to cover the crossed-out attribute.

--- src/terminal/adapter/ut_adapter/adapterTest.cpp
+++ src/terminal/adapter/ut_adapter/adapterTest.cpp
@@ -1260,7 +1260,7 @@ public:
     TEST_METHOD(GraphicsSingleTests)
     {
         BEGIN_TEST_METHOD_PROPERTIES()
-            TEST_METHOD_PROPERTY(L"Data:uiGraphicsOptions", L"{0, 1, 2, 4, 7, 8, 22, 24, 27, 28, 30, 31, 32, 33, 34, 35, 36, 37, 39, 40, 41, 42, 43, 44, 45, 46, 47, 49, 53, 55, 90, 91, 92, 93, 94, 95, 96, 97, 100, 101, 102, 103, 104, 105, 106, 107}") // corresponds to options in DispatchTypes::GraphicsOptions
+            TEST_METHOD_PROPERTY(L"Data:uiGraphicsOptions", L"{0, 1, 2, 4, 7, 8, 9, 22, 24, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 39, 40, 41, 42, 43, 44, 45, 46, 47, 49, 53, 55, 90, 91, 92, 93, 94, 95, 96, 97, 100, 101, 102, 103, 104, 105, 106, 107}") // corresponds to options in DispatchTypes::GraphicsOptions
         END_TEST_METHOD_PROPERTIES()
 
         Log::Comment(L"Starting test...");
@@ -1316,6 +1316,12 @@ public:
             _testGetSet->_expectedAttribute = TextAttribute{ 0 };
             _testGetSet->_expectedAttribute.SetInvisible(true);
             break;
+        case DispatchTypes::GraphicsOptions::CrossedOut:
+            Log::Comment(L"Testing graphics 'Crossed Out'");
+            _testGetSet->_attribute = TextAttribute{ 0 };
+            _testGetSet->_expectedAttribute = TextAttribute{ 0 };
+            _testGetSet->_expectedAttribute.SetCrossedOut(true);
+            break;
         case DispatchTypes::GraphicsOptions::NotBoldOrFaint:
             Log::Comment(L"Testing graphics 'No Bold or Faint'");
             _testGetSet->_attribute = TextAttribute{ 0 };
@@ -1342,6 +1348,12 @@ public:
             Log::Comment(L"Testing graphics 'Visible'");
             _testGetSet->_attribute = TextAttribute{ 0 };
             _testGetSet->_attribute.SetInvisible(true);
+            _testGetSet->_expectedAttribute = TextAttribute{ 0 };
+            break;
+        case DispatchTypes::GraphicsOptions::NotCrossedOut:
+            Log::Comment(L"Testing graphics 'Not Crossed Out'");
+            _testGetSet->_attribute = TextAttribute{ 0 };
+            _testGetSet->_attribute.SetCrossedOut(true);
             _testGetSet->_expectedAttribute = TextAttribute{ 0 };
             break;
         case DispatchTypes::GraphicsOptions::ForegroundBlack:
