7f19353f31f43e62997885ba8e5a0f6c3532312e
Michael Niksa <miniksa@microsoft.com>
2020-08-05 15:33:10 -0700

    Use correct name for conhost uia test dll.

--- build/Helix/runtests.cmd
+++ build/Helix/runtests.cmd
@@ -28,7 +28,7 @@ echo %TIME%
 powershell -ExecutionPolicy Bypass .\InstallTestAppDependencies.ps1
 echo %TIME%
 
-set testBinaryCandidates=TextBuffer.Unit.Tests.dll TerminalApp.LocalTests.dll Host.Tests.UIA.dll
+set testBinaryCandidates=TextBuffer.Unit.Tests.dll TerminalApp.LocalTests.dll Conhost.UIA.Tests.dll
 set testBinaries=
 for %%B in (%testBinaryCandidates%) do (
     if exist %%B (
