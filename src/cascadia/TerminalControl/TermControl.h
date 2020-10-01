09cc5f492c4bf8ffcba3caffabe23283e23c8f14
James Holderness <j4_james@hotmail.com>
2020-10-01 02:00:06 +0100

    Add support for the BEL control in Windows Terminal (#7679)
    This commit makes the Windows Terminal play an audible sound when the
    `BEL` control character is output.
    
    The `BEL` control was already being forwarded through conpty, so it was
    just a matter of hooking up the `WarningBell` dispatch method to
    actually play a sound. I've used the `PlaySound` API to output the sound
    configured for the "Critical Stop" system event (aka _SystemHand_),
    since that is the sound used in conhost.
    
    ## Validation
    
    I've manually confirmed that the terminal produces the expected sound
    when executing `echo ^G` in a cmd shell, or `printf "\a"` in a WSL bash
    shell.
    
    References:
    * There is a separate issue (#1608) to deal with configuring the `BEL`
      to trigger visual forms of notification.
    * There is also an issue (#2360) requesting an option to disable the
      `BEL`.
    
    Closes #4046

--- src/cascadia/TerminalControl/TermControl.h
+++ src/cascadia/TerminalControl/TermControl.h
@@ -149,6 +149,7 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
         DECLARE_EVENT_WITH_TYPED_EVENT_HANDLER(CopyToClipboard,     _clipboardCopyHandlers,     TerminalControl::TermControl, TerminalControl::CopyToClipboardEventArgs);
         DECLARE_EVENT_WITH_TYPED_EVENT_HANDLER(OpenHyperlink, _openHyperlinkHandlers, TerminalControl::TermControl, TerminalControl::OpenHyperlinkEventArgs);
 
+        TYPED_EVENT(WarningBell, IInspectable, IInspectable);
         TYPED_EVENT(ConnectionStateChanged, TerminalControl::TermControl, IInspectable);
         TYPED_EVENT(Initialized, TerminalControl::TermControl, Windows::UI::Xaml::RoutedEventArgs);
         TYPED_EVENT(TabColorChanged, IInspectable, IInspectable);
@@ -261,6 +262,7 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
         void _SwapChainScaleChanged(Windows::UI::Xaml::Controls::SwapChainPanel const& sender, Windows::Foundation::IInspectable const& args);
         void _DoResizeUnderLock(const double newWidth, const double newHeight);
         void _RefreshSizeUnderLock();
+        void _TerminalWarningBell();
         void _TerminalTitleChanged(const std::wstring_view& wstr);
         void _TerminalTabColorChanged(const std::optional<til::color> color);
         void _CopyToClipboard(const std::wstring_view& wstr);
