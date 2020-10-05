856463140246d7b437603777d591298c35b7b7f2
Raphael Horber <raphael.horber@gmail.com>
2020-10-05 13:22:44 +0200

    Add new cursor type

--- src/cascadia/TerminalApp/TerminalSettingsSerializationHelpers.h
+++ src/cascadia/TerminalApp/TerminalSettingsSerializationHelpers.h
@@ -23,12 +23,13 @@ Abstract:
 
 JSON_ENUM_MAPPER(::winrt::Microsoft::Terminal::TerminalControl::CursorStyle)
 {
-    static constexpr std::array<pair_type, 5> mappings = {
+    static constexpr std::array<pair_type, 6> mappings = {
         pair_type{ "bar", ValueType::Bar },
         pair_type{ "vintage", ValueType::Vintage },
         pair_type{ "underscore", ValueType::Underscore },
         pair_type{ "filledBox", ValueType::FilledBox },
-        pair_type{ "emptyBox", ValueType::EmptyBox }
+        pair_type{ "emptyBox", ValueType::EmptyBox },
+        pair_type{ "doubleUnderscore", ValueType::DoubleUnderscore }
     };
 };
 
