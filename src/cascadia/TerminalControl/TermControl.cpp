7d0961cdd57eab5eb8d92e8075bb4215a0d3de72
Nick Bennett <bennett.a.nicholas@gmail.com>
2020-10-07 09:30:58 -0600

    fixed whitespacing

--- src/cascadia/TerminalControl/TermControl.cpp
+++ src/cascadia/TerminalControl/TermControl.cpp
@@ -458,12 +458,14 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
                 SystemParametersInfo(SPI_GETDESKWALLPAPER, MAX_PATH, desktopImage, SPIF_UPDATEINIFILE);
                 _settings.BackgroundImage(desktopImage);
             }
+
             Windows::Foundation::Uri imageUri{ _settings.BackgroundImage() };
 
             // Check if the image brush is already pointing to the image
             // in the modified settings; if it isn't (or isn't there),
             // set a new image source for the brush
             auto imageSource = BackgroundImage().Source().try_as<Media::Imaging::BitmapImage>();
+
             if (imageSource == nullptr ||
                 imageSource.UriSource() == nullptr ||
                 imageSource.UriSource().RawUri() != imageUri.RawUri())
@@ -474,7 +476,6 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
                 // internet.
                 Media::Imaging::BitmapImage image(imageUri);
                 BackgroundImage().Source(image);
-                
             }
             
             // Apply stretch, opacity and alignment settings
@@ -487,8 +488,6 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
         {
             BackgroundImage().Source(nullptr);
         }
-
-        
     }
 
     // Method Description:
