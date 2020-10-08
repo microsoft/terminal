d4231ac09b0612d7151adaa9ee1dbeb87ec760c6
Carlos Zamora <cazamor@microsoft.com>
2020-10-08 10:46:57 -0700

    remove const_cast

--- src/cascadia/inc/cppwinrt_utils.h
+++ src/cascadia/inc/cppwinrt_utils.h
@@ -133,7 +133,7 @@ public:                                                                         
     {                                                                                  \
         if (_##name != value)                                                          \
         {                                                                              \
-            const_cast<type&>(_##name) = value;                                        \
+            _##name = value;                                                           \
             event(*this, Windows::UI::Xaml::Data::PropertyChangedEventArgs{ L#name }); \
         }                                                                              \
     };                                                                                 \
