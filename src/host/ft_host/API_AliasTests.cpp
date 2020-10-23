93b95420ac02149fa776af308963ecde1e36d807
Michael Niksa <miniksa@microsoft.com>
2020-10-23 10:45:39 -0700

    Alias tests.

--- src/host/ft_host/API_AliasTests.cpp
+++ src/host/ft_host/API_AliasTests.cpp
@@ -3,6 +3,8 @@
 
 #include "precomp.h"
 
+#include <future>
+
 // This particular block is necessary so we can include both a UNICODE and non-UNICODE version
 // of our test supporting function so we can accurately portray and measure both types of text
 // and test both versions of the console API.
@@ -47,6 +49,23 @@ class AliasTests
         TEST_METHOD_PROPERTY(L"Data:bUnicode", L"{FALSE, TRUE}")
         TEST_METHOD_PROPERTY(L"Data:bSetFirst", L"{FALSE, TRUE}")
     END_TEST_METHOD()
+
+    BEGIN_TEST_METHOD(TestCookedAliasProcessing)
+        TEST_METHOD_PROPERTY(L"TestTimeout", L"00:01:00")
+    END_TEST_METHOD()
+
+    BEGIN_TEST_METHOD(TestCookedTextEntry)
+        TEST_METHOD_PROPERTY(L"TestTimeout", L"00:01:00")
+    END_TEST_METHOD()
+
+    BEGIN_TEST_METHOD(TestCookedAlphaPermutations)
+        //TEST_METHOD_PROPERTY(L"TestTimeout", L"00:01:00")
+        TEST_METHOD_PROPERTY(L"Data:inputcp", L"{437, 932}")
+        TEST_METHOD_PROPERTY(L"Data:outputcp", L"{437, 932}")
+        TEST_METHOD_PROPERTY(L"Data:inputmode", L"{487, 481}") // 487 is 0x1e7, 485 is 0x1e1 (ENABLE_LINE_INPUT on/off)
+        TEST_METHOD_PROPERTY(L"Data:outputmode", L"{7}")
+    END_TEST_METHOD()
+        
 };
 
 // Caller must free ppsz if not null.
@@ -142,4 +161,232 @@ void AliasTests::TestGetConsoleAlias()
                                    bUnicode,
                                    bSetFirst);
     }
+}
+
+static std::vector<INPUT_RECORD> _stringToInputs(std::wstring_view wstr)
+{
+    std::vector<INPUT_RECORD> result;
+    for (const auto& wch : wstr)
+    {
+        INPUT_RECORD ir = { 0 };
+        ir.EventType = KEY_EVENT;
+        ir.Event.KeyEvent.bKeyDown = TRUE;
+        ir.Event.KeyEvent.dwControlKeyState = 0;
+        ir.Event.KeyEvent.uChar.UnicodeChar = wch;
+        ir.Event.KeyEvent.wRepeatCount = 1;
+        ir.Event.KeyEvent.wVirtualKeyCode = VkKeyScanW(wch);
+        ir.Event.KeyEvent.wVirtualScanCode = gsl::narrow<WORD>(MapVirtualKeyW(ir.Event.KeyEvent.wVirtualKeyCode, MAPVK_VK_TO_VSC));
+
+        result.emplace_back(ir);
+
+        ir.Event.KeyEvent.bKeyDown = FALSE;
+
+        result.emplace_back(ir);
+    }
+    return result;
+}
+
+void AliasTests::TestCookedAliasProcessing()
+{
+    const auto in = GetStdInputHandle();
+
+    DWORD originalInMode = 0;
+    VERIFY_WIN32_BOOL_SUCCEEDED(GetConsoleMode(in, &originalInMode));
+
+    DWORD originalCodepage = GetConsoleCP();
+
+    auto restoreInModeOnExit = wil::scope_exit([&]
+    {
+        SetConsoleMode(in, originalInMode);
+        SetConsoleCP(originalCodepage);
+    });
+
+    const DWORD testInMode = ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT;
+    VERIFY_WIN32_BOOL_SUCCEEDED(SetConsoleMode(in, testInMode));
+
+    auto modulePath = wil::GetModuleFileNameW<std::wstring>(nullptr);
+    std::filesystem::path path{ modulePath };
+    auto fileName = path.filename();
+    auto exeName = fileName.wstring();
+
+    VERIFY_WIN32_BOOL_SUCCEEDED(AddConsoleAliasW(L"foo", L"echo bar$Techo baz$Techo bam", exeName.data()));
+
+    std::wstring commandWritten = L"foo\r\n";
+    std::queue<std::string> commandExpected;
+    commandExpected.push("echo bar\r");
+    commandExpected.push("echo baz\r");
+    commandExpected.push("echo bam\r");
+
+    auto inputs = _stringToInputs(commandWritten);
+
+    DWORD written = 0;
+    VERIFY_WIN32_BOOL_SUCCEEDED(WriteConsoleInputW(in, inputs.data(), gsl::narrow<DWORD>(inputs.size()), &written));
+
+    std::string buf;
+    buf.resize(500);
+
+    while (!commandExpected.empty())
+    {
+        DWORD read = 0;
+
+        auto tryRead = std::async(std::launch::async, [&] {
+            VERIFY_WIN32_BOOL_SUCCEEDED(ReadConsoleA(in, buf.data(), gsl::narrow<DWORD>(buf.size()), &read, nullptr));
+        });
+        
+        if (std::future_status::ready != tryRead.wait_for(std::chrono::seconds{ 5 }))
+        {
+            // Shove something into the input to unstick it then fail.
+            auto events = _stringToInputs(L"a\r\n");
+            WriteConsoleInputW(in, events.data(), gsl::narrow<DWORD>(events.size()), &written);
+            VERIFY_FAILED(HRESULT_FROM_NT(STATUS_TIMEOUT));
+
+            // If somehow this still isn't enough to unstick the thread, the whole test timeout is 1 min
+            // set in the parameters/metadata at the top.
+
+            return;
+        }
+
+        auto actual = std::string{ buf.data(), read };
+
+        auto expected = commandExpected.front();
+        commandExpected.pop();
+
+        VERIFY_ARE_EQUAL(expected, actual);
+    }
+}
+
+void AliasTests::TestCookedTextEntry()
+{
+    const auto in = GetStdInputHandle();
+
+    DWORD originalInMode = 0;
+    VERIFY_WIN32_BOOL_SUCCEEDED(GetConsoleMode(in, &originalInMode));
+
+    DWORD originalCodepage = GetConsoleCP();
+
+    auto restoreInModeOnExit = wil::scope_exit([&] {
+        SetConsoleMode(in, originalInMode);
+        SetConsoleCP(originalCodepage);
+    });
+
+    const DWORD testInMode = ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT;
+    VERIFY_WIN32_BOOL_SUCCEEDED(SetConsoleMode(in, testInMode));
+
+    auto modulePath = wil::GetModuleFileNameW<std::wstring>(nullptr);
+    std::filesystem::path path{ modulePath };
+    auto fileName = path.filename();
+    auto exeName = fileName.wstring();
+
+    
+    std::wstring commandWritten = L"foo\r\n";
+    std::queue<std::string> commandExpected;
+    commandExpected.push("foo\r\n");
+
+    auto inputs = _stringToInputs(commandWritten);
+
+    DWORD written = 0;
+    VERIFY_WIN32_BOOL_SUCCEEDED(WriteConsoleInputW(in, inputs.data(), gsl::narrow<DWORD>(inputs.size()), &written));
+
+    std::string buf;
+    buf.resize(500);
+
+    while (!commandExpected.empty())
+    {
+        DWORD read = 0;
+
+        auto tryRead = std::async(std::launch::async, [&] {
+            VERIFY_WIN32_BOOL_SUCCEEDED(ReadConsoleA(in, buf.data(), gsl::narrow<DWORD>(buf.size()), &read, nullptr));
+        });
+
+        if (std::future_status::ready != tryRead.wait_for(std::chrono::seconds{ 5 }))
+        {
+            // Shove something into the input to unstick it then fail.
+            auto events = _stringToInputs(L"a\r\n");
+            WriteConsoleInputW(in, events.data(), gsl::narrow<DWORD>(events.size()), &written);
+            VERIFY_FAILED(HRESULT_FROM_NT(STATUS_TIMEOUT));
+
+            // If somehow this still isn't enough to unstick the thread, the whole test timeout is 1 min
+            // set in the parameters/metadata at the top.
+
+            return;
+        }
+
+        auto actual = std::string{ buf.data(), read };
+
+        auto expected = commandExpected.front();
+        commandExpected.pop();
+
+        VERIFY_ARE_EQUAL(expected, actual);
+    }
+}
+
+// tests todo:
+// - test alpha in/out in 437/932 permutations
+// - test leftover leadbyte/trailbyte when buffer too small
+void AliasTests::TestCookedAlphaPermutations()
+{
+    DWORD inputcp, outputcp, inputmode, outputmode;
+
+    VERIFY_SUCCEEDED_RETURN(TestData::TryGetValue(L"inputcp", inputcp), L"Get input cp");
+    VERIFY_SUCCEEDED_RETURN(TestData::TryGetValue(L"outputcp", outputcp), L"Get output cp");
+    VERIFY_SUCCEEDED_RETURN(TestData::TryGetValue(L"inputmode", inputmode), L"Get input mode");
+    VERIFY_SUCCEEDED_RETURN(TestData::TryGetValue(L"outputmode", outputmode), L"Get output mode");
+
+    const auto in = GetStdInputHandle();
+    const auto out = GetStdOutputHandle();
+
+    Log::Comment(L"Backup original modes and codepages.");
+
+    DWORD originalInMode, originalOutMode, originalInputCP, originalOutputCP;
+    
+    VERIFY_WIN32_BOOL_SUCCEEDED(GetConsoleMode(in, &originalInMode));
+    VERIFY_WIN32_BOOL_SUCCEEDED(GetConsoleMode(out, &originalOutMode));
+    originalInputCP = GetConsoleCP();
+    originalOutputCP = GetConsoleOutputCP();
+
+    auto restoreModesOnExit = wil::scope_exit([&]
+    {
+        SetConsoleMode(in, originalInMode);
+        SetConsoleMode(out, originalOutMode);
+        SetConsoleCP(originalInputCP);
+        SetConsoleOutputCP(originalOutputCP);
+    });
+
+    Log::Comment(L"Apply our modes and codepages.");
+
+    VERIFY_WIN32_BOOL_SUCCEEDED(SetConsoleMode(in, inputmode));
+    VERIFY_WIN32_BOOL_SUCCEEDED(SetConsoleMode(out, outputmode));
+    VERIFY_WIN32_BOOL_SUCCEEDED(SetConsoleCP(inputcp));
+    VERIFY_WIN32_BOOL_SUCCEEDED(SetConsoleOutputCP(outputcp));
+
+    const wchar_t alpha = L'\u03b1';
+    const std::string alpha437 = "\xe0";
+    const std::string alpha932 = "\x83\xbf";
+
+    std::string expected = inputcp == 932 ? alpha932 : alpha437;
+
+    std::wstring sendInput;
+    sendInput.append(&alpha, 1);
+
+    // If we're in line input, we have to send a newline and we'll get one back.
+    if (WI_IsFlagSet(inputmode, ENABLE_LINE_INPUT))
+    {
+        expected.append("\r\n");
+        sendInput.append(L"\r\n");
+    }
+
+    Log::Comment(L"send the string");
+    auto sendInputRecords = _stringToInputs(sendInput);
+    DWORD written;
+    VERIFY_WIN32_BOOL_SUCCEEDED(WriteConsoleInputW(in, sendInputRecords.data(), gsl::narrow<DWORD>(sendInputRecords.size()), &written));
+
+    Log::Comment(L"receive the string");
+    std::string recvInput;
+    recvInput.resize(500); // excessively big
+    DWORD read;
+    VERIFY_WIN32_BOOL_SUCCEEDED(ReadConsoleA(in, recvInput.data(), gsl::narrow<DWORD>(recvInput.size()), &read, nullptr));
+
+    recvInput.resize(read);
+
+    VERIFY_ARE_EQUAL(expected, recvInput);
 }
