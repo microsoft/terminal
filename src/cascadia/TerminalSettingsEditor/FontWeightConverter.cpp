e124621d351b26d8c9bd02a3502af224163de3fd
cinnamon-msft <cinnamon@microsoft.com>
2020-10-29 00:21:29 -0700

    this isn't going well

--- src/cascadia/TerminalSettingsEditor/FontWeightConverter.cpp
+++ src/cascadia/TerminalSettingsEditor/FontWeightConverter.cpp
@@ -16,59 +16,62 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
                                                           hstring const& /* language */)
     {
         Windows::UI::Text::FontWeight weight;
-        std::string text = unbox_value<std::string>(value);
+        hstring text = unbox_value<hstring>(value);
+
+        text.c_str();
+        std::transform(text.begin(), text.end(), text.begin(), [](auto c) { return static_cast<char>(std::tolower(c)); });
+        if (text == L"extrablack")
+        {
+            weight.Weight = 950;
+        }
+        else if (text == L"black")
+        {
+            weight.Weight = 900;
+        }
+        else if (text == L"extrabold")
+        {
+            weight.Weight = 800;
+        }
+        else if (text == L"bold")
+        {
+            weight.Weight = 700;
+        }
+        else if (text == L"semibold")
+        {
+            weight.Weight = 600;
+        }
+        else if (text == L"medium")
+        {
+            weight.Weight = 500;
+        }
+        else if (text == L"normal")
+        {
+            weight.Weight = 400;
+        }
+        else if (text == L"semilight")
+        {
+            weight.Weight = 350;
+        }
+        else if (text == L"light")
+        {
+            weight.Weight = 300;
+        }
+        else if (text == L"extralight")
+        {
+            weight.Weight = 200;
+        }
+        else if (text == L"thin")
+        {
+            weight.Weight = 100;
+        }
+
         try
         {
-            std::transform(text.begin(), text.end(), text.begin(), [](auto c) { return static_cast<char>(std::tolower(c)); });
-            if (text == "extrablack")
-            {
-                weight.Weight = 950;
-            } 
-            else if (text == "black")
-            {
-                weight.Weight = 900;
-            }
-            else if (text == "extrabold")
-            {
-                weight.Weight = 800;
-            }
-            else if (text == "bold")
-            {
-                weight.Weight = 700;
-            }
-            else if (text == "semibold")
-            {
-                weight.Weight = 600;
-            }
-            else if (text == "medium")
-            {
-                weight.Weight = 500;
-            }
-            else if (text == "normal")
-            {
-                weight.Weight = 400;
-            }
-            else if (text == "semilight")
-            {
-                weight.Weight = 350;
-            }
-            else if (text == "light")
-            {
-                weight.Weight = 300;
-            }
-            else if (text == "extralight")
-            {
-                weight.Weight = 200;
-            }
-            else if (text == "thin")
-            {
-                weight.Weight = 100;
-            }           
+            weight.Weight = static_cast<uint16_t>((std::stoi(text.c_str())));             
         }
         catch (...)
         {
-            std::stoi(text);
-            weight.Weight = static_cast<uint16_t>(winrt::unbox_value<UINT16>(value));
+            weight.Weight = 400;
         }
         
         return winrt::box_value(weight);
