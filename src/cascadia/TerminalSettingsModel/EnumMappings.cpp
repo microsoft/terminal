eb810b47c3a988df28e6aad28aa616449fe3e50b
Carlos Zamora <cazamor@microsoft.com>
2020-10-28 21:14:15 -0700

    Add remaining EnumMappings

--- src/cascadia/TerminalSettingsModel/EnumMappings.cpp
+++ src/cascadia/TerminalSettingsModel/EnumMappings.cpp
@@ -13,15 +13,38 @@ using namespace winrt;
 using namespace winrt::Windows::Foundation::Collections;
 using namespace ::Microsoft::Terminal::Settings::Model;
 
+#define DEFINE_ENUM_MAP(type, name)                                                \
+    IMap<hstring, type> EnumMappings::name()                                       \
+    {                                                                              \
+        auto map = single_threaded_map<hstring, type>();                           \
+        for (auto [enumStr, enumVal] : JsonUtils::ConversionTrait<type>::mappings) \
+        {                                                                          \
+            map.Insert(winrt::to_hstring(enumStr), enumVal);                       \
+        }                                                                          \
+        return map;                                                                \
+    }
+
 namespace winrt::Microsoft::Terminal::Settings::Model::implementation
 {
-    IMap<hstring, winrt::Windows::UI::Xaml::ElementTheme> EnumMappings::ElementTheme()
+    // Global Settings
+    DEFINE_ENUM_MAP(winrt::Windows::UI::Xaml::ElementTheme, ElementTheme);
+    DEFINE_ENUM_MAP(winrt::Microsoft::UI::Xaml::Controls::TabViewWidthMode, TabWidthMode);
+    DEFINE_ENUM_MAP(Model::LaunchMode, LaunchMode);
+
+    // Profile Settings
+    DEFINE_ENUM_MAP(Model::CloseOnExitMode, CloseOnExit);
+    DEFINE_ENUM_MAP(Microsoft::Terminal::TerminalControl::ScrollbarState, ScrollState);
+    IMap<hstring, uint32_t> EnumMappings::FontWeight()
     {
-        auto map = single_threaded_map<hstring, winrt::Windows::UI::Xaml::ElementTheme>();
-        for (auto [enumStr, enumVal] : JsonUtils::ConversionTrait<winrt::Windows::UI::Xaml::ElementTheme>::mappings)
+        auto map = single_threaded_map<hstring, uint32_t>();
+        for (auto [enumStr, enumVal] : JsonUtils::ConversionTrait<Windows::UI::Text::FontWeight>::mappings)
         {
             map.Insert(winrt::to_hstring(enumStr), enumVal);
         }
         return map;
     }
+    DEFINE_ENUM_MAP(Windows::UI::Xaml::Media::Stretch, BackgroundImageStretchMode);
+    DEFINE_ENUM_MAP(Microsoft::Terminal::TerminalControl::TextAntialiasingMode, AntialiasingMode);
+    DEFINE_ENUM_MAP(Microsoft::Terminal::TerminalControl::CursorStyle, CursorShape);
+    DEFINE_ENUM_MAP(Model::BellStyle, BellStyle);
 }
--- src/cascadia/TerminalSettingsModel/EnumMappings.h
+++ src/cascadia/TerminalSettingsModel/EnumMappings.h
@@ -11,7 +11,20 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
     {
     public:
         EnumMappings() = default;
+
+        // Global Settings
         static winrt::Windows::Foundation::Collections::IMap<winrt::hstring, winrt::Windows::UI::Xaml::ElementTheme> ElementTheme();
+        static winrt::Windows::Foundation::Collections::IMap<winrt::hstring, winrt::Microsoft::UI::Xaml::Controls::TabViewWidthMode> TabWidthMode();
+        static winrt::Windows::Foundation::Collections::IMap<winrt::hstring, LaunchMode> LaunchMode();
+
+        // Profile Settings
+        static winrt::Windows::Foundation::Collections::IMap<winrt::hstring, CloseOnExitMode> CloseOnExit();
+        static winrt::Windows::Foundation::Collections::IMap<winrt::hstring, Microsoft::Terminal::TerminalControl::ScrollbarState> ScrollState();
+        static winrt::Windows::Foundation::Collections::IMap<winrt::hstring, uint32_t> FontWeight();
+        static winrt::Windows::Foundation::Collections::IMap<winrt::hstring, winrt::Windows::UI::Xaml::Media::Stretch> BackgroundImageStretchMode();
+        static winrt::Windows::Foundation::Collections::IMap<winrt::hstring, winrt::Microsoft::Terminal::TerminalControl::TextAntialiasingMode> AntialiasingMode();
+        static winrt::Windows::Foundation::Collections::IMap<winrt::hstring, winrt::Microsoft::Terminal::TerminalControl::CursorStyle> CursorShape();
+        static winrt::Windows::Foundation::Collections::IMap<winrt::hstring, BellStyle> BellStyle();
     };
 }
 
