dc7eaab58266d003d87a6dd8b47203e60b7fbbc2
Carlos Zamora <cazamor@microsoft.com>
2020-10-28 21:57:04 -0700

    hook up Launch page

--- src/cascadia/TerminalSettingsEditor/Launch.cpp
+++ src/cascadia/TerminalSettingsEditor/Launch.cpp
@@ -2,23 +2,58 @@
 // Licensed under the MIT license.
 
 #include "pch.h"
+#include "Utils.h"
 #include "Launch.h"
 #include "Launch.g.cpp"
 #include "MainPage.h"
+#include "EnumEntry.h"
 
 using namespace winrt;
 using namespace winrt::Windows::UI::Xaml;
+using namespace winrt::Windows::UI::Xaml::Controls;
 using namespace winrt::Microsoft::Terminal::Settings::Model;
+using namespace winrt::Windows::Foundation::Collections;
 
 namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
 {
     Launch::Launch()
     {
         InitializeComponent();
+
+        auto launchSizeMap = EnumMappings::LaunchMode();
+        for (auto [key, value] : launchSizeMap)
+        {
+            auto enumName = LocalizedNameForEnumName(L"Globals_LaunchSize", key, L"Content");
+            auto entry = winrt::make<EnumEntry>(enumName, winrt::box_value<LaunchMode>(value));
+            _LaunchSizes.Append(entry);
+
+            // Initialize the selected item to be our current setting
+            if (value == GlobalSettings().LaunchMode())
+            {
+                LaunchSizeButtons().SelectedItem(entry);
+            }
+        }
     }
 
     GlobalAppSettings Launch::GlobalSettings()
     {
         return MainPage::Settings().GlobalSettings();
+    }
+
+    IObservableVector<Editor::EnumEntry> Launch::LaunchSizes()
+    {
+        return _LaunchSizes;
+    }
+
+    void Launch::LaunchSizeSelected(IInspectable const& /*sender*/,
+                                    SelectionChangedEventArgs const& args)
+    {
+        if (args.AddedItems().Size() > 0)
+        {
+            if (auto item = args.AddedItems().GetAt(0).try_as<EnumEntry>())
+            {
+                GlobalSettings().LaunchMode(winrt::unbox_value<LaunchMode>(item->EnumValue()));
+            }
+        }
     }
 }
--- src/cascadia/TerminalSettingsEditor/Launch.h
+++ src/cascadia/TerminalSettingsEditor/Launch.h
@@ -10,8 +10,15 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
 {
     struct Launch : LaunchT<Launch>
     {
+    public:
         Launch();
         winrt::Microsoft::Terminal::Settings::Model::GlobalAppSettings GlobalSettings();
+        winrt::Windows::Foundation::Collections::IObservableVector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> LaunchSizes();
+
+        void LaunchSizeSelected(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::Controls::SelectionChangedEventArgs const& args);
+
+    private:
+        winrt::Windows::Foundation::Collections::IObservableVector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> _LaunchSizes;
     };
 }
 
