16a9cae0fe390864701053c9f119a750e6888d65
Carlos Zamora <cazamor@microsoft.com>
2020-11-03 11:40:53 -0800

    data templates are magic

--- src/cascadia/TerminalSettingsEditor/Launch.h
+++ src/cascadia/TerminalSettingsEditor/Launch.h
@@ -16,15 +16,6 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         void DefaultProfileSelectionChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::Controls::SelectionChangedEventArgs const& args);
 
         winrt::Microsoft::Terminal::Settings::Model::GlobalAppSettings GlobalSettings();
-
-        Windows::Foundation::Collections::IObservableVector<hstring> ProfileNameList();
-
-        WINRT_CALLBACK(PropertyChanged, Windows::UI::Xaml::Data::PropertyChangedEventHandler);
-        OBSERVABLE_GETSET_PROPERTY(Model::Profile, CurrentDefaultProfile, _PropertyChangedHandlers, nullptr);
-
-    private:
-        Windows::Foundation::Collections::IObservableVector<hstring> _profileNameList;
-        void _UpdateProfileNameList(Windows::Foundation::Collections::IObservableVector<Model::Profile> profiles);
     };
 }
 
