39c6bad884289e9a2d89328fd5ce227358b7735a
Pankaj Bhojwani <pabhojwa@microsoft.com>
2020-11-16 15:14:52 -0500

    mock term settings

--- src/cascadia/UnitTests_TerminalCore/MockTermSettings.h
+++ src/cascadia/UnitTests_TerminalCore/MockTermSettings.h
@@ -30,6 +30,7 @@ namespace TerminalCoreUnitTests
         uint32_t DefaultBackground() { return COLOR_BLACK; }
         bool SnapOnInput() { return false; }
         bool AltGrAliasing() { return true; }
+        bool DetectPatterns() { return true; }
         uint32_t CursorColor() { return COLOR_WHITE; }
         CursorStyle CursorShape() const noexcept { return CursorStyle::Vintage; }
         uint32_t CursorHeight() { return 42UL; }
@@ -51,6 +52,7 @@ namespace TerminalCoreUnitTests
         void DefaultBackground(uint32_t) {}
         void SnapOnInput(bool) {}
         void AltGrAliasing(bool) {}
+        void DetectPatterns(bool) {}
         void CursorColor(uint32_t) {}
         void CursorShape(CursorStyle const&) noexcept {}
         void CursorHeight(uint32_t) {}
