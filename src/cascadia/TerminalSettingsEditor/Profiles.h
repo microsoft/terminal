1d6f057eadf85173079e4774773282afd11f6544
Carlos Zamora <cazamor@microsoft.com>
2020-11-17 12:13:09 -0800

    bind BI Alignment to ComboBox (bugged)

--- src/cascadia/TerminalSettingsEditor/Profiles.h
+++ src/cascadia/TerminalSettingsEditor/Profiles.h
@@ -41,10 +41,38 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         GETSET_PROPERTY(Editor::ProfilePageNavigationState, State, nullptr);
         GETSET_BINDABLE_ENUM_SETTING(CursorShape, winrt::Microsoft::Terminal::TerminalControl::CursorStyle, State().Profile, CursorShape);
         GETSET_BINDABLE_ENUM_SETTING(BackgroundImageStretchMode, winrt::Windows::UI::Xaml::Media::Stretch, State().Profile, BackgroundImageStretchMode);
+        //GETSET_BINDABLE_ENUM_SETTING(BackgroundImageAlignment, winrt::Microsoft::Terminal::Settings::Model::Alignment, State().Profile, BackgroundImageAlignment);
         GETSET_BINDABLE_ENUM_SETTING(AntiAliasingMode, winrt::Microsoft::Terminal::TerminalControl::TextAntialiasingMode, State().Profile, AntialiasingMode);
         GETSET_BINDABLE_ENUM_SETTING(CloseOnExitMode, winrt::Microsoft::Terminal::Settings::Model::CloseOnExitMode, State().Profile, CloseOnExit);
         GETSET_BINDABLE_ENUM_SETTING(BellStyle, winrt::Microsoft::Terminal::Settings::Model::BellStyle, State().Profile, BellStyle);
         GETSET_BINDABLE_ENUM_SETTING(ScrollState, winrt::Microsoft::Terminal::TerminalControl::ScrollbarState, State().Profile, ScrollState);
+
+    public:
+        winrt::Windows::Foundation::Collections::IObservableVector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> BackgroundImageAlignmentList()
+        {
+            return _BackgroundImageAlignmentList;
+        }
+
+        winrt::Windows::Foundation::IInspectable CurrentBackgroundImageAlignment()
+        {
+            const auto val{ State().Profile().BackgroundImageAlignment() };
+            const auto res{ _BackgroundImageAlignmentMap.Lookup(val) };
+            return winrt::box_value<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>(res);
+            //return winrt::box_value<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>(_BackgroundImageAlignmentMap.Lookup(State().Profile().BackgroundImageAlignment()));
+        }
+
+        void CurrentBackgroundImageAlignment(const winrt::Windows::Foundation::IInspectable& enumEntry)
+        {
+            if (auto ee = enumEntry.try_as<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>())
+            {
+                auto setting = winrt::unbox_value<Model::Alignment>(ee.EnumValue());
+                State().Profile().BackgroundImageAlignment(setting);
+            }
+        }
+
+    private:
+        winrt::Windows::Foundation::Collections::IObservableVector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> _BackgroundImageAlignmentList;
+        winrt::Windows::Foundation::Collections::IMap<Model::Alignment, winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> _BackgroundImageAlignmentMap;
     };
 }
 
