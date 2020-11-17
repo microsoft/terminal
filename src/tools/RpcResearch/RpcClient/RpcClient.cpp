ef34be6518f816572ac717b22612e7ee407076d5
Mike Griese <migrie@microsoft.com>
2020-11-16 21:37:00 -0600

    these are some big brain plays right here

--- src/tools/RpcResearch/RpcClient/RpcClient.cpp
+++ src/tools/RpcResearch/RpcClient/RpcClient.cpp
@@ -9,7 +9,16 @@
 #include <wrl.h>
 using namespace Microsoft::WRL;
 
-const CLSID CLSID_CalculatorComponent = { 0xE68F5EDD, 0x6257, 0x4E72, 0xA1, 0x0B, 0x40, 0x67, 0xED, 0x8E, 0x85, 0xF2 };
+// const CLSID CLSID_CalculatorComponent = { 0xE68F5EDD, 0x6257, 0x4E72, 0xA1, 0x0B, 0x40, 0x67, 0xED, 0x8E, 0x85, 0xF2 };
+// const CLSID CLSID_WindowBroker = { 0xc4e46d11, 0xdd74, 0x43e8, 0xA1, 0x0B, 0x40, 0x67, 0xED, 0x8E, 0x85, 0xF2 };
+// const auto CLSID_CalculatorComponent = uuid("E68F5EDD-6257-4E72-A10B-4067ED8E85F2");
+//
+// HAX: There's no good GUID literal in C++, because we've abandoned this
+// platform for the last decade. So this big-brain move will fwdecl a
+// placeholder implementation class, that we can use to get the __uuidof,
+// letting us define the UUID in a sensible format.:
+class __declspec(uuid("E68F5EDD-6257-4E72-A10B-4067ED8E85F2")) CalculatorComponentImpl;
+class __declspec(uuid("c4e46d11-dd74-43e8-a4b9-d0f789ad3751")) WindowBrokerImpl;
 
 void main()
 {
@@ -19,7 +28,8 @@ void main()
     printf("CoInitialize -> %d\n", hr);
 
     ComPtr<ICalculatorComponent> calc; // Interface to COM component.
-    hr = CoCreateInstance(CLSID_CalculatorComponent,
+    // hr = CoCreateInstance(CLSID_CalculatorComponent,
+    hr = CoCreateInstance(__uuidof(CalculatorComponentImpl),
                           // auto id = __uuidof(ICalculatorComponent);
                           // id;
                           // hr = CoCreateInstance(__uuidof(ICalculatorComponent),
