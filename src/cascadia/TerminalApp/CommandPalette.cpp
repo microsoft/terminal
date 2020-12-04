96da7f8b688213faee78fe205c9cb8d4ad9be23b
nogunumo <yukawa_hidenori@icloud.com>
2020-12-04 17:50:39 +0900

    delete the line from last the PR(8194)

--- src/cascadia/TerminalApp/CommandPalette.cpp
+++ src/cascadia/TerminalApp/CommandPalette.cpp
@@ -290,8 +290,7 @@ namespace winrt::TerminalApp::implementation
                                          Windows::UI::Xaml::Input::KeyRoutedEventArgs const& e)
     {
         auto key = e.OriginalKey();
-        auto const ctrlDown = WI_IsFlagSet(CoreWindow::GetForCurrentThread().GetKeyState(winrt::Windows::System::VirtualKey::Control), CoreVirtualKeyStates::Down);
-
+        
         if (key == VirtualKey::Up)
         {
             // Action Mode: Move focus to the next item in the list.
