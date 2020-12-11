3e2b94334d5b43283e49fce71d4c4f76741a1e36
Dustin Howett <duhowett@microsoft.com>
2020-12-11 13:34:21 -0800

    Introduce the Terminal Settings Editor (#8048)
    This commit introduces the terminal settings editor (to wit: the
    Settings UI) as a standalone project. This project, and this commit, is
    the result of two and a half months of work.
    
    TSE started as a hackathon project in the Microsoft 2020 Hackathon, and
    from there it's grown to be a bona-fide graphical settings editor.
    
    There is a lot of xaml data binding in here, a number of views and a
    number of view models, and a bunch of paradigms that we've been
    reviewing and testing out and designing and refining.
    
    Specified in #6720, #8269
    Follow-up work in #6800
    Closes #1564
    Closes #8048 (PR)
    
    Co-authored-by: Carlos Zamora <carlos.zamora@microsoft.com>
    Co-authored-by: Kayla Cinnamon <cinnamon@microsoft.com>
    Co-authored-by: Alberto Medina Gutierrez <almedina@microsoft.com>
    Co-authored-by: John Grandle <jograndl@microsoft.com>
    Co-authored-by: xerootg <xerootg@users.noreply.github.com>
    Co-authored-by: Scott <sarmiger1@gmail.com>
    Co-authored-by: Vineeth Thomas Alex <vineeththomasalex@gmail.com>
    Co-authored-by: Leon Liang <lelian@microsoft.com>
    Co-authored-by: Dustin L. Howett <duhowett@microsoft.com>
    Signed-off-by: Dustin L. Howett <duhowett@microsoft.com>

---
+++ src/cascadia/TerminalSettingsEditor/Launch.h
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#pragma once
+
+#include "Launch.g.h"
+#include "LaunchPageNavigationState.g.h"
+#include "Utils.h"
+
+namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
+{
+    struct LaunchPageNavigationState : LaunchPageNavigationStateT<LaunchPageNavigationState>
+    {
+    public:
+        LaunchPageNavigationState(const Model::CascadiaSettings& settings) :
+            _Settings{ settings } {}
+
+        GETSET_PROPERTY(Model::CascadiaSettings, Settings, nullptr)
+    };
+
+    struct Launch : LaunchT<Launch>
+    {
+    public:
+        Launch();
+
+        void OnNavigatedTo(const winrt::Windows::UI::Xaml::Navigation::NavigationEventArgs& e);
+
+        IInspectable CurrentDefaultProfile();
+        void CurrentDefaultProfile(const IInspectable& value);
+
+        GETSET_PROPERTY(Editor::LaunchPageNavigationState, State, nullptr);
+
+        GETSET_BINDABLE_ENUM_SETTING(LaunchMode, Model::LaunchMode, State().Settings().GlobalSettings, LaunchMode);
+    };
+}
+
+namespace winrt::Microsoft::Terminal::Settings::Editor::factory_implementation
+{
+    BASIC_FACTORY(Launch);
+}
