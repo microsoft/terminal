0df12a85eee8125af9cb34a10d2f73d4833342b5
nogunumo <yukawa_hidenori@icloud.com>
2020-12-17 19:55:04 +0900

    code format

--- src/cascadia/TerminalApp/CommandPalette.cpp
+++ src/cascadia/TerminalApp/CommandPalette.cpp
@@ -291,7 +291,7 @@ namespace winrt::TerminalApp::implementation
                                          Windows::UI::Xaml::Input::KeyRoutedEventArgs const& e)
     {
         auto key = e.OriginalKey();
-        
+
         if (key == VirtualKey::Up)
         {
             // Action Mode: Move focus to the next item in the list.
