0b0bfd0086e3d11486e393a55fd0db6de25b7b15
Pankaj Bhojwani <pabhojwa@microsoft.com>
2021-01-07 12:44:20 -0800

    rename

--- src/cascadia/TerminalApp/TerminalPage.cpp
+++ src/cascadia/TerminalApp/TerminalPage.cpp
@@ -1973,7 +1973,7 @@ namespace winrt::TerminalApp::implementation
                 }
             }
 
-            const auto hasNewLineLambda = [&](auto c) { return c == L'\n' || c == L'\r'; };
+            const auto isNewLineLambda = [](auto c) { return c == L'\n' || c == L'\r'; };
             const auto hasNewLine = std::find_if(text.cbegin(), text.cend(), hasNewLineLambda) != text.cend();
             const auto warnMultiLine = hasNewLine && _settings.GlobalSettings().WarnAboutMultiLinePaste();
 
