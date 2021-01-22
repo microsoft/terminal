1eae2fc5a142a9c5d4aeade930948f932073b236
Pankaj Bhojwani <pabhojwa@microsoft.com>
2021-01-21 16:01:00 -0800

    Revamp

--- src/cascadia/TerminalSettingsEditor/Profiles.cpp
+++ src/cascadia/TerminalSettingsEditor/Profiles.cpp
@@ -37,6 +37,8 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         // into the path TextBox, we properly update the checkbox and stored
         // _lastBgImagePath. Without this, then we'll permanently hide the text
         // box, prevent it from ever being changed again.
+        //
+        // We do the same for the starting directory path
         PropertyChanged([this](auto&&, const Data::PropertyChangedEventArgs& args) {
             if (args.PropertyName() == L"BackgroundImagePath")
             {
@@ -46,6 +48,11 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
             {
                 _NotifyChanges(L"BackgroundImageSettingsVisible");
             }
+            else if (args.PropertyName() == L"StartingDirectory")
+            {
+                _NotifyChanges(L"UseParentProcessDirectory");
+                _NotifyChanges(L"UseCustomStartingDirectory");
+            }
         });
 
         // Cache the original BG image path. If the user clicks "Use desktop
@@ -54,6 +61,12 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         if (BackgroundImagePath() != L"desktopWallpaper")
         {
             _lastBgImagePath = BackgroundImagePath();
+        }
+
+        // Do the same for the starting directory
+        if (!StartingDirectory().empty())
+        {
+            _lastStartingDirectoryPath = StartingDirectory();
         }
     }
 
@@ -106,6 +119,52 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
             // Restore the path we had previously cached. This might be the
             // empty string.
             BackgroundImagePath(_lastBgImagePath);
+        }
+    }
+
+    bool ProfileViewModel::UseParentProcessDirectory()
+    {
+        return StartingDirectory().empty();
+    }
+
+    // This function simply returns the opposite of UseParentProcessDirectory.
+    // We bind the 'IsEnabled' parameters of the textbox and browse button
+    // to this because it needs to be the reverse of UseParentProcessDirectory
+    // but we don't want to create a whole new converter for inverting a boolean
+    bool ProfileViewModel::UseCustomStartingDirectory()
+    {
+        return !UseParentProcessDirectory();
+    }
+
+    void ProfileViewModel::UseParentProcessDirectory(const bool useParent)
+    {
+        if (useParent)
+        {
+            // Stash the current value of StartingDirectory. If the user
+            // checks and un-checks the "Use parent process directory" button, we want
+            // the path that we display in the text box to remain unchanged.
+            //
+            // Only stash this value if it's not empty
+            if (!StartingDirectory().empty())
+            {
+                _lastStartingDirectoryPath = StartingDirectory();
+            }
+            StartingDirectory(L"");
+        }
+        else
+        {
+            // Restore the path we had previously cached as long as it wasn't empty
+            // If it was empty, set the starting directory to %USERPROFILE%
+            // (we need to set it to something non-empty otherwise we will automatically
+            // disable the text box)
+            if (_lastStartingDirectoryPath.empty())
+            {
+                StartingDirectory(L"%USERPROFILE%");
+            }
+            else
+            {
+                StartingDirectory(_lastStartingDirectoryPath);
+            }
         }
     }
 
@@ -213,22 +272,6 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         state->DeleteProfile();
     }
 
-    void Profiles::UseParentProcessDirectory_Check(IInspectable const& /*sender*/, RoutedEventArgs const& /*e*/)
-    {
-        auto state{ winrt::get_self<ProfilePageNavigationState>(_State) };
-        state->Profile().StartingDirectory(L"");
-
-        // Disable the text box
-        StartingDirectory().IsEnabled(false);
-    }
-
-    void Profiles::UseParentProcessDirectory_Uncheck(IInspectable const& /*sender*/, RoutedEventArgs const& /*e*/)
-    {
-        // Enable the text box and browse button
-        StartingDirectory().IsEnabled(true);
-        StartingDirectory().Focus(winrt::Windows::UI::Xaml::FocusState::Programmatic);
-    }
-
     fire_and_forget Profiles::BackgroundImage_Click(IInspectable const&, RoutedEventArgs const&)
     {
         auto lifetime = get_strong();
@@ -302,15 +345,6 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
             StorageApplicationPermissions::FutureAccessList().AddOrReplace(L"PickedFolderToken", folder);
             _State.Profile().StartingDirectory(folder.Path());
             StartingDirectoryUseParentCheckbox().IsChecked(false);
-        }
-    }
-
-    void Profiles::StartingDirectory_LostFocus(IInspectable const&, RoutedEventArgs const&)
-    {
-        if (StartingDirectory().Text().empty())
-        {
-            StartingDirectoryUseParentCheckbox().IsChecked(true);
-            StartingDirectory().IsEnabled(false);
         }
     }
 
--- src/cascadia/TerminalSettingsEditor/Profiles.h
+++ src/cascadia/TerminalSettingsEditor/Profiles.h
@@ -21,6 +21,9 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
 
         bool UseDesktopBGImage();
         void UseDesktopBGImage(const bool useDesktop);
+        bool UseParentProcessDirectory();
+        void UseParentProcessDirectory(const bool useParent);
+        bool UseCustomStartingDirectory();
         bool BackgroundImageSettingsVisible();
 
         GETSET_PROPERTY(bool, IsBaseLayer, false);
@@ -67,6 +70,7 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
     private:
         Model::Profile _profile;
         winrt::hstring _lastBgImagePath;
+        winrt::hstring _lastStartingDirectoryPath;
     };
 
     struct DeleteProfileEventArgs :
@@ -118,12 +122,9 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         fire_and_forget BackgroundImage_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
         fire_and_forget Commandline_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
         fire_and_forget StartingDirectory_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
-        void StartingDirectory_LostFocus(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
         fire_and_forget Icon_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
         void BIAlignment_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
         void DeleteConfirmation_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
-        void UseParentProcessDirectory_Check(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
-        void UseParentProcessDirectory_Uncheck(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
 
         // CursorShape visibility logic
         void CursorShape_Changed(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
