ed4e829adc9869278d3f08688e93f276b379a1fc
PankajBhojwani <pankaj.d.bhoj@gmail.com>
2021-02-04 10:13:26 -0800

    Fix crash when closing multiple splits from the tab context menu (#9028)
    ## Summary of the Pull Request
    Fix for #9021 
    
    Turns out that what was happening is that the _parent_ pane's `Closed` event was being caught by the tab, and parent panes always have `nullopt` as their id. So now the `Pane::Id()` call always returns an optional, allowing us to check if it has a value before we access it. 
    
    ## PR Checklist
    * [x] Closes #9021 
    
    ## Validation Steps Performed
    No more crash

--- src/cascadia/TerminalApp/TerminalTab.cpp
+++ src/cascadia/TerminalApp/TerminalTab.cpp
@@ -390,9 +390,19 @@ namespace winrt::TerminalApp::implementation
         // Make sure to take the ID before calling Split() - Split() will clear out the active pane's ID
         const auto activePaneId = _activePane->Id();
         auto [first, second] = _activePane->Split(splitType, splitSize, profile, control);
-        first->Id(activePaneId);
-        second->Id(_nextPaneId);
-        ++_nextPaneId;
+        if (activePaneId)
+        {
+            first->Id(activePaneId.value());
+            second->Id(_nextPaneId);
+            ++_nextPaneId;
+        }
+        else
+        {
+            first->Id(_nextPaneId);
+            ++_nextPaneId;
+            second->Id(_nextPaneId);
+            ++_nextPaneId;
+        }
         _activePane = first;
         _AttachEventHandlersToControl(control);
 
@@ -610,16 +620,18 @@ namespace winrt::TerminalApp::implementation
 
         // We need to move the pane to the top of our mru list
         // If its already somewhere in the list, remove it first
-        const auto paneId = pane->Id();
-        for (auto i = _mruPanes.begin(); i != _mruPanes.end(); ++i)
+        if (const auto paneId = pane->Id())
         {
-            if (*i == paneId)
+            for (auto i = _mruPanes.begin(); i != _mruPanes.end(); ++i)
             {
-                _mruPanes.erase(i);
-                break;
+                if (*i == paneId.value())
+                {
+                    _mruPanes.erase(i);
+                    break;
+                }
             }
+            _mruPanes.insert(_mruPanes.begin(), paneId.value());
         }
-        _mruPanes.insert(_mruPanes.begin(), paneId);
         // Raise our own ActivePaneChanged event.
         _ActivePaneChangedHandlers();
     }
