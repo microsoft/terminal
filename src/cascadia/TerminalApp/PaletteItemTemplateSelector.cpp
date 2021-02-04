fb7d73fb84934ce745582808da0e68d11441868d
khvitaly <khvitaly@gmail.com>
2021-02-04 23:53:05 +0200

    Add missing copyright to added files

--- src/cascadia/TerminalApp/PaletteItemTemplateSelector.cpp
+++ src/cascadia/TerminalApp/PaletteItemTemplateSelector.cpp
@@ -1,4 +1,7 @@
-﻿#include "pch.h"
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include "pch.h"
 #include "TabPaletteItem.h"
 #include "PaletteItemTemplateSelector.h"
 #include "PaletteItemTemplateSelector.g.cpp"
--- src/cascadia/TerminalApp/PaletteItemTemplateSelector.h
+++ src/cascadia/TerminalApp/PaletteItemTemplateSelector.h
@@ -1,4 +1,7 @@
-﻿#pragma once
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#pragma once
 
 #include "PaletteItemTemplateSelector.g.h"
 #include "inc/cppwinrt_utils.h"
