f3854d3928df4aeec9ca2478facba9052e524f8b
khvitaly <khvitaly@gmail.com>
2021-02-05 02:49:22 +0200

    Document PaletteItemTemplateSelector

--- src/cascadia/TerminalApp/PaletteItemTemplateSelector.cpp
+++ src/cascadia/TerminalApp/PaletteItemTemplateSelector.cpp
@@ -13,6 +13,15 @@ namespace winrt::TerminalApp::implementation
         return SelectTemplateCore(item);
     }
 
+    // Method Description:
+    // - This method is called once command palette decides how to render a filtered command.
+    //   Currently we support two ways to render command, that depend on its palette item type:
+    //   - For TabPalette item we render an icon, a title, and some tab-related indicators like progress bar (as defined by TabItemTemplate)
+    //   - All other items are currently rendered with icon, title and optional key-chord (as defined by GeneralItemTemplate)
+    // Arguments:
+    // - item - an instance of filtered command to render
+    // Return Value:
+    // - data template to use for rendering
     Windows::UI::Xaml::DataTemplate PaletteItemTemplateSelector::SelectTemplateCore(winrt::Windows::Foundation::IInspectable const& item)
     {
         if (const auto filteredCommand{ item.try_as<winrt::TerminalApp::FilteredCommand>() })
