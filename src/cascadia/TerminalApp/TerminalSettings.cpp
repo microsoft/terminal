f402a8c1ce1d19cc340a06081897a073f02057af
Pankaj Bhojwani <pabhojwa@microsoft.com>
2021-02-08 11:52:25 -0800

    expose campbell color table

--- src/cascadia/TerminalApp/TerminalSettings.cpp
+++ src/cascadia/TerminalApp/TerminalSettings.cpp
@@ -3,7 +3,6 @@
 
 #include "pch.h"
 #include "TerminalSettings.h"
-//#include "../../types/colorTable.cpp"
 
 #include "TerminalSettings.g.cpp"
 
@@ -12,25 +11,6 @@ using namespace winrt::Microsoft::Terminal::Settings::Model;
 
 namespace winrt::TerminalApp::implementation
 {
-    static constexpr std::array<til::color, 16> campbellColorTable{
-        til::color{ 0x0C, 0x0C, 0x0C },
-        til::color{ 0xC5, 0x0F, 0x1F },
-        til::color{ 0x13, 0xA1, 0x0E },
-        til::color{ 0xC1, 0x9C, 0x00 },
-        til::color{ 0x00, 0x37, 0xDA },
-        til::color{ 0x88, 0x17, 0x98 },
-        til::color{ 0x3A, 0x96, 0xDD },
-        til::color{ 0xCC, 0xCC, 0xCC },
-        til::color{ 0x76, 0x76, 0x76 },
-        til::color{ 0xE7, 0x48, 0x56 },
-        til::color{ 0x16, 0xC6, 0x0C },
-        til::color{ 0xF9, 0xF1, 0xA5 },
-        til::color{ 0x3B, 0x78, 0xFF },
-        til::color{ 0xB4, 0x00, 0x9E },
-        til::color{ 0x61, 0xD6, 0xD6 },
-        til::color{ 0xF2, 0xF2, 0xF2 },
-    };
-
     static std::tuple<Windows::UI::Xaml::HorizontalAlignment, Windows::UI::Xaml::VerticalAlignment> ConvertConvergedAlignment(ConvergedAlignment alignment)
     {
         // extract horizontal alignment
@@ -275,16 +255,12 @@ namespace winrt::TerminalApp::implementation
         }
         else
         {
-            std::transform(campbellColorTable.cbegin(), campbellColorTable.cend(), colorTable.begin(), [](auto&& color) {
+            const auto campbellSpan = CampbellColorTable();
+            std::transform(campbellSpan.begin(), campbellSpan.end(), colorTable.begin(), [](auto&& color) {
                 return static_cast<uint32_t>(til::color{ color });
             });
         }
         return colorTable;
-        //return campbellColorTable;
-        // issues:
-        // - converting span to array without brute force?
-        // - converting from array<color> to array<uint32> without brute force?
-        // - including colortable.cpp for the campbell color table causes errors
     }
 
     gsl::span<uint32_t> TerminalSettings::_getColorTableImpl()
