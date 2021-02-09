a90289548f8548bf5c370a4b141b4b815c22616b
PankajBhojwani <pankaj.d.bhoj@gmail.com>
2021-02-08 17:07:11 -0800

    Quickly fix a mistake in #8602 (#9078)
    There was a mistake in #8602, this change fixes that.
    (`using` and `include` moved to cpp file)

--- src/cascadia/TerminalApp/TerminalSettings.cpp
+++ src/cascadia/TerminalApp/TerminalSettings.cpp
@@ -3,11 +3,13 @@
 
 #include "pch.h"
 #include "TerminalSettings.h"
+#include "../../types/inc/colorTable.hpp"
 
 #include "TerminalSettings.g.cpp"
 
 using namespace winrt::Microsoft::Terminal::TerminalControl;
 using namespace winrt::Microsoft::Terminal::Settings::Model;
+using namespace Microsoft::Console::Utils;
 
 namespace winrt::TerminalApp::implementation
 {
