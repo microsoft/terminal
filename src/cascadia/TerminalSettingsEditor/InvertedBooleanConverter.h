ff37bc51769641fd3b96aedd695427dd25de88d5
Carlos Zamora <cazamor@microsoft.com>
2021-02-16 12:58:15 -0800

    use macros

--- src/cascadia/TerminalSettingsEditor/InvertedBooleanConverter.h
+++ src/cascadia/TerminalSettingsEditor/InvertedBooleanConverter.h
@@ -4,27 +4,6 @@
 #pragma once
 
 #include "InvertedBooleanConverter.g.h"
-#include "Utils.h"
+#include "../inc/cppwinrt_utils.h"
 
-namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
-{
-    struct InvertedBooleanConverter : InvertedBooleanConverterT<InvertedBooleanConverter>
-    {
-        InvertedBooleanConverter() = default;
-
-        Windows::Foundation::IInspectable Convert(Windows::Foundation::IInspectable const& value,
-                                                  Windows::UI::Xaml::Interop::TypeName const& targetType,
-                                                  Windows::Foundation::IInspectable const& parameter,
-                                                  hstring const& language);
-
-        Windows::Foundation::IInspectable ConvertBack(Windows::Foundation::IInspectable const& value,
-                                                      Windows::UI::Xaml::Interop::TypeName const& targetType,
-                                                      Windows::Foundation::IInspectable const& parameter,
-                                                      hstring const& language);
-    };
-}
-
-namespace winrt::Microsoft::Terminal::Settings::Editor::factory_implementation
-{
-    BASIC_FACTORY(InvertedBooleanConverter);
-}
+DECLARE_CONVERTER(winrt::Microsoft::Terminal::Settings::Editor, InvertedBooleanConverter);
