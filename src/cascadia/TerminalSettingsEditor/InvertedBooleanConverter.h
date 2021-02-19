177430272ce3c49466de8e60f64e35e6a33f1ba1
Carlos Zamora <carlos.zamora@microsoft.com>
2021-02-19 14:27:30 -0800

    Polish settings UI localized text (#9124)
    Updates the following text in the settings UI
    - focus follow mouse mode is introduced to be more instructional
    - focus follow mouse mode tooltip removed
    - avoid double negative in "disable pane animation"
    
    Closes #8900 
    Updates #6459 Settings UI text

---
+++ src/cascadia/TerminalSettingsEditor/InvertedBooleanConverter.h
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#pragma once
+
+#include "InvertedBooleanConverter.g.h"
+#include "../inc/cppwinrt_utils.h"
+
+DECLARE_CONVERTER(winrt::Microsoft::Terminal::Settings::Editor, InvertedBooleanConverter);
