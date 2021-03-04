d972c6a28412fc7319f447148184a379f1172c8d
Mike Griese <migrie@microsoft.com>
2021-03-04 12:55:41 -0600

    Did you know about X Macros? Now you do!
    **Summary of the Pull Request**
    
    This PR adds an X Macro for defining our ShortcutActions. This means that you can add the action in one place, and have the macro synthesize all sorts of boilerplate for you!
    
    From the `AllShortcutActions.h` file:
    
    > For a clearer explanation of how this file should be used, see:
    > https://en.wikipedia.org/wiki/X_Macro
    >
    > Include this file to be able to quickly define some code in the exact same
    > way for _every single shortcut action_. To use:
    >
    > 1. Include this file
    > 2. Define the ON_ALL_ACTIONS macro with what you want each action to show up
    >    as. Ex:
    >
    >    #define ON_ALL_ACTIONS(action) void action##Handler();
    >
    > 3. Then, use the ALL_SHORTCUT_ACTIONS macro to get the ON_ALL_ACTIONS marcro
    >    repeated once for every ShortcutAction
    >
    > This is used in KeyMapping.idl, ShortcutAction.*, TerminalPage.*, etc. to
    > reduce the number of places where we must copy-paste boiler-plate code for
    > each action. This is _NOT_ something that should be used when any individual
    > case should be customized.
    
    **PR Checklist**
    * [x] Scratches an itch
    * [x] I work here
    * [x] Tests passed
    * [n/a] Requires documentation to be updated
    
    **Detailed Description of the Pull Request / Additional comments**
    
    Originally I had this blocked as a follow up to #9662. However, I've grown tired after a month of merging main into this branch, and I'm just shipping it separately. It will inevitably conflict with anyone who has actions in flight currently.
    
    **Validation Steps Performed**
    The code still builds exactly as before!

--- src/cascadia/TerminalApp/ShortcutActionDispatch.h
+++ src/cascadia/TerminalApp/ShortcutActionDispatch.h
@@ -5,6 +5,7 @@
 
 #include "ShortcutActionDispatch.g.h"
 #include "../inc/cppwinrt_utils.h"
+#include "../TerminalSettingsModel/AllShortcutActions.h"
 
 // fwdecl unittest classes
 namespace TerminalAppLocalTests
@@ -23,55 +24,9 @@ namespace winrt::TerminalApp::implementation
 
         bool DoAction(const Microsoft::Terminal::Settings::Model::ActionAndArgs& actionAndArgs);
 
-        // clang-format off
-        DECLARE_ACTION(CopyText);
-        DECLARE_ACTION(PasteText);
-        DECLARE_ACTION(OpenNewTabDropdown);
-        DECLARE_ACTION(DuplicateTab);
-        DECLARE_ACTION(NewTab);
-        DECLARE_ACTION(CloseWindow);
-        DECLARE_ACTION(CloseTab);
-        DECLARE_ACTION(ClosePane);
-        DECLARE_ACTION(SwitchToTab);
-        DECLARE_ACTION(NextTab);
-        DECLARE_ACTION(PrevTab);
-        DECLARE_ACTION(SendInput);
-        DECLARE_ACTION(SplitPane);
-        DECLARE_ACTION(TogglePaneZoom);
-        DECLARE_ACTION(AdjustFontSize);
-        DECLARE_ACTION(ResetFontSize);
-        DECLARE_ACTION(ScrollUp);
-        DECLARE_ACTION(ScrollDown);
-        DECLARE_ACTION(ScrollUpPage);
-        DECLARE_ACTION(ScrollDownPage);
-        DECLARE_ACTION(ScrollToTop);
-        DECLARE_ACTION(ScrollToBottom);
-        DECLARE_ACTION(OpenSettings);
-        DECLARE_ACTION(ResizePane);
-        DECLARE_ACTION(Find);
-        DECLARE_ACTION(MoveFocus);
-        DECLARE_ACTION(ToggleShaderEffects);
-        DECLARE_ACTION(ToggleFocusMode);
-        DECLARE_ACTION(ToggleFullscreen);
-        DECLARE_ACTION(ToggleAlwaysOnTop);
-        DECLARE_ACTION(ToggleCommandPalette);
-        DECLARE_ACTION(SetColorScheme);
-        DECLARE_ACTION(SetTabColor);
-        DECLARE_ACTION(OpenTabColorPicker);
-        DECLARE_ACTION(RenameTab);
-        DECLARE_ACTION(OpenTabRenamer);
-        DECLARE_ACTION(ExecuteCommandline);
-        DECLARE_ACTION(CloseOtherTabs);
-        DECLARE_ACTION(CloseTabsAfter);
-        DECLARE_ACTION(TabSearch);
-        DECLARE_ACTION(MoveTab);
-        DECLARE_ACTION(BreakIntoDebugger);
-        DECLARE_ACTION(FindMatch);
-        DECLARE_ACTION(TogglePaneReadOnly);
-        DECLARE_ACTION(NewWindow);
-        DECLARE_ACTION(IdentifyWindow);
-        DECLARE_ACTION(IdentifyWindows);
-        // clang-format on
+#define ON_ALL_ACTIONS(action) DECLARE_ACTION(action);
+        ALL_SHORTCUT_ACTIONS
+#undef ON_ALL_ACTIONS
 
     private:
         friend class TerminalAppLocalTests::SettingsTests;
