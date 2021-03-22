3ffefeb39f2b55831a2fa4de5d1602f47c331b27
Don-Vito <khvitaly@gmail.com>
2021-03-22 13:02:30 +0200

    Fix read-only tab dialog to cancel tab closing when dismissed (#9573)
    Currently dismissing "are you sure you wish to close read-only tab or pane"
    dialog by pressing `ESC` will not abort tab closing
    (aka the tab will be closed!)
    
    The reason for this, is that we cancel, only if the "Cancel" is pressed
    (aka result=PrimaryButton, while ESC returns result=None).
    
    This PR fixes this, by doing what we usually do:
    * Putting Cancel in the CloseButton (which is also triggered by ESC)
    * Aborting the action if the result is not a Primary Button
    
    However, since we want Cancel to be a default action,
    we set CloseButton to be the DefaultButton  in XAML
    
    (cherry picked from commit 9bd097f4ada0a6b34e9d48688300e6452e2a9376)

--- src/cascadia/TerminalApp/TerminalPage.cpp
+++ src/cascadia/TerminalApp/TerminalPage.cpp
@@ -1291,8 +1291,8 @@ namespace winrt::TerminalApp::implementation
         {
             ContentDialogResult warningResult = co_await _ShowCloseReadOnlyDialog();
 
-            // The primary action is canceling the removal
-            if (warningResult == ContentDialogResult::Primary)
+            // If the user didn't explicitly click on close tab - leave
+            if (warningResult != ContentDialogResult::Primary)
             {
                 co_return;
             }
@@ -1688,8 +1688,8 @@ namespace winrt::TerminalApp::implementation
                     {
                         ContentDialogResult warningResult = co_await _ShowCloseReadOnlyDialog();
 
-                        // The primary action is canceling the action
-                        if (warningResult == ContentDialogResult::Primary)
+                        // If the user didn't explicitly click on close tab - leave
+                        if (warningResult != ContentDialogResult::Primary)
                         {
                             co_return;
                         }
