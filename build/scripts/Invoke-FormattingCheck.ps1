2486493443b39c62c61cc23e1a4b1e943e83ca31
Mike Griese <migrie@microsoft.com>
2021-03-23 15:44:44 -0500

    This should reall really finally fix the BOMs and line endings
      Presumably, git will fix the line endings in the repo and check out the right ones? lawd everything is awful

--- build/scripts/Invoke-FormattingCheck.ps1
+++ build/scripts/Invoke-FormattingCheck.ps1
@@ -4,9 +4,13 @@
 function Invoke-CheckBadCodeFormatting() {
     Import-Module ./tools/OpenConsole.psm1
     Invoke-CodeFormat
+
     # returns a non-zero exit code if there are any diffs in the tracked files in the repo
     git diff-index --quiet HEAD --
     if ($lastExitCode -eq 1) {
+        # Write the list of files that need updating to the log
+        git diff-index --name-only HEAD
+
         throw "code formatting bad, run Invoke-CodeFormat on branch"
     }
 }
