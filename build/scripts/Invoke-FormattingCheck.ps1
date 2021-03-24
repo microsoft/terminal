7bad6726ff6712c065394e73c72f908fc892ef79
Mike Griese <migrie@microsoft.com>
2021-03-24 04:33:27 -0500

    Tell me why (aint nothing but a heartache)

--- build/scripts/Invoke-FormattingCheck.ps1
+++ build/scripts/Invoke-FormattingCheck.ps1
@@ -8,8 +8,12 @@ function Invoke-CheckBadCodeFormatting() {
     # returns a non-zero exit code if there are any diffs in the tracked files in the repo
     git diff-index --quiet HEAD --
     if ($lastExitCode -eq 1) {
+
+        git --no-pager diff HEAD
+
         # Write the list of files that need updating to the log
         git diff-index --name-only HEAD
+
 
         throw "code formatting bad, run Invoke-CodeFormat on branch"
     }
