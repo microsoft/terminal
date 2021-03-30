c7d2a818b02cfb1dadd8415a07f77c1888421bca
Michael Niksa <miniksa@microsoft.com>
2021-03-30 03:58:11 -0700

    Change TAEF nuget package to use new Microsoft.Taef name; Update to 10.58 release build version. (#9656)
    Change TAEF nuget package to use new Microsoft.Taef name; Update to 10.58 release build version.
    
    ## PR Checklist
    * [x] Closes email from Phil letting us know TAEF has a new release build and a rename.
    * [x] Closes annoying duplicate TAEF import warning in `Parser.UnitTests.vcxproj`
    * [x] I work here.
    * [ ] Need to see the tests run off CI to confirm this is fine for those environments and Helix
    
    ## Validation Steps Performed
    * [x] Build/run tests locally
    * [ ] Build/run unit and feature tests in CI
    * [ ] Build/run Helix-lab tests in CI

--- tools/razzle.cmd
+++ tools/razzle.cmd
@@ -104,7 +104,7 @@ shift
 goto :ARGS_LOOP
 
 :POST_ARGS_LOOP
-set TAEF=%OPENCON%\packages\Taef.Redist.Wlk.10.57.200731005-develop\build\Binaries\%ARCH%\TE.exe
+set TAEF=%OPENCON%\packages\Microsoft.Taef.10.58.210305002\build\Binaries\%ARCH%\TE.exe
 rem Set this envvar so setup won't repeat itself
 set OpenConBuild=true
 
