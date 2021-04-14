0d451cce2d15599699473af819561f2709b93ece
Don-Vito <khvitaly@gmail.com>
2021-04-14 20:11:19 +0300

    Limit terminal warning bells to one per second (#9812)
    * [x] Closes https://github.com/microsoft/terminal/issues/9776
    * [x] CLA signed.
    * [ ] Tests added/passed
    * [ ] Documentation updated.
    * [ ] Schema updated.
    * [ ] I've discussed this with core contributors already.
    
    Use `ThrottledFunc` in `TermControl` to limit bell emission callback to one per second.
    
    (cherry picked from commit 9a2d27e9f6e04db458fcce4215fb35f83cd78d07)

--- src/cascadia/TerminalControl/TermControl.h
+++ src/cascadia/TerminalControl/TermControl.h
@@ -218,6 +218,8 @@ namespace winrt::Microsoft::Terminal::TerminalControl::implementation
 
         std::shared_ptr<ThrottledFunc<>> _updatePatternLocations;
 
+        std::shared_ptr<ThrottledFunc<>> _playWarningBell;
+
         struct ScrollBarUpdate
         {
             std::optional<double> newValue;
