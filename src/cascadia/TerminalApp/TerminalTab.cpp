674c9182d7a6eb43513d4e6fbbffa9b411a5203e
MPela <1140981+mpela81@users.noreply.github.com>
2021-04-21 12:53:19 +0200

    Add Close menu items to the context menu flyout (#9859)
    Add the "Close other tabs"/"Close tabs to the right" menu items straight to the tab context menu to work around #8238.
    We can't add them into a dedicated sub-menu until the upstream crash is fixed.
    
    * [X] Closes #8238
    * [X] CLA signed. If not, go over [here](https://cla.opensource.microsoft.com/microsoft/Terminal) and sign the CLA
    * [ ] Tests added/passed
    * [ ] Documentation updated
    * [ ] Schema updated.
    * [ ] I've discussed this with core contributors already. If not checked, I'm ready to accept this work might be rejected in favor of a different grand plan.
    
    Moved the creation of the close menu items to a single function. Once the originating crash is fixed, the sub-menu can be restored by just replacing a few lines of code.
    
    ![immagine](https://user-images.githubusercontent.com/1140981/115059601-0dbc2480-9ee7-11eb-9889-d9ef8e6e7613.png)
    
    (cherry picked from commit 2065fa7b768ab96305c08003270a8d176fecfeb0)
    (cherry picked from commit c4e02e7274018105b8b7de483b60c8288c4459fd)

--- src/cascadia/TerminalApp/TerminalTab.cpp
+++ src/cascadia/TerminalApp/TerminalTab.cpp
@@ -784,21 +784,6 @@ namespace winrt::TerminalApp::implementation
     {
         auto weakThis{ get_weak() };
 
-        // Close
-        Controls::MenuFlyoutItem closeTabMenuItem;
-        Controls::FontIcon closeSymbol;
-        closeSymbol.FontFamily(Media::FontFamily{ L"Segoe MDL2 Assets" });
-        closeSymbol.Glyph(L"\xE711");
-
-        closeTabMenuItem.Click([weakThis](auto&&, auto&&) {
-            if (auto tab{ weakThis.get() })
-            {
-                tab->_ClosedHandlers(nullptr, nullptr);
-            }
-        });
-        closeTabMenuItem.Text(RS_(L"TabClose"));
-        closeTabMenuItem.Icon(closeSymbol);
-
         // "Color..."
         Controls::MenuFlyoutItem chooseColorMenuItem;
         Controls::FontIcon colorPickSymbol;
@@ -852,8 +837,7 @@ namespace winrt::TerminalApp::implementation
         newTabFlyout.Items().Append(chooseColorMenuItem);
         newTabFlyout.Items().Append(renameTabMenuItem);
         newTabFlyout.Items().Append(menuSeparator);
-        newTabFlyout.Items().Append(_CreateCloseSubMenu());
-        newTabFlyout.Items().Append(closeTabMenuItem);
+        _AppendCloseMenuItems(newTabFlyout);
         TabViewItem().ContextFlyout(newTabFlyout);
     }
 
