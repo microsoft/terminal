a4ebeb0a567b4ca39675eda1905690b917ef0a8e
Mike Griese <migrie@microsoft.com>
2021-05-19 11:14:09 -0500

    This fixes summoning _quake as the MRU window (#10108)
    ## Summary of the Pull Request
    
    This fixes a bug where if you had the `_quake` window open, and you tried to `globalSummon` it (not with the `quakeMode` action, but just with a plain-old `globalSummon` to activate the MRU window), we'd _create a new window_ instead of just summoning the `_quake` window.
    
    ## References
    * regressed in #9956 
    
    ## PR Checklist
    * [x] Closes https://github.com/microsoft/terminal/projects/5#card-60325142
    * [x] I work here
    * [x] Tests added/passed
    * [n/a] Requires documentation to be updated
    
    ## Detailed Description of the Pull Request / Additional comments
    
    It's basically a one-line fix, I just had to update the function signature for `_getMostRecentPeasantID` to allow us to use it differently for glomming vs summoning. When glomming, `ignoreQuakeWindow` should be true. When summoning, `ignoreQuakeWindow` should be false.

--- src/cascadia/Remoting/Monarch.cpp
+++ src/cascadia/Remoting/Monarch.cpp
@@ -311,9 +311,14 @@ namespace winrt::Microsoft::Terminal::Remoting::implementation
     // Arguments:
     // - limitToCurrentDesktop: if true, only return the MRU peasant that's
     //   actually on the current desktop.
+    // - ignoreQuakeWindow: if true, then don't return the _quake window when we
+    //   find it. This allows us to change our behavior for glomming vs
+    //   summoning. When summoning the window, this parameter should be true.
+    //   When glomming, this should be false, as to prevent glomming to the
+    //   _quake window.
     // Return Value:
     // - the ID of the most recent peasant, otherwise 0 if we could not find one.
-    uint64_t Monarch::_getMostRecentPeasantID(const bool limitToCurrentDesktop)
+    uint64_t Monarch::_getMostRecentPeasantID(const bool limitToCurrentDesktop, const bool ignoreQuakeWindow)
     {
         if (_mruPeasants.empty())
         {
@@ -382,7 +387,7 @@ namespace winrt::Microsoft::Terminal::Remoting::implementation
                 continue;
             }
 
-            if (peasant.WindowName() == QuakeWindowName)
+            if (ignoreQuakeWindow && peasant.WindowName() == QuakeWindowName)
             {
                 // The _quake window should never be treated as the MRU window.
                 // Skip it if we see it. Users can still target it with `wt -w
@@ -498,10 +503,12 @@ namespace winrt::Microsoft::Terminal::Remoting::implementation
                 // lookup to find the window that spawned this process (then
                 // fall back to sameDesktop if we can't find a match). For now,
                 // it's good enough to just try to find a match on this desktop.
-                windowID = _getMostRecentPeasantID(true);
+                //
+                // GH#projects/5#card-60325142 - Don't try to glom to the quake window.
+                windowID = _getMostRecentPeasantID(true, true);
                 break;
             case WindowingBehaviorUseAnyExisting:
-                windowID = _getMostRecentPeasantID(false);
+                windowID = _getMostRecentPeasantID(false, true);
                 break;
             case WindowingBehaviorUseName:
                 windowID = _lookupPeasantIdForName(targetWindowName);
@@ -732,7 +739,7 @@ namespace winrt::Microsoft::Terminal::Remoting::implementation
                 // Use the value of the `desktop` arg to determine if we should
                 // limit to the current desktop (desktop:onCurrent) or not
                 // (desktop:any or desktop:toCurrent)
-                windowId = _getMostRecentPeasantID(args.OnCurrentDesktop());
+                windowId = _getMostRecentPeasantID(args.OnCurrentDesktop(), false);
             }
             else
             {
