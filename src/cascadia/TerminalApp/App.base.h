77a9847d9ebc9235f8741077817413e6ff44b9a1
Marcel Wagner <marcel.alex.wagner@outlook.com>
2021-06-04 23:20:44 +0200

    Refactor away getter

--- src/cascadia/TerminalApp/App.base.h
+++ src/cascadia/TerminalApp/App.base.h
@@ -9,23 +9,22 @@ namespace winrt::TerminalApp::implementation
 
         IXamlType GetXamlType(::winrt::Windows::UI::Xaml::Interop::TypeName const& type)
         {
-            return AppProvider().GetXamlType(type);
+            return _appProvider.GetXamlType(type);
         }
 
         IXamlType GetXamlType(::winrt::hstring const& fullName)
         {
-            return AppProvider().GetXamlType(fullName);
+            return _appProvider.GetXamlType(fullName);
         }
 
         ::winrt::com_array<::winrt::Windows::UI::Xaml::Markup::XmlnsDefinition> GetXmlnsDefinitions()
         {
-            return AppProvider().GetXmlnsDefinitions();
+            return _appProvider.GetXmlnsDefinitions();
         }
 
     private:
         bool _contentLoaded{ false };
         winrt::TerminalApp::XamlMetaDataProvider _appProvider;
-        winrt::TerminalApp::XamlMetaDataProvider AppProvider() { return _appProvider; };
     };
 
     template<typename D, typename... I>
