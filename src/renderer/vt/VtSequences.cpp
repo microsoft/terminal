f48e59427f5d0bce727df6f1cb340d25ebd52104
Chester Liu <skyline75489@outlook.com>
2021-06-16 12:11:37 +0800

    Use FMT_COMPILE

--- src/renderer/vt/VtSequences.cpp
+++ src/renderer/vt/VtSequences.cpp
@@ -4,7 +4,6 @@
 #include "precomp.h"
 #include "vtrenderer.hpp"
 #include "../../inc/conattrs.hpp"
-#include "til/format.h"
 
 #pragma hdrstop
 using namespace Microsoft::Console::Render;
@@ -214,8 +213,6 @@ using namespace Microsoft::Console::Render;
 [[nodiscard]] HRESULT VtEngine::_SetGraphicsRendition16Color(const WORD wAttr,
                                                              const bool fIsForeground) noexcept
 {
-    static const std::string fmt = "\x1b[%dm";
-
     // Always check using the foreground flags, because the bg flags constants
     //  are a higher byte
     // Foreground sequences are in [30,37] U [90,97]
@@ -235,9 +232,9 @@ using namespace Microsoft::Console::Render;
                         (WI_IsFlagSet(wAttr, FOREGROUND_GREEN) ? 2 : 0) +
                         (WI_IsFlagSet(wAttr, FOREGROUND_BLUE) ? 4 : 0);
 
-    char buf[6];
-    char* end = til::format::format(buf, "\x1b[{}m", vtIndex);
     // An example string with max length would be "\x1b[100m", which has length = 6.
+    char buf[6];
+    char* end = fmt::format_to(std::begin(buf), FMT_COMPILE("\x1b[{}m"), vtIndex);
     return _Write({ buf, static_cast<std::string_view::size_type>(end - buf) });
 }
 
@@ -252,9 +249,9 @@ using namespace Microsoft::Console::Render;
 [[nodiscard]] HRESULT VtEngine::_SetGraphicsRendition256Color(const WORD index,
                                                               const bool fIsForeground) noexcept
 {
-    char buf[11];
     // An example string with max length would be "\x1b[38;5;128m", which has length = 11.
-    char* end = til::format::format(buf, "\x1b[{};5;{}m", fIsForeground ? 38 : 48, ::Xterm256ToWindowsIndex(index));
+    char buf[11];
+    char* end = fmt::format_to(std::begin(buf), FMT_COMPILE("\x1b[{};5;{}m"), fIsForeground ? 38 : 48, ::Xterm256ToWindowsIndex(index));
     return _Write({buf, static_cast<std::string_view::size_type>(end - buf)});
 }
 
@@ -273,10 +270,10 @@ using namespace Microsoft::Console::Render;
     DWORD const g = GetGValue(color);
     DWORD const b = GetBValue(color);
 
-    char buf[19];
-    char* end = til::format::format(buf, "\x1b[{};2;{};{};{}m", fIsForeground ? 38 : 48, r, g, b);
-
     // An example string with max length would be "\x1b[38;2;128;128;128m", which has length = 19.
+    char buf[19];
+    char* end = fmt::format_to(std::begin(buf), FMT_COMPILE("\x1b[{};2;{};{};{}m"), fIsForeground ? 38 : 48, r, g, b);
+
     return _Write({ buf, static_cast<std::string_view::size_type>(end - buf) });
 }
 
