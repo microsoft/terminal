8287a8108bf74dcc2f9c820cf6d38976c1e6d2a3
Dustin Howett <duhowett@microsoft.com>
2021-07-08 12:12:37 -0500

    Featureize the override button

--- src/cascadia/TerminalSettingsEditor/SettingContainer.cpp
+++ src/cascadia/TerminalSettingsEditor/SettingContainer.cpp
@@ -192,18 +192,25 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
             originTag = profile.Origin();
         }
 
-        if (originTag == Model::OriginTag::Fragment || originTag == Model::OriginTag::Generated)
+        if constexpr (Feature_ShowProfileDefaultsInSettings::IsEnabled())
         {
-            // from a fragment extension
-            return hstring{ fmt::format(std::wstring_view{ RS_(L"SettingContainer_OverrideMessageFragmentExtension") }, source) };
-        }
-        else
-        {
-            // base layer
-            // TODO GH#3818: When we add profile inheritance as a setting,
-            //               we'll need an extra conditional check to see if this
-            //               is the base layer or some other profile
+            // EXPERIMENTAL FEATURE
+            // We will display arrows for all origins, and informative tooltips for Fragments and Generated
+            if (originTag == Model::OriginTag::Fragment || originTag == Model::OriginTag::Generated)
+            {
+                // from a fragment extension or generated profile
+                return hstring{ fmt::format(std::wstring_view{ RS_(L"SettingContainer_OverrideMessageFragmentExtension") }, source) };
+            }
             return RS_(L"SettingContainer_OverrideMessageBaseLayer");
         }
+
+        // STABLE FEATURE
+        // We will only display arrows and informative tooltips for Fragments
+        if (originTag == Model::OriginTag::Fragment)
+        {
+            // from a fragment extension or generated profile
+            return hstring{ fmt::format(std::wstring_view{ RS_(L"SettingContainer_OverrideMessageFragmentExtension") }, source) };
+        }
+        return {}; // no tooltip
     }
 }
