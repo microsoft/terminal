f4de1e47b3117668360a9e2da500e5034ccd3bd5
Leonard Hecker <lhecker@microsoft.com>
2021-07-14 01:00:11 +0200

    Enable /Zc:preprocessor (#10593)
    This commit is a preparation for upcoming changes to KeyChordSerialization for #7539 and #10203.
    In order to support variadic macros, /Zc:preprocessor was enabled, which required changing unrelated parts of the project.
    
    * [x] I work here
    * [x] Tests added/passed
    
    * Project still compiles ✔️
    
    (cherry picked from commit 32fbd4cbb60feb1cc576e63a1e85432d9592cf7c)

--- src/cascadia/TerminalSettingsEditor/Utils.h
+++ src/cascadia/TerminalSettingsEditor/Utils.h
@@ -11,33 +11,39 @@
 // being its localized name, and also initializes the enum to EnumEntry
 // map that's required to tell XAML what enum value the currently active
 // setting has.
-#define INITIALIZE_BINDABLE_ENUM_SETTING(name, enumMappingsName, enumType, resourceSectionAndType, resourceProperty)                                    \
-    std::vector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> name##List;                                                                    \
-    _##name##Map = winrt::single_threaded_map<enumType, winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>();                                     \
-    auto enumMapping##name = winrt::Microsoft::Terminal::Settings::Model::EnumMappings::##enumMappingsName();                                           \
-    for (auto [key, value] : enumMapping##name)                                                                                                         \
-    {                                                                                                                                                   \
-        auto enumName = LocalizedNameForEnumName(resourceSectionAndType, key, resourceProperty);                                                        \
-        auto entry = winrt::make<winrt::Microsoft::Terminal::Settings::Editor::implementation::EnumEntry>(enumName, winrt::box_value<enumType>(value)); \
-        name##List.emplace_back(entry);                                                                                                                 \
-        _##name##Map.Insert(value, entry);                                                                                                              \
-    }                                                                                                                                                   \
-    std::sort(begin(name##List), end(name##List), EnumEntryComparator<enumType>());                                                                     \
-    _##name##List = winrt::single_threaded_observable_vector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>(std::move(name##List));
+#define INITIALIZE_BINDABLE_ENUM_SETTING(name, enumMappingsName, enumType, resourceSectionAndType, resourceProperty)                                        \
+    do                                                                                                                                                      \
+    {                                                                                                                                                       \
+        std::vector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> name##List;                                                                    \
+        _##name##Map = winrt::single_threaded_map<enumType, winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>();                                     \
+        auto enumMapping##name = winrt::Microsoft::Terminal::Settings::Model::EnumMappings::enumMappingsName();                                             \
+        for (auto [key, value] : enumMapping##name)                                                                                                         \
+        {                                                                                                                                                   \
+            auto enumName = LocalizedNameForEnumName(resourceSectionAndType, key, resourceProperty);                                                        \
+            auto entry = winrt::make<winrt::Microsoft::Terminal::Settings::Editor::implementation::EnumEntry>(enumName, winrt::box_value<enumType>(value)); \
+            name##List.emplace_back(entry);                                                                                                                 \
+            _##name##Map.Insert(value, entry);                                                                                                              \
+        }                                                                                                                                                   \
+        std::sort(name##List.begin(), name##List.end(), EnumEntryComparator<enumType>());                                                                   \
+        _##name##List = winrt::single_threaded_observable_vector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>(std::move(name##List));           \
+    } while (0);
 
-#define INITIALIZE_BINDABLE_ENUM_SETTING_REVERSE_ORDER(name, enumMappingsName, enumType, resourceSectionAndType, resourceProperty)                      \
-    std::vector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> name##List;                                                                    \
-    _##name##Map = winrt::single_threaded_map<enumType, winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>();                                     \
-    auto enumMapping##name = winrt::Microsoft::Terminal::Settings::Model::EnumMappings::##enumMappingsName();                                           \
-    for (auto [key, value] : enumMapping##name)                                                                                                         \
-    {                                                                                                                                                   \
-        auto enumName = LocalizedNameForEnumName(resourceSectionAndType, key, resourceProperty);                                                        \
-        auto entry = winrt::make<winrt::Microsoft::Terminal::Settings::Editor::implementation::EnumEntry>(enumName, winrt::box_value<enumType>(value)); \
-        name##List.emplace_back(entry);                                                                                                                 \
-        _##name##Map.Insert(value, entry);                                                                                                              \
-    }                                                                                                                                                   \
-    std::sort(begin(name##List), end(name##List), EnumEntryReverseComparator<enumType>());                                                              \
-    _##name##List = winrt::single_threaded_observable_vector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>(std::move(name##List));
+#define INITIALIZE_BINDABLE_ENUM_SETTING_REVERSE_ORDER(name, enumMappingsName, enumType, resourceSectionAndType, resourceProperty)                          \
+    do                                                                                                                                                      \
+    {                                                                                                                                                       \
+        std::vector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> name##List;                                                                    \
+        _##name##Map = winrt::single_threaded_map<enumType, winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>();                                     \
+        auto enumMapping##name = winrt::Microsoft::Terminal::Settings::Model::EnumMappings::enumMappingsName();                                             \
+        for (auto [key, value] : enumMapping##name)                                                                                                         \
+        {                                                                                                                                                   \
+            auto enumName = LocalizedNameForEnumName(resourceSectionAndType, key, resourceProperty);                                                        \
+            auto entry = winrt::make<winrt::Microsoft::Terminal::Settings::Editor::implementation::EnumEntry>(enumName, winrt::box_value<enumType>(value)); \
+            name##List.emplace_back(entry);                                                                                                                 \
+            _##name##Map.Insert(value, entry);                                                                                                              \
+        }                                                                                                                                                   \
+        std::sort(name##List.begin(), name##List.end(), EnumEntryReverseComparator<enumType>());                                                            \
+        _##name##List = winrt::single_threaded_observable_vector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>(std::move(name##List));           \
+    } while (0);
 
 // This macro must be used alongside INITIALIZE_BINDABLE_ENUM_SETTING.
 // It declares the needed data structures, getters, and setters to make
@@ -45,29 +51,29 @@
 // of EnumEntries so that we may display all possible values of the given
 // enum type and its localized names. It also provides a getter and setter
 // for the setting we wish to bind to.
-#define GETSET_BINDABLE_ENUM_SETTING(name, enumType, settingsModelName, settingNameInModel)                                                                  \
-public:                                                                                                                                                      \
-    winrt::Windows::Foundation::Collections::IObservableVector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>##name##List()                        \
-    {                                                                                                                                                        \
-        return _##name##List;                                                                                                                                \
-    }                                                                                                                                                        \
-                                                                                                                                                             \
-    winrt::Windows::Foundation::IInspectable Current##name()                                                                                                 \
-    {                                                                                                                                                        \
-        return winrt::box_value<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>(_##name##Map.Lookup(##settingsModelName().##settingNameInModel())); \
-    }                                                                                                                                                        \
-                                                                                                                                                             \
-    void Current##name(const winrt::Windows::Foundation::IInspectable& enumEntry)                                                                            \
-    {                                                                                                                                                        \
-        if (auto ee = enumEntry.try_as<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>())                                                           \
-        {                                                                                                                                                    \
-            auto setting = winrt::unbox_value<enumType>(ee.EnumValue());                                                                                     \
-            ##settingsModelName().##settingNameInModel(setting);                                                                                             \
-        }                                                                                                                                                    \
-    }                                                                                                                                                        \
-                                                                                                                                                             \
-private:                                                                                                                                                     \
-    winrt::Windows::Foundation::Collections::IObservableVector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> _##name##List;                       \
+#define GETSET_BINDABLE_ENUM_SETTING(name, enumType, settingsModelName, settingNameInModel)                                                              \
+public:                                                                                                                                                  \
+    winrt::Windows::Foundation::Collections::IObservableVector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> name##List()                     \
+    {                                                                                                                                                    \
+        return _##name##List;                                                                                                                            \
+    }                                                                                                                                                    \
+                                                                                                                                                         \
+    winrt::Windows::Foundation::IInspectable Current##name()                                                                                             \
+    {                                                                                                                                                    \
+        return winrt::box_value<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>(_##name##Map.Lookup(settingsModelName().settingNameInModel())); \
+    }                                                                                                                                                    \
+                                                                                                                                                         \
+    void Current##name(const winrt::Windows::Foundation::IInspectable& enumEntry)                                                                        \
+    {                                                                                                                                                    \
+        if (auto ee = enumEntry.try_as<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry>())                                                       \
+        {                                                                                                                                                \
+            auto setting = winrt::unbox_value<enumType>(ee.EnumValue());                                                                                 \
+            settingsModelName().settingNameInModel(setting);                                                                                             \
+        }                                                                                                                                                \
+    }                                                                                                                                                    \
+                                                                                                                                                         \
+private:                                                                                                                                                 \
+    winrt::Windows::Foundation::Collections::IObservableVector<winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> _##name##List;                   \
     winrt::Windows::Foundation::Collections::IMap<enumType, winrt::Microsoft::Terminal::Settings::Editor::EnumEntry> _##name##Map;
 
 // This macro defines a dependency property for a WinRT class.
--- src/inc/WilErrorReporting.h
+++ src/inc/WilErrorReporting.h
@@ -10,27 +10,6 @@ Module Name:
 #include <winmeta.h>
 #include <wil/common.h>
 
-#define CONSOLE_WIL_TRACELOGGING_COMMON_FAILURE_PARAMS(failure)                                                                                                                                    \
-    TraceLoggingUInt32((failure).hr, "hresult", "Failure error code"),                                                                                                                             \
-        TraceLoggingString((failure).pszFile, "fileName", "Source code file name where the error occurred"),                                                                                       \
-        TraceLoggingUInt32((failure).uLineNumber, "lineNumber", "Line number within the source code file where the error occurred"),                                                               \
-        TraceLoggingString((failure).pszModule, "module", "Name of the binary where the error occurred"),                                                                                          \
-        TraceLoggingUInt32(static_cast<DWORD>((failure).type), "failureType", "Indicates what type of failure was observed (exception, returned error, logged error or fail fast"),                \
-        TraceLoggingWideString((failure).pszMessage, "message", "Custom message associated with the failure (if any)"),                                                                            \
-        TraceLoggingUInt32((failure).threadId, "threadId", "Identifier of the thread the error occurred on"),                                                                                      \
-        TraceLoggingString((failure).pszCallContext, "callContext", "List of containing this error"),                                                                                              \
-        TraceLoggingUInt32((failure).callContextOriginating.contextId, "originatingContextId", "Identifier for the oldest activity containing this error"),                                        \
-        TraceLoggingString((failure).callContextOriginating.contextName, "originatingContextName", "Name of the oldest activity containing this error"),                                           \
-        TraceLoggingWideString((failure).callContextOriginating.contextMessage, "originatingContextMessage", "Custom message associated with the oldest activity containing this error (if any)"), \
-        TraceLoggingUInt32((failure).callContextCurrent.contextId, "currentContextId", "Identifier for the newest activity containing this error"),                                                \
-        TraceLoggingString((failure).callContextCurrent.contextName, "currentContextName", "Name of the newest activity containing this error"),                                                   \
-        TraceLoggingWideString((failure).callContextCurrent.contextMessage, "currentContextMessage", "Custom message associated with the newest activity containing this error (if any)")
-
-#define CONSOLE_WIL_TRACELOGGING_FAILURE_PARAMS(failure)       \
-    TelemetryPrivacyDataTag(PDT_ProductAndServicePerformance), \
-        TraceLoggingStruct(14, "wilResult"),                   \
-        CONSOLE_WIL_TRACELOGGING_COMMON_FAILURE_PARAMS(failure)
-
 namespace Microsoft::Console::ErrorReporting
 {
     __declspec(selectany) TraceLoggingHProvider FallbackProvider;
@@ -39,13 +18,29 @@ namespace Microsoft::Console::ErrorReporting
     {
         if (!alreadyReported && FallbackProvider)
         {
-#pragma warning(suppress : 26477 26485 26494 26482 26446) // We don't control TraceLoggingWrite
-            TraceLoggingWrite(FallbackProvider,
-                              "FallbackError",
-                              TraceLoggingKeyword(MICROSOFT_KEYWORD_TELEMETRY),
-                              TelemetryPrivacyDataTag(PDT_ProductAndServiceUsage),
-                              TraceLoggingLevel(WINEVENT_LEVEL_ERROR),
-                              CONSOLE_WIL_TRACELOGGING_FAILURE_PARAMS(failure));
+#pragma warning(suppress : 26477) // Use 'nullptr' rather than 0 or NULL
+            TraceLoggingWrite(
+                FallbackProvider,
+                "FallbackError",
+                TraceLoggingKeyword(MICROSOFT_KEYWORD_TELEMETRY),
+                TelemetryPrivacyDataTag(PDT_ProductAndServiceUsage),
+                TraceLoggingLevel(WINEVENT_LEVEL_ERROR),
+                TelemetryPrivacyDataTag(PDT_ProductAndServicePerformance),
+                TraceLoggingStruct(14, "wilResult", "wilResult"),
+                TraceLoggingUInt32(failure.hr, "hresult", "Failure error code"),
+                TraceLoggingString(failure.pszFile, "fileName", "Source code file name where the error occurred"),
+                TraceLoggingUInt32(failure.uLineNumber, "lineNumber", "Line number within the source code file where the error occurred"),
+                TraceLoggingString(failure.pszModule, "module", "Name of the binary where the error occurred"),
+                TraceLoggingUInt32(static_cast<DWORD>(failure.type), "failureType", "Indicates what type of failure was observed (exception, returned error, logged error or fail fast"),
+                TraceLoggingWideString(failure.pszMessage, "message", "Custom message associated with the failure (if any)"),
+                TraceLoggingUInt32(failure.threadId, "threadId", "Identifier of the thread the error occurred on"),
+                TraceLoggingString(failure.pszCallContext, "callContext", "List of containing this error"),
+                TraceLoggingUInt32(failure.callContextOriginating.contextId, "originatingContextId", "Identifier for the oldest activity containing this error"),
+                TraceLoggingString(failure.callContextOriginating.contextName, "originatingContextName", "Name of the oldest activity containing this error"),
+                TraceLoggingWideString(failure.callContextOriginating.contextMessage, "originatingContextMessage", "Custom message associated with the oldest activity containing this error (if any)"),
+                TraceLoggingUInt32(failure.callContextCurrent.contextId, "currentContextId", "Identifier for the newest activity containing this error"),
+                TraceLoggingString(failure.callContextCurrent.contextName, "currentContextName", "Name of the newest activity containing this error"),
+                TraceLoggingWideString(failure.callContextCurrent.contextMessage, "currentContextMessage", "Custom message associated with the newest activity containing this error (if any)"));
         }
     }
     catch (...)
