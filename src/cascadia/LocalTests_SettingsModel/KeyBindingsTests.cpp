d465a47bc5bdd6eca2620a67144d139226d1d617
Leonard Hecker <lhecker@microsoft.com>
2021-08-12 01:09:25 +0200

    Fix layering of sc() keybindings with vk() ones (#10917)
    The quake mode keybinding is bound to a scancode. This made it
    impossible to override it with a vkey-based one like "win+\`".
    This commit fixes the issue by making sure that a `KeyChord` always has a vkey,
    and leveraging this fact inside ActionMap, which now ignores the scan-code.
    
    ## PR Checklist
    * [x] Closes #10875
    * [x] I work here
    * [x] Tests added/passed
    
    ## Validation Steps Performed
    
    * quake mode and other keybinding still work ✔️
    * Repro settings from #10875 work correctly ✔️

--- src/cascadia/LocalTests_SettingsModel/KeyBindingsTests.cpp
+++ src/cascadia/LocalTests_SettingsModel/KeyBindingsTests.cpp
@@ -109,24 +109,23 @@ namespace SettingsModelLocalTests
             },
         };
 
-        // Use the KeyChordHash and KeyChordEquality to compare if two
-        // KeyChord's are the same. If you try to just directly compare them
-        // with VERIFY_ARE_EQUAL, that will always fail. It'll revert to the
-        // default winrt equality operator, which will compare if they point to
-        // literally the same object (they won't).
-        implementation::KeyChordHash hash;
-        implementation::KeyChordEquality equals;
         for (const auto& tc : testCases)
         {
             Log::Comment(NoThrowString().Format(L"Testing case:\"%s\"", tc.expected.data()));
 
-            KeyChord expectedKeyChord{ tc.modifiers, tc.vkey, tc.scanCode };
-            const auto actualString = KeyChordSerialization::ToString(expectedKeyChord);
+            const auto actualString = KeyChordSerialization::ToString({ tc.modifiers, tc.vkey, tc.scanCode });
             VERIFY_ARE_EQUAL(tc.expected, actualString);
 
+            auto expectedVkey = tc.vkey;
+            if (!expectedVkey)
+            {
+                expectedVkey = MapVirtualKeyW(tc.scanCode, MAPVK_VSC_TO_VK_EX);
+            }
+
             const auto actualKeyChord = KeyChordSerialization::FromString(actualString);
-            VERIFY_ARE_EQUAL(hash(expectedKeyChord), hash(actualKeyChord));
-            VERIFY_IS_TRUE(equals(expectedKeyChord, actualKeyChord));
+            VERIFY_ARE_EQUAL(tc.modifiers, actualKeyChord.Modifiers());
+            VERIFY_ARE_EQUAL(expectedVkey, actualKeyChord.Vkey());
+            VERIFY_ARE_EQUAL(tc.scanCode, actualKeyChord.ScanCode());
         }
     }
 
