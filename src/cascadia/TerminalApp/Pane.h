0c1665b77477c8f021e2a84bea86e079d003fc3d
Schuyler Rosefield <Rosefield@users.noreply.github.com>
2021-08-26 15:28:36 -0400

    RIP ActualSize navigation Jul 2021 - Aug 2021

--- src/cascadia/TerminalApp/Pane.h
+++ src/cascadia/TerminalApp/Pane.h
@@ -203,7 +203,7 @@ private:
 
     std::shared_ptr<Pane> _FindParentOfPane(const std::shared_ptr<Pane> pane);
     std::pair<PanePoint, PanePoint> _GetOffsetsForPane(const PanePoint parentOffset) const;
-    bool _IsAdjacent(const std::shared_ptr<Pane> first, const PanePoint firstOffset, const std::shared_ptr<Pane> second, const PanePoint secondOffset, const winrt::Microsoft::Terminal::Settings::Model::FocusDirection& direction) const;
+    bool _IsAdjacent(const PanePoint firstOffset, const PanePoint secondOffset, const winrt::Microsoft::Terminal::Settings::Model::FocusDirection& direction) const;
     PaneNeighborSearch _FindNeighborForPane(const winrt::Microsoft::Terminal::Settings::Model::FocusDirection& direction,
                                             PaneNeighborSearch searchResult,
                                             const bool focusIsSecondSide,
