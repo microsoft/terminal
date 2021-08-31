81af0651a2e3e99978f866ac114d7e0ab2bc4f34
Schuyler Rosefield <Rosefield@users.noreply.github.com>
2021-08-31 14:40:05 -0400

    even merges arent safe from the formatter.

--- src/cascadia/TerminalSettingsModel/ApplicationState.h
+++ src/cascadia/TerminalSettingsModel/ApplicationState.h
@@ -25,8 +25,8 @@ Abstract:
 //   (type, function name, JSON key, ...variadic construction arguments)
 namespace winrt::Microsoft::Terminal::Settings::Model::implementation
 {
-#define MTSM_APPLICATION_STATE_FIELDS(X)                                       \
-    X(std::unordered_set<winrt::guid>, GeneratedProfiles, "generatedProfiles") \
+#define MTSM_APPLICATION_STATE_FIELDS(X)                                                                                \
+    X(std::unordered_set<winrt::guid>, GeneratedProfiles, "generatedProfiles")                                          \
     X(Windows::Foundation::Collections::IVector<Model::WindowLayout>, PersistedWindowLayouts, "persistedWindowLayouts") \
     X(Windows::Foundation::Collections::IVector<hstring>, RecentCommands, "recentCommands")
 
