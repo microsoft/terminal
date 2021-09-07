d16fa55f032eed3e06d3571cddecba47c2f91244
Mike Griese <migrie@microsoft.com>
2021-09-07 16:39:31 -0500

    Revert "In retrospect, I hate doing it this way"
    This reverts commit cb4f58dc7e0c1804f8506dd63031ee761aed585c.

--- src/cascadia/TerminalSettingsModel/TerminalSettings.h
+++ src/cascadia/TerminalSettingsModel/TerminalSettings.h
@@ -118,7 +118,7 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
 
         INHERITABLE_SETTING(Model::TerminalSettings, hstring, ProfileName);
         INHERITABLE_SETTING(Model::TerminalSettings, bool, UseAcrylic, false);
-        INHERITABLE_SETTING(Model::TerminalSettings, uint32_t, Opacity, UseAcrylic() ? 50 : 100);
+        INHERITABLE_SETTING(Model::TerminalSettings, double, Opacity, 0.5);
         INHERITABLE_SETTING(Model::TerminalSettings, hstring, Padding, DEFAULT_PADDING);
         INHERITABLE_SETTING(Model::TerminalSettings, hstring, FontFace, DEFAULT_FONT_FACE);
         INHERITABLE_SETTING(Model::TerminalSettings, int32_t, FontSize, DEFAULT_FONT_SIZE);
