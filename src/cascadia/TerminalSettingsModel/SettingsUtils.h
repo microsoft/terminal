85f487389d27df763cb71e10a631869e8d1045fa
Pankaj Bhojwani <pabhojwa@microsoft.com>
2021-10-06 14:56:53 -0700

    format

--- src/cascadia/TerminalSettingsModel/SettingsUtils.h
+++ src/cascadia/TerminalSettingsModel/SettingsUtils.h
@@ -26,34 +26,34 @@ Author(s):
     X(bool, TrimBlockSelection, false)                  \
     X(bool, DetectURLs, true)
 
-#define GLOBAL_APP_SETTINGS(X)                          \
-    X(bool, AlwaysShowTabs, true) \
-    X(bool, ShowTitleInTitlebar, true) \
-    X(bool, ConfirmCloseAllTabs, true) \
-    X(hstring, Language) \
-    X(winrt::Windows::UI::Xaml::ElementTheme, Theme, winrt::Windows::UI::Xaml::ElementTheme::Default) \
+#define GLOBAL_APP_SETTINGS(X)                                                                                                             \
+    X(bool, AlwaysShowTabs, true)                                                                                                          \
+    X(bool, ShowTitleInTitlebar, true)                                                                                                     \
+    X(bool, ConfirmCloseAllTabs, true)                                                                                                     \
+    X(hstring, Language)                                                                                                                   \
+    X(winrt::Windows::UI::Xaml::ElementTheme, Theme, winrt::Windows::UI::Xaml::ElementTheme::Default)                                      \
     X(winrt::Microsoft::UI::Xaml::Controls::TabViewWidthMode, TabWidthMode, winrt::Microsoft::UI::Xaml::Controls::TabViewWidthMode::Equal) \
-    X(bool, UseAcrylicInTabRow, false) \
-    X(bool, ShowTabsInTitlebar, true) \
-    X(bool, InputServiceWarning, true) \
-    X(winrt::Microsoft::Terminal::Control::CopyFormat, CopyFormatting, 0) \
-    X(bool, WarnAboutLargePaste, true) \
-    X(bool, WarnAboutMultiLinePaste, true) \
-    X(Model::LaunchPosition, InitialPosition, nullptr, nullptr) \
-    X(bool, CenterOnLaunch, false) \
-    X(Model::FirstWindowPreference, FirstWindowPreference, FirstWindowPreference::DefaultProfile) \
-    X(Model::LaunchMode, LaunchMode, LaunchMode::DefaultMode) \
-    X(bool, SnapToGridOnResize, true) \
-    X(bool, DebugFeaturesEnabled, debugFeaturesDefault) \
-    X(bool, StartOnUserLogin, false) \
-    X(bool, AlwaysOnTop, false) \
-    X(Model::TabSwitcherMode, TabSwitcherMode, Model::TabSwitcherMode::InOrder) \
-    X(bool, DisableAnimations, false) \
-    X(hstring, StartupActions, L"") \
-    X(Model::WindowingMode, WindowingBehavior, Model::WindowingMode::UseNew) \
-    X(bool, MinimizeToNotificationArea, false) \
-    X(bool, AlwaysShowNotificationIcon, false) \
-    X(winrt::Windows::Foundation::Collections::IVector<winrt::hstring>, DisabledProfileSources, nullptr) \
+    X(bool, UseAcrylicInTabRow, false)                                                                                                     \
+    X(bool, ShowTabsInTitlebar, true)                                                                                                      \
+    X(bool, InputServiceWarning, true)                                                                                                     \
+    X(winrt::Microsoft::Terminal::Control::CopyFormat, CopyFormatting, 0)                                                                  \
+    X(bool, WarnAboutLargePaste, true)                                                                                                     \
+    X(bool, WarnAboutMultiLinePaste, true)                                                                                                 \
+    X(Model::LaunchPosition, InitialPosition, nullptr, nullptr)                                                                            \
+    X(bool, CenterOnLaunch, false)                                                                                                         \
+    X(Model::FirstWindowPreference, FirstWindowPreference, FirstWindowPreference::DefaultProfile)                                          \
+    X(Model::LaunchMode, LaunchMode, LaunchMode::DefaultMode)                                                                              \
+    X(bool, SnapToGridOnResize, true)                                                                                                      \
+    X(bool, DebugFeaturesEnabled, debugFeaturesDefault)                                                                                    \
+    X(bool, StartOnUserLogin, false)                                                                                                       \
+    X(bool, AlwaysOnTop, false)                                                                                                            \
+    X(Model::TabSwitcherMode, TabSwitcherMode, Model::TabSwitcherMode::InOrder)                                                            \
+    X(bool, DisableAnimations, false)                                                                                                      \
+    X(hstring, StartupActions, L"")                                                                                                        \
+    X(Model::WindowingMode, WindowingBehavior, Model::WindowingMode::UseNew)                                                               \
+    X(bool, MinimizeToNotificationArea, false)                                                                                             \
+    X(bool, AlwaysShowNotificationIcon, false)                                                                                             \
+    X(winrt::Windows::Foundation::Collections::IVector<winrt::hstring>, DisabledProfileSources, nullptr)                                   \
     X(bool, ShowAdminShield, true)
 
 #define PROFILE_CONTROL_SETTINGS(X)                                                                                     \
@@ -66,13 +66,13 @@ Author(s):
     X(Microsoft::Terminal::Control::ScrollbarState, ScrollState, Microsoft::Terminal::Control::ScrollbarState::Visible) \
     X(Microsoft::Terminal::Control::TextAntialiasingMode, AntialiasingMode, Microsoft::Terminal::Control::TextAntialiasingMode::Grayscale)
 
-#define PROFILE_APP_SETTINGS(X)     \
-    X(guid, ConnectionType) \
-    X(hstring, Icon, L"\uE756") \
+#define PROFILE_APP_SETTINGS(X)                                \
+    X(guid, ConnectionType)                                    \
+    X(hstring, Icon, L"\uE756")                                \
     X(CloseOnExitMode, CloseOnExit, CloseOnExitMode::Graceful) \
-    X(hstring, TabTitle) \
-    X(bool, SuppressApplicationTitle, false) \
-    X(hstring, StartingDirectory) \
+    X(hstring, TabTitle)                                       \
+    X(bool, SuppressApplicationTitle, false)                   \
+    X(hstring, StartingDirectory)                              \
     X(Model::BellStyle, BellStyle, BellStyle::Audible)
 
 #define FONT_SETTINGS(X)                                \
@@ -90,9 +90,9 @@ Author(s):
     X(bool, RetroTerminalEffect, false)                                                                                              \
     X(hstring, PixelShaderPath)
 
-#define APPEARANCE_APP_SETTINGS(X)                                                                                                   \
+#define APPEARANCE_APP_SETTINGS(X)                                                                                               \
     X(ConvergedAlignment, BackgroundImageAlignment, ConvergedAlignment::Horizontal_Center | ConvergedAlignment::Vertical_Center) \
-    X(hstring, ColorSchemeName, L"Campbell") \
-    X(hstring, BackgroundImagePath) \
-    X(Model::IntenseStyle, IntenseTextStyle, Model::IntenseStyle::Bright) \
+    X(hstring, ColorSchemeName, L"Campbell")                                                                                     \
+    X(hstring, BackgroundImagePath)                                                                                              \
+    X(Model::IntenseStyle, IntenseTextStyle, Model::IntenseStyle::Bright)                                                        \
     X(double, Opacity, 1.0)
