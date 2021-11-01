684956d848fabc9e971813606d905c66ae14894d
Mike Griese <migrie@microsoft.com>
2021-11-01 14:57:53 -0500

    thanks for nothing VS

--- src/cascadia/TerminalApp/Pane.cpp
+++ src/cascadia/TerminalApp/Pane.cpp
@@ -1162,7 +1162,7 @@ void Pane::_ControlWarningBellHandler(const winrt::Windows::Foundation::IInspect
                 if (sounds && sounds.Size() > 0)
                 {
                     winrt::hstring soundPath{ wil::ExpandEnvironmentStringsW<std::wstring>(sounds.GetAt(rand() % sounds.Size()).c_str()) };
-                    winrt::Windows::Foundation::Uri uri { soundPath };
+                    winrt::Windows::Foundation::Uri uri{ soundPath };
                     _playBellSound(uri);
                 }
                 else
