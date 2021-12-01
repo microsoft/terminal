microsoft/terminal

commit b1131263cf6d7338810557cafe6378a9d13a1e81

Fix alt+space opening system menu and sending keys to terminal (#10988)
If both of the following are true

1. alt+space is not explicitly unbound
2. alt+space is not bound to a command

Then the window procedure will handle the alt+space to open up the context menu.
In this case, we need to make sure we don't send the keys to terminal.

Closes #10935

src/cascadia/TerminalControl/TermControl.cpp
@@ -900,6 +900,24 @@ namespace winrt::Microsoft::Terminal::Control::implementation
             return;
         }
 
+        if (vkey == VK_SPACE && modifiers.IsAltPressed())
+        {
+            if (const auto bindings = _settings.KeyBindings())
+            {
+                if (!bindings.IsKeyChordExplicitlyUnbound({ modifiers.IsCtrlPressed(), modifiers.IsAltPressed(), modifiers.IsShiftPressed(), modifiers.IsWinPressed(), vkey, scanCode }))
+                {
+                    // If we get here, it means that
+                    //      1. we do not have a command bound to alt+space
+                    //      2. alt+space was not explicitly unbound
+                    // That means that XAML handled the alt+space to open up the context menu, and
+                    // so we don't want to send anything to the terminal
+                    // TODO GH#11018: Add a new "openSystemMenu" keybinding
+                    e.Handled(true);
+                    return;
+                }
+            }
+        }
+
         if (_TrySendKeyEvent(vkey, scanCode, modifiers, keyDown))
         {
             e.Handled(true);

commit 2bd376832f1a3c71c5a810bd063ef5757c7204f6

Fix AltGr+Space not working (#11832)
This fixes a regression introduced in #10988 for the 1.11 release branch.
For later branches this issue was fixed in #11086.

## PR Checklist
* [x] Closes #11649
* [x] I work here
* [x] Tests added/passed

## Validation Steps Performed
* AltGr+Space produces "_" with the bépo keyboard layout

src/cascadia/TerminalControl/TermControl.cpp
@@ -909,7 +909,7 @@ namespace winrt::Microsoft::Terminal::Control::implementation
             return;
         }
 
-        if (vkey == VK_SPACE && modifiers.IsAltPressed())
+        if (vkey == VK_SPACE && modifiers.IsAltPressed() && !modifiers.IsAltGrPressed())
         {
             if (const auto bindings = _settings.KeyBindings())
             {