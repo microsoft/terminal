696bfa51dba9427afb553e6f810043004b037011
Mike Griese <migrie@microsoft.com>
2022-01-11 16:58:17 -0600

    the core one

--- src/cascadia/UnitTests_TerminalCore/MockTermSettings.h
+++ src/cascadia/UnitTests_TerminalCore/MockTermSettings.h
@@ -4,6 +4,7 @@
 #include <WexTestClass.h>
 
 #include "DefaultSettings.h"
+#include "../../inc/ControlProperties.h"
 
 #include <winrt/Microsoft.Terminal.Core.h>
 
@@ -13,75 +14,30 @@ namespace TerminalCoreUnitTests
 {
     class MockTermSettings : public winrt::implements<MockTermSettings, ICoreSettings, ICoreAppearance>
     {
+        // Color Table is special because it's an array
+        std::array<winrt::Microsoft::Terminal::Core::Color, COLOR_TABLE_SIZE> _ColorTable;
+
+#define SETTINGS_GEN(type, name, ...) WINRT_PROPERTY(type, name, __VA_ARGS__);
+        CORE_SETTINGS(SETTINGS_GEN)
+        CORE_APPEARANCE_SETTINGS(SETTINGS_GEN)
+#undef SETTINGS_GEN
+
     public:
         MockTermSettings(int32_t historySize, int32_t initialRows, int32_t initialCols) :
-            _historySize(historySize),
-            _initialRows(initialRows),
-            _initialCols(initialCols)
+            _HistorySize(historySize),
+            _InitialRows(initialRows),
+            _InitialCols(initialCols)
         {
         }
 
-        // property getters - all implemented
-        int32_t HistorySize() { return _historySize; }
-        int32_t InitialRows() { return _initialRows; }
-        int32_t InitialCols() { return _initialCols; }
-        til::color DefaultForeground() { return COLOR_WHITE; }
-        til::color DefaultBackground() { return COLOR_BLACK; }
-        bool SnapOnInput() { return false; }
-        bool AltGrAliasing() { return true; }
-        til::color CursorColor() { return COLOR_WHITE; }
-        CursorStyle CursorShape() const noexcept { return CursorStyle::Vintage; }
-        uint32_t CursorHeight() { return 42UL; }
-        winrt::hstring WordDelimiters() { return winrt::hstring(DEFAULT_WORD_DELIMITERS); }
-        bool CopyOnSelect() { return _copyOnSelect; }
-        bool FocusFollowMouse() { return _focusFollowMouse; }
-        winrt::hstring StartingTitle() { return _startingTitle; }
-        bool SuppressApplicationTitle() { return _suppressApplicationTitle; }
-        til::color SelectionBackground() { return COLOR_WHITE; }
-        bool ForceVTInput() { return false; }
-        ICoreAppearance UnfocusedAppearance() { return {}; };
-        winrt::Windows::Foundation::IReference<winrt::Microsoft::Terminal::Core::Color> TabColor() { return nullptr; }
-        winrt::Windows::Foundation::IReference<winrt::Microsoft::Terminal::Core::Color> StartingTabColor() { return nullptr; }
-        bool TrimBlockSelection() { return false; }
-        bool DetectURLs() { return true; }
-
-        // other implemented methods
-        til::color GetColorTableEntry(int32_t) const { return 123; }
-
-        // property setters - all unimplemented
-        void HistorySize(int32_t) {}
-        void InitialRows(int32_t) {}
-        void InitialCols(int32_t) {}
-        void DefaultForeground(til::color) {}
-        void DefaultBackground(til::color) {}
-        void SnapOnInput(bool) {}
-        void AltGrAliasing(bool) {}
-        void CursorColor(til::color) {}
-        void CursorShape(CursorStyle const&) noexcept {}
-        void CursorHeight(uint32_t) {}
-        void WordDelimiters(winrt::hstring) {}
-        void CopyOnSelect(bool copyOnSelect) { _copyOnSelect = copyOnSelect; }
-        void FocusFollowMouse(bool focusFollowMouse) { _focusFollowMouse = focusFollowMouse; }
-        void StartingTitle(winrt::hstring const& value) { _startingTitle = value; }
-        void SuppressApplicationTitle(bool suppressApplicationTitle) { _suppressApplicationTitle = suppressApplicationTitle; }
-        void SelectionBackground(til::color) {}
-        void ForceVTInput(bool) {}
-        void UnfocusedAppearance(ICoreAppearance) {}
-        void TabColor(const IInspectable&) {}
-        void StartingTabColor(const IInspectable&) {}
-        void TrimBlockSelection(bool) {}
-        void DetectURLs(bool) {}
-
-        WINRT_PROPERTY(bool, IntenseIsBright, true);
-        WINRT_PROPERTY(bool, AdjustIndistinguishableColors, true);
-
-    private:
-        int32_t _historySize;
-        int32_t _initialRows;
-        int32_t _initialCols;
-        bool _copyOnSelect{ false };
-        bool _focusFollowMouse{ false };
-        bool _suppressApplicationTitle{ false };
-        winrt::hstring _startingTitle;
+        winrt::Microsoft::Terminal::Core::Color GetColorTableEntry(int32_t index) noexcept
+        {
+            return _ColorTable.at(index);
+        }
+        void SetColorTableEntry(int32_t index,
+                                winrt::Microsoft::Terminal::Core::Color color) noexcept
+        {
+            _ColorTable.at(index) = color;
+        }
     };
 }
