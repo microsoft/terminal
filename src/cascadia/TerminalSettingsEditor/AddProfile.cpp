78530742c3286ca76ced3321a2d1bce7af05d92e
Carlos Zamora <cazamor@microsoft.com>
2022-02-01 15:29:23 -0800

    Update accessible name for 'new profile' button

--- src/cascadia/TerminalSettingsEditor/AddProfile.cpp
+++ src/cascadia/TerminalSettingsEditor/AddProfile.cpp
@@ -7,9 +7,12 @@
 #include "AddProfilePageNavigationState.g.cpp"
 #include "EnumEntry.h"
 
+#include <LibraryResources.h>
+
 using namespace winrt::Windows::Foundation;
 using namespace winrt::Windows::System;
 using namespace winrt::Windows::UI::Core;
+using namespace winrt::Windows::UI::Xaml;
 using namespace winrt::Windows::UI::Xaml::Navigation;
 using namespace winrt::Microsoft::Terminal::Settings::Model;
 
@@ -18,6 +21,8 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
     AddProfile::AddProfile()
     {
         InitializeComponent();
+
+        Automation::AutomationProperties::SetName(AddNewButton(), RS_(L"AddProfile_AddNewTextBlock/Text"));
     }
 
     void AddProfile::OnNavigatedTo(const NavigationEventArgs& e)
