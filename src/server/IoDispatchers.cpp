microsoft/terminal

commit 2b202ce6d993404010337fe8ff205d7b240d60f5

Enable /permissive- and remaining /Zc flags (#11816)
This commit enables /permissive- for all projects, as well as all other /Zc
flags not enabled by default by /permissive-. Some projects continue to be
built under /Zc:twoPhase- as JsonUtils.h fails to compile otherwise.

## PR Checklist
* [x] Closes #10703
* [x] I work here
* [x] Tests added/passed

src/server/IoDispatchers.cpp
@@ -92,7 +92,9 @@ PCONSOLE_API_MSG IoDispatchers::ConsoleCreateObject(_In_ PCONSOLE_API_MSG pMessa
 
     if (!NT_SUCCESS(Status))
     {
-        goto Error;
+        UnlockConsole();
+        pMessage->SetReplyStatus(Status);
+        return pMessage;
     }
 
     auto deviceComm{ ServiceLocator::LocateGlobals().pDeviceComm };
@@ -110,16 +112,6 @@ PCONSOLE_API_MSG IoDispatchers::ConsoleCreateObject(_In_ PCONSOLE_API_MSG pMessa
     UnlockConsole();
 
     return nullptr;
-
-Error:
-
-    FAIL_FAST_IF(NT_SUCCESS(Status));
-
-    UnlockConsole();
-
-    pMessage->SetReplyStatus(Status);
-
-    return pMessage;
 }
 
 // Routine Description:
@@ -256,17 +248,32 @@ PCONSOLE_API_MSG IoDispatchers::ConsoleHandleConnectionRequest(_In_ PCONSOLE_API
     Telemetry::Instance().LogApiCall(Telemetry::ApiCall::AttachConsole);
 
     ConsoleProcessHandle* ProcessData = nullptr;
+    NTSTATUS Status;
 
     LockConsole();
+
+    const auto cleanup = wil::scope_exit([&]() noexcept {
+        UnlockConsole();
+
+        if (!NT_SUCCESS(Status))
+        {
+            pReceiveMsg->SetReplyStatus(Status);
+            if (ProcessData != nullptr)
+            {
+                CommandHistory::s_Free(ProcessData);
+                gci.ProcessHandleList.FreeProcessData(ProcessData);
+            }
+        }
+    });
 
     DWORD const dwProcessId = (DWORD)pReceiveMsg->Descriptor.Process;
     DWORD const dwThreadId = (DWORD)pReceiveMsg->Descriptor.Object;
 
     CONSOLE_API_CONNECTINFO Cac;
-    NTSTATUS Status = ConsoleInitializeConnectInfo(pReceiveMsg, &Cac);
+    Status = ConsoleInitializeConnectInfo(pReceiveMsg, &Cac);
     if (!NT_SUCCESS(Status))
     {
-        goto Error;
+        return pReceiveMsg;
     }
 
     // If we pass the tests...
@@ -354,7 +361,7 @@ PCONSOLE_API_MSG IoDispatchers::ConsoleHandleConnectionRequest(_In_ PCONSOLE_API
 
     if (!NT_SUCCESS(Status))
     {
-        goto Error;
+        return pReceiveMsg;
     }
 
     ProcessData->fRootProcess = WI_IsFlagClear(gci.Flags, CONSOLE_INITIALIZED);
@@ -376,7 +383,7 @@ PCONSOLE_API_MSG IoDispatchers::ConsoleHandleConnectionRequest(_In_ PCONSOLE_API
         Status = ConsoleAllocateConsole(&Cac);
         if (!NT_SUCCESS(Status))
         {
-            goto Error;
+            return pReceiveMsg;
         }
 
         WI_SetFlag(gci.Flags, CONSOLE_INITIALIZED);
@@ -389,7 +396,7 @@ PCONSOLE_API_MSG IoDispatchers::ConsoleHandleConnectionRequest(_In_ PCONSOLE_API
     catch (...)
     {
         LOG_CAUGHT_EXCEPTION();
-        goto Error;
+        return pReceiveMsg;
     }
 
     gci.ProcessHandleList.ModifyConsoleProcessFocus(WI_IsFlagSet(gci.Flags, CONSOLE_HAS_FOCUS));
@@ -403,7 +410,7 @@ PCONSOLE_API_MSG IoDispatchers::ConsoleHandleConnectionRequest(_In_ PCONSOLE_API
 
     if (!NT_SUCCESS(Status))
     {
-        goto Error;
+        return pReceiveMsg;
     }
 
     auto& screenInfo = gci.GetActiveOutputBuffer().GetMainBuffer();
@@ -414,7 +421,7 @@ PCONSOLE_API_MSG IoDispatchers::ConsoleHandleConnectionRequest(_In_ PCONSOLE_API
 
     if (!NT_SUCCESS(Status))
     {
-        goto Error;
+        return pReceiveMsg;
     }
 
     // Complete the request.
@@ -434,24 +441,7 @@ PCONSOLE_API_MSG IoDispatchers::ConsoleHandleConnectionRequest(_In_ PCONSOLE_API
 
     Tracing::s_TraceConsoleAttachDetach(ProcessData, true);
 
-    UnlockConsole();
-
     return nullptr;
-
-Error:
-    FAIL_FAST_IF(NT_SUCCESS(Status));
-
-    if (ProcessData != nullptr)
-    {
-        CommandHistory::s_Free((HANDLE)ProcessData);
-        gci.ProcessHandleList.FreeProcessData(ProcessData);
-    }
-
-    UnlockConsole();
-
-    pReceiveMsg->SetReplyStatus(Status);
-
-    return pReceiveMsg;
 }
 
 // Routine Description:

src/cascadia/LocalTests_SettingsModel/SettingsModel.LocalTests.vcxproj
@@ -69,7 +69,8 @@
     <ClCompile>
       <AdditionalIncludeDirectories>..;$(OpenConsoleDir)\dep;$(OpenConsoleDir)\dep\jsoncpp\json;$(OpenConsoleDir)src\inc;$(OpenConsoleDir)src\inc\test;$(WinRT_IncludePath)\..\cppwinrt\winrt;"$(OpenConsoleDir)\src\cascadia\TerminalSettingsModel\Generated Files";%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <PrecompiledHeaderFile>pch.h</PrecompiledHeaderFile>
-
+      <!-- Required for JsonUtils.h. -->
+      <AdditionalOptions>%(AdditionalOptions) /Zc:twoPhase-</AdditionalOptions>
       <!-- Manually disable unreachable code warning, because jconcpp has a ton of that. -->
       <DisableSpecificWarnings>4702;%(DisableSpecificWarnings)</DisableSpecificWarnings>
     </ClCompile>

src/cascadia/LocalTests_TerminalApp/TestHostApp/TestHostApp.vcxproj
@@ -51,10 +51,10 @@
 
   <ItemDefinitionGroup>
     <ClCompile>
+      <!-- Disable two-phase name lookup for C++/CLI & C++/CX code. -->
+      <AdditionalOptions>%(AdditionalOptions) /Zc:twoPhase-</AdditionalOptions>
       <DisableSpecificWarnings>4453;%(DisableSpecificWarnings)</DisableSpecificWarnings>
-      <AdditionalIncludeDirectories>%(AdditionalIncludeDirectories)
-        ;$(IntermediateOutputPath)
-      </AdditionalIncludeDirectories>
+      <AdditionalIncludeDirectories>%(AdditionalIncludeDirectories);$(IntermediateOutputPath)</AdditionalIncludeDirectories>
       <PreprocessorDefinitions>INLINE_TEST_METHOD_MARKUP;%(PreprocessorDefinitions)</PreprocessorDefinitions>
     </ClCompile>
     <Link>

src/cascadia/TerminalControl/pch.h
@@ -57,6 +57,7 @@
 TRACELOGGING_DECLARE_PROVIDER(g_hTerminalControlProvider);
 #include <telemetry/ProjectTelemetry.h>
 
+#include <ShlObj_core.h>
 #include <WinUser.h>
 
 #include "til.h"

src/cascadia/TerminalSettingsModel/Microsoft.Terminal.Settings.ModelLib.vcxproj
@@ -251,6 +251,8 @@
     <ClCompile>
       <PrecompiledHeaderFile>pch.h</PrecompiledHeaderFile>
       <AdditionalIncludeDirectories>..;$(OpenConsoleDir)\dep\jsoncpp\json;%(AdditionalIncludeDirectories);</AdditionalIncludeDirectories>
+      <!-- Required for JsonUtils.h. -->
+      <AdditionalOptions>%(AdditionalOptions) /Zc:twoPhase-</AdditionalOptions>
       <!-- Manually disable unreachable code warning, because jconcpp has a ton of that. -->
       <DisableSpecificWarnings>4702;%(DisableSpecificWarnings)</DisableSpecificWarnings>
     </ClCompile>

src/cascadia/UnitTests_TerminalCore/TestUtils.h
@@ -171,7 +171,7 @@ public:
             auto mismatched = (actualChars != expectedChars || actualAttrs != expectedAttrs);
             if (mismatched)
             {
-                Log::Comment(NoThrowString().Format(
+                WEX::Logging::Log::Comment(WEX::Common::NoThrowString().Format(
                     L"Character or attribute at index %d was mismatched", charsProcessed));
             }
 

src/cascadia/ut_app/TerminalApp.UnitTests.vcxproj
@@ -60,7 +60,8 @@
     <ClCompile>
       <AdditionalIncludeDirectories>..;$(OpenConsoleDir)src\cascadia\inc;$(OpenConsoleDir)\dep\jsoncpp\json;$(OpenConsoleDir)src\inc;$(OpenConsoleDir)src\inc\test;$(WinRT_IncludePath)\..\cppwinrt\winrt;"$(OpenConsoleDir)\src\cascadia\TerminalApp\Generated Files";"$(OpenConsoleDir)\src\cascadia\TerminalSettingsModel\Generated Files";%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <PrecompiledHeaderFile>precomp.h</PrecompiledHeaderFile>
-
+      <!-- Required for JsonUtils.h. -->
+      <AdditionalOptions>%(AdditionalOptions) /Zc:twoPhase-</AdditionalOptions>
       <!-- Manually disable unreachable code warning, because jconcpp has a ton of that. -->
       <DisableSpecificWarnings>4702;%(DisableSpecificWarnings)</DisableSpecificWarnings>
     </ClCompile>

src/common.build.pre.props
@@ -116,9 +116,11 @@
       <MultiProcessorCompilation>true</MultiProcessorCompilation>
       <MinimalRebuild>false</MinimalRebuild>
       <RuntimeTypeInfo>false</RuntimeTypeInfo>
+      <ConformanceMode>true</ConformanceMode>
+      <UseStandardPreprocessor>true</UseStandardPreprocessor>
       <LanguageStandard>stdcpp17</LanguageStandard>
       <LanguageStandard_C>stdc17</LanguageStandard_C>
-      <AdditionalOptions>/utf-8 %(AdditionalOptions)</AdditionalOptions>
+      <AdditionalOptions>%(AdditionalOptions) /utf-8 /Zc:externConstexpr /Zc:lambda /Zc:throwingNew</AdditionalOptions>
       <ControlFlowGuard>Guard</ControlFlowGuard>
       <FloatingPointModel>Fast</FloatingPointModel>
     </ClCompile>

commit 79a08ecd18c4d00bc1cc48cb0d81fe9ed49c97c3

Fix fail-fast due to unlocked FreeProcessData call (#12599)
2b202ce6 introduced a bug, where FreeProcessData was called without the console
lock being held. The previous code can be found in 40e3dea, on line 441-454.

## PR Checklist
* [x] Closes MSFT:21372705
* [x] I work here
* [x] Tests added/passed

## Validation Steps Performed
None, as this fix is purely theoretic, but it matches the stack trace
and 40e3dea clearly wasn't correctly ported to strict C++ either.

src/server/IoDispatchers.cpp
@@ -288,8 +288,6 @@ PCONSOLE_API_MSG IoDispatchers::ConsoleHandleConnectionRequest(_In_ PCONSOLE_API
     LockConsole();
 
     const auto cleanup = wil::scope_exit([&]() noexcept {
-        UnlockConsole();
-
         if (!NT_SUCCESS(Status))
         {
             pReceiveMsg->SetReplyStatus(Status);
@@ -299,6 +297,9 @@ PCONSOLE_API_MSG IoDispatchers::ConsoleHandleConnectionRequest(_In_ PCONSOLE_API
                 gci.ProcessHandleList.FreeProcessData(ProcessData);
             }
         }
+
+        // FreeProcessData() above requires the console to be locked.
+        UnlockConsole();
     });
 
     DWORD const dwProcessId = (DWORD)pReceiveMsg->Descriptor.Process;