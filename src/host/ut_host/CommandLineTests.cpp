31c66efae026b841123efc6999aeb91eec878d7d
Leonard Hecker <lhecker@microsoft.com>
2022-03-19 00:55:25 +0100

    Make conhost history deduplication case-sensitive (#12700)
    The legacy console used to use case-sensitive history deduplication and
    this change reverts the logic to restore ye olde history functionality.
    
    This commit additionally changes the other remaining `std::equal` plus
    `std::towlower` check into a `CompareStringOrdinal` call,
    just because that's what MSDN suggests to use in such situations.
    
    ## PR Checklist
    * [x] Closes #4186
    * [x] I work here
    * [x] Tests added/passed
    
    ## Validation Steps Performed
    * Enter `test /v`
    * Enter `test /V`
    * Browsing through the history yields both items ✅
    
    (cherry picked from commit 6bc2b4af09cf3032e2fccd405302ffac70fd348c)

--- src/host/ut_host/CommandLineTests.cpp
+++ src/host/ut_host/CommandLineTests.cpp
@@ -450,20 +450,21 @@ class CommandLineTests
         VERIFY_SUCCEEDED(m_pHistory->Add(L"I'm a little teapot", false));
         VERIFY_SUCCEEDED(m_pHistory->Add(L"short and stout", false));
         VERIFY_SUCCEEDED(m_pHistory->Add(L"inflammable", false));
+        VERIFY_SUCCEEDED(m_pHistory->Add(L"Indestructible", false));
 
-        SetPrompt(cookedReadData, L"i");
+        SetPrompt(cookedReadData, L"I");
 
         auto& commandLine = CommandLine::Instance();
         commandLine._cycleMatchingCommandHistoryToPrompt(cookedReadData);
-        VerifyPromptText(cookedReadData, L"inflammable");
+        VerifyPromptText(cookedReadData, L"Indestructible");
 
-        // make sure we skip to the next "i" history item
+        // make sure we skip to the next "I" history item
         commandLine._cycleMatchingCommandHistoryToPrompt(cookedReadData);
         VerifyPromptText(cookedReadData, L"I'm a little teapot");
 
         // should cycle back to the start of the command history
         commandLine._cycleMatchingCommandHistoryToPrompt(cookedReadData);
-        VerifyPromptText(cookedReadData, L"inflammable");
+        VerifyPromptText(cookedReadData, L"Indestructible");
     }
 
     TEST_METHOD(CmdlineCtrlHomeFullwidthChars)
