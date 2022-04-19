fb7fd634e9ea4297f236c964cb6cb5ab37c0a484
Mike Griese <migrie@microsoft.com>
2022-04-19 15:14:59 -0500

    Actually allow terminal to use FG events

--- src/terminal/adapter/InteractDispatch.cpp
+++ src/terminal/adapter/InteractDispatch.cpp
@@ -188,7 +188,6 @@ bool InteractDispatch::IsVtInputEnabled() const
 //   which will end up here. This will update the console's internal tracker if
 //   it's focused or not, as to match the end-terminal's state.
 // - Used to call ConsoleControl(ConsoleSetForeground,...).
-// - Full support for this sequence was added in GH#11682.
 // Arguments:
 // - focused: if the terminal is now focused
 // Return Value:
@@ -247,6 +246,7 @@ bool InteractDispatch::FocusChanged(const bool focused) const
 
         WI_UpdateFlag(gci.Flags, CONSOLE_HAS_FOCUS, shouldActuallyFocus);
         gci.ProcessHandleList.ModifyConsoleProcessFocus(shouldActuallyFocus);
+        gci.pInputBuffer->Write(std::make_unique<FocusEvent>(focused));
     }
     // Does nothing outside of ConPTY. If there's a real HWND, then the HWND is solely in charge.
 
