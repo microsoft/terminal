9fd9c80e9ea3633520f5af685bfb0cd650e47b86
dansmor7 <101892345+dansmor7@users.noreply.github.com>
2022-04-23 23:02:11 -0700

    Update color buttons

--- src/cascadia/TerminalSettingsEditor/Converters.h
+++ src/cascadia/TerminalSettingsEditor/Converters.h
@@ -15,7 +15,6 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         static double FontWeightToDouble(winrt::Windows::UI::Text::FontWeight fontWeight);
         static bool InvertBoolean(bool value);
         static winrt::Windows::UI::Xaml::Visibility InvertedBooleanToVisibility(bool value);
-        static winrt::Windows::UI::Color LightenColor(winrt::Windows::UI::Color color);
         static double MaxValueFromPaddingString(winrt::hstring paddingString);
         static int PercentageToPercentageValue(double value);
         static double PercentageValueToPercentage(double value);
