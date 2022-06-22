0ec4991cd7020c35d3c669a449b65dc6c79645de
Dustin Howett <duhowett@microsoft.com>
2022-06-22 12:25:40 -0500

    Fix up some more stuff

--- src/server/ConsoleShimPolicy.cpp
+++ src/server/ConsoleShimPolicy.cpp
@@ -15,28 +15,25 @@ ConsoleShimPolicy::ConsoleShimPolicy(const HANDLE hProcess)
     try
     {
         const std::filesystem::path processName = wil::GetModuleFileNameExW<std::wstring>(hProcess, nullptr);
-        auto clientName = processName.filename().wstring();
+        auto clientName = processName.filename().native();
         // For whatever reason, wil::GetModuleFileNameExW leaves trailing nulls, so get rid of them.
         clientName.erase(std::find(clientName.begin(), clientName.end(), '\0'), clientName.end());
 
-        // Convert to lower case, just in case
-        std::transform(clientName.begin(), clientName.end(), clientName.begin(), std::towlower);
+        _isCmd = til::equals_insensitive_ascii(clientName, L"cmd.exe");
 
-        _isCmd = clientName.compare(L"cmd.exe") == 0;
-
-        // For powershell, we need both Windows PowersShell (powershell.exe) and
-        // PowerShell Core (pwsh.exe). If PowerShell Core is ever updated to use
+        // For powershell, we need both Windows Powershell (powershell.exe) and
+        // Powershell Core (pwsh.exe). If Powershell Core is ever updated to use
         // ^[[3J for Clear-Host, then it won't ever hit the shim code path, but
         // we're keeping this for the long tail of pwsh versions that still
         // _don't_ use that sequence.
-        const auto isInboxPowershell = clientName.compare(L"powershell.exe") == 0;
-        const auto isPwsh = clientName.compare(L"pwsh.exe") == 0;
+        const auto isInboxPowershell = til::equals_insensitive_ascii(clientName, L"powershell.exe");
+        const auto isPwsh = til::equals_insensitive_ascii(clientName, L"pwsh.exe");
         _isPowershell = isInboxPowershell || isPwsh;
 
-        // Inside Windows, we are guaranteed that we're building alongside a new (good) inbox PowerShell.
+        // Inside Windows, we are guaranteed that we're building alongside a new (good) inbox Powershell.
         // Therefore, we can default _requiresVtColorQuirk to false.
 #ifndef __INSIDE_WINDOWS
-        // Outside of Windows, we need to check the OS version: PowerShell was fixed in early Iron builds.
+        // Outside of Windows, we need to check the OS version: Powershell was fixed in early Iron builds.
         static auto doesInboxPowershellVersionRequireQuirk = [] {
             OSVERSIONINFOEXW osver{};
             osver.dwOSVersionInfoSize = sizeof(osver);
