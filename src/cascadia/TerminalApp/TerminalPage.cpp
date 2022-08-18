microsoft/terminal

commit e7a79d9c9ebb00433e0ecfcf4215a7b5cc6fdbc0

Added Source Code and Send Feedback buttons to About dialog (#13510)
<!-- Enter a brief description/summary of your PR here. What does it fix/what does it change/how was it tested (even manually, if necessary)? -->
## Summary of the Pull Request
Added two new buttons to the About dialog. Source Code and Send Feedback buttons link directly to the Terminal project on GitHub and to the Issues page respectively.

<!-- Other than the issue solved, is this relevant to any other issues/existing PRs? --> 
## References
#13371

<!-- Please review the items on the PR checklist before submitting-->
## PR Checklist
* [ ] Closes #13371
* [x] CLA signed. If not, go over [here](https://cla.opensource.microsoft.com/microsoft/Terminal) and sign the CLA
* [ ] Tests added/passed
* [ ] Documentation updated. If checked, please file a pull request on [our docs repo](https://github.com/MicrosoftDocs/terminal) and link it here: #xxx
* [ ] Schema updated.
* [x] I've discussed this with core contributors already. If not checked, I'm ready to accept this work might be rejected in favor of a different grand plan. Issue number where discussion took place: #13371

<!-- Describe how you validated the behavior. Add automated tests wherever possible, but list manual validation steps taken as well -->
## Validation Steps Performed
Performed manual testing and confirmed that the implementation works.

src/cascadia/TerminalApp/TerminalPage.cpp
@@ -693,8 +693,8 @@ namespace winrt::TerminalApp::implementation
 
     // Method Description:
     // - Show a dialog with "About" information. Displays the app's Display
-    //   Name, version, getting started link, documentation link, release
-    //   Notes link, and privacy policy link.
+    //   Name, version, getting started link, source code link, documentation link, release
+    //   Notes link, send feedback link and privacy policy link.
     void TerminalPage::_ShowAboutDialog()
     {
         _ShowDialogHelper(L"AboutDialog");
@@ -708,6 +708,11 @@ namespace winrt::TerminalApp::implementation
     winrt::hstring TerminalPage::ApplicationVersion()
     {
         return CascadiaSettings::ApplicationVersion();
+    }
+
+    void TerminalPage::_SendFeedbackOnClick(const IInspectable& /*sender*/, const Windows::UI::Xaml::Controls::ContentDialogButtonClickEventArgs& /*eventArgs*/)
+    {
+        ShellExecute(nullptr, nullptr, L"https://github.com/microsoft/terminal/issues", nullptr, nullptr, SW_SHOW);
     }
 
     void TerminalPage::_ThirdPartyNoticesOnClick(const IInspectable& /*sender*/, const Windows::UI::Xaml::RoutedEventArgs& /*eventArgs*/)

src/cascadia/TerminalApp/TerminalPage.xaml
@@ -100,13 +100,16 @@
                        x:Uid="AboutDialog"
                        Grid.Row="2"
                        x:Load="False"
-                       DefaultButton="Close">
+                       DefaultButton="Close"
+                       PrimaryButtonClick="_SendFeedbackOnClick">
             <StackPanel Orientation="Vertical">
                 <TextBlock IsTextSelectionEnabled="True">
                     <Run Text="{x:Bind ApplicationDisplayName}" /> <LineBreak />
                     <Run x:Uid="AboutDialog_VersionLabel" />
                     <Run Text="{x:Bind ApplicationVersion}" />
                 </TextBlock>
+                <HyperlinkButton x:Uid="AboutDialog_SourceCodeLink"
+                                 NavigateUri="https://github.com/microsoft/terminal" />
                 <HyperlinkButton x:Uid="AboutDialog_DocumentationLink"
                                  NavigateUri="https://go.microsoft.com/fwlink/?linkid=2125416" />
                 <HyperlinkButton x:Uid="AboutDialog_ReleaseNotesLink"

src/cascadia/TerminalApp/Resources/en-US/Resources.resw
@@ -436,6 +436,9 @@
   <data name="AboutDialog.Title" xml:space="preserve">
     <value>About</value>
   </data>
+  <data name="AboutDialog.PrimaryButtonText" xml:space="preserve">
+    <value>Send Feedback</value>
+  </data>
   <data name="AboutDialog.CloseButtonText" xml:space="preserve">
     <value>OK</value>
   </data>
@@ -446,6 +449,10 @@
   <data name="AboutDialog_GettingStartedLink.Content" xml:space="preserve">
     <value>Getting Started</value>
     <comment>A hyperlink name for a guide on how to get started using Terminal</comment>
+  </data>
+  <data name="AboutDialog_SourceCodeLink.Content" xml:space="preserve">
+    <value>Source Code</value>
+    <comment>A hyperlink name for the Terminal's documentation</comment>
   </data>
   <data name="AboutDialog_DocumentationLink.Content" xml:space="preserve">
     <value>Documentation</value>

src/cascadia/TerminalApp/TerminalPage.h
@@ -251,6 +251,7 @@ namespace winrt::TerminalApp::implementation
         void _CommandPaletteButtonOnClick(const IInspectable& sender, const Windows::UI::Xaml::RoutedEventArgs& eventArgs);
         void _AboutButtonOnClick(const IInspectable& sender, const Windows::UI::Xaml::RoutedEventArgs& eventArgs);
         void _ThirdPartyNoticesOnClick(const IInspectable& sender, const Windows::UI::Xaml::RoutedEventArgs& eventArgs);
+        void _SendFeedbackOnClick(const IInspectable& sender, const Windows::UI::Xaml::Controls::ContentDialogButtonClickEventArgs& eventArgs);
 
         void _KeyDownHandler(const Windows::Foundation::IInspectable& sender, const Windows::UI::Xaml::Input::KeyRoutedEventArgs& e);
         static ::Microsoft::Terminal::Core::ControlKeyStates _GetPressedModifierKeys() noexcept;

commit 9f9f5de78decb3ff5726ef8b2a708288836635da

Use the redirection service for recently added URLs (#13759)
We shouldn't add URLs into our binaries that we can't directly control.
This commit fixes the issue for URLs recently introduced in #13510.

Closes #13541

## Validation Steps Performed
This change is trivial enough that I simply opened the new redirects
in my browser, ensuring that they open the expected websites.

src/cascadia/TerminalApp/TerminalPage.cpp
@@ -732,7 +732,11 @@ namespace winrt::TerminalApp::implementation
 
     void TerminalPage::_SendFeedbackOnClick(const IInspectable& /*sender*/, const Windows::UI::Xaml::Controls::ContentDialogButtonClickEventArgs& /*eventArgs*/)
     {
-        ShellExecute(nullptr, nullptr, L"https://github.com/microsoft/terminal/issues", nullptr, nullptr, SW_SHOW);
+#if defined(WT_BRANDING_RELEASE)
+        ShellExecute(nullptr, nullptr, L"https://go.microsoft.com/fwlink/?linkid=2125419", nullptr, nullptr, SW_SHOW);
+#else
+        ShellExecute(nullptr, nullptr, L"https://go.microsoft.com/fwlink/?linkid=2204904", nullptr, nullptr, SW_SHOW);
+#endif
     }
 
     void TerminalPage::_ThirdPartyNoticesOnClick(const IInspectable& /*sender*/, const Windows::UI::Xaml::RoutedEventArgs& /*eventArgs*/)

src/cascadia/TerminalApp/TerminalPage.xaml
@@ -109,7 +109,7 @@
                     <Run Text="{x:Bind ApplicationVersion}" />
                 </TextBlock>
                 <HyperlinkButton x:Uid="AboutDialog_SourceCodeLink"
-                                 NavigateUri="https://github.com/microsoft/terminal" />
+                                 NavigateUri="https://go.microsoft.com/fwlink/?linkid=2203152" />
                 <HyperlinkButton x:Uid="AboutDialog_DocumentationLink"
                                  NavigateUri="https://go.microsoft.com/fwlink/?linkid=2125416" />
                 <HyperlinkButton x:Uid="AboutDialog_ReleaseNotesLink"