f14ae3313e3e5661af8c72560b60e9c3cbafb34f
Leonard Hecker <lhecker@microsoft.com>
2022-09-15 22:48:34 +0200

    Fix test failures

--- src/inc/test/CommonState.hpp
+++ src/inc/test/CommonState.hpp
@@ -81,7 +81,7 @@ public:
     {
         Globals& g = Microsoft::Console::Interactivity::ServiceLocator::LocateGlobals();
         CONSOLE_INFORMATION& gci = g.getConsoleInformation();
-        g.pRender = new Microsoft::Console::Render::Renderer(gci.GetRenderSettings(), &gci.renderData, nullptr, 0, nullptr);
+        g.pRender = new Microsoft::Console::Render::Renderer(gci.GetRenderSettings(), &gci.renderData, nullptr, 0, std::make_unique<RenderThread>());
     }
 
     void CleanupGlobalRenderer()
