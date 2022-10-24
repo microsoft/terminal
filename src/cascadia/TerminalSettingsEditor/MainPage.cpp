4f1a72ae1c3316a3ecbe0fa83bfff02f42bb224b
Carlos Zamora <carlos.zamora@microsoft.com>
2022-10-24 12:38:27 -0700

    Remove redundant tooltips from settings UI (#14244)
    This removes all of the redundant tooltips from the settings UI. Since all of the settings are added through the SettingsContainer, it's a pretty simple change.
    
    Closes #14184
    
    - [X] hover over all settings in the settings UI
    - [X] hover over all entries in the SUI nav view
    
    (cherry picked from commit 3eaa781499f5af0094fbcbdea4fdcc839c7e1b2d)
    Service-Card-Id: 86390068
    Service-Version: 1.15

--- src/cascadia/TerminalSettingsEditor/MainPage.cpp
+++ src/cascadia/TerminalSettingsEditor/MainPage.cpp
@@ -502,7 +502,6 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
             if (!profile.Deleted())
             {
                 auto navItem = _CreateProfileNavViewItem(_viewModelForProfile(profile, _settingsClone));
-                Controls::ToolTipService::SetToolTip(navItem, box_value(profile.Name()));
                 menuItems.Append(navItem);
             }
         }
@@ -510,7 +509,6 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         // Top off (the end of the nav view) with the Add Profile item
         MUX::Controls::NavigationViewItem addProfileItem;
         addProfileItem.Content(box_value(RS_(L"Nav_AddNewProfile/Content")));
-        Controls::ToolTipService::SetToolTip(addProfileItem, box_value(RS_(L"Nav_AddNewProfile/Content")));
         addProfileItem.Tag(box_value(addProfileTag));
 
         FontIcon icon;
