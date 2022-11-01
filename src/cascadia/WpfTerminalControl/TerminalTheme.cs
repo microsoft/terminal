d6110639722363c51d0d93dd95974daacb86a3b7
Rose <83477269+AtariDreams@users.noreply.github.com>
2022-11-01 12:03:17 -0400

    No more readonly

--- src/cascadia/WpfTerminalControl/TerminalTheme.cs
+++ src/cascadia/WpfTerminalControl/TerminalTheme.cs
@@ -58,32 +58,32 @@ namespace Microsoft.Terminal.Wpf
         /// <summary>
         /// The default background color of the terminal, represented in Win32 COLORREF format.
         /// </summary>
-        public readonly uint DefaultBackground;
+        public uint DefaultBackground;
 
         /// <summary>
         /// The default foreground color of the terminal, represented in Win32 COLORREF format.
         /// </summary>
-        public readonly uint DefaultForeground;
+        public uint DefaultForeground;
 
         /// <summary>
         /// The default selection background color of the terminal, represented in Win32 COLORREF format.
         /// </summary>
-        public readonly uint DefaultSelectionBackground;
+        public uint DefaultSelectionBackground;
 
         /// <summary>
         /// The opacity alpha for the selection color of the terminal, must be between 1.0 and 0.0.
         /// </summary>
-        public readonly float SelectionBackgroundAlpha;
+        public float SelectionBackgroundAlpha;
 
         /// <summary>
         /// The style of cursor to use in the terminal.
         /// </summary>
-        public readonly CursorStyle CursorStyle;
+        public CursorStyle CursorStyle;
 
         /// <summary>
         /// The color array to use for the terminal, filling the standard vt100 16 color table, represented in Win32 COLORREF format.
         /// </summary>
         [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = 16)]
-        public readonly uint[] ColorTable;
+        public uint[] ColorTable;
     }
 }
