microsoft/terminal

commit a01500f051bc896465ac4bd37811142bc2347fb8

Rewrite ROW to be Unicode capable (#13626)
This commit is a from-scratch rewrite of `ROW` with the primary goal to get
rid of the rather bodgy `UnicodeStorage` class and improve Unicode support.

Previously a 120x9001 terminal buffer would store a vector of 9001 `ROW`s
where each `ROW` stored exactly 120 `wchar_t`. Glyphs exceeding their
allocated space would be stored in the `UnicodeStorage` which was basically
a `hashmap<Coordinate, String>`. Iterating over the text in a `ROW` would
require us to check each glyph and fetch it from the map conditionally.
On newlines we'd have to invalidate all map entries that are now gone,
so for every invalidated `ROW` we'd iterate through all glyphs again and if
a single one was stored in `UnicodeStorage`, we'd then iterate through the
entire hashmap to remove all coordinates that were residing on that `ROW`.
All in all, this wasn't the most robust nor performant code.

The new implementation is simple (from a design perspective):
Store all text in a `ROW` in a regular string. Grow the string if needed.
The association between columns and text works by storing character offsets
in a column-wide array. This algorithm is <100 LOC and removes ~1000.

As an aside this PR does a few more things that go hand in hand:
* Remove most of `ROW` helper classes, which aren't needed anymore.
* Allocate backing memory in a single `VirtualAlloc` call.
* Rewrite `IsCursorDoubleWidth` to use `DbcsAttrAt` directly.
  Improves overall performance by 10-20% and makes this implementation
  faster than the previous NxM storage, despite the added complexity.

Part of #8000

## Validation Steps Performed
* Existing and new unit and feature tests complete ✅
* Printing Unicode completes without crashing ✅
* Resizing works without crashing ✅

src/cascadia/TerminalControl/ControlCore.cpp
@@ -1735,26 +1735,25 @@ namespace winrt::Microsoft::Terminal::Control::implementation
 
         const auto& textBuffer = _terminal->GetTextBuffer();
 
-        std::wstringstream ss;
+        std::wstring str;
         const auto lastRow = textBuffer.GetLastNonSpaceCharacter().Y;
         for (auto rowIndex = 0; rowIndex <= lastRow; rowIndex++)
         {
             const auto& row = textBuffer.GetRowByOffset(rowIndex);
-            auto rowText = row.GetText();
+            const auto rowText = row.GetText();
             const auto strEnd = rowText.find_last_not_of(UNICODE_SPACE);
-            if (strEnd != std::string::npos)
+            if (strEnd != decltype(rowText)::npos)
             {
-                rowText.erase(strEnd + 1);
-                ss << rowText;
+                str.append(rowText.substr(strEnd + 1));
             }
 
             if (!row.WasWrapForced())
             {
-                ss << UNICODE_CARRIAGERETURN << UNICODE_LINEFEED;
+                str.append(L"\r\n");
             }
         }
 
-        return hstring(ss.str());
+        return hstring{ str };
     }
 
     // Helper to check if we're on Windows 11 or not. This is used to check if

src/buffer/out/AttrRow.cpp
@@ -1,134 +1,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-#include "precomp.h"
-#include "AttrRow.hpp"
-
-// Routine Description:
-// - constructor
-// Arguments:
-// - cchRowWidth - the length of the default text attribute
-// - attr - the default text attribute
-// Return Value:
-// - constructed object
-ATTR_ROW::ATTR_ROW(const til::CoordType width, const TextAttribute attr) :
-    _data(gsl::narrow_cast<uint16_t>(width), attr) {}
-
-// Routine Description:
-// - Sets all properties of the ATTR_ROW to default values
-// Arguments:
-// - attr - The default text attributes to use on text in this row.
-void ATTR_ROW::Reset(const TextAttribute attr)
-{
-    _data.replace(0, _data.size(), attr);
-}
-
-// Routine Description:
-// - Takes an existing row of attributes, and changes the length so that it fills the NewWidth.
-//     If the new size is bigger, then the last attr is extended to fill the NewWidth.
-//     If the new size is smaller, the runs are cut off to fit.
-// Arguments:
-// - oldWidth - The original width of the row.
-// - newWidth - The new width of the row.
-// Return Value:
-// - <none>, throws exceptions on failures.
-void ATTR_ROW::Resize(const til::CoordType newWidth)
-{
-    _data.resize_trailing_extent(gsl::narrow<uint16_t>(newWidth));
-}
-
-// Routine Description:
-// - returns a copy of the TextAttribute at the specified column
-// Arguments:
-// - column - the column to get the attribute for
-// Return Value:
-// - the text attribute at column
-// Note:
-// - will throw on error
-TextAttribute ATTR_ROW::GetAttrByColumn(const til::CoordType column) const
-{
-    return _data.at(gsl::narrow<uint16_t>(column));
-}
-
-// Routine Description:
-// - Finds the hyperlink IDs present in this row and returns them
-// Return value:
-// - The hyperlink IDs present in this row
-std::vector<uint16_t> ATTR_ROW::GetHyperlinks() const
-{
-    std::vector<uint16_t> ids;
-    for (const auto& run : _data.runs())
-    {
-        if (run.value.IsHyperlink())
-        {
-            ids.emplace_back(run.value.GetHyperlinkId());
-        }
-    }
-    return ids;
-}
-
-// Routine Description:
-// - Sets the attributes (colors) of all character positions from the given position through the end of the row.
-// Arguments:
-// - iStart - Starting index position within the row
-// - attr - Attribute (color) to fill remaining characters with
-// Return Value:
-// - <none>
-bool ATTR_ROW::SetAttrToEnd(const til::CoordType beginIndex, const TextAttribute attr)
-{
-    _data.replace(gsl::narrow<uint16_t>(beginIndex), _data.size(), attr);
-    return true;
-}
-
-// Method Description:
-// - Replaces all runs in the row with the given toBeReplacedAttr with the new
-//      attribute replaceWith.
-// Arguments:
-// - toBeReplacedAttr - the attribute to replace in this row.
-// - replaceWith - the new value for the matching runs' attributes.
-// Return Value:
-// - <none>
-void ATTR_ROW::ReplaceAttrs(const TextAttribute& toBeReplacedAttr, const TextAttribute& replaceWith)
-{
-    _data.replace_values(toBeReplacedAttr, replaceWith);
-}
-
-// Routine Description:
-// - Takes an attribute, and merges it into this row from beginIndex (inclusive) to endIndex (exclusive).
-// - For example, if the current row was [{4, BLUE}], the merge arguments were
-//   { beginIndex = 1, endIndex = 3, newAttr = RED }, then the row would modified to be
-//   [{ 1, BLUE}, {2, RED}, {1, BLUE}].
-// Arguments:
-// - beginIndex, endIndex: The [beginIndex, endIndex) range that's to be replaced with newAttr.
-// - newAttr: The attribute to merge into this row.
-// Return Value:
-// - <none>
-void ATTR_ROW::Replace(const til::CoordType beginIndex, const til::CoordType endIndex, const TextAttribute& newAttr)
-{
-    _data.replace(gsl::narrow<uint16_t>(beginIndex), gsl::narrow<uint16_t>(endIndex), newAttr);
-}
-
-ATTR_ROW::const_iterator ATTR_ROW::begin() const noexcept
-{
-    return _data.begin();
-}
-
-ATTR_ROW::const_iterator ATTR_ROW::end() const noexcept
-{
-    return _data.end();
-}
-
-ATTR_ROW::const_iterator ATTR_ROW::cbegin() const noexcept
-{
-    return _data.cbegin();
-}
-
-ATTR_ROW::const_iterator ATTR_ROW::cend() const noexcept
-{
-    return _data.cend();
-}
-
-bool operator==(const ATTR_ROW& a, const ATTR_ROW& b) noexcept
-{
-    return a._data == b._data;
-}

src/buffer/out/AttrRow.hpp
@@ -1,68 +1,0 @@
-/*++
-Copyright (c) Microsoft Corporation
-Licensed under the MIT license.
-
-Module Name:
-- AttrRow.hpp
-
-Abstract:
-- contains data structure for the attributes of one row of screen buffer
-
-Author(s):
-- Michael Niksa (miniksa) 10-Apr-2014
-- Paul Campbell (paulcam) 10-Apr-2014
-
-Revision History:
-- From components of output.h/.c
-  by Therese Stowell (ThereseS) 1990-1991
-- Pulled into its own file from textBuffer.hpp/cpp (AustDi, 2017)
---*/
-
-#pragma once
-
-#include "til/rle.h"
-#include "TextAttribute.hpp"
-
-class ATTR_ROW final
-{
-    using rle_vector = til::small_rle<TextAttribute, uint16_t, 1>;
-
-public:
-    using const_iterator = rle_vector::const_iterator;
-
-    ATTR_ROW(til::CoordType width, TextAttribute attr);
-
-    ~ATTR_ROW() = default;
-
-    ATTR_ROW(const ATTR_ROW&) = default;
-    ATTR_ROW& operator=(const ATTR_ROW&) = default;
-    ATTR_ROW(ATTR_ROW&&)
-    noexcept = default;
-    ATTR_ROW& operator=(ATTR_ROW&&) noexcept = default;
-
-    TextAttribute GetAttrByColumn(til::CoordType column) const;
-    std::vector<uint16_t> GetHyperlinks() const;
-
-    bool SetAttrToEnd(til::CoordType beginIndex, TextAttribute attr);
-    void ReplaceAttrs(const TextAttribute& toBeReplacedAttr, const TextAttribute& replaceWith);
-    void Resize(til::CoordType newWidth);
-    void Replace(til::CoordType beginIndex, til::CoordType endIndex, const TextAttribute& newAttr);
-
-    const_iterator begin() const noexcept;
-    const_iterator end() const noexcept;
-
-    const_iterator cbegin() const noexcept;
-    const_iterator cend() const noexcept;
-
-    friend bool operator==(const ATTR_ROW& a, const ATTR_ROW& b) noexcept;
-    friend class ROW;
-
-private:
-    void Reset(const TextAttribute attr);
-
-    rle_vector _data;
-
-#ifdef UNIT_TESTING
-    friend class CommonState;
-#endif
-};

src/buffer/out/CharRow.cpp
@@ -1,281 +1,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-#include "precomp.h"
-
-#include "CharRow.hpp"
-#include "unicode.hpp"
-#include "Row.hpp"
-
-// Routine Description:
-// - constructor
-// Arguments:
-// - rowWidth - the size (in wchar_t) of the char and attribute rows
-// - pParent - the parent ROW
-// Return Value:
-// - instantiated object
-// Note: will through if unable to allocate char/attribute buffers
-#pragma warning(push)
-#pragma warning(disable : 26447) // small_vector's constructor says it can throw but it should not given how we use it.  This suppresses this error for the AuditMode build.
-CharRow::CharRow(til::CoordType rowWidth, ROW* const pParent) noexcept :
-    _data(rowWidth, value_type()),
-    _pParent{ FAIL_FAST_IF_NULL(pParent) }
-{
-}
-#pragma warning(pop)
-
-// Routine Description:
-// - gets the size of the row, in glyph cells
-// Arguments:
-// - <none>
-// Return Value:
-// - the size of the row
-til::CoordType CharRow::size() const noexcept
-{
-    return gsl::narrow_cast<til::CoordType>(_data.size());
-}
-
-// Routine Description:
-// - Sets all properties of the CharRowBase to default values
-// Arguments:
-// - sRowWidth - The width of the row.
-// Return Value:
-// - <none>
-void CharRow::Reset() noexcept
-{
-    for (auto& cell : _data)
-    {
-        cell.Reset();
-    }
-}
-
-// Routine Description:
-// - resizes the width of the CharRowBase
-// Arguments:
-// - newSize - the new width of the character and attributes rows
-// Return Value:
-// - S_OK on success, otherwise relevant error code
-[[nodiscard]] HRESULT CharRow::Resize(const til::CoordType newSize) noexcept
-{
-    try
-    {
-        const value_type insertVals;
-        _data.resize(newSize, insertVals);
-    }
-    CATCH_RETURN();
-
-    return S_OK;
-}
-
-typename CharRow::iterator CharRow::begin() noexcept
-{
-    return _data.begin();
-}
-
-typename CharRow::const_iterator CharRow::cbegin() const noexcept
-{
-    return _data.cbegin();
-}
-
-typename CharRow::iterator CharRow::end() noexcept
-{
-    return _data.end();
-}
-
-typename CharRow::const_iterator CharRow::cend() const noexcept
-{
-    return _data.cend();
-}
-
-// Routine Description:
-// - Inspects the current internal string to find the left edge of it
-// Arguments:
-// - <none>
-// Return Value:
-// - The calculated left boundary of the internal string.
-til::CoordType CharRow::MeasureLeft() const noexcept
-{
-    auto it = _data.cbegin();
-    while (it != _data.cend() && it->IsSpace())
-    {
-        ++it;
-    }
-    return gsl::narrow_cast<til::CoordType>(it - _data.cbegin());
-}
-
-// Routine Description:
-// - Inspects the current internal string to find the right edge of it
-// Arguments:
-// - <none>
-// Return Value:
-// - The calculated right boundary of the internal string.
-til::CoordType CharRow::MeasureRight() const
-{
-    auto it = _data.crbegin();
-    while (it != _data.crend() && it->IsSpace())
-    {
-        ++it;
-    }
-    return gsl::narrow_cast<til::CoordType>(_data.crend() - it);
-}
-
-void CharRow::ClearCell(const til::CoordType column)
-{
-    _data.at(column).Reset();
-}
-
-// Routine Description:
-// - Tells you whether or not this row contains any valid text.
-// Arguments:
-// - <none>
-// Return Value:
-// - True if there is valid text in this row. False otherwise.
-bool CharRow::ContainsText() const noexcept
-{
-    for (const auto& cell : _data)
-    {
-        if (!cell.IsSpace())
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
-// Routine Description:
-// - gets the attribute at the specified column
-// Arguments:
-// - column - the column to get the attribute for
-// Return Value:
-// - the attribute
-// Note: will throw exception if column is out of bounds
-const DbcsAttribute& CharRow::DbcsAttrAt(const til::CoordType column) const
-{
-    return _data.at(column).DbcsAttr();
-}
-
-// Routine Description:
-// - gets the attribute at the specified column
-// Arguments:
-// - column - the column to get the attribute for
-// Return Value:
-// - the attribute
-// Note: will throw exception if column is out of bounds
-DbcsAttribute& CharRow::DbcsAttrAt(const til::CoordType column)
-{
-    return _data.at(column).DbcsAttr();
-}
-
-// Routine Description:
-// - resets text data at column
-// Arguments:
-// - column - column index to clear text data from
-// Return Value:
-// - <none>
-// Note: will throw exception if column is out of bounds
-void CharRow::ClearGlyph(const til::CoordType column)
-{
-    _data.at(column).EraseChars();
-}
-
-// Routine Description:
-// - returns text data at column as a const reference.
-// Arguments:
-// - column - column to get text data for
-// Return Value:
-// - text data at column
-// - Note: will throw exception if column is out of bounds
-const CharRow::reference CharRow::GlyphAt(const til::CoordType column) const
-{
-    THROW_HR_IF(E_INVALIDARG, column < 0 || column >= gsl::narrow_cast<til::CoordType>(_data.size()));
-    return { const_cast<CharRow&>(*this), column };
-}
-
-// Routine Description:
-// - returns text data at column as a reference.
-// Arguments:
-// - column - column to get text data for
-// Return Value:
-// - text data at column
-// - Note: will throw exception if column is out of bounds
-CharRow::reference CharRow::GlyphAt(const til::CoordType column)
-{
-    THROW_HR_IF(E_INVALIDARG, column < 0 || column >= gsl::narrow_cast<til::CoordType>(_data.size()));
-    return { *this, column };
-}
-
-std::wstring CharRow::GetText() const
-{
-    std::wstring wstr;
-    wstr.reserve(_data.size());
-
-    for (til::CoordType i = 0; i < gsl::narrow_cast<til::CoordType>(_data.size()); ++i)
-    {
-        const auto glyph = GlyphAt(i);
-        if (!DbcsAttrAt(i).IsTrailing())
-        {
-            for (const auto wch : glyph)
-            {
-                wstr.push_back(wch);
-            }
-        }
-    }
-    return wstr;
-}
-
-// Method Description:
-// - get delimiter class for a position in the char row
-// - used for double click selection and uia word navigation
-// Arguments:
-// - column: column to get text data for
-// - wordDelimiters: the delimiters defined as a part of the DelimiterClass::DelimiterChar
-// Return Value:
-// - the delimiter class for the given char
-const DelimiterClass CharRow::DelimiterClassAt(const til::CoordType column, const std::wstring_view wordDelimiters) const
-{
-    THROW_HR_IF(E_INVALIDARG, column < 0 || column >= gsl::narrow_cast<til::CoordType>(_data.size()));
-
-    const auto glyph = *GlyphAt(column).begin();
-    if (glyph <= UNICODE_SPACE)
-    {
-        return DelimiterClass::ControlChar;
-    }
-    else if (wordDelimiters.find(glyph) != std::wstring_view::npos)
-    {
-        return DelimiterClass::DelimiterChar;
-    }
-    else
-    {
-        return DelimiterClass::RegularChar;
-    }
-}
-
-UnicodeStorage& CharRow::GetUnicodeStorage() noexcept
-{
-    return _pParent->GetUnicodeStorage();
-}
-
-const UnicodeStorage& CharRow::GetUnicodeStorage() const noexcept
-{
-    return _pParent->GetUnicodeStorage();
-}
-
-// Routine Description:
-// - calculates the key used by the given column of the char row to store glyph data in UnicodeStorage
-// Arguments:
-// - column - the column to generate the key for
-// Return Value:
-// - the til::point key for data access from UnicodeStorage for the column
-til::point CharRow::GetStorageKey(const til::CoordType column) const noexcept
-{
-    return { column, _pParent->GetId() };
-}
-
-// Routine Description:
-// - Updates the pointer to the parent row (which might change if we shuffle the rows around)
-// Arguments:
-// - pParent - Pointer to the parent row
-void CharRow::UpdateParent(ROW* const pParent)
-{
-    _pParent = FAIL_FAST_IF_NULL(pParent);
-}

src/buffer/out/CharRow.hpp
@@ -1,117 +1,0 @@
-/*++
-Copyright (c) Microsoft Corporation
-Licensed under the MIT license.
-
-Module Name:
-- CharRow.hpp
-
-Abstract:
-- contains data structure for UCS2 encoded character data of a row
-
-Author(s):
-- Michael Niksa (miniksa) 10-Apr-2014
-- Paul Campbell (paulcam) 10-Apr-2014
-
-Revision History:
-- From components of output.h/.c
-  by Therese Stowell (ThereseS) 1990-1991
-- Pulled into its own file from textBuffer.hpp/cpp (AustDi, 2017)
---*/
-
-#pragma once
-
-#include <til/small_vector.h>
-
-#include "DbcsAttribute.hpp"
-#include "CharRowCellReference.hpp"
-#include "CharRowCell.hpp"
-#include "UnicodeStorage.hpp"
-
-class ROW;
-
-enum class DelimiterClass
-{
-    ControlChar,
-    DelimiterChar,
-    RegularChar
-};
-
-// the characters of one row of screen buffer
-// we keep the following values so that we don't write
-// more pixels to the screen than we have to:
-// left is initialized to screenbuffer width.  right is
-// initialized to zero.
-//
-//      [     foo.bar    12-12-61                       ]
-//       ^    ^                  ^                     ^
-//       |    |                  |                     |
-//     Chars Left               Right                end of Chars buffer
-class CharRow final
-{
-public:
-    using glyph_type = wchar_t;
-    using value_type = CharRowCell;
-    using iterator = til::small_vector<value_type, 120>::iterator;
-    using const_iterator = til::small_vector<value_type, 120>::const_iterator;
-    using const_reverse_iterator = til::small_vector<value_type, 120>::const_reverse_iterator;
-    using reference = CharRowCellReference;
-
-    CharRow(til::CoordType rowWidth, ROW* const pParent) noexcept;
-
-    til::CoordType size() const noexcept;
-    [[nodiscard]] HRESULT Resize(const til::CoordType newSize) noexcept;
-    til::CoordType MeasureLeft() const noexcept;
-    til::CoordType MeasureRight() const;
-    bool ContainsText() const noexcept;
-    const DbcsAttribute& DbcsAttrAt(const til::CoordType column) const;
-    DbcsAttribute& DbcsAttrAt(const til::CoordType column);
-    void ClearGlyph(const til::CoordType column);
-
-    const DelimiterClass DelimiterClassAt(const til::CoordType column, const std::wstring_view wordDelimiters) const;
-
-    // working with glyphs
-    const reference GlyphAt(const til::CoordType column) const;
-    reference GlyphAt(const til::CoordType column);
-
-    // iterators
-    iterator begin() noexcept;
-    const_iterator cbegin() const noexcept;
-    const_iterator begin() const noexcept { return cbegin(); }
-
-    iterator end() noexcept;
-    const_iterator cend() const noexcept;
-    const_iterator end() const noexcept { return cend(); }
-
-    UnicodeStorage& GetUnicodeStorage() noexcept;
-    const UnicodeStorage& GetUnicodeStorage() const noexcept;
-    til::point GetStorageKey(const til::CoordType column) const noexcept;
-
-    void UpdateParent(ROW* const pParent);
-
-    friend CharRowCellReference;
-    friend class ROW;
-
-private:
-    void Reset() noexcept;
-    void ClearCell(const til::CoordType column);
-    std::wstring GetText() const;
-
-protected:
-    // storage for glyph data and dbcs attributes
-    til::small_vector<value_type, 120> _data;
-
-    // ROW that this CharRow belongs to
-    ROW* _pParent;
-};
-
-template<typename InputIt1, typename InputIt2>
-void OverwriteColumns(InputIt1 startChars, InputIt1 endChars, InputIt2 startAttrs, CharRow::iterator outIt)
-{
-    std::transform(startChars,
-                   endChars,
-                   startAttrs,
-                   outIt,
-                   [](const wchar_t wch, const DbcsAttribute attr) {
-                       return CharRow::value_type{ wch, attr };
-                   });
-}

src/buffer/out/CharRowCell.cpp
@@ -1,73 +1,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-#include "precomp.h"
-
-#include "CharRowCell.hpp"
-#include "unicode.hpp"
-
-// default glyph value, used for resetting the character data portion of a cell
-static constexpr wchar_t DefaultValue = UNICODE_SPACE;
-
-// Routine Description:
-// - "erases" the glyph. really sets it back to the default "empty" value
-void CharRowCell::EraseChars() noexcept
-{
-    if (_attr.IsGlyphStored())
-    {
-        _attr.SetGlyphStored(false);
-    }
-    _wch = DefaultValue;
-}
-
-// Routine Description:
-// - resets this object back to the defaults it would have from the default constructor
-void CharRowCell::Reset() noexcept
-{
-    _attr.Reset();
-    _wch = DefaultValue;
-}
-
-// Routine Description:
-// - checks if cell contains a space glyph
-// Return Value:
-// - true if cell contains a space glyph, false otherwise
-bool CharRowCell::IsSpace() const noexcept
-{
-    return !_attr.IsGlyphStored() && _wch == UNICODE_SPACE;
-}
-
-// Routine Description:
-// - Access the DbcsAttribute for the cell
-// Return Value:
-// - ref to the cells' DbcsAttribute
-DbcsAttribute& CharRowCell::DbcsAttr() noexcept
-{
-    return _attr;
-}
-
-// Routine Description:
-// - Access the DbcsAttribute for the cell
-// Return Value:
-// - ref to the cells' DbcsAttribute
-const DbcsAttribute& CharRowCell::DbcsAttr() const noexcept
-{
-    return _attr;
-}
-
-// Routine Description:
-// - Access the cell's wchar field. this does not access any char data through UnicodeStorage.
-// Return Value:
-// - the cell's wchar field
-wchar_t& CharRowCell::Char() noexcept
-{
-    return _wch;
-}
-
-// Routine Description:
-// - Access the cell's wchar field. this does not access any char data through UnicodeStorage.
-// Return Value:
-// - the cell's wchar field
-const wchar_t& CharRowCell::Char() const noexcept
-{
-    return _wch;
-}

src/buffer/out/CharRowCell.hpp
@@ -1,65 +1,0 @@
-/*++
-Copyright (c) Microsoft Corporation
-Licensed under the MIT license.
-
-Module Name:
-- CharRowCell.hpp
-
-Abstract:
-- data structure for one cell of a char row. contains the char data for one
-  coordinate position in the output buffer (leading/trailing information and
-  the char itself.
-
-Author(s):
-- Austin Diviness (AustDi) 02-May-2018
---*/
-
-#pragma once
-
-#include "DbcsAttribute.hpp"
-#include "unicode.hpp"
-
-#if (defined(_M_IX86) || defined(_M_AMD64))
-// currently CharRowCell's fields use 3 bytes of memory, leaving the 4th byte in unused. this leads
-// to a rather large amount of useless memory allocated. so instead, pack CharRowCell by bytes instead of words.
-#pragma pack(push, 1)
-#endif
-
-class CharRowCell final
-{
-public:
-    CharRowCell() noexcept = default;
-    CharRowCell(const wchar_t wch, const DbcsAttribute attr) noexcept
-        :
-        _wch(wch),
-        _attr(attr)
-    {
-    }
-
-    void EraseChars() noexcept;
-    void Reset() noexcept;
-
-    bool IsSpace() const noexcept;
-
-    DbcsAttribute& DbcsAttr() noexcept;
-    const DbcsAttribute& DbcsAttr() const noexcept;
-
-    wchar_t& Char() noexcept;
-    const wchar_t& Char() const noexcept;
-
-    friend constexpr bool operator==(const CharRowCell& a, const CharRowCell& b) noexcept;
-
-private:
-    wchar_t _wch{ UNICODE_SPACE };
-    DbcsAttribute _attr{};
-};
-
-#if (defined(_M_IX86) || defined(_M_AMD64))
-#pragma pack(pop)
-#endif
-
-constexpr bool operator==(const CharRowCell& a, const CharRowCell& b) noexcept
-{
-    return (a._wch == b._wch &&
-            a._attr == b._attr);
-}

commit 6b4b63b18a5ec0305eac22387ecc5bb4858523ab

Ensure reading the buffer content actually returns the content (#14379)
## Summary of the Pull Request
Ensures that reading the buffer content actually returns the content.

## References
Regressed in #13626.

## PR Checklist
* [x] Closes #14378
* [x] CLA signed. If not, go over [here](https://cla.opensource.microsoft.com/microsoft/Terminal) and sign the CLA
* [x] Tests added/passed

## Validation Steps Performed
Added a test.

src/cascadia/TerminalControl/ControlCore.cpp
@@ -1744,7 +1744,7 @@ namespace winrt::Microsoft::Terminal::Control::implementation
             const auto strEnd = rowText.find_last_not_of(UNICODE_SPACE);
             if (strEnd != decltype(rowText)::npos)
             {
-                str.append(rowText.substr(strEnd + 1));
+                str.append(rowText.substr(0, strEnd + 1));
             }
 
             if (!row.WasWrapForced())

src/cascadia/UnitTests_Control/ControlCoreTests.cpp
@@ -36,6 +36,7 @@ namespace ControlUnitTests
         TEST_METHOD(TestClearScrollback);
         TEST_METHOD(TestClearScreen);
         TEST_METHOD(TestClearAll);
+        TEST_METHOD(TestReadEntireBuffer);
 
         TEST_CLASS_SETUP(ModuleSetup)
         {
@@ -338,6 +339,24 @@ namespace ControlUnitTests
         // clear scrollback.
         //
         // The ConptyRoundtripTests test the actual clearing of the contents.
+    }
+
+    void ControlCoreTests::TestReadEntireBuffer()
+    {
+        auto [settings, conn] = _createSettingsAndConnection();
+        Log::Comment(L"Create ControlCore object");
+        auto core = createCore(*settings, *conn);
+        VERIFY_IS_NOT_NULL(core);
+        _standardInit(core);
+
+        Log::Comment(L"Print some text");
+        conn->WriteInput(L"This is some text     \r\n");
+        conn->WriteInput(L"with varying amounts  \r\n");
+        conn->WriteInput(L"of whitespace         \r\n");
+
+        Log::Comment(L"Check the buffer contents");
+        VERIFY_ARE_EQUAL(L"This is some text\r\nwith varying amounts\r\nof whitespace\r\n",
+                         core->ReadEntireBuffer());
     }
 
 }