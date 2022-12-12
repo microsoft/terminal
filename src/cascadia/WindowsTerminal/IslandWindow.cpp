microsoft/terminal

commit da2b80bc0a38f5df49a4b987527decb302d16b7b

Add support for switching the scheme based on the app's theme (#14064)
<!-- Enter a brief description/summary of your PR here. What does it fix/what does it change/how was it tested (even manually, if necessary)? -->
## Summary of the Pull Request
This pull request solved the problem of users not being able to set color schemes specifically for dark or light mode. Now the code has been updated to accept a dark and light color scheme in the json. The old setting is still compatible. Keep in mind if you update your color scheme through the settings UI, it will set both dark and light to the color scheme selected. This is because the settings UI update for selecting both Dark and Light color schemes is not supported yet.

This also solves the problem of the UI not using the system OS theme. Now you can select system theme and your color scheme will be selected based on if the system theme is dark or light.


<!-- Other than the issue solved, is this relevant to any other issues/existing PRs? --> 
## References
#4066 

<!-- Please review the items on the PR checklist before submitting-->
## PR Checklist
* [x] Closes #4066 
* [x] Closes #14050
* [x] CLA signed. If not, go over [here](https://cla.opensource.microsoft.com/microsoft/Terminal) and sign the CLA. 
* [x] Tests added/passed I believe so, added one test to ColorSchemeTests.cpp and I believe it passed. Also had to modify TerminalSettingsTests.cpp to accept the new ApplyAppearanceSettings function template
* [ ] Documentation updated. If checked, please file a pull request on [our docs repo](https://github.com/MicrosoftDocs/terminal) and link it here: #xxx
* [x] Schema updated.
* [x] I've discussed this with core contributors already. If not checked, I'm ready to accept this work might be rejected in favor of a different grand plan. Issue number where discussion took place: #4066 and also teams messages with @carlos-zamora

src/cascadia/WindowsTerminal/IslandWindow.cpp
@@ -658,6 +658,18 @@ long IslandWindow::_calculateTotalSize(const bool isWidth, const long clientSize
         }
         break;
     }
+    case WM_SETTINGCHANGE:
+    {
+        const std::wstring param{ (wchar_t*)lparam };
+        // ImmersiveColorSet seems to be the notification that the OS theme
+        // changed. If that happens, let the app know, so it can hot-reload
+        // themes, color schemes that might depend on the OS theme
+        if (param == L"ImmersiveColorSet")
+        {
+            _UpdateSettingsRequestedHandlers();
+        }
+        break;
+    }
     case WM_ENDSESSION:
     {
         // For WM_QUERYENDSESSION and WM_ENDSESSION, refer to:

src/cascadia/LocalTests_SettingsModel/ColorSchemeTests.cpp
@@ -217,6 +217,30 @@ namespace SettingsModelLocalTests
                     {
                         "name": "Different reference",
                         "colorScheme": "One Half Dark"
+                    },
+                    {
+                        "name": "rename neither",
+                        "colorScheme":
+                        {
+                            "dark": "One Half Dark",
+                            "light": "One Half Light"
+                        }
+                    },
+                    {
+                        "name": "rename only light",
+                        "colorScheme":
+                        {
+                            "dark": "One Half Dark",
+                            "light": "Campbell"
+                        }
+                    },
+                    {
+                        "name": "rename only dark",
+                        "colorScheme":
+                        {
+                            "dark": "Campbell",
+                            "light": "One Half Light"
+                        }
                     }
                 ]
             },
@@ -289,6 +313,28 @@ namespace SettingsModelLocalTests
                     "selectionBackground": "#FFFFFF",
                     "white": "#DCDFE4",
                     "yellow": "#E5C07B"
+                },
+                {
+                    "name": "One Half Light",
+                    "foreground": "#383A42",
+                    "background": "#FAFAFA",
+                    "cursorColor": "#4F525D",
+                    "black": "#383A42",
+                    "red": "#E45649",
+                    "green": "#50A14F",
+                    "yellow": "#C18301",
+                    "blue": "#0184BC",
+                    "purple": "#A626A4",
+                    "cyan": "#0997B3",
+                    "white": "#FAFAFA",
+                    "brightBlack": "#4F525D",
+                    "brightRed": "#DF6C75",
+                    "brightGreen": "#98C379",
+                    "brightYellow": "#E4C07A",
+                    "brightBlue": "#61AFEF",
+                    "brightPurple": "#C577DD",
+                    "brightCyan": "#56B5C1",
+                    "brightWhite": "#FFFFFF"
                 }
             ]
         })json" };
@@ -296,31 +342,63 @@ namespace SettingsModelLocalTests
         const auto settings{ winrt::make_self<CascadiaSettings>(settingsString) };
 
         const auto newName{ L"Campbell (renamed)" };
+
         settings->UpdateColorSchemeReferences(L"Campbell", newName);
 
-        VERIFY_ARE_EQUAL(newName, settings->ProfileDefaults().DefaultAppearance().ColorSchemeName());
-        VERIFY_IS_TRUE(settings->ProfileDefaults().DefaultAppearance().HasColorSchemeName());
+        VERIFY_ARE_EQUAL(newName, settings->ProfileDefaults().DefaultAppearance().DarkColorSchemeName());
+        VERIFY_ARE_EQUAL(newName, settings->ProfileDefaults().DefaultAppearance().LightColorSchemeName());
+        VERIFY_IS_TRUE(settings->ProfileDefaults().DefaultAppearance().HasDarkColorSchemeName());
+        VERIFY_IS_TRUE(settings->ProfileDefaults().DefaultAppearance().HasLightColorSchemeName());
 
         const auto& profiles{ settings->AllProfiles() };
         {
             const auto& prof{ profiles.GetAt(0) };
-            VERIFY_ARE_EQUAL(newName, prof.DefaultAppearance().ColorSchemeName());
-            VERIFY_IS_TRUE(prof.DefaultAppearance().HasColorSchemeName());
+            VERIFY_ARE_EQUAL(newName, prof.DefaultAppearance().DarkColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasDarkColorSchemeName());
+            VERIFY_ARE_EQUAL(newName, prof.DefaultAppearance().LightColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasLightColorSchemeName());
         }
         {
             const auto& prof{ profiles.GetAt(1) };
-            VERIFY_ARE_EQUAL(newName, prof.DefaultAppearance().ColorSchemeName());
-            VERIFY_IS_TRUE(prof.DefaultAppearance().HasColorSchemeName());
+            VERIFY_ARE_EQUAL(newName, prof.DefaultAppearance().DarkColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasDarkColorSchemeName());
+            VERIFY_ARE_EQUAL(newName, prof.DefaultAppearance().LightColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasLightColorSchemeName());
         }
         {
             const auto& prof{ profiles.GetAt(2) };
-            VERIFY_ARE_EQUAL(newName, prof.DefaultAppearance().ColorSchemeName());
-            VERIFY_IS_FALSE(prof.DefaultAppearance().HasColorSchemeName());
+            VERIFY_ARE_EQUAL(newName, prof.DefaultAppearance().DarkColorSchemeName());
+            VERIFY_IS_FALSE(prof.DefaultAppearance().HasDarkColorSchemeName());
+            VERIFY_ARE_EQUAL(newName, prof.DefaultAppearance().LightColorSchemeName());
+            VERIFY_IS_FALSE(prof.DefaultAppearance().HasLightColorSchemeName());
         }
         {
             const auto& prof{ profiles.GetAt(3) };
-            VERIFY_ARE_EQUAL(L"One Half Dark", prof.DefaultAppearance().ColorSchemeName());
-            VERIFY_IS_TRUE(prof.DefaultAppearance().HasColorSchemeName());
+            VERIFY_ARE_EQUAL(L"One Half Dark", prof.DefaultAppearance().DarkColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasDarkColorSchemeName());
+            VERIFY_ARE_EQUAL(L"One Half Dark", prof.DefaultAppearance().LightColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasLightColorSchemeName());
+        }
+        {
+            const auto& prof{ profiles.GetAt(4) };
+            VERIFY_ARE_EQUAL(L"One Half Dark", prof.DefaultAppearance().DarkColorSchemeName());
+            VERIFY_ARE_EQUAL(L"One Half Light", prof.DefaultAppearance().LightColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasDarkColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasLightColorSchemeName());
+        }
+        {
+            const auto& prof{ profiles.GetAt(5) };
+            VERIFY_ARE_EQUAL(L"One Half Dark", prof.DefaultAppearance().DarkColorSchemeName());
+            VERIFY_ARE_EQUAL(newName, prof.DefaultAppearance().LightColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasDarkColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasLightColorSchemeName());
+        }
+        {
+            const auto& prof{ profiles.GetAt(6) };
+            VERIFY_ARE_EQUAL(newName, prof.DefaultAppearance().DarkColorSchemeName());
+            VERIFY_ARE_EQUAL(L"One Half Light", prof.DefaultAppearance().LightColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasDarkColorSchemeName());
+            VERIFY_IS_TRUE(prof.DefaultAppearance().HasLightColorSchemeName());
         }
     }
 }

src/cascadia/LocalTests_SettingsModel/DeserializationTests.cpp
@@ -744,7 +744,8 @@ namespace SettingsModelLocalTests
         VERIFY_ARE_EQUAL(3u, settings->AllProfiles().Size());
         for (const auto& profile : settings->AllProfiles())
         {
-            VERIFY_ARE_EQUAL(L"Campbell", profile.DefaultAppearance().ColorSchemeName());
+            VERIFY_ARE_EQUAL(L"Campbell", profile.DefaultAppearance().DarkColorSchemeName());
+            VERIFY_ARE_EQUAL(L"Campbell", profile.DefaultAppearance().LightColorSchemeName());
         }
     }
 
@@ -959,6 +960,10 @@ namespace SettingsModelLocalTests
                 },
                 {
                     "name": "profile3",
+                    "closeOnExit": "automatic"
+                },
+                {
+                    "name": "profile4",
                     "closeOnExit": null
                 }
             ]
@@ -968,9 +973,10 @@ namespace SettingsModelLocalTests
         VERIFY_ARE_EQUAL(CloseOnExitMode::Graceful, settings->AllProfiles().GetAt(0).CloseOnExit());
         VERIFY_ARE_EQUAL(CloseOnExitMode::Always, settings->AllProfiles().GetAt(1).CloseOnExit());
         VERIFY_ARE_EQUAL(CloseOnExitMode::Never, settings->AllProfiles().GetAt(2).CloseOnExit());
+        VERIFY_ARE_EQUAL(CloseOnExitMode::Automatic, settings->AllProfiles().GetAt(3).CloseOnExit());
 
-        // Unknown modes parse as "Graceful"
-        VERIFY_ARE_EQUAL(CloseOnExitMode::Graceful, settings->AllProfiles().GetAt(3).CloseOnExit());
+        // Unknown modes parse as "Automatic"
+        VERIFY_ARE_EQUAL(CloseOnExitMode::Automatic, settings->AllProfiles().GetAt(4).CloseOnExit());
     }
 
     void DeserializationTests::TestCloseOnExitCompatibilityShim()

src/cascadia/LocalTests_SettingsModel/SerializationTests.cpp
@@ -134,7 +134,7 @@ namespace SettingsModelLocalTests
 
                 "font": {
                     "face": "Cascadia Mono",
-                    "size": 12,
+                    "size": 12.0,
                     "weight": "normal"
                 },
                 "padding": "8, 8, 8, 8",
@@ -250,8 +250,8 @@ namespace SettingsModelLocalTests
 
         // complex command with key chords
         static constexpr std::string_view actionsString4A{ R"([
-                                                { "command": { "action": "adjustFontSize", "delta": 1 }, "keys": "ctrl+c" },
-                                                { "command": { "action": "adjustFontSize", "delta": 1 }, "keys": "ctrl+d" }
+                                                { "command": { "action": "adjustFontSize", "delta": 1.0 }, "keys": "ctrl+c" },
+                                                { "command": { "action": "adjustFontSize", "delta": 1.0 }, "keys": "ctrl+d" }
                                             ])" };
         // GH#13323 - these can be fragile. In the past, the order these get
         // re-serialized as has been not entirely stable. We don't really care
@@ -260,7 +260,7 @@ namespace SettingsModelLocalTests
         // itself. Feel free to change as needed.
         static constexpr std::string_view actionsString4B{ R"([
                                                 { "command": { "action": "findMatch", "direction": "prev" }, "keys": "ctrl+shift+r" },
-                                                { "command": { "action": "adjustFontSize", "delta": 1 }, "keys": "ctrl+d" }
+                                                { "command": { "action": "adjustFontSize", "delta": 1.0 }, "keys": "ctrl+d" }
                                             ])" };
 
         // command with name and icon and multiple key chords
@@ -284,8 +284,8 @@ namespace SettingsModelLocalTests
                                                 {
                                                     "name": "Change font size...",
                                                     "commands": [
-                                                        { "command": { "action": "adjustFontSize", "delta": 1 } },
-                                                        { "command": { "action": "adjustFontSize", "delta": -1 } },
+                                                        { "command": { "action": "adjustFontSize", "delta": 1.0 } },
+                                                        { "command": { "action": "adjustFontSize", "delta": -1.0 } },
                                                         { "command": "resetFontSize" },
                                                     ]
                                                 }
@@ -478,7 +478,7 @@ namespace SettingsModelLocalTests
                 "name": "Profile with legacy font settings",
 
                 "fontFace": "Cascadia Mono",
-                "fontSize": 12,
+                "fontSize": 12.0,
                 "fontWeight": "normal"
             })" };
 
@@ -488,7 +488,7 @@ namespace SettingsModelLocalTests
 
                 "font": {
                     "face": "Cascadia Mono",
-                    "size": 12,
+                    "size": 12.0,
                     "weight": "normal"
                 }
             })" };

src/cascadia/LocalTests_SettingsModel/TerminalSettingsTests.cpp
@@ -779,21 +779,22 @@ namespace SettingsModelLocalTests
         VERIFY_ARE_EQUAL(6u, settings->ActiveProfiles().Size());
         VERIFY_ARE_EQUAL(2u, settings->GlobalSettings().ColorSchemes().Size());
 
-        auto createTerminalSettings = [&](const auto& profile, const auto& schemes) {
+        auto createTerminalSettings = [&](const auto& profile, const auto& schemes, const auto& Theme) {
             auto terminalSettings{ winrt::make_self<implementation::TerminalSettings>() };
             terminalSettings->_ApplyProfileSettings(profile);
-            terminalSettings->_ApplyAppearanceSettings(profile.DefaultAppearance(), schemes);
+            terminalSettings->_ApplyAppearanceSettings(profile.DefaultAppearance(), schemes, Theme);
             return terminalSettings;
         };
 
         const auto activeProfiles = settings->ActiveProfiles();
         const auto colorSchemes = settings->GlobalSettings().ColorSchemes();
-        const auto terminalSettings0 = createTerminalSettings(activeProfiles.GetAt(0), colorSchemes);
-        const auto terminalSettings1 = createTerminalSettings(activeProfiles.GetAt(1), colorSchemes);
-        const auto terminalSettings2 = createTerminalSettings(activeProfiles.GetAt(2), colorSchemes);
-        const auto terminalSettings3 = createTerminalSettings(activeProfiles.GetAt(3), colorSchemes);
-        const auto terminalSettings4 = createTerminalSettings(activeProfiles.GetAt(4), colorSchemes);
-        const auto terminalSettings5 = createTerminalSettings(activeProfiles.GetAt(5), colorSchemes);
+        const auto currentTheme = settings->GlobalSettings().CurrentTheme();
+        const auto terminalSettings0 = createTerminalSettings(activeProfiles.GetAt(0), colorSchemes, currentTheme);
+        const auto terminalSettings1 = createTerminalSettings(activeProfiles.GetAt(1), colorSchemes, currentTheme);
+        const auto terminalSettings2 = createTerminalSettings(activeProfiles.GetAt(2), colorSchemes, currentTheme);
+        const auto terminalSettings3 = createTerminalSettings(activeProfiles.GetAt(3), colorSchemes, currentTheme);
+        const auto terminalSettings4 = createTerminalSettings(activeProfiles.GetAt(4), colorSchemes, currentTheme);
+        const auto terminalSettings5 = createTerminalSettings(activeProfiles.GetAt(5), colorSchemes, currentTheme);
 
         VERIFY_ARE_EQUAL(til::color(0x12, 0x34, 0x56), terminalSettings0->CursorColor()); // from color scheme
         VERIFY_ARE_EQUAL(DEFAULT_CURSOR_COLOR, terminalSettings1->CursorColor()); // default

src/cascadia/LocalTests_SettingsModel/pch.h
@@ -44,6 +44,7 @@ Author(s):
 #include <Windows.Graphics.Imaging.Interop.h>
 #include <winrt/windows.ui.core.h>
 #include <winrt/Windows.ui.input.h>
+#include <winrt/Windows.UI.ViewManagement.h>
 #include <winrt/Windows.UI.Xaml.Controls.h>
 #include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Windows.ui.xaml.media.h>

src/cascadia/TerminalApp/App.cpp
@@ -54,7 +54,7 @@ namespace winrt::TerminalApp::implementation
             {
                 auto logic = Logic();
                 logic.RunAsUwp(); // Must set UWP status first, settings might change based on it.
-                logic.LoadSettings();
+                logic.ReloadSettings();
                 logic.Create();
 
                 auto page = logic.GetRoot().as<TerminalPage>();

src/cascadia/TerminalApp/AppLogic.cpp
@@ -191,7 +191,7 @@ namespace winrt::TerminalApp::implementation
         _reloadSettings = std::make_shared<ThrottledFuncTrailing<>>(winrt::Windows::System::DispatcherQueue::GetForCurrentThread(), std::chrono::milliseconds(100), [weakSelf = get_weak()]() {
             if (auto self{ weakSelf.get() })
             {
-                self->_ReloadSettings();
+                self->ReloadSettings();
             }
         });
 
@@ -615,7 +615,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
 
         winrt::Windows::Foundation::Size proposedSize{};
@@ -696,7 +696,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
 
         // GH#4620/#5801 - If the user passed --maximized or --fullscreen on the
@@ -730,7 +730,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
 
         auto initialPosition{ _settings.GlobalSettings().InitialPosition() };
@@ -760,7 +760,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
         // If the position has been specified on the commandline, don't center on launch
         return _settings.GlobalSettings().CenterOnLaunch() && !_appArgs.GetPosition().has_value();
@@ -776,7 +776,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
 
         return _settings.GlobalSettings().ShowTabsInTitlebar();
@@ -787,7 +787,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
 
         return _settings.GlobalSettings().AlwaysOnTop();
@@ -867,36 +867,6 @@ namespace winrt::TerminalApp::implementation
         return hr;
     }
 
-    // Method Description:
-    // - Initialized our settings. See CascadiaSettings for more details.
-    //      Additionally hooks up our callbacks for keybinding events to the
-    //      keybindings object.
-    // NOTE: This must be called from a MTA if we're running as a packaged
-    //      application. The Windows.Storage APIs require a MTA. If this isn't
-    //      happening during startup, it'll need to happen on a background thread.
-    void AppLogic::LoadSettings()
-    {
-        // Attempt to load the settings.
-        // If it fails,
-        //  - use Default settings,
-        //  - don't persist them (LoadAll won't save them in this case).
-        //  - _settingsLoadedResult will be set to an error, indicating that
-        //    we should display the loading error.
-        //    * We can't display the error now, because we might not have a
-        //      UI yet. We'll display the error in _OnLoaded.
-        _settingsLoadedResult = _TryLoadSettings();
-
-        if (FAILED(_settingsLoadedResult))
-        {
-            _settings = CascadiaSettings::LoadDefaults();
-        }
-
-        _loadedInitialSettings = true;
-
-        // Register for directory change notification.
-        _RegisterSettingsChange();
-    }
-
     // Call this function after loading your _settings.
     // It handles any CPU intensive settings updates (like updating the Jumplist)
     // which should thus only occur if the settings file actually changed.
@@ -920,7 +890,7 @@ namespace winrt::TerminalApp::implementation
 
     // Method Description:
     // - Registers for changes to the settings folder and upon a updated settings
-    //      profile calls _ReloadSettings().
+    //      profile calls ReloadSettings().
     // Arguments:
     // - <none>
     // Return Value:
@@ -1055,7 +1025,14 @@ namespace winrt::TerminalApp::implementation
 
     // Method Description:
     // - Reloads the settings from the settings.json file.
-    void AppLogic::_ReloadSettings()
+    // - When this is called the first time, this initializes our settings. See
+    //   CascadiaSettings for more details. Additionally hooks up our callbacks
+    //   for keybinding events to the keybindings object.
+    //   - NOTE: when called initially, this must be called from a MTA if we're
+    //     running as a packaged application. The Windows.Storage APIs require a
+    //     MTA. If this isn't happening during startup, it'll need to happen on
+    //     a background thread.
+    void AppLogic::ReloadSettings()
     {
         // Attempt to load our settings.
         // If it fails,
@@ -1064,11 +1041,28 @@ namespace winrt::TerminalApp::implementation
         //  - display a loading error
         _settingsLoadedResult = _TryLoadSettings();
 
+        const auto initialLoad = !_loadedInitialSettings;
+        _loadedInitialSettings = true;
+
         if (FAILED(_settingsLoadedResult))
         {
-            const winrt::hstring titleKey = USES_RESOURCE(L"ReloadJsonParseErrorTitle");
-            const winrt::hstring textKey = USES_RESOURCE(L"ReloadJsonParseErrorText");
-            _ShowLoadErrorsDialog(titleKey, textKey, _settingsLoadedResult);
+            if (initialLoad)
+            {
+                _settings = CascadiaSettings::LoadDefaults();
+            }
+            else
+            {
+                const winrt::hstring titleKey = USES_RESOURCE(L"ReloadJsonParseErrorTitle");
+                const winrt::hstring textKey = USES_RESOURCE(L"ReloadJsonParseErrorText");
+                _ShowLoadErrorsDialog(titleKey, textKey, _settingsLoadedResult);
+                return;
+            }
+        }
+
+        if (initialLoad)
+        {
+            // Register for directory change notification.
+            _RegisterSettingsChange();
             return;
         }
 
@@ -1375,7 +1369,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
 
         return AppLogic::_doFindTargetWindow(args, _settings.GlobalSettings().WindowingBehavior());
@@ -1532,7 +1526,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
 
         return _settings.GlobalSettings().AutoHideWindow();
@@ -1553,7 +1547,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
 
         return _root != nullptr ? _root->ShouldImmediatelyHandoffToElevated(_settings) : false;
@@ -1666,7 +1660,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
 
         return _settings.GlobalSettings().MinimizeToNotificationArea();
@@ -1677,7 +1671,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
 
         return _settings.GlobalSettings().AlwaysShowNotificationIcon();
@@ -1693,7 +1687,7 @@ namespace winrt::TerminalApp::implementation
         if (!_loadedInitialSettings)
         {
             // Load settings if we haven't already
-            LoadSettings();
+            ReloadSettings();
         }
         return _settings.GlobalSettings().CurrentTheme();
     }

commit 58ce22d484c7dd305d7e441325eaf576f9ae9abf

Don't crash when turning on Voice Access (#14534)
I should have checked the `wparam`. 

Regressed in #14064.

Tested manually. No more crashy. 

Closes #14504

src/cascadia/WindowsTerminal/IslandWindow.cpp
@@ -660,13 +660,19 @@ long IslandWindow::_calculateTotalSize(const bool isWidth, const long clientSize
     }
     case WM_SETTINGCHANGE:
     {
-        const std::wstring param{ (wchar_t*)lparam };
-        // ImmersiveColorSet seems to be the notification that the OS theme
-        // changed. If that happens, let the app know, so it can hot-reload
-        // themes, color schemes that might depend on the OS theme
-        if (param == L"ImmersiveColorSet")
+        // Currently, we only support checking when the OS theme changes. In
+        // that case, wParam is 0. Re-evaluate when we decide to reload env vars
+        // (GH#1125)
+        if (wparam == 0)
         {
-            _UpdateSettingsRequestedHandlers();
+            const std::wstring param{ (wchar_t*)lparam };
+            // ImmersiveColorSet seems to be the notification that the OS theme
+            // changed. If that happens, let the app know, so it can hot-reload
+            // themes, color schemes that might depend on the OS theme
+            if (param == L"ImmersiveColorSet")
+            {
+                _UpdateSettingsRequestedHandlers();
+            }
         }
         break;
     }