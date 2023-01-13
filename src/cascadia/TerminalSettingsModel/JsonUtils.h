31d4345ea491661e96773af0f75a33c406296da9
Dustin L. Howett <duhowett@microsoft.com>
2023-01-12 18:43:24 -0600

    Fix an issue where JsonUtils produces a bad error message (#14668)
    CascadiaSettings relies on getting a JsonUtils::DeserializationException
    from the various JSON Utility functions, and then formatting that into
    an error message. Well, DeserializationException tries to include an
    object representation in its what() message . . . and generates an
    exception trying to do so. CascadiaSettings never gets the
    DeserializationException, and displays a weird message.
    
    It's safe to remove the stringification in DeserializationException
    because CascadiaSettings was never using it (_and_ because
    CascadiaSettings was using an even better version of the same logic.)
    
    Fixes #14373
    
    (cherry picked from commit 239b4d16b9ac179b5264774baee6de58254d8cce)
    Service-Card-Id: 87568498
    Service-Version: 1.16

--- src/cascadia/TerminalSettingsModel/JsonUtils.h
+++ src/cascadia/TerminalSettingsModel/JsonUtils.h
@@ -101,7 +101,7 @@ namespace Microsoft::Terminal::Settings::Model::JsonUtils
     {
     public:
         DeserializationError(const Json::Value& value) :
-            runtime_error(std::string("failed to deserialize ") + (value.isNull() ? "" : value.asString())),
+            runtime_error(std::string{ "failed to deserialize JSON value" }),
             jsonValue{ value } {}
 
         void SetKey(std::string_view newKey)
