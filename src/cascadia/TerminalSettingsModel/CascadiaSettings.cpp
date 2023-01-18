70bf9105571f318a9069202bd8f3bf7b1ca06628
Dustin L. Howett <duhowett@microsoft.com>
2023-01-18 15:37:05 -0600

    Update src/cascadia/TerminalSettingsModel/CascadiaSettings.cpp

--- src/cascadia/TerminalSettingsModel/CascadiaSettings.cpp
+++ src/cascadia/TerminalSettingsModel/CascadiaSettings.cpp
@@ -1015,7 +1015,7 @@ winrt::hstring CascadiaSettings::ApplicationVersion()
     {
         const auto package{ winrt::Windows::ApplicationModel::Package::Current() };
         const auto version{ package.Id().Version() };
-        // As of about 2022, the one's digit of the Build of our version is a
+        // As of about 2022, the ones digit of the Build of our version is a
         // placeholder value to differentiate the Windows 10 build from the
         // Windows 11 build. Let's trim that out. For additional clarity,
         // let's omit the Revision, which _must_ be .0, and doesn't provide any
