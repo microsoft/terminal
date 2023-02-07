2a6520dfef0e5364e69b120cd93f1ddb85069655
Mike Griese <migrie@microsoft.com>
2023-02-07 06:11:26 -0600

    cleanup

--- src/cascadia/TerminalControl/ControlCore.cpp
+++ src/cascadia/TerminalControl/ControlCore.cpp
@@ -2189,12 +2189,10 @@ namespace winrt::Microsoft::Terminal::Control::implementation
 
             const auto bufferSize{ _terminal->GetTextBuffer().GetSize() };
             bufferSize.DecrementInBounds(end);
-            // _renderer->TriggerSelection();
 
             auto lock = _terminal->LockForWriting();
             _terminal->SelectNewRegion(start, end);
             _renderer->TriggerSelection();
-            // _renderer->TriggerRedrawAll();
         }
     }
 
