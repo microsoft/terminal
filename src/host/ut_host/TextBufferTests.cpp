934a06d61ac34351729db468dec40a27f5e05841
Leonard Hecker <lhecker@microsoft.com>
2023-03-18 16:11:00 +0100

    Fix unit tests

--- src/host/ut_host/TextBufferTests.cpp
+++ src/host/ut_host/TextBufferTests.cpp
@@ -147,7 +147,7 @@ class TextBufferTests
 
     TEST_METHOD(TestBurrito);
     TEST_METHOD(TestOverwriteChars);
-    TEST_METHOD(TestRowWrite);
+    TEST_METHOD(TestRowReplaceText);
 
     TEST_METHOD(TestAppendRTFText);
 
@@ -2047,46 +2047,53 @@ void TextBufferTests::TestOverwriteChars()
 #undef complex1
 }
 
-void TextBufferTests::TestRowWrite()
+void TextBufferTests::TestRowReplaceText()
 {
     til::size bufferSize{ 10, 3 };
     UINT cursorSize = 12;
     TextAttribute attr{ 0x7f };
     TextBuffer buffer{ bufferSize, attr, cursorSize, false, _renderer };
     auto& row = buffer.GetRowByOffset(0);
-    std::wstring_view str;
-    til::CoordType pos = 0;
+    RowWriteState state;
 
 #define complex L"\U0001F41B"
 
     // Not enough space -> early exit
-    str = complex;
-    pos = row.Write(2, 2, str);
-    VERIFY_ARE_EQUAL(2, pos);
-    VERIFY_ARE_EQUAL(complex, str);
+    state.text = complex;
+    state.columnBegin = 2;
+    state.columnLimit = 2;
+    row.ReplaceText(state);
+    VERIFY_ARE_EQUAL(2, state.columnEndDirty);
+    VERIFY_ARE_EQUAL(complex, state.text);
     VERIFY_ARE_EQUAL(L"          ", row.GetText());
 
     // Writing with the exact right amount of space
-    str = complex;
-    pos = row.Write(2, 4, str);
-    VERIFY_ARE_EQUAL(4, pos);
-    VERIFY_ARE_EQUAL(L"", str);
+    state.text = complex;
+    state.columnBegin = 2;
+    state.columnLimit = 4;
+    row.ReplaceText(state);
+    VERIFY_ARE_EQUAL(4, state.columnEndDirty);
+    VERIFY_ARE_EQUAL(L"", state.text);
     VERIFY_ARE_EQUAL(L"  " complex L"      ", row.GetText());
 
     // Overwrite a wide character, but with not enough space left
-    str = complex complex;
-    pos = row.Write(0, 3, str);
+    state.text = complex complex;
+    state.columnBegin = 0;
+    state.columnLimit = 3;
+    row.ReplaceText(state);
     // It's not quite clear what Write() should return in that case,
     // so this simply asserts on what it happens to return right now.
-    VERIFY_ARE_EQUAL(4, pos);
-    VERIFY_ARE_EQUAL(complex, str);
+    VERIFY_ARE_EQUAL(4, state.columnEndDirty);
+    VERIFY_ARE_EQUAL(complex, state.text);
     VERIFY_ARE_EQUAL(complex L"        ", row.GetText());
 
     // Various text, too much to fit into the row
-    str = L"a" complex L"b" complex L"c" complex L"foo";
-    pos = row.Write(1, til::CoordTypeMax, str);
-    VERIFY_ARE_EQUAL(10, pos);
-    VERIFY_ARE_EQUAL(L"foo", str);
+    state.text = L"a" complex L"b" complex L"c" complex L"foo";
+    state.columnBegin = 1;
+    state.columnLimit = til::CoordTypeMax;
+    row.ReplaceText(state);
+    VERIFY_ARE_EQUAL(10, state.columnEndDirty);
+    VERIFY_ARE_EQUAL(L"foo", state.text);
     VERIFY_ARE_EQUAL(L" a" complex L"b" complex L"c" complex, row.GetText());
 
 #undef complex
