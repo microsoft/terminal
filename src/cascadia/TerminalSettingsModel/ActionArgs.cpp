dea426a3bf94901f924a7dbffa7adde603f57beb
Mike Griese <migrie@microsoft.com>
2023-03-21 11:00:22 -0500

    And my command name

--- src/cascadia/TerminalSettingsModel/ActionArgs.cpp
+++ src/cascadia/TerminalSettingsModel/ActionArgs.cpp
@@ -676,15 +676,23 @@ namespace winrt::Microsoft::Terminal::Settings::Model::implementation
 
     winrt::hstring SuggestionsArgs::GenerateName() const
     {
+        auto base{ RS_(L"SuggestionsCommandKey") };
         switch (Source())
         {
         case SuggestionsSource::CommandHistory:
-            return RS_(L"SuggestionsCommandHistoryCommandKey");
+            base = RS_(L"SuggestionsCommandHistoryCommandKey");
         }
 
-        // TODO!: useCommandline
-
-        return RS_(L"SuggestionsCommandKey");
+        if (UseCommandline())
+        {
+            return winrt::hstring{
+                fmt::format(L"{}, useCommandline:true", std::wstring_view(base))
+            };
+        }
+        else
+        {
+            return base;
+        }
     }
 
     winrt::hstring FindMatchArgs::GenerateName() const
