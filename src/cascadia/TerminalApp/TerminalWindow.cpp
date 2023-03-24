microsoft/terminal

commit b9248fa903eaa8d187c72bf08f8aa17b25f9d6bb

One process to rule them all (#14843)
## Summary

_In the days of old, the windows were sundered, each with its own
process, like the scattered stars in the sky. But a new age hath dawned,
for all windows now reside within a single process, like the bright gems
of a single crown._
_And lo, there came the `WindowEmperor`, a new lord to rule over the
global state, wielding the power of hotkeys and the beacon of the
notification icon. The `WindowManager` was cast aside, no longer needed
to seek out other processes or determine the `Monarch`._
_Should the `WindowEmperor` determine that a new window shall be raised,
it shall set forth a new thread, born from the ether, to govern this new
realm. On the main thread shall reside a message loop, charged with the
weighty task of preserving the global state, guarded by hotkeys and the
beacon of the notification icon._
_Each window doth live on its own thread, each guarded by the new
`WindowThread`, a knightly champion to hold the `TerminalWindow`,
`AppHost`, and `IslandWindow` in its grasp. And so the windows shall run
free, no longer burdened by their former ways._


All windows are now in a single process, rather than in one process per
window. We'll add a new `WindowEmperor` class to manage global state
such as hotkeys and the notification icon. The `WindowManager` has been
streamlined and no longer needs to connect to other processes or
determine a new `Monarch`. Each window will run on its own thread, using
the new `WindowThread` class to encapsulate the thread and manage the
`TerminalWindow`, `AppHost`, and `IslandWindow`.


* Related to #5000
* Related to #1256

## Windows Terminal Process Model 3.0

Everything is now one process. All the windows for a single Terminal
instance live in a singular Terminal process. When a new terminal
process launches, it will still attempt to communicate with an existing
one. If it finds one, it'll pass the commandline to that process and

src/cascadia/TerminalApp/TerminalWindow.cpp
@@ -123,11 +123,8 @@ namespace winrt::TerminalApp::implementation
         _initialLoadResult{ settingsLoadedResult },
         _WindowProperties{ winrt::make_self<TerminalApp::implementation::WindowProperties>() }
     {
-        // The TerminalPage has to be constructed during our construction, to
-        // make sure that there's a terminal page for callers of
-        // SetTitleBarContent
-        _root = winrt::make_self<TerminalPage>(*_WindowProperties);
-        _dialog = ContentDialog{};
+        // The TerminalPage has to ABSOLUTELY NOT BE constructed during our
+        // construction. We can't do ANY xaml till Initialize() is called.
 
         // For your own sanity, it's better to do setup outside the ctor.
         // If you do any setup in the ctor that ends up throwing an exception,
@@ -140,6 +137,10 @@ namespace winrt::TerminalApp::implementation
     // - Implements the IInitializeWithWindow interface from shobjidl_core.
     HRESULT TerminalWindow::Initialize(HWND hwnd)
     {
+        // Now that we know we can do XAML, build our page.
+        _root = winrt::make_self<TerminalPage>(*_WindowProperties);
+        _dialog = ContentDialog{};
+
         // Pass commandline args into the TerminalPage. If we were supposed to
         // load from a persisted layout, do that instead.
 
@@ -222,8 +223,9 @@ namespace winrt::TerminalApp::implementation
             _root->SetStartupActions(_settingsStartupArgs);
         }
 
-        _root->SetSettings(_settings, false);
-        _root->Loaded({ this, &TerminalWindow::_OnLoaded });
+        _root->SetSettings(_settings, false); // We're on our UI thread right now, so this is safe
+        _root->Loaded({ get_weak(), &TerminalWindow::_OnLoaded });
+
         _root->Initialized([this](auto&&, auto&&) {
             // GH#288 - When we finish initialization, if the user wanted us
             // launched _fullscreen_, toggle fullscreen mode. This will make sure
@@ -715,32 +717,40 @@ namespace winrt::TerminalApp::implementation
         _RequestedThemeChangedHandlers(*this, Theme());
     }
 
+    // This may be called on a background thread, or the main thread, but almost
+    // definitely not on OUR UI thread.
     winrt::fire_and_forget TerminalWindow::UpdateSettings(winrt::TerminalApp::SettingsLoadEventArgs args)
     {
         _settings = args.NewSettings();
-        // Update the settings in TerminalPage
-        _root->SetSettings(_settings, true);
 
+        const auto weakThis{ get_weak() };
         co_await wil::resume_foreground(_root->Dispatcher());
-
-        // Bubble the notification up to the AppHost, now that we've updated our _settings.
-        _SettingsChangedHandlers(*this, args);
-
-        if (FAILED(args.Result()))
+        // Back on our UI thread...
+        if (auto logic{ weakThis.get() })
         {
-            const winrt::hstring titleKey = USES_RESOURCE(L"ReloadJsonParseErrorTitle");
-            const winrt::hstring textKey = USES_RESOURCE(L"ReloadJsonParseErrorText");
-            _ShowLoadErrorsDialog(titleKey,
-                                  textKey,
-                                  gsl::narrow_cast<HRESULT>(args.Result()),
-                                  args.ExceptionText());
-            co_return;
+            // Update the settings in TerminalPage
+            // We're on our UI thread right now, so this is safe
+            _root->SetSettings(_settings, true);
+
+            // Bubble the notification up to the AppHost, now that we've updated our _settings.
+            _SettingsChangedHandlers(*this, args);
+
+            if (FAILED(args.Result()))
+            {
+                const winrt::hstring titleKey = USES_RESOURCE(L"ReloadJsonParseErrorTitle");
+                const winrt::hstring textKey = USES_RESOURCE(L"ReloadJsonParseErrorText");
+                _ShowLoadErrorsDialog(titleKey,
+                                      textKey,
+                                      gsl::narrow_cast<HRESULT>(args.Result()),
+                                      args.ExceptionText());
+                co_return;
+            }
+            else if (args.Result() == S_FALSE)
+            {
+                _ShowLoadWarningsDialog(args.Warnings());
+            }
+            _RefreshThemeRoutine();
         }
-        else if (args.Result() == S_FALSE)
-        {
-            _ShowLoadWarningsDialog(args.Warnings());
-        }
-        _RefreshThemeRoutine();
     }
 
     void TerminalWindow::_OpenSettingsUI()
@@ -1107,6 +1117,22 @@ namespace winrt::TerminalApp::implementation
         return *_cachedLayout;
     }
 
+    void TerminalWindow::RequestExitFullscreen()
+    {
+        _root->SetFullscreen(false);
+    }
+
+    bool TerminalWindow::AutoHideWindow()
+    {
+        return _settings.GlobalSettings().AutoHideWindow();
+    }
+
+    void TerminalWindow::UpdateSettingsHandler(const winrt::IInspectable& /*sender*/,
+                                               const winrt::TerminalApp::SettingsLoadEventArgs& args)
+    {
+        UpdateSettings(args);
+    }
+
     void TerminalWindow::IdentifyWindow()
     {
         if (_root)
@@ -1140,22 +1166,6 @@ namespace winrt::TerminalApp::implementation
     void TerminalWindow::WindowId(const uint64_t& id)
     {
         _WindowProperties->WindowId(id);
-    }
-
-    void TerminalWindow::RequestExitFullscreen()
-    {
-        _root->SetFullscreen(false);
-    }
-
-    bool TerminalWindow::AutoHideWindow()
-    {
-        return _settings.GlobalSettings().AutoHideWindow();
-    }
-
-    void TerminalWindow::UpdateSettingsHandler(const winrt::IInspectable& /*sender*/,
-                                               const winrt::TerminalApp::SettingsLoadEventArgs& arg)
-    {
-        UpdateSettings(arg);
     }
 
     bool TerminalWindow::ShouldImmediatelyHandoffToElevated()
@@ -1209,12 +1219,7 @@ namespace winrt::TerminalApp::implementation
 
     void WindowProperties::WindowId(const uint64_t& value)
     {
-        if (_WindowId != value)
-        {
-            _WindowId = value;
-            _PropertyChangedHandlers(*this, Windows::UI::Xaml::Data::PropertyChangedEventArgs{ L"WindowId" });
-            _PropertyChangedHandlers(*this, Windows::UI::Xaml::Data::PropertyChangedEventArgs{ L"WindowIdForDisplay" });
-        }
+        _WindowId = value;
     }
 
     // Method Description:

src/cascadia/LocalTests_TerminalApp/SettingsTests.cpp
@@ -98,6 +98,26 @@ namespace TerminalAppLocalTests
                 }
             }
         }
+        void _logCommands(winrt::Windows::Foundation::Collections::IVector<Command> commands, const int indentation = 1)
+        {
+            if (indentation == 1)
+            {
+                Log::Comment((commands.Size() == 0) ? L"Commands:\n  <none>" : L"Commands:");
+            }
+            for (const auto& cmd : commands)
+            {
+                Log::Comment(fmt::format(L"{0:>{1}}* {2}",
+                                         L"",
+                                         indentation,
+                                         cmd.Name())
+                                 .c_str());
+
+                if (cmd.HasNestedCommands())
+                {
+                    _logCommandNames(cmd.NestedCommands(), indentation + 2);
+                }
+            }
+        }
     };
 
     void SettingsTests::TestIterateCommands()
@@ -164,14 +184,15 @@ namespace TerminalAppLocalTests
             VERIFY_ARE_EQUAL(L"${profile.name}", realArgs.TerminalArgs().Profile());
         }
 
-        auto expandedCommands = winrt::TerminalApp::implementation::TerminalPage::_ExpandCommands(nameMap, settings.ActiveProfiles().GetView(), settings.GlobalSettings().ColorSchemes());
-        _logCommandNames(expandedCommands.GetView());
+        const auto& expandedCommands{ settings.GlobalSettings().ActionMap().ExpandedCommands() };
+        _logCommands(expandedCommands);
 
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(3u, expandedCommands.Size());
 
         {
-            auto command = expandedCommands.Lookup(L"iterable command profile0");
+            auto command = expandedCommands.GetAt(0);
+            VERIFY_ARE_EQUAL(L"iterable command profile0", command.Name());
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -189,7 +210,8 @@ namespace TerminalAppLocalTests
         }
 
         {
-            auto command = expandedCommands.Lookup(L"iterable command profile1");
+            auto command = expandedCommands.GetAt(1);
+            VERIFY_ARE_EQUAL(L"iterable command profile1", command.Name());
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -207,7 +229,8 @@ namespace TerminalAppLocalTests
         }
 
         {
-            auto command = expandedCommands.Lookup(L"iterable command profile2");
+            auto command = expandedCommands.GetAt(2);
+            VERIFY_ARE_EQUAL(L"iterable command profile2", command.Name());
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -287,14 +310,16 @@ namespace TerminalAppLocalTests
             VERIFY_ARE_EQUAL(L"${profile.name}", realArgs.TerminalArgs().Profile());
         }
 
-        auto expandedCommands = winrt::TerminalApp::implementation::TerminalPage::_ExpandCommands(nameMap, settings.ActiveProfiles().GetView(), settings.GlobalSettings().ColorSchemes());
-        _logCommandNames(expandedCommands.GetView());
+        const auto& expandedCommands{ settings.GlobalSettings().ActionMap().ExpandedCommands() };
+        _logCommands(expandedCommands);
 
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(3u, expandedCommands.Size());
 
         {
-            auto command = expandedCommands.Lookup(L"Split pane, profile: profile0");
+            auto command = expandedCommands.GetAt(0);
+            VERIFY_ARE_EQUAL(L"Split pane, profile: profile0", command.Name());
+
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -312,7 +337,9 @@ namespace TerminalAppLocalTests
         }
 
         {
-            auto command = expandedCommands.Lookup(L"Split pane, profile: profile1");
+            auto command = expandedCommands.GetAt(1);
+            VERIFY_ARE_EQUAL(L"Split pane, profile: profile1", command.Name());
+
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -330,7 +357,9 @@ namespace TerminalAppLocalTests
         }
 
         {
-            auto command = expandedCommands.Lookup(L"Split pane, profile: profile2");
+            auto command = expandedCommands.GetAt(2);
+            VERIFY_ARE_EQUAL(L"Split pane, profile: profile2", command.Name());
+
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -412,14 +441,16 @@ namespace TerminalAppLocalTests
             VERIFY_ARE_EQUAL(L"${profile.name}", realArgs.TerminalArgs().Profile());
         }
 
-        auto expandedCommands = winrt::TerminalApp::implementation::TerminalPage::_ExpandCommands(nameMap, settings.ActiveProfiles().GetView(), settings.GlobalSettings().ColorSchemes());
-        _logCommandNames(expandedCommands.GetView());
+        const auto& expandedCommands{ settings.GlobalSettings().ActionMap().ExpandedCommands() };
+        _logCommands(expandedCommands);
 
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(3u, expandedCommands.Size());
 
         {
-            auto command = expandedCommands.Lookup(L"iterable command profile0");
+            auto command = expandedCommands.GetAt(0);
+            VERIFY_ARE_EQUAL(L"iterable command profile0", command.Name());
+
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -437,7 +468,9 @@ namespace TerminalAppLocalTests
         }
 
         {
-            auto command = expandedCommands.Lookup(L"iterable command profile1\"");
+            auto command = expandedCommands.GetAt(1);
+            VERIFY_ARE_EQUAL(L"iterable command profile1\"", command.Name());
+
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -455,7 +488,9 @@ namespace TerminalAppLocalTests
         }
 
         {
-            auto command = expandedCommands.Lookup(L"iterable command profile2");
+            auto command = expandedCommands.GetAt(2);
+            VERIFY_ARE_EQUAL(L"iterable command profile2", command.Name());
+
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -527,14 +562,15 @@ namespace TerminalAppLocalTests
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(3u, settings.ActiveProfiles().Size());
 
-        auto expandedCommands = winrt::TerminalApp::implementation::TerminalPage::_ExpandCommands(settings.ActionMap().NameMap(), settings.ActiveProfiles().GetView(), settings.GlobalSettings().ColorSchemes());
-        _logCommandNames(expandedCommands.GetView());
+        const auto& expandedCommands{ settings.GlobalSettings().ActionMap().ExpandedCommands() };
+        _logCommands(expandedCommands);
 
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(1u, expandedCommands.Size());
 
-        auto rootCommand = expandedCommands.Lookup(L"Connect to ssh...");
+        auto rootCommand = expandedCommands.GetAt(0);
         VERIFY_IS_NOT_NULL(rootCommand);
+        VERIFY_ARE_EQUAL(L"Connect to ssh...", rootCommand.Name());
         auto rootActionAndArgs = rootCommand.ActionAndArgs();
         VERIFY_IS_NOT_NULL(rootActionAndArgs);
         VERIFY_ARE_EQUAL(ShortcutAction::Invalid, rootActionAndArgs.Action());
@@ -621,14 +657,16 @@ namespace TerminalAppLocalTests
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(3u, settings.ActiveProfiles().Size());
 
-        auto expandedCommands = winrt::TerminalApp::implementation::TerminalPage::_ExpandCommands(settings.ActionMap().NameMap(), settings.ActiveProfiles().GetView(), settings.GlobalSettings().ColorSchemes());
-        _logCommandNames(expandedCommands.GetView());
+        const auto& expandedCommands{ settings.GlobalSettings().ActionMap().ExpandedCommands() };
+        _logCommands(expandedCommands);
 
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(1u, expandedCommands.Size());
 
-        auto grandparentCommand = expandedCommands.Lookup(L"grandparent");
+        auto grandparentCommand = expandedCommands.GetAt(0);
         VERIFY_IS_NOT_NULL(grandparentCommand);
+        VERIFY_ARE_EQUAL(L"grandparent", grandparentCommand.Name());
+
         auto grandparentActionAndArgs = grandparentCommand.ActionAndArgs();
         VERIFY_IS_NOT_NULL(grandparentActionAndArgs);
         VERIFY_ARE_EQUAL(ShortcutAction::Invalid, grandparentActionAndArgs.Action());
@@ -744,17 +782,22 @@ namespace TerminalAppLocalTests
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(3u, settings.ActiveProfiles().Size());
 
-        auto expandedCommands = winrt::TerminalApp::implementation::TerminalPage::_ExpandCommands(settings.ActionMap().NameMap(), settings.ActiveProfiles().GetView(), settings.GlobalSettings().ColorSchemes());
-        _logCommandNames(expandedCommands.GetView());
+        const auto& expandedCommands{ settings.GlobalSettings().ActionMap().ExpandedCommands() };
+        _logCommands(expandedCommands);
 
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
 
         VERIFY_ARE_EQUAL(3u, expandedCommands.Size());
 
-        for (auto name : std::vector<std::wstring>({ L"profile0", L"profile1", L"profile2" }))
+        const std::vector<std::wstring> profileNames{ L"profile0", L"profile1", L"profile2" };
+        for (auto i = 0u; i < profileNames.size(); i++)
         {
-            winrt::hstring commandName{ name + L"..." };
-            auto command = expandedCommands.Lookup(commandName);
+            const auto& name{ profileNames[i] };
+            winrt::hstring commandName{ profileNames[i] + L"..." };
+
+            auto command = expandedCommands.GetAt(i);
+            VERIFY_ARE_EQUAL(commandName, command.Name());
+
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -880,14 +923,16 @@ namespace TerminalAppLocalTests
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(3u, settings.ActiveProfiles().Size());
 
-        auto expandedCommands = winrt::TerminalApp::implementation::TerminalPage::_ExpandCommands(settings.ActionMap().NameMap(), settings.ActiveProfiles().GetView(), settings.GlobalSettings().ColorSchemes());
-        _logCommandNames(expandedCommands.GetView());
+        const auto& expandedCommands{ settings.GlobalSettings().ActionMap().ExpandedCommands() };
+        _logCommands(expandedCommands);
 
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(1u, expandedCommands.Size());
 
-        auto rootCommand = expandedCommands.Lookup(L"New Tab With Profile...");
+        auto rootCommand = expandedCommands.GetAt(0);
         VERIFY_IS_NOT_NULL(rootCommand);
+        VERIFY_ARE_EQUAL(L"New Tab With Profile...", rootCommand.Name());
+
         auto rootActionAndArgs = rootCommand.ActionAndArgs();
         VERIFY_IS_NOT_NULL(rootActionAndArgs);
         VERIFY_ARE_EQUAL(ShortcutAction::Invalid, rootActionAndArgs.Action());
@@ -982,13 +1027,16 @@ namespace TerminalAppLocalTests
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(3u, settings.ActiveProfiles().Size());
 
-        auto expandedCommands = winrt::TerminalApp::implementation::TerminalPage::_ExpandCommands(settings.ActionMap().NameMap(), settings.ActiveProfiles().GetView(), settings.GlobalSettings().ColorSchemes());
-        _logCommandNames(expandedCommands.GetView());
+        const auto& expandedCommands{ settings.GlobalSettings().ActionMap().ExpandedCommands() };
+        _logCommands(expandedCommands);
 
         VERIFY_ARE_EQUAL(0u, settings.Warnings().Size());
         VERIFY_ARE_EQUAL(1u, expandedCommands.Size());
 
-        auto rootCommand = expandedCommands.Lookup(L"New Pane...");
+        auto rootCommand = expandedCommands.GetAt(0);
+        VERIFY_IS_NOT_NULL(rootCommand);
+        VERIFY_ARE_EQUAL(L"New Pane...", rootCommand.Name());
+
         VERIFY_IS_NOT_NULL(rootCommand);
         auto rootActionAndArgs = rootCommand.ActionAndArgs();
         VERIFY_IS_NOT_NULL(rootActionAndArgs);
@@ -1205,8 +1253,8 @@ namespace TerminalAppLocalTests
             VERIFY_ARE_EQUAL(L"${scheme.name}", realArgs.TerminalArgs().Profile());
         }
 
-        auto expandedCommands = winrt::TerminalApp::implementation::TerminalPage::_ExpandCommands(nameMap, settings.ActiveProfiles().GetView(), settings.GlobalSettings().ColorSchemes());
-        _logCommandNames(expandedCommands.GetView());
+        const auto& expandedCommands{ settings.GlobalSettings().ActionMap().ExpandedCommands() };
+        _logCommands(expandedCommands);
 
         VERIFY_ARE_EQUAL(3u, expandedCommands.Size());
 
@@ -1215,7 +1263,9 @@ namespace TerminalAppLocalTests
         // just easy tests to write.
 
         {
-            auto command = expandedCommands.Lookup(L"iterable command Campbell");
+            auto command = expandedCommands.GetAt(0);
+            VERIFY_ARE_EQUAL(L"iterable command Campbell", command.Name());
+
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -1233,7 +1283,9 @@ namespace TerminalAppLocalTests
         }
 
         {
-            auto command = expandedCommands.Lookup(L"iterable command Campbell PowerShell");
+            auto command = expandedCommands.GetAt(1);
+            VERIFY_ARE_EQUAL(L"iterable command Campbell PowerShell", command.Name());
+
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);
@@ -1251,7 +1303,9 @@ namespace TerminalAppLocalTests
         }
 
         {
-            auto command = expandedCommands.Lookup(L"iterable command Vintage");
+            auto command = expandedCommands.GetAt(2);
+            VERIFY_ARE_EQUAL(L"iterable command Vintage", command.Name());
+
             VERIFY_IS_NOT_NULL(command);
             auto actionAndArgs = command.ActionAndArgs();
             VERIFY_IS_NOT_NULL(actionAndArgs);

src/cascadia/Remoting/Monarch.cpp
@@ -10,6 +10,7 @@
 #include "ProposeCommandlineResult.h"
 
 #include "Monarch.g.cpp"
+#include "WindowRequestedArgs.g.cpp"
 #include "../../types/inc/utils.hpp"
 
 using namespace winrt;
@@ -658,6 +659,13 @@ namespace winrt::Microsoft::Terminal::Remoting::implementation
                           TraceLoggingLevel(WINEVENT_LEVEL_VERBOSE),
                           TraceLoggingKeyword(TIL_KEYWORD_TRACE));
 
+        if (targetWindow == WindowingBehaviorUseNone)
+        {
+            // In this case, the targetWindow was UseNone, which means that we
+            // want to make a message box, but otherwise not make a Terminal
+            // window.
+            return winrt::make<Remoting::implementation::ProposeCommandlineResult>(false);
+        }
         // If there's a valid ID returned, then let's try and find the peasant
         // that goes with it. Alternatively, if we were given a magic windowing
         // constant, we can use that to look up an appropriate peasant.
@@ -687,6 +695,11 @@ namespace winrt::Microsoft::Terminal::Remoting::implementation
             case WindowingBehaviorUseName:
                 windowID = _lookupPeasantIdForName(targetWindowName);
                 break;
+            case WindowingBehaviorUseNone:
+                // This should be impossible. The if statement above should have
+                // prevented WindowingBehaviorUseNone from falling in here.
+                // Explode, because this is a programming error.
+                THROW_HR(E_UNEXPECTED);
             default:
                 windowID = ::base::saturated_cast<uint64_t>(targetWindow);
                 break;
@@ -724,6 +737,8 @@ namespace winrt::Microsoft::Terminal::Remoting::implementation
                     result->WindowName(targetWindowName);
                     result->ShouldCreateWindow(true);
 
+                    _RequestNewWindowHandlers(*this, *winrt::make_self<WindowRequestedArgs>(*result, args));
+
                     // If this fails, it'll be logged in the following
                     // TraceLoggingWrite statement, with succeeded=false
                 }
@@ -759,6 +774,9 @@ namespace winrt::Microsoft::Terminal::Remoting::implementation
                 auto result{ winrt::make_self<Remoting::implementation::ProposeCommandlineResult>(true) };
                 result->Id(windowID);
                 result->WindowName(targetWindowName);
+
+                _RequestNewWindowHandlers(*this, *winrt::make_self<WindowRequestedArgs>(*result, args));
+
                 return *result;
             }
         }
@@ -773,6 +791,9 @@ namespace winrt::Microsoft::Terminal::Remoting::implementation
         // In this case, no usable ID was provided. Return { true, nullopt }
         auto result = winrt::make_self<Remoting::implementation::ProposeCommandlineResult>(true);
         result->WindowName(targetWindowName);
+
+        _RequestNewWindowHandlers(*this, *winrt::make_self<WindowRequestedArgs>(*result, args));
+
         return *result;
     }
 

commit b34444f40a311423bb1ac3e418d8a3e8d83f0d0f

Fix `wt -w _quake` by not throwing when setting the window name (#15030)
If we get initialized with a window name, this will be called before
XAML is stood up, and constructing a PropertyChangedEventArgs will
throw. So don't.


Regressed in #14843 

Related to #5000, #14957

src/cascadia/TerminalApp/TerminalWindow.cpp
@@ -1155,6 +1155,10 @@ namespace winrt::TerminalApp::implementation
     {
         const auto oldIsQuakeMode = _WindowProperties->IsQuakeWindow();
         _WindowProperties->WindowName(name);
+        if (!_root)
+        {
+            return;
+        }
         const auto newIsQuakeMode = _WindowProperties->IsQuakeWindow();
         if (newIsQuakeMode != oldIsQuakeMode)
         {
@@ -1210,8 +1214,15 @@ namespace winrt::TerminalApp::implementation
         if (_WindowName != value)
         {
             _WindowName = value;
-            _PropertyChangedHandlers(*this, Windows::UI::Xaml::Data::PropertyChangedEventArgs{ L"WindowName" });
-            _PropertyChangedHandlers(*this, Windows::UI::Xaml::Data::PropertyChangedEventArgs{ L"WindowNameForDisplay" });
+            // If we get initialized with a window name, this will be called
+            // before XAML is stood up, and constructing a
+            // PropertyChangedEventArgs will throw.
+            try
+            {
+                _PropertyChangedHandlers(*this, Windows::UI::Xaml::Data::PropertyChangedEventArgs{ L"WindowName" });
+                _PropertyChangedHandlers(*this, Windows::UI::Xaml::Data::PropertyChangedEventArgs{ L"WindowNameForDisplay" });
+            }
+            CATCH_LOG();
         }
     }
     uint64_t WindowProperties::WindowId() const noexcept