1e641c4a1cb83ecc93a9894963e9acc1ca67d024
James Holderness <j4_james@hotmail.com>
2023-04-22 19:27:41 +0100

    Add DECLRMM to DECRQM unit test.

--- src/terminal/adapter/ut_adapter/adapterTest.cpp
+++ src/terminal/adapter/ut_adapter/adapterTest.cpp
@@ -1779,7 +1779,7 @@ public:
         // and DECRQM would not then be applicable.
 
         BEGIN_TEST_METHOD_PROPERTIES()
-            TEST_METHOD_PROPERTY(L"Data:modeNumber", L"{1, 3, 5, 6, 8, 12, 25, 40, 66, 67, 1000, 1002, 1003, 1004, 1005, 1006, 1007, 1049, 9001}")
+            TEST_METHOD_PROPERTY(L"Data:modeNumber", L"{1, 3, 5, 6, 8, 12, 25, 40, 66, 67, 69, 1000, 1002, 1003, 1004, 1005, 1006, 1007, 1049, 9001}")
         END_TEST_METHOD_PROPERTIES()
 
         VTInt modeNumber;
