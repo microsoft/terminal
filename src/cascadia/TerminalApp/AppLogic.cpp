microsoft/terminal

commit 5b434dcda4b267aed2c12205c5b78cf66bb54b32

Split `AppLogic` into "App logic" and "Window logic" (#14825)
_And so begins the first chapter in the epic tale of the Terminal's tab
tear-out. This commit, though humble in its nature, shall mark the
beginning of a grand journey._
_This initial offering, though small in its scope, doth serve to divide
the code that currently resides within TerminalPage and AppLogic, moving
it unto a new entity known as TerminalWindow. In the ages to come, these
classes shall take on separate responsibilities, each with their own
purpose._
_The AppLogic shall hold sway over the entire process, shared among all
Terminal windows, while the AppHost, TerminalWindow, and TerminalPage
shall rule over each individual window._
_This pull request prepares the way for the future, moving state that
pertains to the individual windows into the TerminalWindow. This is a
task of great labor, for it requires moving much code, but the end
result shall bring greater organization to the codebase._
_And so the stage is set, for in the next pull request, the Process
Model v3 shall be revealed, unifying all Terminal windows into a single
process, a grand accomplishment indeed._

_courtesy of G.P.T. Tolkien_

<details>
<summary>Or, as I wrote it originally. </summary>

This is the first of the commits in the long saga which will culminate
in tab tear-out for the Terminal.

This the most functionally trivial of the PRs. It mostly just splits up
code that's currently in TerminalPage & AppLogic, and moves it into a
new class `TerminalWindow`. In the future, these classes will separate
responsibility as such:
* There will be one `AppLogic` per process, shared across all Terminal
windows.
* There will be one `AppHost`, `TerminalWindow`, and `TerminalPage` for
each individual window in the process.

This PR prepares for that by moving some state that's applicable to
_individual windows_ into `TerminalWindow`. This is almost exclusively a

src/cascadia/TerminalApp/AppLogic.h
@@ -5,10 +5,11 @@
 
 #include "AppLogic.g.h"
 #include "FindTargetWindowResult.g.h"
-#include "SystemMenuChangeArgs.g.h"
+
 #include "Jumplist.h"
 #include "LanguageProfileNotifier.h"
-#include "TerminalPage.h"
+#include "AppCommandlineArgs.h"
+#include "TerminalWindow.h"
 
 #include <inc/cppwinrt_utils.h>
 #include <ThrottledFunc.h>
@@ -36,18 +37,7 @@ namespace winrt::TerminalApp::implementation
             FindTargetWindowResult(id, L""){};
     };
 
-    struct SystemMenuChangeArgs : SystemMenuChangeArgsT<SystemMenuChangeArgs>
-    {
-        WINRT_PROPERTY(winrt::hstring, Name, L"");
-        WINRT_PROPERTY(SystemMenuChangeAction, Action, SystemMenuChangeAction::Add);
-        WINRT_PROPERTY(SystemMenuItemHandler, Handler, nullptr);
-
-    public:
-        SystemMenuChangeArgs(const winrt::hstring& name, SystemMenuChangeAction action, SystemMenuItemHandler handler = nullptr) :
-            _Name{ name }, _Action{ action }, _Handler{ handler } {};
-    };
-
-    struct AppLogic : AppLogicT<AppLogic, IInitializeWithWindow>
+    struct AppLogic : AppLogicT<AppLogic>
     {
     public:
         static AppLogic* Current() noexcept;
@@ -56,123 +46,46 @@ namespace winrt::TerminalApp::implementation
         AppLogic();
         ~AppLogic() = default;
 
-        STDMETHODIMP Initialize(HWND hwnd);
-
         void Create();
         bool IsUwp() const noexcept;
         void RunAsUwp();
         bool IsElevated() const noexcept;
         void ReloadSettings();
 
+        bool HasSettingsStartupActions() const noexcept;
+
+        bool ShouldUsePersistedLayout() const;
+        void SaveWindowLayoutJsons(const Windows::Foundation::Collections::IVector<hstring>& layouts);
+
         [[nodiscard]] Microsoft::Terminal::Settings::Model::CascadiaSettings GetSettings() const noexcept;
 
-        void Quit();
-
-        bool HasCommandlineArguments() const noexcept;
-        bool HasSettingsStartupActions() const noexcept;
-        int32_t SetStartupCommandline(array_view<const winrt::hstring> actions);
-        int32_t ExecuteCommandline(array_view<const winrt::hstring> actions, const winrt::hstring& cwd);
         TerminalApp::FindTargetWindowResult FindTargetWindow(array_view<const winrt::hstring> actions);
-        winrt::hstring ParseCommandlineMessage();
-        bool ShouldExitEarly();
-
-        bool FocusMode() const;
-        bool Fullscreen() const;
-        void Maximized(bool newMaximized);
-        bool AlwaysOnTop() const;
-        bool AutoHideWindow();
-
-        bool ShouldUsePersistedLayout();
-        bool ShouldImmediatelyHandoffToElevated();
-        void HandoffToElevated();
-        hstring GetWindowLayoutJson(Microsoft::Terminal::Settings::Model::LaunchPosition position);
-        void SaveWindowLayoutJsons(const Windows::Foundation::Collections::IVector<hstring>& layouts);
-        void IdentifyWindow();
-        void RenameFailed();
-        winrt::hstring WindowName();
-        void WindowName(const winrt::hstring& name);
-        uint64_t WindowId();
-        void WindowId(const uint64_t& id);
-        void SetPersistedLayoutIdx(const uint32_t idx);
-        void SetNumberOfOpenWindows(const uint64_t num);
-        bool IsQuakeWindow() const noexcept;
-        void RequestExitFullscreen();
-
-        Windows::Foundation::Size GetLaunchDimensions(uint32_t dpi);
-        bool CenterOnLaunch();
-        TerminalApp::InitialPosition GetInitialPosition(int64_t defaultInitialX, int64_t defaultInitialY);
-        winrt::Windows::UI::Xaml::ElementTheme GetRequestedTheme();
-        Microsoft::Terminal::Settings::Model::LaunchMode GetLaunchMode();
-        bool GetShowTabsInTitlebar();
-        bool GetInitialAlwaysOnTop();
-        float CalcSnappedDimension(const bool widthOrHeight, const float dimension) const;
-
-        Windows::UI::Xaml::UIElement GetRoot() noexcept;
-
-        void SetInboundListener();
-
-        hstring Title();
-        void TitlebarClicked();
-        bool OnDirectKeyEvent(const uint32_t vkey, const uint8_t scanCode, const bool down);
-
-        void CloseWindow(Microsoft::Terminal::Settings::Model::LaunchPosition position);
-        void WindowVisibilityChanged(const bool showOrHide);
-
-        winrt::TerminalApp::TaskbarState TaskbarState();
-        winrt::Windows::UI::Xaml::Media::Brush TitlebarBrush();
-        void WindowActivated(const bool activated);
-
-        bool GetMinimizeToNotificationArea();
-        bool GetAlwaysShowNotificationIcon();
-        bool GetShowTitleInTitlebar();
-
-        winrt::Windows::Foundation::IAsyncOperation<winrt::Windows::UI::Xaml::Controls::ContentDialogResult> ShowDialog(winrt::Windows::UI::Xaml::Controls::ContentDialog dialog);
-        void DismissDialog();
 
         Windows::Foundation::Collections::IMapView<Microsoft::Terminal::Control::KeyChord, Microsoft::Terminal::Settings::Model::Command> GlobalHotkeys();
 
         Microsoft::Terminal::Settings::Model::Theme Theme();
 
-        // -------------------------------- WinRT Events ---------------------------------
-        // PropertyChanged is surprisingly not a typed event, so we'll define that one manually.
-        // Usually we'd just do
-        //    WINRT_CALLBACK(PropertyChanged, Windows::UI::Xaml::Data::PropertyChangedEventHandler);
-        //
-        // But what we're doing here is exposing the Page's PropertyChanged _as
-        // our own event_. It's a FORWARDED_CALLBACK, essentially.
-        winrt::event_token PropertyChanged(Windows::UI::Xaml::Data::PropertyChangedEventHandler const& handler) { return _root->PropertyChanged(handler); }
-        void PropertyChanged(winrt::event_token const& token) { _root->PropertyChanged(token); }
+        TerminalApp::TerminalWindow CreateNewWindow();
 
-        TYPED_EVENT(RequestedThemeChanged, winrt::Windows::Foundation::IInspectable, winrt::Microsoft::Terminal::Settings::Model::Theme);
-        TYPED_EVENT(SettingsChanged, winrt::Windows::Foundation::IInspectable, winrt::Windows::Foundation::IInspectable);
-        TYPED_EVENT(SystemMenuChangeRequested, winrt::Windows::Foundation::IInspectable, winrt::TerminalApp::SystemMenuChangeArgs);
+        TYPED_EVENT(SettingsChanged, winrt::Windows::Foundation::IInspectable, winrt::TerminalApp::SettingsLoadEventArgs);
 
     private:
         bool _isUwp{ false };
         bool _isElevated{ false };
 
-        // If you add controls here, but forget to null them either here or in
-        // the ctor, you're going to have a bad time. It'll mysteriously fail to
-        // activate the AppLogic.
-        // ALSO: If you add any UIElements as roots here, make sure they're
-        // updated in _ApplyTheme. The root currently is _root.
-        winrt::com_ptr<TerminalPage> _root{ nullptr };
         Microsoft::Terminal::Settings::Model::CascadiaSettings _settings{ nullptr };
 
         winrt::hstring _settingsLoadExceptionText;
         HRESULT _settingsLoadedResult = S_OK;
         bool _loadedInitialSettings = false;
 
-        uint64_t _numOpenWindows{ 0 };
-
-        std::shared_mutex _dialogLock;
-        winrt::Windows::UI::Xaml::Controls::ContentDialog _dialog;
-
-        ::TerminalApp::AppCommandlineArgs _appArgs;
+        bool _hasSettingsStartupActions{ false };
         ::TerminalApp::AppCommandlineArgs _settingsAppArgs;
 
         std::shared_ptr<ThrottledFuncTrailing<>> _reloadSettings;
         til::throttled_func_trailing<> _reloadState;
+
+        std::vector<Microsoft::Terminal::Settings::Model::SettingsLoadWarnings> _warnings{};
 
         // These fields invoke _reloadSettings and must be destroyed before _reloadSettings.
         // (C++ destroys members in reverse-declaration-order.)
@@ -182,46 +95,13 @@ namespace winrt::TerminalApp::implementation
         static TerminalApp::FindTargetWindowResult _doFindTargetWindow(winrt::array_view<const hstring> args,
                                                                        const Microsoft::Terminal::Settings::Model::WindowingMode& windowingBehavior);
 
-        void _ShowLoadErrorsDialog(const winrt::hstring& titleKey, const winrt::hstring& contentKey, HRESULT settingsLoadedResult);
-        void _ShowLoadWarningsDialog();
-        bool _IsKeyboardServiceEnabled();
-
         void _ApplyLanguageSettingChange() noexcept;
-        void _RefreshThemeRoutine();
         fire_and_forget _ApplyStartupTaskStateChange();
-
-        void _OnLoaded(const IInspectable& sender, const Windows::UI::Xaml::RoutedEventArgs& eventArgs);
 
         [[nodiscard]] HRESULT _TryLoadSettings() noexcept;
         void _ProcessLazySettingsChanges();
         void _RegisterSettingsChange();
         fire_and_forget _DispatchReloadSettings();
-        void _OpenSettingsUI();
-
-        bool _hasCommandLineArguments{ false };
-        bool _hasSettingsStartupActions{ false };
-        std::vector<Microsoft::Terminal::Settings::Model::SettingsLoadWarnings> _warnings;
-
-        // These are events that are handled by the TerminalPage, but are
-        // exposed through the AppLogic. This macro is used to forward the event
-        // directly to them.
-        FORWARDED_TYPED_EVENT(SetTitleBarContent, winrt::Windows::Foundation::IInspectable, winrt::Windows::UI::Xaml::UIElement, _root, SetTitleBarContent);
-        FORWARDED_TYPED_EVENT(TitleChanged, winrt::Windows::Foundation::IInspectable, winrt::hstring, _root, TitleChanged);
-        FORWARDED_TYPED_EVENT(LastTabClosed, winrt::Windows::Foundation::IInspectable, winrt::TerminalApp::LastTabClosedEventArgs, _root, LastTabClosed);
-        FORWARDED_TYPED_EVENT(FocusModeChanged, winrt::Windows::Foundation::IInspectable, winrt::Windows::Foundation::IInspectable, _root, FocusModeChanged);
-        FORWARDED_TYPED_EVENT(FullscreenChanged, winrt::Windows::Foundation::IInspectable, winrt::Windows::Foundation::IInspectable, _root, FullscreenChanged);
-        FORWARDED_TYPED_EVENT(ChangeMaximizeRequested, winrt::Windows::Foundation::IInspectable, winrt::Windows::Foundation::IInspectable, _root, ChangeMaximizeRequested);
-        FORWARDED_TYPED_EVENT(AlwaysOnTopChanged, winrt::Windows::Foundation::IInspectable, winrt::Windows::Foundation::IInspectable, _root, AlwaysOnTopChanged);
-        FORWARDED_TYPED_EVENT(RaiseVisualBell, winrt::Windows::Foundation::IInspectable, winrt::Windows::Foundation::IInspectable, _root, RaiseVisualBell);
-        FORWARDED_TYPED_EVENT(SetTaskbarProgress, winrt::Windows::Foundation::IInspectable, winrt::Windows::Foundation::IInspectable, _root, SetTaskbarProgress);
-        FORWARDED_TYPED_EVENT(IdentifyWindowsRequested, Windows::Foundation::IInspectable, Windows::Foundation::IInspectable, _root, IdentifyWindowsRequested);
-        FORWARDED_TYPED_EVENT(RenameWindowRequested, Windows::Foundation::IInspectable, winrt::TerminalApp::RenameWindowRequestedArgs, _root, RenameWindowRequested);
-        FORWARDED_TYPED_EVENT(IsQuakeWindowChanged, Windows::Foundation::IInspectable, Windows::Foundation::IInspectable, _root, IsQuakeWindowChanged);
-        FORWARDED_TYPED_EVENT(SummonWindowRequested, Windows::Foundation::IInspectable, Windows::Foundation::IInspectable, _root, SummonWindowRequested);
-        FORWARDED_TYPED_EVENT(CloseRequested, Windows::Foundation::IInspectable, Windows::Foundation::IInspectable, _root, CloseRequested);
-        FORWARDED_TYPED_EVENT(OpenSystemMenu, Windows::Foundation::IInspectable, Windows::Foundation::IInspectable, _root, OpenSystemMenu);
-        FORWARDED_TYPED_EVENT(QuitRequested, Windows::Foundation::IInspectable, Windows::Foundation::IInspectable, _root, QuitRequested);
-        FORWARDED_TYPED_EVENT(ShowWindowChanged, Windows::Foundation::IInspectable, winrt::Microsoft::Terminal::Control::ShowWindowArgs, _root, ShowWindowChanged);
 
 #ifdef UNIT_TESTING
         friend class TerminalAppLocalTests::CommandlineTest;

src/cascadia/LocalTests_TerminalApp/TabTests.cpp
@@ -4,6 +4,7 @@
 #include "pch.h"
 
 #include "../TerminalApp/TerminalPage.h"
+#include "../TerminalApp/TerminalWindow.h"
 #include "../TerminalApp/MinMaxCloseControl.h"
 #include "../TerminalApp/TabRowControl.h"
 #include "../TerminalApp/ShortcutActionDispatch.h"
@@ -110,6 +111,7 @@ namespace TerminalAppLocalTests
         void _initializeTerminalPage(winrt::com_ptr<winrt::TerminalApp::implementation::TerminalPage>& page,
                                      CascadiaSettings initialSettings);
         winrt::com_ptr<winrt::TerminalApp::implementation::TerminalPage> _commonSetup();
+        winrt::com_ptr<winrt::TerminalApp::implementation::WindowProperties> _windowProperties;
     };
 
     template<typename TFunction>
@@ -194,8 +196,11 @@ namespace TerminalAppLocalTests
     {
         winrt::com_ptr<winrt::TerminalApp::implementation::TerminalPage> page{ nullptr };
 
-        auto result = RunOnUIThread([&page]() {
-            page = winrt::make_self<winrt::TerminalApp::implementation::TerminalPage>();
+        _windowProperties = winrt::make_self<winrt::TerminalApp::implementation::WindowProperties>();
+        winrt::TerminalApp::WindowProperties props = *_windowProperties;
+
+        auto result = RunOnUIThread([&page, props]() {
+            page = winrt::make_self<winrt::TerminalApp::implementation::TerminalPage>(props);
             VERIFY_IS_NOT_NULL(page);
         });
         VERIFY_SUCCEEDED(result);
@@ -239,9 +244,11 @@ namespace TerminalAppLocalTests
         // it's weird.
         winrt::TerminalApp::TerminalPage projectedPage{ nullptr };
 
+        _windowProperties = winrt::make_self<winrt::TerminalApp::implementation::WindowProperties>();
+        winrt::TerminalApp::WindowProperties props = *_windowProperties;
         Log::Comment(NoThrowString().Format(L"Construct the TerminalPage"));
-        auto result = RunOnUIThread([&projectedPage, &page, initialSettings]() {
-            projectedPage = winrt::TerminalApp::TerminalPage();
+        auto result = RunOnUIThread([&projectedPage, &page, initialSettings, props]() {
+            projectedPage = winrt::TerminalApp::TerminalPage(props);
             page.copy_from(winrt::get_self<winrt::TerminalApp::implementation::TerminalPage>(projectedPage));
             page->_settings = initialSettings;
         });
@@ -1242,14 +1249,16 @@ namespace TerminalAppLocalTests
         END_TEST_METHOD_PROPERTIES()
 
         auto page = _commonSetup();
-        page->RenameWindowRequested([&page](auto&&, const winrt::TerminalApp::RenameWindowRequestedArgs args) {
+        page->RenameWindowRequested([&page, this](auto&&, const winrt::TerminalApp::RenameWindowRequestedArgs args) {
             // In the real terminal, this would bounce up to the monarch and
             // come back down. Instead, immediately call back and set the name.
-            page->WindowName(args.ProposedName());
+            //
+            // This replicates how TerminalWindow works
+            _windowProperties->WindowName(args.ProposedName());
         });
 
         auto windowNameChanged = false;
-        page->PropertyChanged([&page, &windowNameChanged](auto&&, const winrt::WUX::Data::PropertyChangedEventArgs& args) mutable {
+        _windowProperties->PropertyChanged([&page, &windowNameChanged](auto&&, const winrt::WUX::Data::PropertyChangedEventArgs& args) mutable {
             if (args.PropertyName() == L"WindowNameForDisplay")
             {
                 windowNameChanged = true;
@@ -1260,7 +1269,7 @@ namespace TerminalAppLocalTests
             page->_RequestWindowRename(winrt::hstring{ L"Foo" });
         });
         TestOnUIThread([&]() {
-            VERIFY_ARE_EQUAL(L"Foo", page->_WindowName);
+            VERIFY_ARE_EQUAL(L"Foo", page->WindowProperties().WindowName());
             VERIFY_IS_TRUE(windowNameChanged,
                            L"The window name should have changed, and we should have raised a notification that WindowNameForDisplay changed");
         });

src/cascadia/TerminalApp/App.cpp
@@ -77,22 +77,7 @@ namespace winrt::TerminalApp::implementation
     /// <param name="e">Details about the launch request and process.</param>
     void App::OnLaunched(const LaunchActivatedEventArgs& /*e*/)
     {
-        // if this is a UWP... it means its our problem to hook up the content to the window here.
-        if (_isUwp)
-        {
-            auto content = Window::Current().Content();
-            if (content == nullptr)
-            {
-                auto logic = Logic();
-                logic.RunAsUwp(); // Must set UWP status first, settings might change based on it.
-                logic.ReloadSettings();
-                logic.Create();
-
-                auto page = logic.GetRoot().as<TerminalPage>();
-
-                Window::Current().Content(page);
-                Window::Current().Activate();
-            }
-        }
+        // We used to support a pure UWP version of the Terminal. This method
+        // was only ever used to do UWP-specific setup of our App.
     }
 }

commit 405fb51201e013ee11d47d3a9f7aa03ab758c79a

Fix AppInitialized latency metric (#15206)
The AppInitialized latency metric logs how long the application needs
to initialize the UI. 5b434dc broke this metric, because it was now
executing the code outside of the `Initialized` callback.
It's the difference between a "latency" of ~50ms and ~350ms.

As an added bonus it moves the `_ApplyStartupTaskStateChange` task
into the `Initialized` callback as well, because why not.

## Validation Steps Performed
* Breakpoint into "AppInitialized" - latency is now correct ✅

Co-authored-by: Dustin L. Howett <duhowett@microsoft.com>

src/cascadia/TerminalApp/AppLogic.cpp
@@ -182,45 +182,7 @@ namespace winrt::TerminalApp::implementation
         // this as a MTA, before the app is Create()'d
         WINRT_ASSERT(_loadedInitialSettings);
 
-        // These used to be in `TerminalPage::Initialized`, so that they started
-        // _after_ the Terminal window was started and displayed. These could
-        // theoretically move there again too. TODO:GH#14957 - evaluate moving
-        // this after the Page is initialized
-        {
-            // Both LoadSettings and ReloadSettings are supposed to call this function,
-            // but LoadSettings skips it, so that the UI starts up faster.
-            // Now that the UI is present we can do them with a less significant UX impact.
-            _ProcessLazySettingsChanges();
-
-            FILETIME creationTime, exitTime, kernelTime, userTime, now;
-            if (GetThreadTimes(GetCurrentThread(), &creationTime, &exitTime, &kernelTime, &userTime))
-            {
-                static constexpr auto asInteger = [](const FILETIME& f) {
-                    ULARGE_INTEGER i;
-                    i.LowPart = f.dwLowDateTime;
-                    i.HighPart = f.dwHighDateTime;
-                    return i.QuadPart;
-                };
-                static constexpr auto asSeconds = [](uint64_t v) {
-                    return v * 1e-7f;
-                };
-
-                GetSystemTimeAsFileTime(&now);
-
-                const auto latency = asSeconds(asInteger(now) - asInteger(creationTime));
-
-                TraceLoggingWrite(
-                    g_hTerminalAppProvider,
-                    "AppInitialized",
-                    TraceLoggingDescription("Event emitted once the app is initialized"),
-                    TraceLoggingFloat32(latency, "latency"),
-                    TraceLoggingKeyword(MICROSOFT_KEYWORD_MEASURES),
-                    TelemetryPrivacyDataTag(PDT_ProductAndServiceUsage));
-            }
-        }
-
         _ApplyLanguageSettingChange();
-        _ApplyStartupTaskStateChange();
 
         TraceLoggingWrite(
             g_hTerminalAppProvider,
@@ -393,33 +355,12 @@ namespace winrt::TerminalApp::implementation
     }
     CATCH_LOG()
 
-    // Function Description:
-    // Returns the current app package or nullptr.
-    // TRANSITIONAL
-    // Exists to work around a compiler bug. This function encapsulates the
-    // exception handling that we used to keep around calls to Package::Current,
-    // so that when it's called inside a coroutine and fails it doesn't explode
-    // terribly.
-    static winrt::Windows::ApplicationModel::Package GetCurrentPackageNoThrow() noexcept
-    {
-        try
-        {
-            return winrt::Windows::ApplicationModel::Package::Current();
-        }
-        catch (...)
-        {
-            // discard any exception -- literally pretend we're not in a package
-        }
-        return nullptr;
-    }
-
     fire_and_forget AppLogic::_ApplyStartupTaskStateChange()
     try
     {
         // First, make sure we're running in a packaged context. This method
         // won't work, and will crash mysteriously if we're running unpackaged.
-        const auto package{ GetCurrentPackageNoThrow() };
-        if (package == nullptr)
+        if (!IsPackaged())
         {
             co_return;
         }
@@ -517,6 +458,47 @@ namespace winrt::TerminalApp::implementation
                                                           warnings,
                                                           _settings);
         _SettingsChangedHandlers(*this, *ev);
+    }
+
+    // This is a continuation of AppLogic::Create() and includes the more expensive parts.
+    void AppLogic::NotifyRootInitialized()
+    {
+        if (_notifyRootInitializedCalled.exchange(true, std::memory_order_relaxed))
+        {
+            return;
+        }
+
+        // Both LoadSettings and ReloadSettings are supposed to call this function,
+        // but LoadSettings skips it, so that the UI starts up faster.
+        // Now that the UI is present we can do them with a less significant UX impact.
+        _ApplyStartupTaskStateChange();
+        _ProcessLazySettingsChanges();
+
+        FILETIME creationTime, exitTime, kernelTime, userTime, now;
+        if (GetThreadTimes(GetCurrentThread(), &creationTime, &exitTime, &kernelTime, &userTime))
+        {
+            static constexpr auto asInteger = [](const FILETIME& f) {
+                ULARGE_INTEGER i;
+                i.LowPart = f.dwLowDateTime;
+                i.HighPart = f.dwHighDateTime;
+                return i.QuadPart;
+            };
+            static constexpr auto asSeconds = [](uint64_t v) {
+                return v * 1e-7f;
+            };
+
+            GetSystemTimeAsFileTime(&now);
+
+            const auto latency = asSeconds(asInteger(now) - asInteger(creationTime));
+
+            TraceLoggingWrite(
+                g_hTerminalAppProvider,
+                "AppInitialized",
+                TraceLoggingDescription("Event emitted once the app is initialized"),
+                TraceLoggingFloat32(latency, "latency"),
+                TraceLoggingKeyword(MICROSOFT_KEYWORD_MEASURES),
+                TelemetryPrivacyDataTag(PDT_ProductAndServiceUsage));
+        }
     }
 
     // Method Description:

src/cascadia/TerminalApp/AppLogic.h
@@ -45,12 +45,12 @@ namespace winrt::TerminalApp::implementation
         static const Microsoft::Terminal::Settings::Model::CascadiaSettings CurrentAppSettings();
 
         AppLogic();
-        ~AppLogic() = default;
 
         void Create();
         bool IsRunningElevated() const noexcept;
         bool CanDragDrop() const noexcept;
         void ReloadSettings();
+        void NotifyRootInitialized();
 
         bool HasSettingsStartupActions() const noexcept;
 
@@ -79,6 +79,7 @@ namespace winrt::TerminalApp::implementation
     private:
         bool _isElevated{ false };
         bool _canDragDrop{ false };
+        std::atomic<bool> _notifyRootInitializedCalled{ false };
 
         Microsoft::Terminal::Settings::Model::CascadiaSettings _settings{ nullptr };
 

src/cascadia/TerminalApp/TerminalWindow.cpp
@@ -3,16 +3,15 @@
 
 #include "pch.h"
 #include "TerminalWindow.h"
+
+#include "AppLogic.h"
 #include "../inc/WindowingBehavior.h"
+
+#include <LibraryResources.h>
+
 #include "TerminalWindow.g.cpp"
 #include "SettingsLoadEventArgs.g.cpp"
 #include "WindowProperties.g.cpp"
-
-#include <LibraryResources.h>
-#include <WtExeUtils.h>
-#include <wil/token_helpers.h>
-
-#include "../../types/inc/utils.hpp"
 
 using namespace winrt::Windows::ApplicationModel;
 using namespace winrt::Windows::ApplicationModel::DataTransfer;
@@ -235,6 +234,8 @@ namespace winrt::TerminalApp::implementation
             {
                 _root->SetFullscreen(true);
             }
+
+            AppLogic::Current()->NotifyRootInitialized();
         });
         _root->Create();
 