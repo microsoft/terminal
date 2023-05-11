microsoft/terminal

commit 210414e5a8182eaa3e7f1ed2d07da50edf80e242

Default to XamlRoot when unable to find focused object (#15189)
Default to XamlRoot when unable to find a focused object in
DirectKeyEvents

This may not be the most appropriate "fix" for this. Certainly open to
criticism and feedback. We are trapping the alt+space key chord on the
win32 side and forwarding it to the xaml side. There we try to find a
focused object by walking the xaml tree. If we are unable to find a
focused object we return false and do nothing. I suspect that the area
that has focus that prevents this from working normally is on the win32
side. Since we want to handle the system menu anyway and are explicitly
trapping that key combo and forwarding it on I thought this was the best
approach. If we cant find a focused object default to the xaml root.

## Validation Steps Performed
System menu opens as it should.

Closes #14397

src/cascadia/TerminalApp/TerminalWindow.cpp
@@ -900,6 +900,12 @@ namespace winrt::TerminalApp::implementation
                     if (!focusedObject)
                     {
                         focusedObject = winrt::Windows::UI::Xaml::Media::VisualTreeHelper::GetParent(focusedElement);
+
+                        // We were unable to find a focused object. Default to the xaml root so that the alt+space menu still works.
+                        if (!focusedObject)
+                        {
+                            focusedObject = _root.try_as<IInspectable>();
+                        }
                     }
                 }
                 else
@@ -908,6 +914,7 @@ namespace winrt::TerminalApp::implementation
                 }
             } while (focusedObject);
         }
+
         return false;
     }
 

commit 0553f3ebf185b4c1354cd70e6f4eaf79db0698fd

Fix an infinite loop when pressing `alt` (#15253)
As discussed in
https://github.com/microsoft/terminal/issues/14051#issuecomment-1517973776

regressed in #15189

src/cascadia/TerminalApp/TerminalWindow.cpp
@@ -870,10 +870,19 @@ namespace winrt::TerminalApp::implementation
                     {
                         focusedObject = winrt::Windows::UI::Xaml::Media::VisualTreeHelper::GetParent(focusedElement);
 
-                        // We were unable to find a focused object. Default to the xaml root so that the alt+space menu still works.
+                        // We were unable to find a focused object. Give the
+                        // TerminalPage one last chance to let the alt+space
+                        // menu still work.
+                        //
+                        // We return always, because the TerminalPage handler
+                        // will return false for just a bare `alt` press, and
+                        // don't want to go around the loop again.
                         if (!focusedObject)
                         {
-                            focusedObject = _root.try_as<IInspectable>();
+                            if (auto keyListener{ _root.try_as<IDirectKeyListener>() })
+                            {
+                                return keyListener.OnDirectKeyEvent(vkey, scanCode, down);
+                            }
                         }
                     }
                 }