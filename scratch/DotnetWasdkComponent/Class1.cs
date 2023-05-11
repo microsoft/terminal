1eda70fea776d514f5e3b2fd3a9b407c7d05f24d
Mike Griese <migrie@microsoft.com>
2023-05-11 07:02:46 -0500

    this was kinda a dead end

--- scratch/DotnetWasdkComponent/Class1.cs
+++ scratch/DotnetWasdkComponent/Class1.cs
@@ -1,21 +1,35 @@
+using WinRT.Interop;
+using WinRT;
+using SampleExtensions;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace DotnetWasdkComponent
 {
-    public class Class1
-    {
+    public sealed class Class1 : IExtension
+    {        
         public int MySharpProperty { get; set; }
+
+        public Windows.UI.Xaml.FrameworkElement PaneContent => throw new NotImplementedException();
 
         public Class1()
         {
             MySharpProperty = 11;
+        }
+
+        public event TypedEventHandler<object, SendInputArgs> SendInputRequested;
+
+        public int DoTheThing()
+        {
+            throw new NotImplementedException();
         }
     }
 }
