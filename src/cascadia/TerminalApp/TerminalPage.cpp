microsoft/terminal

commit def3742a2edf7e82212f0a6e06fe64bc26a904af

Fix focus issue when profile selected from nested menu entry (#15077)
Original bug report #15049
Relates to feature #1571 

MenuFlyoutSubItem, when collapsing from profile selection, move focus
back to the titlebar.
An extra Closing event handler is needed to keep focus on the command
shell.

Closes #15049

src/cascadia/TerminalApp/TerminalPage.cpp
@@ -848,6 +848,10 @@ namespace winrt::TerminalApp::implementation
         newTabFlyout.Opening([this](auto&&, auto&&) {
             _FocusCurrentTab(true);
         });
+        // Necessary for fly-out sub items to get focus on a tab before collapsing. Related to #15049
+        newTabFlyout.Closing([this](auto&&, auto&&) {
+            _FocusCurrentTab(true);
+        });
         _newTabButton.Flyout(newTabFlyout);
     }
 

commit 92260863e02bd6c1bbc66812e0fb7ff40641c6c6

Don't dismiss the command palette when the new tab menu closes
Transient UIs are hard.

Regressed in #15077.

Closes #15305

src/cascadia/TerminalApp/TerminalPage.cpp
@@ -851,7 +851,14 @@ namespace winrt::TerminalApp::implementation
         });
         // Necessary for fly-out sub items to get focus on a tab before collapsing. Related to #15049
         newTabFlyout.Closing([this](auto&&, auto&&) {
-            _FocusCurrentTab(true);
+            if (const auto p = CommandPaletteElement(); p && p.Visibility() == Visibility::Visible)
+            {
+                // Don't focus the terminal, that'll dismiss the command palette.
+            }
+            else
+            {
+                _FocusCurrentTab(true);
+            }
         });
         _newTabButton.Flyout(newTabFlyout);
     }