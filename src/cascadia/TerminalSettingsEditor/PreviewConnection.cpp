fbf78caec451d512af37dca440a1037c61d7341b
Dustin L. Howett <duhowett@microsoft.com>
2023-05-16 14:46:03 -0500

    Detect Powerline fonts, and add a powerline preview?

--- src/cascadia/TerminalSettingsEditor/PreviewConnection.cpp
+++ src/cascadia/TerminalSettingsEditor/PreviewConnection.cpp
@@ -8,15 +8,20 @@
 using namespace ::winrt::Microsoft::Terminal::TerminalConnection;
 using namespace ::winrt::Windows::Foundation;
 
+static constexpr std::wstring_view PromptTextPlain{ L"C:\\> " };
+static constexpr std::wstring_view PromptTextPowerline{ L"\x1b[49;34m\xe0b6\x1b[1;97;44m C:\\ \x1b[m\x1b[46;34m\xe0b8\x1b[49;36m\xe0b8\x1b[m " };
+
 // clang-format off
 static constexpr std::wstring_view PreviewText{
+    L"\x001b"
+    L"c" // Hard Reset (RIS); on separate lines to avoid becoming 0x01BC
     L"Windows Terminal\r\n"
-    L"C:\\> \x1b[93m" L"git\x1b[m diff \x1b[90m-w\x1b[m\r\n"
+    L"{0}\x1b[93m" L"git\x1b[m diff \x1b[90m-w\x1b[m\r\n"
     L"\x1b[1m" L"diff --git a/win b/win\x1b[m\r\n"
     L"\x1b[36m@@ -1 +1 @@\x1b[m\r\n"
     L"\x1b[31m-    Windows Console\x1b[m\r\n"
     L"\x1b[32m+    Windows Terminal!\x1b[m\r\n"
-    L"C:\\> \x1b[93mWrite-Host \x1b[36m\"\xd83c\xdf2f!\"\x1b[1D\x1b[m"
+    L"{0}\x1b[93mWrite-Host \x1b[36m\"\xd83c\xdf2f!\"\x1b[1D\x1b[m"
 };
 // clang-format on
 
@@ -27,7 +32,7 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
     void PreviewConnection::Start() noexcept
     {
         // Send the preview text
-        _TerminalOutputHandlers(PreviewText);
+        _TerminalOutputHandlers(fmt::format(PreviewText, _displayPowerlineGlyphs ? PromptTextPowerline : PromptTextPlain));
     }
 
     void PreviewConnection::Initialize(const Windows::Foundation::Collections::ValueSet& /*settings*/) noexcept
@@ -44,5 +49,15 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
 
     void PreviewConnection::Close() noexcept
     {
+    }
+
+    void PreviewConnection::DisplayPowerlineGlyphs(bool d) noexcept
+    {
+        std::swap(d, _displayPowerlineGlyphs);
+        if (_displayPowerlineGlyphs != d)
+        {
+            // If it changed, redraw
+            Start();
+        }
     }
 }
--- src/cascadia/TerminalSettingsEditor/PreviewConnection.h
+++ src/cascadia/TerminalSettingsEditor/PreviewConnection.h
@@ -27,9 +27,14 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         void Resize(uint32_t rows, uint32_t columns) noexcept;
         void Close() noexcept;
 
+        void DisplayPowerlineGlyphs(bool d) noexcept;
+
         winrt::Microsoft::Terminal::TerminalConnection::ConnectionState State() const noexcept { return winrt::Microsoft::Terminal::TerminalConnection::ConnectionState::Connected; }
 
         WINRT_CALLBACK(TerminalOutput, winrt::Microsoft::Terminal::TerminalConnection::TerminalOutputHandler);
         TYPED_EVENT(StateChanged, winrt::Microsoft::Terminal::TerminalConnection::ITerminalConnection, IInspectable);
+
+    private:
+        bool _displayPowerlineGlyphs{ false };
     };
 }
