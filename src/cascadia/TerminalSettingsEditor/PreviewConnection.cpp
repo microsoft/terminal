bdd12aca30db79cb6cc38f132d3e9ba46056e21d
Dustin Howett <duhowett@microsoft.com>
2023-05-15 19:09:20 -0500

    Add a fun new preview text

--- src/cascadia/TerminalSettingsEditor/PreviewConnection.cpp
+++ src/cascadia/TerminalSettingsEditor/PreviewConnection.cpp
@@ -8,7 +8,14 @@
 using namespace ::winrt::Microsoft::Terminal::TerminalConnection;
 using namespace ::winrt::Windows::Foundation;
 
-static constexpr std::wstring_view PreviewText{ L"Windows Terminal\r\nCopyright (c) Microsoft Corporation\r\n\nC:\\Windows\\Terminal> " };
+static constexpr std::wstring_view PreviewText{
+    L"Windows Terminal\r\n"
+    L"C:\\> \e[93mgit\e[m diff\r\n"
+    L"\e[1mdiff --git a/hi b/hi\e[m\r\n"
+    L"\e[36m@@ -0,1 +0,1 @@\e[m\r\n"
+    L"\e[31m-    Windows Console\e[m\r\n"
+    L"\e[32m-    Windows Terminal!\e[m\r\n"
+};
 
 namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
 {
