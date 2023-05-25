microsoft/terminal

commit f06cd1759fe37277b43340fbe420272c3def5bf1

VsDevCmdGenerator: respect the user's startingDirectory (#15035)
The PowerShell equivalent was added in the initial pull request, #7774.

Closes #13721

src/cascadia/TerminalSettingsModel/VsDevCmdGenerator.cpp
@@ -42,6 +42,9 @@ std::wstring VsDevCmdGenerator::GetProfileCommandLine(const VsSetupConfiguration
     commandLine.reserve(256);
     commandLine.append(LR"(cmd.exe /k ")");
     commandLine.append(GetDevCmdScriptPath(instance));
+    // The "-startdir" parameter will prevent "vsdevcmd" from automatically
+    // setting the shell path so the path in the profile will be used instead.
+    commandLine.append(LR"(" -startdir=none)");
 #if defined(_M_ARM64)
     commandLine.append(LR"(" -arch=arm64 -host_arch=x64)");
 #elif defined(_M_AMD64)

commit 0073e36d811462d6d17ea4ae5563b83567af4d8b

Fix VS profile command generation (#15439)
This regressed in f06cd17. It seems like the change went untested,
because it appends an extra " after -startdir=none.
This changeset also avoids calling `append()` twice.

Closes #15436

## Validation Steps Performed
* VS Developer Command Prompt works ✅

src/cascadia/TerminalSettingsModel/VsDevCmdGenerator.cpp
@@ -44,11 +44,12 @@ std::wstring VsDevCmdGenerator::GetProfileCommandLine(const VsSetupConfiguration
     commandLine.append(GetDevCmdScriptPath(instance));
     // The "-startdir" parameter will prevent "vsdevcmd" from automatically
     // setting the shell path so the path in the profile will be used instead.
-    commandLine.append(LR"(" -startdir=none)");
 #if defined(_M_ARM64)
-    commandLine.append(LR"(" -arch=arm64 -host_arch=x64)");
+    commandLine.append(LR"(" -startdir=none -arch=arm64 -host_arch=x64)");
 #elif defined(_M_AMD64)
-    commandLine.append(LR"(" -arch=x64 -host_arch=x64)");
+    commandLine.append(LR"(" -startdir=none -arch=x64 -host_arch=x64)");
+#else
+    commandLine.append(LR"(" -startdir=none)");
 #endif
     return commandLine;
 }