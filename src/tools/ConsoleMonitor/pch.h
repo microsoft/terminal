9005e40e69a0cec16da94f02e48037c86b2a63ce
Leonard Hecker <lhecker@microsoft.com>
2023-07-12 01:02:49 +0200

    Add ConsoleMonitor: A tool for displaying ConPTY contents

---
+++ src/tools/ConsoleMonitor/pch.h
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#pragma once
+
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <wil/resource.h>
+
+#include <vector>
