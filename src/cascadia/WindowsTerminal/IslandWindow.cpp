0e9c5490508fd1e3df01c74a06bdf0e738ad9ae1
Mike Griese <migrie@microsoft.com>
2023-07-19 10:52:18 -0500

    Refrigerate our threads for later reuse (#15424)
    This is my proposed solution to #15384.
    
    Basically, the issue is that we cannot ever close a
    `DesktopWindowXamlSource` ("DWXS"). If we do, then any other thread that
    tries to access XAML metadata will explode, which happens frequently. A
    DWXS is inextricably linked to an HWND. That means we have to not only
    reuse DWXS's, but the HWNDs themselves. XAML also isn't agile, so we've
    got to keep the `thread` that the DWXS was started on alive as well.
    
    To do this, we're going to introduce the ability to "refrigerate" and
    "reheat" window threads.
    * A window thread is "**hot**" if it's actively got a window, and is
    pumping window messages, and generally, is a normal thing.
    * When a window is closed, we need to "**refrigerate**" it's
    `WindowThread` and `IslandWindow`. `WindowEmperor` will take care of
    tracking the threads that are refrigerated.
    * When a new window is requested, the Emperor first try to
    "**reheat**"/"**microwave**" a refrigerated thread. When a thread gets
    reheated, we'll create a new AppHost (and `TerminalWindow`/`Page`), and
    we'll use the _existing_ `IslandWindow` for that instance.
    
    <sub>The metaphor is obviously ridiculous, but _you get it_ so who
    cares.</sub>
    
    In this way, we'll keep all the windows we've ever created around in
    memory, for later reuse. This means that the leak goes from (~12MB x
    number of windows closed) to (~12MB x maximum number of simultaneously
    open Terminal windows). It's still not good.
    
    We won't do this on Windows 11, because the bug that is the fundamental
    premise of this issue is fixed already in the OS.
    
    I'm not 100% confident in this yet.
    * [x] There's still a d3d leak of some sort on exit in debug builds.
    (maybe #15306 related)
    * havent seen this in a while. Must have been a issue in an earlier
    revision.
    * [x] I need to validate more on Windows 11
    * [x] **BAD**: Closing the last tab on Windows 11 doesn't close the
    window
    * [x] **BAD**: Closing a window on Windows 11 doesn't close the window -
    it just closes the one tab item and keeps on choochin'
    * [x] **BAD**: Close last tab, open new one, attempt to close window -
    ALL windows go \*poof\*. Cause of course. No break into post-mortem
    either.
    * [x] more comments
    * [ ] maybe a diagram
    * [x] Restoring windows is at the wrong place entirely? I once reopened
    the Terminal with two persisted windows, and it created one at 0,0
    * [x] Remaining code TODO!s: 0 (?)
    * [ ] "warm-reloading" `useTabsInTitlebar` (change while terminal is
    running after closing a window, open a new one) REALLY doesn't work.
    Obviously restores the last kind of window. Yike.
    
    is all about #15384
    
    closes #15410 along the way. Might fork that fix off.
    
    (cherry picked from commit 701062649768c4f6bfb5ea77eab9154014995918)
    Service-Card-Id: 89927087
    Service-Version: 1.18

--- src/cascadia/WindowsTerminal/IslandWindow.cpp
+++ src/cascadia/WindowsTerminal/IslandWindow.cpp
@@ -42,46 +42,6 @@ IslandWindow::~IslandWindow()
 
 void IslandWindow::Close()
 {
-    static const bool isWindows11 = []() {
-        OSVERSIONINFOEXW osver{};
-        osver.dwOSVersionInfoSize = sizeof(osver);
-        osver.dwBuildNumber = 22000;
-
-        DWORDLONG dwlConditionMask = 0;
-        VER_SET_CONDITION(dwlConditionMask, VER_BUILDNUMBER, VER_GREATER_EQUAL);
-
-        if (VerifyVersionInfoW(&osver, VER_BUILDNUMBER, dwlConditionMask) != FALSE)
-        {
-            return true;
-        }
-        return false;
-    }();
-
-    if (!isWindows11)
-    {
-        // BODGY
-        //  ____   ____  _____   _______     __
-        // |  _ \ / __ \|  __ \ / ____\ \   / /
-        // | |_) | |  | | |  | | |  __ \ \_/ /
-        // |  _ <| |  | | |  | | | |_ | \   /
-        // | |_) | |__| | |__| | |__| |  | |
-        // |____/ \____/|_____/ \_____|  |_|
-        //
-        // There's a bug in Windows 10 where closing a DesktopWindowXamlSource
-        // on any thread will free an internal static resource that's used by
-        // XAML for the entire process. This would result in closing window
-        // essentially causing the entire app to crash.
-        //
-        // To avoid this, leak the XAML island. We only need to leak this on
-        // Windows 10, since the bug is fixed in Windows 11.
-        //
-        // See GH #15384, MSFT:32109540
-        auto a{ _source };
-        winrt::detach_abi(_source);
-
-        // </BODGY>
-    }
-
     // GH#15454: Unset the user data for the window. This will prevent future
     // callbacks that come onto our window message loop from being sent to the
     // IslandWindow (or other derived class's) implementation.
@@ -99,6 +59,27 @@ void IslandWindow::Close()
     }
 }
 
+// Clear out any state that might be associated with this app instance, so that
+// we can later re-use this HWND for another instance.
+//
+// This doesn't actually close out our HWND or DesktopWindowXamlSource, but it
+// will remove all our content, and SW_HIDE the window, so it isn't accessible.
+void IslandWindow::Refrigerate() noexcept
+{
+    // Similar to in Close - unset our HWND's user data. We'll re-set this when
+    // we get re-heated, so that while we're refrigerated, we won't have
+    // unexpected callbacks into us while we don't have content.
+    //
+    // This pointer will get re-set in _warmInitialize
+    SetWindowLongPtr(_window.get(), GWLP_USERDATA, 0);
+
+    _pfnCreateCallback = nullptr;
+    _pfnSnapDimensionCallback = nullptr;
+
+    _rootGrid.Children().Clear();
+    ShowWindow(_window.get(), SW_HIDE);
+}
+
 HWND IslandWindow::GetInteropHandle() const
 {
     return _interopWindowHandle;
@@ -112,6 +93,12 @@ HWND IslandWindow::GetInteropHandle() const
 // - <none>
 void IslandWindow::MakeWindow() noexcept
 {
+    if (_window)
+    {
+        // no-op if we already have a window.
+        return;
+    }
+
     WNDCLASS wc{};
     wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
     wc.hInstance = reinterpret_cast<HINSTANCE>(&__ImageBase);
@@ -345,7 +332,30 @@ LRESULT IslandWindow::_OnMoving(const WPARAM /*wParam*/, const LPARAM lParam)
     return false;
 }
 
-void IslandWindow::Initialize()
+// return true if this was a "cold" initialize, that didn't start XAML before.
+bool IslandWindow::Initialize()
+{
+    if (!_source)
+    {
+        _coldInitialize();
+        return true;
+    }
+    else
+    {
+        // This was a "warm" initialize - we've already got an HWND, but we need
+        // to move it to the new correct place, new size, and reset any leftover
+        // runtime state.
+        _warmInitialize();
+        return false;
+    }
+}
+
+// Method Description:
+// - Start this window for the first time. This will instantiate our XAML
+//   island, set up our root grid, and initialize some other members that only
+//   need to be initialized once.
+// - This should only be called once.
+void IslandWindow::_coldInitialize()
 {
     _source = DesktopWindowXamlSource{};
 
@@ -378,9 +388,32 @@ void IslandWindow::Initialize()
     // We don't really care if this failed or not.
     TerminalTrySetTransparentBackground(true);
 }
+void IslandWindow::_warmInitialize()
+{
+    // re-add the pointer to us to our HWND's user data, so that we can start
+    // getting window proc callbacks again.
+    _setupUserData();
+
+    // Manually ask how we want to be created.
+    if (_pfnCreateCallback)
+    {
+        til::rect rc{ GetWindowRect() };
+        _pfnCreateCallback(_window.get(), rc);
+    }
+
+    // Don't call IslandWindow::OnSize - that will set the Width/Height members
+    // of the _rootGrid. However, NonClientIslandWindow doesn't use those! If you set them, here,
+    // the contents of the window will never resize.
+    UpdateWindow(_window.get());
+    ForceResize();
+}
 
 void IslandWindow::OnSize(const UINT width, const UINT height)
 {
+    // NOTE: This _isn't_ called by NonClientIslandWindow::OnSize. The
+    // NonClientIslandWindow has very different logic for positioning the
+    // DesktopWindowXamlSource inside its HWND.
+
     // update the interop window size
     SetWindowPos(_interopWindowHandle, nullptr, 0, 0, width, height, SWP_SHOWWINDOW | SWP_NOACTIVATE);
 
