c3f2f6332e9dbddd4844bf04ffc8b74686763fc1
Dustin Howett <duhowett@microsoft.com>
2023-07-26 18:24:49 -0500

    oof bar

--- build/scripts/Get-DependenciesFromAppxRecipe.ps1
+++ build/scripts/Get-DependenciesFromAppxRecipe.ps1
@@ -10,8 +10,8 @@ $Recipe = [xml](Get-Content $Path)
 $ResolvedSDKReferences = $Recipe.Project.ItemGroup.ResolvedSDKReference
 
 $ResolvedSDKReferences |
-	Where-Object Architecture -eq $Recipe.Project.PropertyGroup.PackageArchitecture |
-	ForEach-Object {
-		$l = [Uri]::UnescapeDataString($_.AppxLocation)
-		Get-Item $l
-	}
+    Where-Object Architecture -eq $Recipe.Project.PropertyGroup.PackageArchitecture |
+    ForEach-Object {
+        $l = [Uri]::UnescapeDataString($_.AppxLocation)
+        Get-Item $l
+    }
