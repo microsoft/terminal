ed4312c02d4d2799b7029e7b71f92d1fb0dd6c6a
Mike Griese <migrie@microsoft.com>
2023-09-20 06:40:42 -0500

    even better

--- src/cascadia/TerminalSettingsModel/KeyChordSerialization.cpp
+++ src/cascadia/TerminalSettingsModel/KeyChordSerialization.cpp
@@ -123,6 +123,11 @@ static int32_t parseNumericCode(const std::wstring_view& str, const std::wstring
 // - a newly constructed KeyChord
 static KeyChord _fromString(std::wstring_view wstr)
 {
+    if (wstr.empty())
+    {
+        return nullptr;
+    }
+
     using nameToVkeyPair = std::pair<std::wstring_view, int32_t>;
     static constinit til::static_map nameToVkey{
     // The above VKEY_NAME_PAIRS macro contains a list of key-binding names for each virtual key.
@@ -348,7 +353,7 @@ KeyChord ConversionTrait<KeyChord>::FromJson(const Json::Value& json)
         {
             throw winrt::hresult_invalid_argument{};
         }
-        return keyChordText.empty() ? nullptr : _fromString(til::u8u16(keyChordText));
+        return _fromString(til::u8u16(keyChordText));
     }
     catch (...)
     {
