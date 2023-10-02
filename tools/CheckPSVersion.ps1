c0008955b91fe293d2b333f310c1b90354f8736c
Jvr <109031036+Jvr2022@users.noreply.github.com>
2023-10-02 15:21:47 +0200

    Add Success Message when Correct PowerShell Version is Installed

--- tools/CheckPSVersion.ps1
+++ tools/CheckPSVersion.ps1
@@ -1,4 +1,7 @@
-if($PSVersionTable.PSVersion.Major -lt 7){
-   Write-Error "Incorrect version of PowerShell installed.`nMake sure you have at least PowerShell Core 7.0.0."
-   Exit 1 
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+    Write-Error "Incorrect version of PowerShell installed.`nMake sure you have at least PowerShell Core 7.0.0."
+    Exit 1
+} else {
+    Write-Output "Correct version of PowerShell installed."
+    Exit 0
 }
