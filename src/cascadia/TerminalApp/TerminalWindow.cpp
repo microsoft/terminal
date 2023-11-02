811046d60a14228831267c13eb65f2c889072829
Dustin L. Howett <duhowett@microsoft.com>
2023-11-01 22:45:48 -0500

    Update for recent changes in main

--- src/cascadia/TerminalApp/TerminalWindow.cpp
+++ src/cascadia/TerminalApp/TerminalWindow.cpp
@@ -891,7 +891,7 @@ namespace winrt::TerminalApp::implementation
                         // don't want to go around the loop again.
                         if (!focusedObject)
                         {
-                            if (auto keyListener{ _root.try_as<IDirectKeyListener>() })
+                            if (auto keyListener{ _root.try_as<UI::IDirectKeyListener>() })
                             {
                                 return keyListener.OnDirectKeyEvent(vkey, scanCode, down);
                             }
