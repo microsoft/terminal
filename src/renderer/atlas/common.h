921e7731acc0baa66f5cee9dfcf60b79a506c6f6
Eric Nelson <e82.eric@gmail.com>
2023-11-27 21:06:07 -0800

    Removed unused searchSelectionsColor memeber

--- src/renderer/atlas/common.h
+++ src/renderer/atlas/common.h
@@ -377,7 +377,6 @@ namespace Microsoft::Console::Render::Atlas
     {
         u32 backgroundColor = 0;
         u32 selectionColor = 0x7fffffff;
-        u32 searchSelectionColor = 0x5fff0000;
         std::wstring customPixelShaderPath;
         bool useRetroTerminalEffect = false;
     };
