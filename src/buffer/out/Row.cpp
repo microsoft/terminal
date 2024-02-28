microsoft/terminal

commit cd80f3c76496c7664ed710907d7d473f634191bc

Use ICU for text search (#15858)
The ultimate goal of this PR was to use ICU for text search to
* Improve Unicode support
  Previously we used `towlower` and only supported BMP glphs.
* Improve search performance (10-100x)
  This allows us to search for all results in the entire text buffer
  at once without having to do so asynchronously.

Unfortunately, this required some significant changes too:
* ICU's search facilities operate on text positions which we need to be
  mapped back to buffer coordinates. This required the introduction of
  `CharToColumnMapper` to implement sort of a reverse-`_charOffsets`
  mapping. It turns text (character) positions back into coordinates.
* Previously search restarted every time you clicked the search button.
  It used the current selection as the starting position for the new
  search. But since ICU's `uregex` cannot search backwards we're
  required to accumulate all results in a vector first and so we
  need to cache that vector in between searches.
* We need to know when the cached vector became invalid and so we have
  to track any changes made to `TextBuffer`. The way this commit solves
  it is by splitting `GetRowByOffset` into `GetRowByOffset` for
  `const ROW` access and `GetMutableRowByOffset` which increments a
  mutation counter on each call. The `Search` instance can then compare
  its cached mutation count against the previous mutation count.

Finally, this commit makes 2 semi-unrelated changes:
* URL search now also uses ICU, since it's closely related to regular
  text search anyways. This significantly improves performance at
  large window sizes.
* A few minor issues in `UiaTracing` were fixed. In particular
  2 functions which passed strings as `wstring` by copy are now
  using `wstring_view` and `TraceLoggingCountedWideString`.

Related to #6319 and #8000

## Validation Steps Performed
* Search upward/downward in conhost ✅
* Search upward/downward in WT ✅

src/buffer/out/Row.cpp
@@ -10,7 +10,18 @@
 #include "textBuffer.hpp"
 #include "../../types/inc/GlyphWidth.hpp"
 
+// It would be nice to add checked array access in the future, but it's a little annoying to do so without impacting
+// performance (including Debug performance). Other languages are a little bit more ergonomic there than C++.
+#pragma warning(disable : 26481) // Don't use pointer arithmetic. Use span instead (bounds.1).)
+#pragma warning(disable : 26446) // Prefer to use gsl::at() instead of unchecked subscript operator (bounds.4).
+#pragma warning(disable : 26472) // Don't use a static_cast for arithmetic conversions. Use brace initialization, gsl::narrow_cast or gsl::narrow (type.1).
+
 extern "C" int __isa_available;
+
+constexpr auto clamp(auto value, auto lo, auto hi)
+{
+    return value < lo ? lo : (value > hi ? hi : value);
+}
 
 // The STL is missing a std::iota_n analogue for std::iota, so I made my own.
 template<typename OutIt, typename Diff, typename T>
@@ -71,6 +82,86 @@ constexpr OutIt copy_n_small(InIt first, Diff count, OutIt dest)
     return dest;
 }
 
+CharToColumnMapper::CharToColumnMapper(const wchar_t* chars, const uint16_t* charOffsets, ptrdiff_t lastCharOffset, til::CoordType currentColumn) noexcept :
+    _chars{ chars },
+    _charOffsets{ charOffsets },
+    _lastCharOffset{ lastCharOffset },
+    _currentColumn{ currentColumn }
+{
+}
+
+// If given a position (`offset`) inside the ROW's text, this function will return the corresponding column.
+// This function in particular returns the glyph's first column.
+til::CoordType CharToColumnMapper::GetLeadingColumnAt(ptrdiff_t offset) noexcept
+{
+    offset = clamp(offset, 0, _lastCharOffset);
+
+    auto col = _currentColumn;
+    const auto currentOffset = _charOffsets[col];
+
+    // Goal: Move the _currentColumn cursor to a cell which contains the given target offset.
+    // Depending on where the target offset is we have to either search forward or backward.
+    if (offset < currentOffset)
+    {
+        // Backward search.
+        // Goal: Find the first preceding column where the offset is <= the target offset. This results in the first
+        // cell that contains our target offset, even if that offset is in the middle of a long grapheme.
+        //
+        // We abuse the fact that the trailing half of wide glyphs is marked with CharOffsetsTrailer to our advantage.
+        // Since they're >0x8000, the `offset < _charOffsets[col]` check will always be true and ensure we iterate over them.
+        //
+        // Since _charOffsets cannot contain negative values and because offset has been
+        // clamped to be positive we naturally exit when reaching the first column.
+        for (; offset < _charOffsets[col - 1]; --col)
+        {
+        }
+    }
+    else if (offset > currentOffset)
+    {
+        // Forward search.
+        // Goal: Find the first subsequent column where the offset is > the target offset.
+        // We stop 1 column before that however so that the next loop works correctly.
+        // It's the inverse of the loop above.
+        //
+        // Since offset has been clamped to be at most 1 less than the maximum
+        // _charOffsets value the loop naturally exits before hitting the end.
+        for (; offset >= (_charOffsets[col + 1] & CharOffsetsMask); ++col)
+        {
+        }
+        // Now that we found the cell that definitely includes this char offset,
+        // we have to iterate back to the cell's starting column.
+        for (; WI_IsFlagSet(_charOffsets[col], CharOffsetsTrailer); --col)
+        {
+        }
+    }
+
+    _currentColumn = col;
+    return col;
+}
+
+// If given a position (`offset`) inside the ROW's text, this function will return the corresponding column.
+// This function in particular returns the glyph's last column (this matters for wide glyphs).
+til::CoordType CharToColumnMapper::GetTrailingColumnAt(ptrdiff_t offset) noexcept
+{
+    auto col = GetLeadingColumnAt(offset);
+    // This loop is a little redundant with the forward search loop in GetLeadingColumnAt()
+    // but it's realistically not worth caring about this. This code is not a bottleneck.
+    for (; WI_IsFlagSet(_charOffsets[col + 1], CharOffsetsTrailer); ++col)
+    {
+    }
+    return col;
+}
+
+til::CoordType CharToColumnMapper::GetLeadingColumnAt(const wchar_t* str) noexcept
+{
+    return GetLeadingColumnAt(str - _chars);
+}
+
+til::CoordType CharToColumnMapper::GetTrailingColumnAt(const wchar_t* str) noexcept
+{
+    return GetTrailingColumnAt(str - _chars);
+}
+
 // Routine Description:
 // - constructor
 // Arguments:
@@ -118,10 +209,17 @@ LineRendition ROW::GetLineRendition() const noexcept
     return _lineRendition;
 }
 
-uint16_t ROW::GetLineWidth() const noexcept
+// Returns the index 1 past the last (technically) valid column in the row.
+// The interplay between the old console and newer VT APIs which support line renditions is
+// still unclear so it might be necessary to add two kinds of this function in the future.
+// Console APIs treat the buffer as a large NxM matrix after all.
+til::CoordType ROW::GetReadableColumnCount() const noexcept
 {
-    const auto scale = _lineRendition != LineRendition::SingleWidth ? 1 : 0;
-    return _columnCount >> scale;
+    if (_lineRendition == LineRendition::SingleWidth) [[likely]]
+    {
+        return _columnCount - _doubleBytePadded;
+    }
+    return (_columnCount - (_doubleBytePadded << 1)) >> 1;
 }
 
 // Routine Description:
@@ -285,26 +383,6 @@ til::CoordType ROW::NavigateToPrevious(til::CoordType column) const noexcept
 til::CoordType ROW::NavigateToNext(til::CoordType column) const noexcept
 {
     return _adjustForward(_clampedColumn(column + 1));
-}
-
-uint16_t ROW::_adjustBackward(uint16_t column) const noexcept
-{
-    // Safety: This is a little bit more dangerous. The first column is supposed
-    // to never be a trailer and so this loop should exit if column == 0.
-    for (; _uncheckedIsTrailer(column); --column)
-    {
-    }
-    return column;
-}
-
-uint16_t ROW::_adjustForward(uint16_t column) const noexcept
-{
-    // Safety: This is a little bit more dangerous. The last column is supposed
-    // to never be a trailer and so this loop should exit if column == _columnCount.
-    for (; _uncheckedIsTrailer(column); ++column)
-    {
-    }
-    return column;
 }
 
 // Routine Description:
@@ -841,12 +919,6 @@ uint16_t ROW::size() const noexcept
     return _columnCount;
 }
 
-til::CoordType ROW::LineRenditionColumns() const noexcept
-{
-    const auto scale = _lineRendition != LineRendition::SingleWidth ? 1 : 0;
-    return _columnCount >> scale;
-}
-
 til::CoordType ROW::MeasureLeft() const noexcept
 {
     const auto text = GetText();
@@ -945,18 +1017,29 @@ DbcsAttribute ROW::DbcsAttrAt(til::CoordType column) const noexcept
 
 std::wstring_view ROW::GetText() const noexcept
 {
-    return { _chars.data(), _charSize() };
+    const auto width = size_t{ til::at(_charOffsets, GetReadableColumnCount()) } & CharOffsetsMask;
+    return { _chars.data(), width };
 }
 
 std::wstring_view ROW::GetText(til::CoordType columnBegin, til::CoordType columnEnd) const noexcept
 {
     const til::CoordType columns = _columnCount;
-    const auto colBeg = std::max(0, std::min(columns, columnBegin));
-    const auto colEnd = std::max(colBeg, std::min(columns, columnEnd));
+    const auto colBeg = clamp(columnBegin, 0, columns);
+    const auto colEnd = clamp(columnEnd, colBeg, columns);
     const size_t chBeg = _uncheckedCharOffset(gsl::narrow_cast<size_t>(colBeg));
     const size_t chEnd = _uncheckedCharOffset(gsl::narrow_cast<size_t>(colEnd));
 #pragma warning(suppress : 26481) // Don't use pointer arithmetic. Use span instead (bounds.1).
     return { _chars.data() + chBeg, chEnd - chBeg };
+}
+
+til::CoordType ROW::GetLeadingColumnAtCharOffset(const ptrdiff_t offset) const noexcept
+{
+    return _createCharToColumnMapper(offset).GetLeadingColumnAt(offset);
+}
+
+til::CoordType ROW::GetTrailingColumnAtCharOffset(const ptrdiff_t offset) const noexcept
+{
+    return _createCharToColumnMapper(offset).GetTrailingColumnAt(offset);
 }
 
 DelimiterClass ROW::DelimiterClassAt(til::CoordType column, const std::wstring_view& wordDelimiters) const noexcept
@@ -982,43 +1065,80 @@ DelimiterClass ROW::DelimiterClassAt(til::CoordType column, const std::wstring_v
 template<typename T>
 constexpr uint16_t ROW::_clampedUint16(T v) noexcept
 {
-    return static_cast<uint16_t>(std::max(T{ 0 }, std::min(T{ 65535 }, v)));
+    return static_cast<uint16_t>(clamp(v, 0, 65535));
 }
 
 template<typename T>
 constexpr uint16_t ROW::_clampedColumn(T v) const noexcept
 {
-    return static_cast<uint16_t>(std::max(T{ 0 }, std::min<T>(_columnCount - 1u, v)));
+    return static_cast<uint16_t>(clamp(v, 0, _columnCount - 1));
 }
 
 template<typename T>
 constexpr uint16_t ROW::_clampedColumnInclusive(T v) const noexcept
 {
-    return static_cast<uint16_t>(std::max(T{ 0 }, std::min<T>(_columnCount, v)));
-}
-
-// Safety: off must be [0, _charSize()].
-wchar_t ROW::_uncheckedChar(size_t off) const noexcept
-{
-    return til::at(_chars, off);
+    return static_cast<uint16_t>(clamp(v, 0, _columnCount));
 }
 
 uint16_t ROW::_charSize() const noexcept
 {
     // Safety: _charOffsets is an array of `_columnCount + 1` entries.
-    return til::at(_charOffsets, _columnCount);
+    return _charOffsets[_columnCount];
+}
+
+// Safety: off must be [0, _charSize()].
+template<typename T>
+wchar_t ROW::_uncheckedChar(T off) const noexcept
+{
+    return _chars[off];
 }
 
 // Safety: col must be [0, _columnCount].
-uint16_t ROW::_uncheckedCharOffset(size_t col) const noexcept
+template<typename T>
+uint16_t ROW::_uncheckedCharOffset(T col) const noexcept
 {
     assert(col < _charOffsets.size());
-    return til::at(_charOffsets, col) & CharOffsetsMask;
+    return _charOffsets[col] & CharOffsetsMask;
 }
 
 // Safety: col must be [0, _columnCount].
-bool ROW::_uncheckedIsTrailer(size_t col) const noexcept
+template<typename T>
+bool ROW::_uncheckedIsTrailer(T col) const noexcept
 {
     assert(col < _charOffsets.size());
-    return WI_IsFlagSet(til::at(_charOffsets, col), CharOffsetsTrailer);
+    return WI_IsFlagSet(_charOffsets[col], CharOffsetsTrailer);
+}
+
+template<typename T>
+T ROW::_adjustBackward(T column) const noexcept
+{
+    // Safety: This is a little bit more dangerous. The first column is supposed
+    // to never be a trailer and so this loop should exit if column == 0.
+    for (; _uncheckedIsTrailer(column); --column)
+    {
+    }
+    return column;
+}
+
+template<typename T>
+T ROW::_adjustForward(T column) const noexcept
+{
+    // Safety: This is a little bit more dangerous. The last column is supposed
+    // to never be a trailer and so this loop should exit if column == _columnCount.
+    for (; _uncheckedIsTrailer(column); ++column)
+    {
+    }
+    return column;
+}
+
+// Creates a CharToColumnMapper given an offset into _chars.data().
+// In other words, for a 120 column ROW with just ASCII text, the offset should be [0,120).
+CharToColumnMapper ROW::_createCharToColumnMapper(ptrdiff_t offset) const noexcept
+{
+    const auto charsSize = _charSize();
+    const auto lastChar = gsl::narrow_cast<ptrdiff_t>(charsSize - 1);
+    // We can sort of guess what column belongs to what offset because BMP glyphs are very common and
+    // UTF-16 stores them in 1 char. In other words, usually a ROW will have N chars for N columns.
+    const auto guessedColumn = gsl::narrow_cast<til::CoordType>(clamp(offset, 0, _columnCount));
+    return CharToColumnMapper{ _chars.data(), _charOffsets.data(), lastChar, guessedColumn };
 }

src/buffer/out/Row.hpp
@@ -65,6 +65,28 @@ struct RowCopyTextFromState
     til::CoordType sourceColumnEnd = 0; // OUT
 };
 
+// This structure is basically an inverse of ROW::_charOffsets. If you have a pointer
+// into a ROW's text this class can tell you what cell that pointer belongs to.
+struct CharToColumnMapper
+{
+    CharToColumnMapper(const wchar_t* chars, const uint16_t* charOffsets, ptrdiff_t lastCharOffset, til::CoordType currentColumn) noexcept;
+
+    til::CoordType GetLeadingColumnAt(ptrdiff_t offset) noexcept;
+    til::CoordType GetTrailingColumnAt(ptrdiff_t offset) noexcept;
+    til::CoordType GetLeadingColumnAt(const wchar_t* str) noexcept;
+    til::CoordType GetTrailingColumnAt(const wchar_t* str) noexcept;
+
+private:
+    // See ROW and its members with identical name.
+    static constexpr uint16_t CharOffsetsTrailer = 0x8000;
+    static constexpr uint16_t CharOffsetsMask = 0x7fff;
+
+    const wchar_t* _chars;
+    const uint16_t* _charOffsets;
+    ptrdiff_t _lastCharOffset;
+    til::CoordType _currentColumn;
+};
+
 class ROW final
 {
 public:
@@ -106,7 +128,7 @@ public:
     bool WasDoubleBytePadded() const noexcept;
     void SetLineRendition(const LineRendition lineRendition) noexcept;
     LineRendition GetLineRendition() const noexcept;
-    uint16_t GetLineWidth() const noexcept;
+    til::CoordType GetReadableColumnCount() const noexcept;
 
     void Reset(const TextAttribute& attr) noexcept;
     void TransferAttributes(const til::small_rle<TextAttribute, uint16_t, 1>& attr, til::CoordType newWidth);
@@ -128,7 +150,6 @@ public:
     TextAttribute GetAttrByColumn(til::CoordType column) const;
     std::vector<uint16_t> GetHyperlinks() const;
     uint16_t size() const noexcept;
-    til::CoordType LineRenditionColumns() const noexcept;
     til::CoordType MeasureLeft() const noexcept;
     til::CoordType MeasureRight() const noexcept;
     bool ContainsText() const noexcept;
@@ -136,6 +157,8 @@ public:
     DbcsAttribute DbcsAttrAt(til::CoordType column) const noexcept;
     std::wstring_view GetText() const noexcept;
     std::wstring_view GetText(til::CoordType columnBegin, til::CoordType columnEnd) const noexcept;
+    til::CoordType GetLeadingColumnAtCharOffset(ptrdiff_t offset) const noexcept;
+    til::CoordType GetTrailingColumnAtCharOffset(ptrdiff_t offset) const noexcept;
     DelimiterClass DelimiterClassAt(til::CoordType column, const std::wstring_view& wordDelimiters) const noexcept;
 
     auto AttrBegin() const noexcept { return _attr.begin(); }
@@ -206,16 +229,21 @@ private:
     template<typename T>
     constexpr uint16_t _clampedColumnInclusive(T v) const noexcept;
 
-    uint16_t _adjustBackward(uint16_t column) const noexcept;
-    uint16_t _adjustForward(uint16_t column) const noexcept;
-
-    wchar_t _uncheckedChar(size_t off) const noexcept;
     uint16_t _charSize() const noexcept;
-    uint16_t _uncheckedCharOffset(size_t col) const noexcept;
-    bool _uncheckedIsTrailer(size_t col) const noexcept;
+    template<typename T>
+    wchar_t _uncheckedChar(T off) const noexcept;
+    template<typename T>
+    uint16_t _uncheckedCharOffset(T col) const noexcept;
+    template<typename T>
+    bool _uncheckedIsTrailer(T col) const noexcept;
+    template<typename T>
+    T _adjustBackward(T column) const noexcept;
+    template<typename T>
+    T _adjustForward(T column) const noexcept;
 
     void _init() noexcept;
     void _resizeChars(uint16_t colEndDirty, uint16_t chBegDirty, size_t chEndDirty, uint16_t chEndDirtyOld);
+    CharToColumnMapper _createCharToColumnMapper(ptrdiff_t offset) const noexcept;
 
     // These fields are a bit "wasteful", but it makes all this a bit more robust against
     // programming errors during initial development (which is when this comment was written).

commit e7796e7db3662c9edc5a4306e647602f07690166

Fix the hyperlink detection when there are leading wide glyph in the row (#16775)
## Summary of the Pull Request

URL detection was broken again in #15858. When the regex matched, we
calculate the column(cell) by its offset, we use forward or backward
iteration of the column to find the correct column that displays the
glyphs of `_chars[offset]`.


https://github.com/microsoft/terminal/blob/abf5d9423a23b13e9af4c19ca35858f9aaf0a63f/src/buffer/out/Row.cpp#L95-L104

However, when calculating the `currentOffset` we forget that MSB of
`_charOffsets[col]` could be `1`, or col is pointing to another glyph in
preceding column.


https://github.com/microsoft/terminal/blob/abf5d9423a23b13e9af4c19ca35858f9aaf0a63f/src/buffer/out/Row.hpp#L223-L226

src/buffer/out/Row.cpp
@@ -97,7 +97,7 @@ til::CoordType CharToColumnMapper::GetLeadingColumnAt(ptrdiff_t offset) noexcept
     offset = clamp(offset, 0, _lastCharOffset);
 
     auto col = _currentColumn;
-    const auto currentOffset = _charOffsets[col];
+    const auto currentOffset = _charOffsets[col] & CharOffsetsMask;
 
     // Goal: Move the _currentColumn cursor to a cell which contains the given target offset.
     // Depending on where the target offset is we have to either search forward or backward.