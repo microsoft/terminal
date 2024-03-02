0fb3e20133a7f0edfc52022c7499d55c28ca1c56
Jvr <109031036+Jvr2022@users.noreply.github.com>
2024-03-02 11:51:24 +0100

    Chromium 124.0.6333.1

--- oss/chromium/base/numerics/math_constants.h
+++ oss/chromium/base/numerics/math_constants.h
@@ -1,4 +1,4 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,10 +6,6 @@
 #define BASE_NUMERICS_MATH_CONSTANTS_H_
 
 namespace base {
-
-constexpr double kPiDouble = 3.14159265358979323846;
-constexpr float kPiFloat = 3.14159265358979323846f;
-
 // The mean acceleration due to gravity on Earth in m/s^2.
 constexpr double kMeanGravityDouble = 9.80665;
 constexpr float kMeanGravityFloat = 9.80665f;
