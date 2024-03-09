cf31f7b0e8a907f42066dfe311377e6aad64cc68
Pankaj Bhojwani <pankaj.d.bhoj@gmail.com>
2024-03-08 16:13:25 -0800

    format

--- src/cascadia/TerminalSettingsModel/resource.h
+++ src/cascadia/TerminalSettingsModel/resource.h
@@ -2,6 +2,6 @@
 // Microsoft Visual C++ generated include file.
 // Used by WindowsTerminal.rc
 //
-#define IDR_DEFAULTS                    101
-#define IDR_USER_DEFAULTS               102
-#define IDR_ENABLE_COLOR_SELECTION      103
+#define IDR_DEFAULTS 101
+#define IDR_USER_DEFAULTS 102
+#define IDR_ENABLE_COLOR_SELECTION 103
