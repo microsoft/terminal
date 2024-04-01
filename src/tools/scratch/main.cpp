0a9ad5d453605141978a94c48b352bc9bad895fb
Mike Griese <migrie@microsoft.com>
2024-04-01 09:41:05 -0500

    that's what I get for trying to be fancy with git

--- src/tools/scratch/main.cpp
+++ src/tools/scratch/main.cpp
@@ -2,12 +2,9 @@
 // Licensed under the MIT license.
 
 #include <windows.h>
-#include <stdio.h>
 
 // This wmain exists for help in writing scratch programs while debugging.
 int __cdecl wmain(int /*argc*/, WCHAR* /*argv[]*/)
 {
-
-    wprintf(L"Hello \n goodbye \n");
     return 0;
 }
