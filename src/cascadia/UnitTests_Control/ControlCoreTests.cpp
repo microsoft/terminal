microsoft/terminal

commit 90b8bb7c2d8932fc601aed9c26f13c77d5a1321d

Improve Search Highlighting (#16611)
### The changeset involves:
- Decoupling Selection and Search Highlighting code paths.
- We no longer invalidate search highlights when:
  - Left-clicking on terminal
  - A new selection is made
  - Left-clicking on Search-box
- Dispatching Find Next/Prev Match Action. (The search highlight was
removed after pressing the first key of the Action's key combination)
- And, anything that doesn't change buffer content, shouldn't invalidate
the highlighted region (E.g. Cursor movement)
- Highlighting foreground color is *actually* applied to the highlighted
text.
- Double-clicking on SearchBox no longer starts a text selection in the
terminal.
- Selected text is properly populated in the Search Box (#16355)

Closes: #16355


![image](https://github.com/microsoft/terminal/assets/55626797/8fd0345b-a8b2-4bc2-a25e-15d710127b63)

## Some Implementation Details

### Detecting text layout changes in the Control layer

As Search Highlight regions need to be removed when new text is added,
or the existing text is re-arranged due to window resize or similar
events, a new event `TextLayoutUpdated` is added that notifies
`CoreControl` of any text layout changes. The event is used to
invalidate and remove all search highlight regions from the buffer
(because the regions might not be _fresh_ anymore.

The new event is raised when:
1. `AdaptDispatch` writes new text into the buffer.
2. MainBuffer is switched to AltBuffer or vice-versa.
3. The user resized the window.
4. Font size changed.
5. Zoom level changed.

(Intensionally,) It's not raised when:
1. Buffer is scrolled.
2. The text cursor is moved.

When `ControlCore` receives a `TextLayoutUpdated` event, it clears the
Search Highlights in the *render data*, and raises an
`UpdateSearchResults` event to notify `TermControl` to update the Search
UI (`SearchBoxControl`).

In the future, we can use `TextLayoutUpdated` event to start a new

src/cascadia/TerminalControl/ControlCore.cpp
@@ -120,6 +120,9 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         auto pfnShowWindowChanged = std::bind(&ControlCore::_terminalShowWindowChanged, this, std::placeholders::_1);
         _terminal->SetShowWindowCallback(pfnShowWindowChanged);
 
+        auto pfnTextLayoutUpdated = std::bind(&ControlCore::_terminalTextLayoutUpdated, this);
+        _terminal->SetTextLayoutUpdatedCallback(pfnTextLayoutUpdated);
+
         auto pfnPlayMidiNote = std::bind(&ControlCore::_terminalPlayMidiNote, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
         _terminal->SetPlayMidiNoteCallback(pfnPlayMidiNote);
 
@@ -1123,6 +1126,9 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         if (SUCCEEDED(hr) && hr != S_FALSE)
         {
             _connection.Resize(vp.Height(), vp.Width());
+
+            // let the UI know that the text layout has been updated
+            _terminal->NotifyTextLayoutUpdated();
         }
     }
 
@@ -1638,6 +1644,16 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         ShowWindowChanged.raise(*this, *showWindow);
     }
 
+    void ControlCore::_terminalTextLayoutUpdated()
+    {
+        ClearSearch();
+
+        // send an UpdateSearchResults event to the UI to put the Search UI into inactive state.
+        auto evArgs = winrt::make_self<implementation::UpdateSearchResultsEventArgs>();
+        evArgs->State(SearchState::Inactive);
+        UpdateSearchResults.raise(*this, *evArgs);
+    }
+
     // Method Description:
     // - Plays a single MIDI note, blocking for the duration.
     // Arguments:
@@ -1701,43 +1717,45 @@ namespace winrt::Microsoft::Terminal::Control::implementation
     {
         const auto lock = _terminal->LockForWriting();
 
-        if (_searcher.ResetIfStale(*GetRenderData(), text, !goForward, !caseSensitive))
+        std::vector<til::point_span> oldResults;
+        if (_searcher.ResetIfStale(*GetRenderData(), text, !goForward, !caseSensitive, &oldResults))
         {
-            _searcher.HighlightResults();
-            _searcher.MoveToCurrentSelection();
             _cachedSearchResultRows = {};
+            if (SnapSearchResultToSelection())
+            {
+                _searcher.MoveToCurrentSelection();
+                SnapSearchResultToSelection(false);
+            }
+
+            _terminal->SetSearchHighlights(_searcher.Results());
+            _terminal->SetSearchHighlightFocused(_searcher.CurrentMatch());
         }
         else
         {
             _searcher.FindNext();
+            _terminal->SetSearchHighlightFocused(_searcher.CurrentMatch());
         }
+        _renderer->TriggerSearchHighlight(oldResults);
 
-        const auto foundMatch = _searcher.SelectCurrent();
-        auto foundResults = winrt::make_self<implementation::FoundResultsArgs>(foundMatch);
-        if (foundMatch)
+        auto evArgs = winrt::make_self<implementation::UpdateSearchResultsEventArgs>();
+        if (!text.empty())
         {
-            // this is used for search,
-            // DO NOT call _updateSelectionUI() here.
-            // We don't want to show the markers so manually tell it to clear it.
-            _terminal->SetBlockSelection(false);
-            UpdateSelectionMarkers.raise(*this, winrt::make<implementation::UpdateSelectionMarkersEventArgs>(true));
-
-            foundResults->TotalMatches(gsl::narrow<int32_t>(_searcher.Results().size()));
-            foundResults->CurrentMatch(gsl::narrow<int32_t>(_searcher.CurrentMatch()));
-
-            _terminal->AlwaysNotifyOnBufferRotation(true);
+            evArgs->State(SearchState::Active);
+            if (_searcher.GetCurrent())
+            {
+                evArgs->FoundMatch(true);
+                evArgs->TotalMatches(gsl::narrow<int32_t>(_searcher.Results().size()));
+                evArgs->CurrentMatch(gsl::narrow<int32_t>(_searcher.CurrentMatch()));
+            }
         }
-        _renderer->TriggerSelection();
 
-        // Raise a FoundMatch event, which the control will use to notify
-        // narrator if there was any results in the buffer
-        FoundMatch.raise(*this, *foundResults);
+        // Raise an UpdateSearchResults event, which the control will use to update the
+        // UI and notify the narrator about the updated search results in the buffer
+        UpdateSearchResults.raise(*this, *evArgs);
     }
 
     Windows::Foundation::Collections::IVector<int32_t> ControlCore::SearchResultRows()
     {
-        const auto lock = _terminal->LockForReading();
-
         if (!_cachedSearchResultRows)
         {
             auto results = std::vector<int32_t>();
@@ -1761,8 +1779,32 @@ namespace winrt::Microsoft::Terminal::Control::implementation
 
     void ControlCore::ClearSearch()
     {
-        _terminal->AlwaysNotifyOnBufferRotation(false);
-        _searcher = {};
+        // nothing to clear if there's no results
+        if (_searcher.GetCurrent())
+        {
+            const auto lock = _terminal->LockForWriting();
+            _terminal->SetSearchHighlights({});
+            _terminal->SetSearchHighlightFocused({});
+            _renderer->TriggerSearchHighlight(_searcher.Results());
+            _searcher = {};
+            _cachedSearchResultRows = {};
+        }
+    }
+
+    // Method Description:
+    // - Tells ControlCore to snap the current search result index to currently
+    //   selected text if the search was started using it.
+    void ControlCore::SnapSearchResultToSelection(bool shouldSnap) noexcept
+    {
+        _snapSearchResultToSelection = shouldSnap;
+    }
+
+    // Method Description:
+    // - Returns true, if we should snap the current search result index to
+    //   the currently selected text after a new search is started, else false.
+    bool ControlCore::SnapSearchResultToSelection() const noexcept
+    {
+        return _snapSearchResultToSelection;
     }
 
     void ControlCore::Close()

src/cascadia/TerminalCore/terminalrenderdata.cpp
@@ -150,32 +150,37 @@ catch (...)
     return {};
 }
 
-std::vector<Microsoft::Console::Types::Viewport> Terminal::GetSearchSelectionRects() noexcept
-try
+// Method Description:
+// - Helper to determine the search highlights in the buffer. Used for rendering.
+// Return Value:
+// - A vector of rectangles representing the regions to select, line by line. They are absolute coordinates relative to the buffer origin.
+std::span<const til::point_span> Terminal::GetSearchHighlights() const noexcept
 {
-    std::vector<Viewport> result;
+    _assertLocked();
+    return _searchHighlights;
+}
 
-    for (const auto& lineRect : _GetSearchSelectionRects(_GetVisibleViewport()))
+const til::point_span* Terminal::GetSearchHighlightFocused() const noexcept
+{
+    _assertLocked();
+    if (_searchHighlightFocused < _searchHighlights.size())
     {
-        result.emplace_back(Viewport::FromInclusive(lineRect));
+        return &til::at(_searchHighlights, _searchHighlightFocused);
     }
-
-    return result;
-}
-catch (...)
-{
-    LOG_CAUGHT_EXCEPTION();
-    return {};
+    return nullptr;
 }
 
-void Terminal::SelectNewRegion(const til::point coordStart, const til::point coordEnd)
+// Method Description:
+// - If necessary, scrolls the viewport such that the start point is in the
+//   viewport, and if that's already the case, also brings the end point inside
+//   the viewport
+// Arguments:
+// - coordStart - The start point
+// - coordEnd - The end point
+// Return Value:
+// - The updated scroll offset
+til::CoordType Terminal::_ScrollToPoints(const til::point coordStart, const til::point coordEnd)
 {
-#pragma warning(push)
-#pragma warning(disable : 26496) // cpp core checks wants these const, but they're decremented below.
-    auto realCoordStart = coordStart;
-    auto realCoordEnd = coordEnd;
-#pragma warning(pop)
-
     auto notifyScrollChange = false;
     if (coordStart.y < _VisibleStartIndex())
     {
@@ -199,28 +204,18 @@ void Terminal::SelectNewRegion(const til::point coordStart, const til::point coo
         _NotifyScrollEvent();
     }
 
-    realCoordStart.y -= _VisibleStartIndex();
-    realCoordEnd.y -= _VisibleStartIndex();
-
-    SetSelectionAnchor(realCoordStart);
-    SetSelectionEnd(realCoordEnd, SelectionExpansion::Char);
+    return _scrollOffset;
 }
 
-void Terminal::SelectSearchRegions(std::vector<til::inclusive_rect> rects)
+void Terminal::SelectNewRegion(const til::point coordStart, const til::point coordEnd)
 {
-    _searchSelections.clear();
-    for (auto& rect : rects)
-    {
-        rect.top -= _VisibleStartIndex();
-        rect.bottom -= _VisibleStartIndex();
+    const auto newScrollOffset = _ScrollToPoints(coordStart, coordEnd);
 
-        const auto realStart = _ConvertToBufferCell(til::point{ rect.left, rect.top });
-        const auto realEnd = _ConvertToBufferCell(til::point{ rect.right, rect.bottom });
-
-        auto rr = til::inclusive_rect{ realStart.x, realStart.y, realEnd.x, realEnd.y };
-
-        _searchSelections.emplace_back(rr);
-    }
+    // update the selection coordinates so they're relative to the new scroll-offset
+    const auto newCoordStart = til::point{ coordStart.x, coordStart.y - newScrollOffset };
+    const auto newCoordEnd = til::point{ coordEnd.x, coordEnd.y - newScrollOffset };
+    SetSelectionAnchor(newCoordStart);
+    SetSelectionEnd(newCoordEnd, SelectionExpansion::Char);
 }
 
 const std::wstring_view Terminal::GetConsoleTitle() const noexcept

src/buffer/out/search.cpp
@@ -8,7 +8,7 @@
 
 using namespace Microsoft::Console::Types;
 
-bool Search::ResetIfStale(Microsoft::Console::Render::IRenderData& renderData, const std::wstring_view& needle, bool reverse, bool caseInsensitive)
+bool Search::ResetIfStale(Microsoft::Console::Render::IRenderData& renderData, const std::wstring_view& needle, bool reverse, bool caseInsensitive, std::vector<til::point_span>* prevResults)
 {
     const auto& textBuffer = renderData.GetTextBuffer();
     const auto lastMutationId = textBuffer.GetLastMutationId();
@@ -26,10 +26,13 @@ bool Search::ResetIfStale(Microsoft::Console::Render::IRenderData& renderData, c
     _caseInsensitive = caseInsensitive;
     _lastMutationId = lastMutationId;
 
+    if (prevResults)
+    {
+        *prevResults = std::move(_results);
+    }
     _results = textBuffer.SearchText(needle, caseInsensitive);
     _index = reverse ? gsl::narrow_cast<ptrdiff_t>(_results.size()) - 1 : 0;
     _step = reverse ? -1 : 1;
-
     return true;
 }
 
@@ -109,28 +112,6 @@ const til::point_span* Search::GetCurrent() const noexcept
         return &til::at(_results, index);
     }
     return nullptr;
-}
-
-void Search::HighlightResults() const
-{
-    std::vector<til::inclusive_rect> toSelect;
-    const auto& textBuffer = _renderData->GetTextBuffer();
-
-    for (const auto& r : _results)
-    {
-        const auto rbStart = textBuffer.BufferToScreenPosition(r.start);
-        const auto rbEnd = textBuffer.BufferToScreenPosition(r.end);
-
-        til::inclusive_rect re;
-        re.top = rbStart.y;
-        re.bottom = rbEnd.y;
-        re.left = rbStart.x;
-        re.right = rbEnd.x;
-
-        toSelect.emplace_back(re);
-    }
-
-    _renderData->SelectSearchRegions(std::move(toSelect));
 }
 
 // Routine Description:

src/buffer/out/search.h
@@ -25,7 +25,11 @@ class Search final
 public:
     Search() = default;
 
-    bool ResetIfStale(Microsoft::Console::Render::IRenderData& renderData, const std::wstring_view& needle, bool reverse, bool caseInsensitive);
+    bool ResetIfStale(Microsoft::Console::Render::IRenderData& renderData,
+                      const std::wstring_view& needle,
+                      bool reverse,
+                      bool caseInsensitive,
+                      std::vector<til::point_span>* prevResults = nullptr);
 
     void MoveToCurrentSelection();
     void MoveToPoint(til::point anchor) noexcept;
@@ -33,7 +37,6 @@ public:
     void FindNext() noexcept;
 
     const til::point_span* GetCurrent() const noexcept;
-    void HighlightResults() const;
     bool SelectCurrent() const;
 
     const std::vector<til::point_span>& Results() const noexcept;

src/cascadia/TerminalControl/ControlCore.h
@@ -221,6 +221,8 @@ namespace winrt::Microsoft::Terminal::Control::implementation
 
         void Search(const winrt::hstring& text, const bool goForward, const bool caseSensitive);
         void ClearSearch();
+        void SnapSearchResultToSelection(bool snap) noexcept;
+        bool SnapSearchResultToSelection() const noexcept;
 
         Windows::Foundation::Collections::IVector<int32_t> SearchResultRows();
 
@@ -281,7 +283,7 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         til::typed_event<IInspectable, Control::NoticeEventArgs> RaiseNotice;
         til::typed_event<IInspectable, Control::TransparencyChangedEventArgs> TransparencyChanged;
         til::typed_event<> ReceivedOutput;
-        til::typed_event<IInspectable, Control::FoundResultsArgs> FoundMatch;
+        til::typed_event<IInspectable, Control::UpdateSearchResultsEventArgs> UpdateSearchResults;
         til::typed_event<IInspectable, Control::ShowWindowArgs> ShowWindowChanged;
         til::typed_event<IInspectable, Control::UpdateSelectionMarkersEventArgs> UpdateSelectionMarkers;
         til::typed_event<IInspectable, Control::OpenHyperlinkEventArgs> OpenHyperlink;
@@ -321,6 +323,7 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         std::unique_ptr<::Microsoft::Console::Render::Renderer> _renderer{ nullptr };
 
         ::Search _searcher;
+        bool _snapSearchResultToSelection;
 
         winrt::handle _lastSwapChainHandle{ nullptr };
 
@@ -379,6 +382,7 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         void _terminalCursorPositionChanged();
         void _terminalTaskbarProgressChanged();
         void _terminalShowWindowChanged(bool showOrHide);
+        void _terminalTextLayoutUpdated();
         void _terminalPlayMidiNote(const int noteNumber,
                                    const int velocity,
                                    const std::chrono::microseconds duration);

src/cascadia/TerminalControl/ControlCore.idl
@@ -131,6 +131,7 @@ namespace Microsoft.Terminal.Control
         void Search(String text, Boolean goForward, Boolean caseSensitive);
         void ClearSearch();
         IVector<Int32> SearchResultRows { get; };
+        Boolean SnapSearchResultToSelection;
 
         Microsoft.Terminal.Core.Color ForegroundColor { get; };
         Microsoft.Terminal.Core.Color BackgroundColor { get; };
@@ -179,7 +180,7 @@ namespace Microsoft.Terminal.Control
         event Windows.Foundation.TypedEventHandler<Object, NoticeEventArgs> RaiseNotice;
         event Windows.Foundation.TypedEventHandler<Object, TransparencyChangedEventArgs> TransparencyChanged;
         event Windows.Foundation.TypedEventHandler<Object, Object> ReceivedOutput;
-        event Windows.Foundation.TypedEventHandler<Object, FoundResultsArgs> FoundMatch;
+        event Windows.Foundation.TypedEventHandler<Object, UpdateSearchResultsEventArgs> UpdateSearchResults;
         event Windows.Foundation.TypedEventHandler<Object, UpdateSelectionMarkersEventArgs> UpdateSelectionMarkers;
         event Windows.Foundation.TypedEventHandler<Object, OpenHyperlinkEventArgs> OpenHyperlink;
         event Windows.Foundation.TypedEventHandler<Object, Object> CloseTerminalRequested;

src/cascadia/TerminalControl/EventArgs.cpp
@@ -12,7 +12,7 @@
 #include "ScrollPositionChangedArgs.g.cpp"
 #include "RendererWarningArgs.g.cpp"
 #include "TransparencyChangedEventArgs.g.cpp"
-#include "FoundResultsArgs.g.cpp"
+#include "UpdateSearchResultsEventArgs.g.cpp"
 #include "ShowWindowArgs.g.cpp"
 #include "UpdateSelectionMarkersEventArgs.g.cpp"
 #include "CompletionsChangedEventArgs.g.cpp"

src/cascadia/TerminalControl/EventArgs.h
@@ -12,7 +12,7 @@
 #include "ScrollPositionChangedArgs.g.h"
 #include "RendererWarningArgs.g.h"
 #include "TransparencyChangedEventArgs.g.h"
-#include "FoundResultsArgs.g.h"
+#include "UpdateSearchResultsEventArgs.g.h"
 #include "ShowWindowArgs.g.h"
 #include "UpdateSelectionMarkersEventArgs.g.h"
 #include "CompletionsChangedEventArgs.g.h"
@@ -141,14 +141,12 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         WINRT_PROPERTY(double, Opacity);
     };
 
-    struct FoundResultsArgs : public FoundResultsArgsT<FoundResultsArgs>
+    struct UpdateSearchResultsEventArgs : public UpdateSearchResultsEventArgsT<UpdateSearchResultsEventArgs>
     {
     public:
-        FoundResultsArgs(const bool foundMatch) :
-            _FoundMatch(foundMatch)
-        {
-        }
+        UpdateSearchResultsEventArgs() = default;
 
+        WINRT_PROPERTY(SearchState, State, SearchState::Inactive);
         WINRT_PROPERTY(bool, FoundMatch);
         WINRT_PROPERTY(int32_t, TotalMatches);
         WINRT_PROPERTY(int32_t, CurrentMatch);

commit 77087e6282cbab33932198c7d47795c7b9158235

Fix the location that selecting a mark uses (#17138)
I think this subtly regressed in #16611. Jump to
https://github.com/microsoft/terminal/commit/90b8bb7c2d8932fc601aed9c26f13c77d5a1321d#diff-f9112caf8cb75e7a48a7b84987724d754181227385fbfcc2cc09a879b1f97c12L171-L223

`Terminal::SelectNewRegion` is the only thing that uses the return value
from `Terminal::_ScrollToPoints`. Before that PR, `_ScrollToPoints` was
just a part of `SelectNewRegion`, and it moved the start & end coords by
the `_VisibleStartIndex`, not the `_scrollOffset`.

Kinda weird there weren't any _other_ tests for `SelectNewRegion`?

I also caught a second bug while I was here - If you had a line with an
exact wrap, and tried to select that like with selectOutput, we'd
explode.

Closes #17131

src/cascadia/TerminalControl/ControlCore.cpp
@@ -2614,12 +2614,27 @@ namespace winrt::Microsoft::Terminal::Control::implementation
 
         CompletionsChanged.raise(*this, *args);
     }
+
+    // Select the region of text between [s.start, s.end), in buffer space
     void ControlCore::_selectSpan(til::point_span s)
     {
+        // s.end is an _exclusive_ point. We need an inclusive one. But
+        // decrement in bounds wants an inclusive one. If you pass an exclusive
+        // one, then it might assert at you for being out of bounds. So we also
+        // take care of the case that the end point is outside the viewport
+        // manually.
         const auto bufferSize{ _terminal->GetTextBuffer().GetSize() };
-        bufferSize.DecrementInBounds(s.end);
+        til::point inclusiveEnd = s.end;
+        if (s.end.x == bufferSize.Width())
+        {
+            inclusiveEnd = til::point{ std::max(0, s.end.x - 1), s.end.y };
+        }
+        else
+        {
+            bufferSize.DecrementInBounds(inclusiveEnd);
+        }
 
-        _terminal->SelectNewRegion(s.start, s.end);
+        _terminal->SelectNewRegion(s.start, inclusiveEnd);
         _renderer->TriggerSelection();
     }
 

src/cascadia/TerminalCore/terminalrenderdata.cpp
@@ -199,7 +199,7 @@ til::CoordType Terminal::_ScrollToPoints(const til::point coordStart, const til:
         _NotifyScrollEvent();
     }
 
-    return _scrollOffset;
+    return _VisibleStartIndex();
 }
 
 void Terminal::SelectNewRegion(const til::point coordStart, const til::point coordEnd)

src/cascadia/UnitTests_Control/ControlCoreTests.cpp
@@ -40,6 +40,8 @@ namespace ControlUnitTests
 
         TEST_METHOD(TestSelectCommandSimple);
         TEST_METHOD(TestSelectOutputSimple);
+        TEST_METHOD(TestSelectOutputScrolling);
+        TEST_METHOD(TestSelectOutputExactWrap);
 
         TEST_METHOD(TestSimpleClickSelection);
 
@@ -496,6 +498,140 @@ namespace ControlUnitTests
         }
 
         VERIFY_IS_FALSE(core->HasSelection());
+        core->SelectOutput(true);
+        VERIFY_IS_TRUE(core->HasSelection());
+        {
+            const auto& start = core->_terminal->GetSelectionAnchor();
+            const auto& end = core->_terminal->GetSelectionEnd();
+            const til::point expectedStart{ 24, 0 }; // The character after the prompt
+            const til::point expectedEnd{ 21, 3 }; // x = the end of the text
+            VERIFY_ARE_EQUAL(expectedStart, start);
+            VERIFY_ARE_EQUAL(expectedEnd, end);
+        }
+    }
+
+    void ControlCoreTests::TestSelectOutputScrolling()
+    {
+        auto [settings, conn] = _createSettingsAndConnection();
+        Log::Comment(L"Create ControlCore object");
+        auto core = createCore(*settings, *conn);
+        VERIFY_IS_NOT_NULL(core);
+        _standardInit(core);
+
+        Log::Comment(L"Print some text");
+
+        _writePrompt(conn, L"C:\\Windows"); // row 0
+        conn->WriteInput(L"Foo-bar"); // row 0
+        conn->WriteInput(L"\x1b]133;C\x7");
+
+        conn->WriteInput(L"\r\n");
+        conn->WriteInput(L"This is some text     \r\n"); // row 1
+        conn->WriteInput(L"with varying amounts  \r\n"); // row 2
+        conn->WriteInput(L"of whitespace         \r\n"); // row 3
+
+        _writePrompt(conn, L"C:\\Windows"); // row 4
+        conn->WriteInput(L"gci");
+        conn->WriteInput(L"\x1b]133;C\x7");
+        conn->WriteInput(L"\r\n");
+
+        // enough to scroll
+        for (auto i = 0; i < 30; i++) // row 5-34
+        {
+            conn->WriteInput(L"-a--- 2/8/2024  9:47 README\r\n");
+        }
+
+        _writePrompt(conn, L"C:\\Windows");
+
+        Log::Comment(L"Check the buffer contents");
+        const auto& buffer = core->_terminal->GetTextBuffer();
+        const auto& cursor = buffer.GetCursor();
+
+        {
+            const til::point expectedCursor{ 17, 35 };
+            VERIFY_ARE_EQUAL(expectedCursor, cursor.GetPosition());
+        }
+
+        VERIFY_IS_FALSE(core->HasSelection());
+
+        // The second mark is the first one we'll see
+        core->SelectOutput(true);
+        VERIFY_IS_TRUE(core->HasSelection());
+        {
+            const auto& start = core->_terminal->GetSelectionAnchor();
+            const auto& end = core->_terminal->GetSelectionEnd();
+            const til::point expectedStart{ 20, 4 }; // The character after the prompt
+            const til::point expectedEnd{ 26, 34 }; // x = the end of the text
+            VERIFY_ARE_EQUAL(expectedStart, start);
+            VERIFY_ARE_EQUAL(expectedEnd, end);
+        }
+        core->SelectOutput(true);
+        VERIFY_IS_TRUE(core->HasSelection());
+        {
+            const auto& start = core->_terminal->GetSelectionAnchor();
+            const auto& end = core->_terminal->GetSelectionEnd();
+            const til::point expectedStart{ 24, 0 }; // The character after the prompt
+            const til::point expectedEnd{ 21, 3 }; // x = the end of the text
+            VERIFY_ARE_EQUAL(expectedStart, start);
+            VERIFY_ARE_EQUAL(expectedEnd, end);
+        }
+    }
+
+    void ControlCoreTests::TestSelectOutputExactWrap()
+    {
+        // Just like the TestSelectOutputScrolling test, but these lines will
+        // exactly wrap to the right edge of the buffer, to catch a edge case
+        // present in `ControlCore::_selectSpan`
+        auto [settings, conn] = _createSettingsAndConnection();
+        Log::Comment(L"Create ControlCore object");
+        auto core = createCore(*settings, *conn);
+        VERIFY_IS_NOT_NULL(core);
+        _standardInit(core);
+
+        Log::Comment(L"Print some text");
+
+        _writePrompt(conn, L"C:\\Windows"); // row 0
+        conn->WriteInput(L"Foo-bar"); // row 0
+        conn->WriteInput(L"\x1b]133;C\x7");
+
+        conn->WriteInput(L"\r\n");
+        conn->WriteInput(L"This is some text     \r\n"); // row 1
+        conn->WriteInput(L"with varying amounts  \r\n"); // row 2
+        conn->WriteInput(L"of whitespace         \r\n"); // row 3
+
+        _writePrompt(conn, L"C:\\Windows"); // row 4
+        conn->WriteInput(L"gci");
+        conn->WriteInput(L"\x1b]133;C\x7");
+        conn->WriteInput(L"\r\n");
+
+        // enough to scroll
+        for (auto i = 0; i < 30; i++) // row 5-35
+        {
+            conn->WriteInput(L"-a--- 2/8/2024  9:47 README.md\r\n");
+        }
+
+        _writePrompt(conn, L"C:\\Windows");
+
+        Log::Comment(L"Check the buffer contents");
+        const auto& buffer = core->_terminal->GetTextBuffer();
+        const auto& cursor = buffer.GetCursor();
+
+        {
+            const til::point expectedCursor{ 17, 35 };
+            VERIFY_ARE_EQUAL(expectedCursor, cursor.GetPosition());
+        }
+
+        VERIFY_IS_FALSE(core->HasSelection());
+        // The second mark is the first one we'll see
+        core->SelectOutput(true);
+        VERIFY_IS_TRUE(core->HasSelection());
+        {
+            const auto& start = core->_terminal->GetSelectionAnchor();
+            const auto& end = core->_terminal->GetSelectionEnd();
+            const til::point expectedStart{ 20, 4 }; // The character after the prompt
+            const til::point expectedEnd{ 29, 34 }; // x = the end of the text
+            VERIFY_ARE_EQUAL(expectedStart, start);
+            VERIFY_ARE_EQUAL(expectedEnd, end);
+        }
         core->SelectOutput(true);
         VERIFY_IS_TRUE(core->HasSelection());
         {