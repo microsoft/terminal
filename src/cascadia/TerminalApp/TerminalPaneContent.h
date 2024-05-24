864f1b53f4876e8992cdd84e5e2c0c4aa7a956fc
Mike Griese <migrie@microsoft.com>
2024-05-24 16:11:00 -0500

    base classes for all, for fun and profit

--- src/cascadia/TerminalApp/TerminalPaneContent.h
+++ src/cascadia/TerminalApp/TerminalPaneContent.h
@@ -4,6 +4,7 @@
 #pragma once
 #include "TerminalPaneContent.g.h"
 #include "BellEventArgs.g.h"
+#include "BasicPaneEvents.h"
 
 namespace winrt::TerminalApp::implementation
 {
@@ -16,7 +17,7 @@ namespace winrt::TerminalApp::implementation
         til::property<bool> FlashTaskbar;
     };
 
-    struct TerminalPaneContent : TerminalPaneContentT<TerminalPaneContent>
+    struct TerminalPaneContent : TerminalPaneContentT<TerminalPaneContent>, BasicPaneEvents
     {
         TerminalPaneContent(const winrt::Microsoft::Terminal::Settings::Model::Profile& profile,
                             const TerminalApp::TerminalSettingsCache& cache,
@@ -52,14 +53,7 @@ namespace winrt::TerminalApp::implementation
 
         til::typed_event<TerminalApp::TerminalPaneContent, winrt::Windows::Foundation::IInspectable> RestartTerminalRequested;
 
-        til::typed_event<> ConnectionStateChanged;
-        til::typed_event<IPaneContent> CloseRequested;
-        til::typed_event<IPaneContent, winrt::TerminalApp::BellEventArgs> BellRequested;
-        til::typed_event<IPaneContent> TitleChanged;
-        til::typed_event<IPaneContent> TabColorChanged;
-        til::typed_event<IPaneContent> TaskbarProgressChanged;
-        til::typed_event<IPaneContent> ReadOnlyChanged;
-        til::typed_event<IPaneContent> FocusRequested;
+        // See BasicPaneEvents for most generic event definitions
 
     private:
         winrt::Microsoft::Terminal::Control::TermControl _control{ nullptr };
