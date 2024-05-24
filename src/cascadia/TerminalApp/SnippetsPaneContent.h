fa7609343abaece75f8749f77bc3d6de2bae2f5e
Mike Griese <migrie@microsoft.com>
2024-05-24 16:00:46 -0500

    hey this works great

--- src/cascadia/TerminalApp/SnippetsPaneContent.h
+++ src/cascadia/TerminalApp/SnippetsPaneContent.h
@@ -4,13 +4,18 @@
 #pragma once
 #include "SnippetsPaneContent.g.h"
 #include "FilteredTask.g.h"
+#include "BasicPaneEvents.h"
 #include "FilteredCommand.h"
 #include "ActionPaletteItem.h"
 #include <LibraryResources.h>
 
 namespace winrt::TerminalApp::implementation
 {
+<<<<<<< HEAD:src/cascadia/TerminalApp/SnippetsPaneContent.h
     struct SnippetsPaneContent : SnippetsPaneContentT<SnippetsPaneContent>
+=======
+    struct TasksPaneContent : TasksPaneContentT<TasksPaneContent>, BasicPaneEvents
+>>>>>>> f8e5bfa09 (hey this works great):src/cascadia/TerminalApp/TasksPaneContent.h
     {
         SnippetsPaneContent();
 
@@ -34,16 +39,7 @@ namespace winrt::TerminalApp::implementation
         void SetLastActiveControl(const Microsoft::Terminal::Control::TermControl& control);
         bool HasSnippets() const;
 
-        til::typed_event<> ConnectionStateChanged;
-        til::typed_event<IPaneContent> CloseRequested;
-        til::typed_event<IPaneContent, winrt::TerminalApp::BellEventArgs> BellRequested;
-        til::typed_event<IPaneContent> TitleChanged;
-        til::typed_event<IPaneContent> TabColorChanged;
-        til::typed_event<IPaneContent> TaskbarProgressChanged;
-        til::typed_event<IPaneContent> ReadOnlyChanged;
-        til::typed_event<IPaneContent> FocusRequested;
-
-        til::typed_event<winrt::Windows::Foundation::IInspectable, Microsoft::Terminal::Settings::Model::Command> DispatchCommandRequested;
+        // See BasicPaneEvents for most generic event definitions
 
         til::property_changed_event PropertyChanged;
 
