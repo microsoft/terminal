microsoft/terminal

commit 4a243f044572146e18e0051badb1b5b3f3c28ac8

Add support for VT paging operations (#16615)
This PR adds support for multiples pages in the VT architecture, along
with new operations for moving between those pages: `NP` (Next Page),
`PP` (Preceding Page), `PPA` (Page Position Absolute), `PPR` (Page
Position Relative), and `PPB` (Page Position Back).

There's also a new mode, `DECPCCM` (Page Cursor Coupling Mode), which
determines whether or not the active page is also the visible page, and
a new query sequence, `DECRQDE` (Request Displayed Extent), which can be
used to query the visible page.

## References and Relevant Issues

When combined with `DECCRA` (Copy Rectangular Area), which can copy
between pages, you can layer content on top of existing output, and
still restore the original data afterwards. So this could serve as an
alternative solution to #10810.

## Detailed Description of the Pull Request / Additional comments

On the original DEC terminals that supported paging, you couldn't have
both paging and scrollback at the same time - only the one or the other.
But modern terminals typically allow both, so we support that too.

The way it works, the currently visible page will be attached to the
scrollback, and any content that scrolls off the top will thus be saved.
But the background pages will not have scrollback, so their content is
lost if it scrolls off the top.

And when the screen is resized, only the visible page will be reflowed.
Background pages are not affected by a resize until they become active.
At that point they just receive the traditional style of resize, where
the content is clipped or padded to match the new dimensions.

I'm not sure this is the best way to handle resizing, but we can always
consider other approaches once people have had a chance to try it out.

## Validation Steps Performed

I've added some unit tests covering the new operations, and also done a
lot of manual testing.

Closes #13892
Tests added/passed

src/terminal/adapter/PageManager.cpp
@@ -1,0 +1,254 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include "precomp.h"
+
+#include "PageManager.hpp"
+#include "../../renderer/base/renderer.hpp"
+
+using namespace Microsoft::Console::VirtualTerminal;
+
+Page::Page(TextBuffer& buffer, const til::rect& viewport, const til::CoordType number) noexcept :
+    _buffer{ buffer },
+    _viewport{ viewport },
+    _number(number)
+{
+}
+
+TextBuffer& Page::Buffer() const noexcept
+{
+    return _buffer;
+}
+
+til::rect Page::Viewport() const noexcept
+{
+    return _viewport;
+}
+
+til::CoordType Page::Number() const noexcept
+{
+    return _number;
+}
+
+Cursor& Page::Cursor() const noexcept
+{
+    return _buffer.GetCursor();
+}
+
+const TextAttribute& Page::Attributes() const noexcept
+{
+    return _buffer.GetCurrentAttributes();
+}
+
+void Page::SetAttributes(const TextAttribute& attr, ITerminalApi* api) const
+{
+    _buffer.SetCurrentAttributes(attr);
+    // If the api parameter was specified, we need to pass the new attributes
+    // through to the api. This occurs when there's a potential for the colors
+    // to be changed, which may require some legacy remapping in conhost.
+    if (api)
+    {
+        api->SetTextAttributes(attr);
+    }
+}
+
+til::CoordType Page::Top() const noexcept
+{
+    // If we ever support vertical window panning, the page top won't
+    // necessarily align with the viewport top, so it's best we always
+    // treat them as distinct properties.
+    return _viewport.top;
+}
+
+til::CoordType Page::Bottom() const noexcept
+{
+    // Similarly, the page bottom won't always match the viewport bottom.
+    return _viewport.bottom;
+}
+
+til::CoordType Page::Width() const noexcept
+{
+    // The page width could also one day be different from the buffer width,
+    // so again it's best treated as a distinct property.
+    return _buffer.GetSize().Width();
+}
+
+til::CoordType Page::Height() const noexcept
+{
+    return Bottom() - Top();
+}
+
+til::CoordType Page::BufferHeight() const noexcept
+{
+    return _buffer.GetSize().Height();
+}
+
+til::CoordType Page::XPanOffset() const noexcept
+{
+    return _viewport.left;
+}
+
+til::CoordType Page::YPanOffset() const noexcept
+{
+    return 0; // Vertical panning is not yet supported
+}
+
+PageManager::PageManager(ITerminalApi& api, Renderer& renderer) noexcept :
+    _api{ api },
+    _renderer{ renderer }
+{
+}
+
+void PageManager::Reset()
+{
+    _activePageNumber = 1;
+    _visiblePageNumber = 1;
+    _buffers = {};
+}
+
+Page PageManager::Get(const til::CoordType pageNumber) const
+{
+    const auto requestedPageNumber = std::min(std::max(pageNumber, 1), MAX_PAGES);
+    auto [visibleBuffer, visibleViewport, isMainBuffer] = _api.GetBufferAndViewport();
+
+    // If we're not in the main buffer (either because an app has enabled the
+    // alternate buffer mode, or switched the conhost screen buffer), then VT
+    // paging doesn't apply, so we disregard the requested page number and just
+    // use the visible buffer (with a fixed page number of 1).
+    if (!isMainBuffer)
+    {
+        return { visibleBuffer, visibleViewport, 1 };
+    }
+
+    // If the requested page number happens to be the visible page, then we
+    // can also just use the visible buffer as is.
+    if (requestedPageNumber == _visiblePageNumber)
+    {
+        return { visibleBuffer, visibleViewport, _visiblePageNumber };
+    }
+
+    // Otherwise we're working with a background buffer, so we need to
+    // retrieve that from the buffer array, and resize it to match the
+    // active page size.
+    const auto pageSize = visibleViewport.size();
+    auto& pageBuffer = _getBuffer(requestedPageNumber, pageSize);
+    return { pageBuffer, til::rect{ pageSize }, requestedPageNumber };
+}
+
+Page PageManager::ActivePage() const
+{
+    return Get(_activePageNumber);
+}
+
+Page PageManager::VisiblePage() const
+{
+    return Get(_visiblePageNumber);
+}
+
+void PageManager::MoveTo(const til::CoordType pageNumber, const bool makeVisible)
+{
+    auto [visibleBuffer, visibleViewport, isMainBuffer] = _api.GetBufferAndViewport();
+    if (!isMainBuffer)
+    {
+        return;
+    }
+
+    const auto pageSize = visibleViewport.size();
+    const auto visibleTop = visibleViewport.top;
+    const auto wasVisible = _activePageNumber == _visiblePageNumber;
+    const auto newPageNumber = std::min(std::max(pageNumber, 1), MAX_PAGES);
+    auto redrawRequired = false;
+
+    // If we're changing the visible page, what we do is swap out the current
+    // visible page into its backing buffer, and swap in the new page from the
+    // backing buffer to the main buffer. That way the rest of the system only
+    // ever has to deal with the main buffer.
+    if (makeVisible && _visiblePageNumber != newPageNumber)
+    {
+        const auto& newBuffer = _getBuffer(newPageNumber, pageSize);
+        auto& saveBuffer = _getBuffer(_visiblePageNumber, pageSize);
+        for (auto i = 0; i < pageSize.height; i++)
+        {
+            saveBuffer.GetMutableRowByOffset(i).CopyFrom(visibleBuffer.GetRowByOffset(visibleTop + i));
+        }
+        for (auto i = 0; i < pageSize.height; i++)
+        {
+            visibleBuffer.GetMutableRowByOffset(visibleTop + i).CopyFrom(newBuffer.GetRowByOffset(i));
+        }
+        _visiblePageNumber = newPageNumber;
+        redrawRequired = true;
+    }
+
+    // If the active page was previously visible, and is now still visible,
+    // there is no need to update any buffer properties, because we'll have
+    // been using the main buffer in both cases.
+    const auto isVisible = newPageNumber == _visiblePageNumber;
+    if (!wasVisible || !isVisible)
+    {
+        // Otherwise we need to copy the properties from the old buffer to the
+        // new, so we retain the current attributes and cursor position. This
+        // is only needed if they are actually different.
+        auto& oldBuffer = wasVisible ? visibleBuffer : _getBuffer(_activePageNumber, pageSize);
+        auto& newBuffer = isVisible ? visibleBuffer : _getBuffer(newPageNumber, pageSize);
+        if (&oldBuffer != &newBuffer)
+        {
+            // When copying the cursor position, we need to adjust the y
+            // coordinate to account for scrollback in the visible buffer.
+            const auto oldTop = wasVisible ? visibleTop : 0;
+            const auto newTop = isVisible ? visibleTop : 0;
+            auto position = oldBuffer.GetCursor().GetPosition();
+            position.y = position.y - oldTop + newTop;
+            newBuffer.SetCurrentAttributes(oldBuffer.GetCurrentAttributes());
+            newBuffer.CopyProperties(oldBuffer);
+            newBuffer.GetCursor().SetPosition(position);
+        }
+        // If we moved from the visible buffer to a background buffer we need
+        // to hide the cursor in the visible buffer. This is because the page
+        // number is like a third dimension in the cursor coordinate system.
+        // If the cursor isn't on the visible page, it's the same as if its
+        // x/y coordinates are outside the visible viewport.
+        if (wasVisible && !isVisible)
+        {
+            visibleBuffer.GetCursor().SetIsVisible(false);
+        }
+    }
+
+    _activePageNumber = newPageNumber;
+    if (redrawRequired)
+    {
+        _renderer.TriggerRedrawAll();
+    }
+}
+
+void PageManager::MoveRelative(const til::CoordType pageCount, const bool makeVisible)
+{
+    MoveTo(_activePageNumber + pageCount, makeVisible);
+}
+
+void PageManager::MakeActivePageVisible()
+{
+    if (_activePageNumber != _visiblePageNumber)
+    {
+        MoveTo(_activePageNumber, true);
+    }
+}
+
+TextBuffer& PageManager::_getBuffer(const til::CoordType pageNumber, const til::size pageSize) const
+{
+    auto& buffer = til::at(_buffers, pageNumber - 1);
+    if (buffer == nullptr)
+    {
+        // Page buffers are created on demand, and are sized to match the active
+        // page dimensions without any scrollback rows.
+        buffer = std::make_unique<TextBuffer>(pageSize, TextAttribute{}, 0, false, _renderer);
+    }
+    else if (buffer->GetSize().Dimensions() != pageSize)
+    {
+        // If a buffer already exists for the page, and the page dimensions have
+        // changed while it was inactive, it will need to be resized.
+        // TODO: We don't currently reflow the existing content in this case, but
+        // that may be something we want to reconsider.
+        buffer->ResizeTraditional(pageSize);
+    }
+    return *buffer;
+}

src/terminal/adapter/PageManager.hpp
@@ -1,0 +1,67 @@
+/*++
+Copyright (c) Microsoft Corporation
+Licensed under the MIT license.
+
+Module Name:
+- PageManager.hpp
+
+Abstract:
+- This manages the text buffers required by the VT paging operations.
+--*/
+
+#pragma once
+
+#include "ITerminalApi.hpp"
+#include "til.h"
+
+namespace Microsoft::Console::VirtualTerminal
+{
+    class Page
+    {
+    public:
+        Page(TextBuffer& buffer, const til::rect& viewport, const til::CoordType number) noexcept;
+        TextBuffer& Buffer() const noexcept;
+        til::rect Viewport() const noexcept;
+        til::CoordType Number() const noexcept;
+        Cursor& Cursor() const noexcept;
+        const TextAttribute& Attributes() const noexcept;
+        void SetAttributes(const TextAttribute& attr, ITerminalApi* api = nullptr) const;
+        til::CoordType Top() const noexcept;
+        til::CoordType Bottom() const noexcept;
+        til::CoordType Width() const noexcept;
+        til::CoordType Height() const noexcept;
+        til::CoordType BufferHeight() const noexcept;
+        til::CoordType XPanOffset() const noexcept;
+        til::CoordType YPanOffset() const noexcept;
+
+    private:
+        TextBuffer& _buffer;
+        til::rect _viewport;
+        til::CoordType _number;
+    };
+
+    class PageManager
+    {
+        using Renderer = Microsoft::Console::Render::Renderer;
+
+    public:
+        PageManager(ITerminalApi& api, Renderer& renderer) noexcept;
+        void Reset();
+        Page Get(const til::CoordType pageNumber) const;
+        Page ActivePage() const;
+        Page VisiblePage() const;
+        void MoveTo(const til::CoordType pageNumber, const bool makeVisible);
+        void MoveRelative(const til::CoordType pageCount, const bool makeVisible);
+        void MakeActivePageVisible();
+
+    private:
+        TextBuffer& _getBuffer(const til::CoordType pageNumber, const til::size pageSize) const;
+
+        ITerminalApi& _api;
+        Renderer& _renderer;
+        til::CoordType _activePageNumber = 1;
+        til::CoordType _visiblePageNumber = 1;
+        static constexpr til::CoordType MAX_PAGES = 6;
+        mutable std::array<std::unique_ptr<TextBuffer>, MAX_PAGES> _buffers;
+    };
+}

src/terminal/adapter/adaptDispatch.hpp
@@ -18,6 +18,7 @@ Author(s):
 #include "ITerminalApi.hpp"
 #include "FontBuffer.hpp"
 #include "MacroBuffer.hpp"
+#include "PageManager.hpp"
 #include "terminalOutput.hpp"
 #include "../input/terminalInput.hpp"
 #include "../../types/inc/sgrStack.hpp"
@@ -81,6 +82,12 @@ namespace Microsoft::Console::VirtualTerminal
         bool RequestTerminalParameters(const DispatchTypes::ReportingPermission permission) override; // DECREQTPARM
         bool ScrollUp(const VTInt distance) override; // SU
         bool ScrollDown(const VTInt distance) override; // SD
+        bool NextPage(const VTInt pageCount) override; // NP
+        bool PrecedingPage(const VTInt pageCount) override; // PP
+        bool PagePositionAbsolute(const VTInt page) override; // PPA
+        bool PagePositionRelative(const VTInt pageCount) override; // PPR
+        bool PagePositionBack(const VTInt pageCount) override; // PPB
+        bool RequestDisplayedExtent() override; // DECRQDE
         bool InsertLine(const VTInt distance) override; // IL
         bool DeleteLine(const VTInt distance) override; // DL
         bool InsertColumn(const VTInt distance) override; // DECIC
@@ -178,7 +185,8 @@ namespace Microsoft::Console::VirtualTerminal
             AllowDECCOLM,
             AllowDECSLRM,
             EraseColor,
-            RectangularChangeExtent
+            RectangularChangeExtent,
+            PageCursorCoupling
         };
         enum class ScrollDirection
         {
@@ -189,6 +197,7 @@ namespace Microsoft::Console::VirtualTerminal
         {
             VTInt Row = 1;
             VTInt Column = 1;
+            VTInt Page = 1;
             bool IsDelayedEOLWrap = false;
             bool IsOriginModeRelative = false;
             TextAttribute Attributes = {};
@@ -214,20 +223,20 @@ namespace Microsoft::Console::VirtualTerminal
         };
 
         void _WriteToBuffer(const std::wstring_view string);
-        std::pair<int, int> _GetVerticalMargins(const til::rect& viewport, const bool absolute) noexcept;
+        std::pair<int, int> _GetVerticalMargins(const Page& page, const bool absolute) noexcept;
         std::pair<int, int> _GetHorizontalMargins(const til::CoordType bufferWidth) noexcept;
         bool _CursorMovePosition(const Offset rowOffset, const Offset colOffset, const bool clampInMargins);
         void _ApplyCursorMovementFlags(Cursor& cursor) noexcept;
-        void _FillRect(TextBuffer& textBuffer, const til::rect& fillRect, const std::wstring_view& fillChar, const TextAttribute& fillAttrs) const;
-        void _SelectiveEraseRect(TextBuffer& textBuffer, const til::rect& eraseRect);
-        void _ChangeRectAttributes(TextBuffer& textBuffer, const til::rect& changeRect, const ChangeOps& changeOps);
+        void _FillRect(const Page& page, const til::rect& fillRect, const std::wstring_view& fillChar, const TextAttribute& fillAttrs) const;
+        void _SelectiveEraseRect(const Page& page, const til::rect& eraseRect);
+        void _ChangeRectAttributes(const Page& page, const til::rect& changeRect, const ChangeOps& changeOps);
         void _ChangeRectOrStreamAttributes(const til::rect& changeArea, const ChangeOps& changeOps);
-        til::rect _CalculateRectArea(const VTInt top, const VTInt left, const VTInt bottom, const VTInt right, const til::size bufferSize);
+        til::rect _CalculateRectArea(const Page& page, const VTInt top, const VTInt left, const VTInt bottom, const VTInt right);
         bool _EraseScrollback();
         bool _EraseAll();
-        TextAttribute _GetEraseAttributes(const TextBuffer& textBuffer) const noexcept;
-        void _ScrollRectVertically(TextBuffer& textBuffer, const til::rect& scrollRect, const VTInt delta);
-        void _ScrollRectHorizontally(TextBuffer& textBuffer, const til::rect& scrollRect, const VTInt delta);
+        TextAttribute _GetEraseAttributes(const Page& page) const noexcept;
+        void _ScrollRectVertically(const Page& page, const til::rect& scrollRect, const VTInt delta);
+        void _ScrollRectHorizontally(const Page& page, const til::rect& scrollRect, const VTInt delta);
         void _InsertDeleteCharacterHelper(const VTInt delta);
         void _InsertDeleteLineHelper(const VTInt delta);
         void _InsertDeleteColumnHelper(const VTInt delta);
@@ -240,7 +249,7 @@ namespace Microsoft::Console::VirtualTerminal
                                              const VTInt rightMargin,
                                              const bool homeCursor = false);
 
-        void _DoLineFeed(TextBuffer& textBuffer, const bool withReturn, const bool wrapForced);
+        void _DoLineFeed(const Page& page, const bool withReturn, const bool wrapForced);
 
         void _DeviceStatusReport(const wchar_t* parameters) const;
         void _CursorPositionReport(const bool extendedReport);
@@ -281,6 +290,7 @@ namespace Microsoft::Console::VirtualTerminal
         RenderSettings& _renderSettings;
         TerminalInput& _terminalInput;
         TerminalOutput _termOutput;
+        PageManager _pages;
         std::unique_ptr<FontBuffer> _fontBuffer;
         std::shared_ptr<MacroBuffer> _macroBuffer;
         std::optional<unsigned int> _initialCodePage;
@@ -295,7 +305,7 @@ namespace Microsoft::Console::VirtualTerminal
 
         til::inclusive_rect _scrollMargins;
 
-        til::enumset<Mode> _modes;
+        til::enumset<Mode> _modes{ Mode::PageCursorCoupling };
 
         SgrStack _sgrStack;
 

src/cascadia/TerminalCore/Terminal.hpp
@@ -131,8 +131,7 @@ public:
     // These methods are defined in TerminalApi.cpp
     void ReturnResponse(const std::wstring_view response) override;
     Microsoft::Console::VirtualTerminal::StateMachine& GetStateMachine() noexcept override;
-    TextBuffer& GetTextBuffer() noexcept override;
-    til::rect GetViewport() const noexcept override;
+    BufferState GetBufferAndViewport() noexcept override;
     void SetViewportPosition(const til::point position) noexcept override;
     void SetTextAttributes(const TextAttribute& attrs) noexcept override;
     void SetSystemMode(const Mode mode, const bool enabled) noexcept override;

src/cascadia/TerminalCore/TerminalApi.cpp
@@ -34,14 +34,9 @@ Microsoft::Console::VirtualTerminal::StateMachine& Terminal::GetStateMachine() n
     return *_stateMachine;
 }
 
-TextBuffer& Terminal::GetTextBuffer() noexcept
+ITerminalApi::BufferState Terminal::GetBufferAndViewport() noexcept
 {
-    return _activeBuffer();
-}
-
-til::rect Terminal::GetViewport() const noexcept
-{
-    return til::rect{ _GetMutableViewport().ToInclusive() };
+    return { _activeBuffer(), til::rect{ _GetMutableViewport().ToInclusive() }, !_inAltBuffer() };
 }
 
 void Terminal::SetViewportPosition(const til::point position) noexcept

commit babd344816a5fe9240731f6e3c46d36105048791

Account for viewport movement when wrapping over multiple rows (#17353)
## Summary of the Pull Request

When we receive a stream of output at the bottom of the page that wraps
over more than two lines, that is expected to pan the viewport down by
multiple rows to accommodate all of the output. However, when the output
is received in a single write, that did not work correctly.

The problem was that we were reusing a `Page` instance across multiple
`_DoLineFeed` calls, and the viewport cached in that `Page` wasn't valid
after the first call. This PR fixes the issue by adjusting the cached
viewport when we determine it has been moved by `_DoLineFeed`.

## References and Relevant Issues

The bug was introduced in PR #16615 when paging support was added.

## Validation Steps Performed

I've verified that the test case in #17351 is now working correctly, and
have added a unit test covering this scenario.

## PR Checklist
- [x] Closes #17351
- [x] Tests added/passed

src/terminal/adapter/PageManager.cpp
@@ -93,6 +93,12 @@ til::CoordType Page::YPanOffset() const noexcept
     return 0; // Vertical panning is not yet supported
 }
 
+void Page::MoveViewportDown() noexcept
+{
+    _viewport.top++;
+    _viewport.bottom++;
+}
+
 PageManager::PageManager(ITerminalApi& api, Renderer& renderer) noexcept :
     _api{ api },
     _renderer{ renderer }

src/terminal/adapter/PageManager.hpp
@@ -33,6 +33,7 @@ namespace Microsoft::Console::VirtualTerminal
         til::CoordType BufferHeight() const noexcept;
         til::CoordType XPanOffset() const noexcept;
         til::CoordType YPanOffset() const noexcept;
+        void MoveViewportDown() noexcept;
 
     private:
         TextBuffer& _buffer;

src/terminal/adapter/adaptDispatch.cpp
@@ -73,14 +73,14 @@ void AdaptDispatch::PrintString(const std::wstring_view string)
 
 void AdaptDispatch::_WriteToBuffer(const std::wstring_view string)
 {
-    const auto page = _pages.ActivePage();
+    auto page = _pages.ActivePage();
     auto& textBuffer = page.Buffer();
     auto& cursor = page.Cursor();
     auto cursorPosition = cursor.GetPosition();
     const auto wrapAtEOL = _api.GetSystemMode(ITerminalApi::Mode::AutoWrap);
     const auto& attributes = page.Attributes();
 
-    const auto [topMargin, bottomMargin] = _GetVerticalMargins(page, true);
+    auto [topMargin, bottomMargin] = _GetVerticalMargins(page, true);
     const auto [leftMargin, rightMargin] = _GetHorizontalMargins(page.Width());
 
     auto lineWidth = textBuffer.GetLineWidth(cursorPosition.y);
@@ -107,7 +107,14 @@ void AdaptDispatch::_WriteToBuffer(const std::wstring_view string)
             // different position from where the EOL was marked.
             if (delayedCursorPosition == cursorPosition)
             {
-                _DoLineFeed(page, true, true);
+                if (_DoLineFeed(page, true, true))
+                {
+                    // If the line feed caused the viewport to move down, we
+                    // need to adjust the page viewport and margins to match.
+                    page.MoveViewportDown();
+                    std::tie(topMargin, bottomMargin) = _GetVerticalMargins(page, true);
+                }
+
                 cursorPosition = cursor.GetPosition();
                 // We need to recalculate the width when moving to a new line.
                 lineWidth = textBuffer.GetLineWidth(cursorPosition.y);
@@ -2505,14 +2512,15 @@ bool AdaptDispatch::CarriageReturn()
 // - withReturn - Set to true if a carriage return should be performed as well.
 // - wrapForced - Set to true is the line feed was the result of the line wrapping.
 // Return Value:
-// - <none>
-void AdaptDispatch::_DoLineFeed(const Page& page, const bool withReturn, const bool wrapForced)
+// - True if the viewport panned down. False if not.
+bool AdaptDispatch::_DoLineFeed(const Page& page, const bool withReturn, const bool wrapForced)
 {
     auto& textBuffer = page.Buffer();
     const auto pageWidth = page.Width();
     const auto bufferHeight = page.BufferHeight();
     const auto [topMargin, bottomMargin] = _GetVerticalMargins(page, true);
     const auto [leftMargin, rightMargin] = _GetHorizontalMargins(pageWidth);
+    auto viewportMoved = false;
 
     auto& cursor = page.Cursor();
     const auto currentPosition = cursor.GetPosition();
@@ -2555,6 +2563,7 @@ void AdaptDispatch::_DoLineFeed(const Page& page, const bool withReturn, const b
         // the end of the buffer.
         _api.SetViewportPosition({ page.XPanOffset(), page.Top() + 1 });
         newPosition.y++;
+        viewportMoved = true;
 
         // And if the bottom margin didn't cover the full page, we copy the
         // lower part of the page down so it remains static. But for a full
@@ -2594,6 +2603,7 @@ void AdaptDispatch::_DoLineFeed(const Page& page, const bool withReturn, const b
 
     cursor.SetPosition(newPosition);
     _ApplyCursorMovementFlags(cursor);
+    return viewportMoved;
 }
 
 // Routine Description:

src/terminal/adapter/adaptDispatch.hpp
@@ -249,7 +249,7 @@ namespace Microsoft::Console::VirtualTerminal
                                              const VTInt rightMargin,
                                              const bool homeCursor = false);
 
-        void _DoLineFeed(const Page& page, const bool withReturn, const bool wrapForced);
+        bool _DoLineFeed(const Page& page, const bool withReturn, const bool wrapForced);
 
         void _DeviceStatusReport(const wchar_t* parameters) const;
         void _CursorPositionReport(const bool extendedReport);

src/host/ut_host/ScreenBufferTests.cpp
@@ -262,6 +262,7 @@ class ScreenBufferTests
     TEST_METHOD(CopyDoubleWidthRectangularArea);
 
     TEST_METHOD(DelayedWrapReset);
+    TEST_METHOD(MultilineWrap);
 
     TEST_METHOD(EraseColorMode);
 
@@ -8321,6 +8322,39 @@ void ScreenBufferTests::DelayedWrapReset()
         VERIFY_IS_FALSE(cursor.IsDelayedEOLWrap());
         VERIFY_ARE_EQUAL(expectedPos, actualPos);
     }
+}
+
+void ScreenBufferTests::MultilineWrap()
+{
+    auto& gci = ServiceLocator::LocateGlobals().getConsoleInformation();
+    auto& si = gci.GetActiveOutputBuffer().GetActiveBuffer();
+    auto& stateMachine = si.GetStateMachine();
+    const auto bufferAttr = si.GetTextBuffer().GetCurrentAttributes();
+    const auto width = si.GetTextBuffer().GetSize().Width();
+    const auto bottomRow = si.GetViewport().BottomInclusive();
+
+    // Starting on the bottom row.
+    si.GetTextBuffer().GetCursor().SetPosition({ 0, bottomRow });
+
+    // Write out enough text to wrap over four lines.
+    auto fourLines = std::wstring{};
+    fourLines += L"1";
+    fourLines += std::wstring(width - 1, L' ');
+    fourLines += L"2";
+    fourLines += std::wstring(width - 1, L' ');
+    fourLines += L"3";
+    fourLines += std::wstring(width - 1, L' ');
+    fourLines += L"4";
+    stateMachine.ProcessString(fourLines);
+
+    Log::Comment(L"Cursor should have moved down three rows");
+    VERIFY_ARE_EQUAL(bottomRow + 3, si.GetTextBuffer().GetCursor().GetPosition().y);
+
+    Log::Comment(L"Bottom four rows should have the content 1, 2, 3, and 4");
+    VERIFY_IS_TRUE(_ValidateLineContains(bottomRow + 0, L"1", bufferAttr));
+    VERIFY_IS_TRUE(_ValidateLineContains(bottomRow + 1, L"2", bufferAttr));
+    VERIFY_IS_TRUE(_ValidateLineContains(bottomRow + 2, L"3", bufferAttr));
+    VERIFY_IS_TRUE(_ValidateLineContains(bottomRow + 3, L"4", bufferAttr));
 }
 
 void ScreenBufferTests::EraseColorMode()