6291946de6a95324f8ce304f356bd46ce2935b7c
Nicolas De Carli <nidecarl@microsoft.com>
2024-06-11 17:44:54 -0700

    Format rapidhash.h

--- src/inc/til/rapidhash.h
+++ src/inc/til/rapidhash.h
@@ -36,7 +36,7 @@
 #ifdef __cplusplus
 #define RAPIDHASH_NOEXCEPT noexcept
 #else
-#define RAPIDHASH_NOEXCEPT 
+#define RAPIDHASH_NOEXCEPT
 #endif
 
 /*
@@ -45,10 +45,10 @@
 #include <stdint.h>
 #include <string.h>
 #if defined(_MSC_VER)
-  #include <intrin.h>
-  #if defined(_M_X64) && !defined(_M_ARM64EC)
-    #pragma intrinsic(_umul128)
-  #endif
+#include <intrin.h>
+#if defined(_M_X64) && !defined(_M_ARM64EC)
+#pragma intrinsic(_umul128)
+#endif
 #endif
 
 /*
@@ -58,9 +58,9 @@
  *  RAPIDHASH_PROTECTED: Extra protection against entropy loss.
  */
 #ifndef RAPIDHASH_PROTECTED
-  #define RAPIDHASH_FAST
+#define RAPIDHASH_FAST
 #elif defined(RAPIDHASH_FAST)
-  #error "cannot define RAPIDHASH_PROTECTED and RAPIDHASH_FAST simultaneously."
+#error "cannot define RAPIDHASH_PROTECTED and RAPIDHASH_FAST simultaneously."
 #endif
 
 /*
@@ -74,34 +74,34 @@
  *  These macros do not alter the output hash.
  */
 #ifndef RAPIDHASH_COMPACT
-  #define RAPIDHASH_UNROLLED
+#define RAPIDHASH_UNROLLED
 #elif defined(RAPIDHASH_UNROLLED)
-  #error "cannot define RAPIDHASH_COMPACT and RAPIDHASH_UNROLLED simultaneously."
+#error "cannot define RAPIDHASH_COMPACT and RAPIDHASH_UNROLLED simultaneously."
 #endif
 
 /*
  *  Likely and unlikely macros.
  */
 #if defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
-  #define _likely_(x)  __builtin_expect(x,1)
-  #define _unlikely_(x)  __builtin_expect(x,0)
+#define _likely_(x) __builtin_expect(x, 1)
+#define _unlikely_(x) __builtin_expect(x, 0)
 #else
-  #define _likely_(x) (x)
-  #define _unlikely_(x) (x)
+#define _likely_(x) (x)
+#define _unlikely_(x) (x)
 #endif
 
 /*
  *  Endianness macros.
  */
 #ifndef RAPIDHASH_LITTLE_ENDIAN
-  #if defined(_WIN32) || defined(__LITTLE_ENDIAN__) || (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
-    #define RAPIDHASH_LITTLE_ENDIAN
-  #elif defined(__BIG_ENDIAN__) || (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
-    #define RAPIDHASH_BIG_ENDIAN
-  #else
-    #warning "could not determine endianness! Falling back to little endian."
-    #define RAPIDHASH_LITTLE_ENDIAN
-  #endif
+#if defined(_WIN32) || defined(__LITTLE_ENDIAN__) || (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+#define RAPIDHASH_LITTLE_ENDIAN
+#elif defined(__BIG_ENDIAN__) || (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+#define RAPIDHASH_BIG_ENDIAN
+#else
+#warning "could not determine endianness! Falling back to little endian."
+#define RAPIDHASH_LITTLE_ENDIAN
+#endif
 #endif
 
 /*
@@ -112,7 +112,7 @@
 /*
  *  Default secret parameters.
  */
-static const uint64_t rapid_secret[3] = {0x2d358dccaa6c78a5ull, 0x8bb84b93962eacc9ull, 0x4b33a62ed433d4a3ull};
+static const uint64_t rapid_secret[3] = { 0x2d358dccaa6c78a5ull, 0x8bb84b93962eacc9ull, 0x4b33a62ed433d4a3ull };
 
 /*
  *  64*64 -> 128bit multiply function.
@@ -130,45 +130,55 @@ static const uint64_t rapid_secret[3] = {0x2d358dccaa6c78a5ull, 0x8bb84b93962eac
  *  Xors and overwrites A contents with C's low 64 bits.
  *  Xors and overwrites B contents with C's high 64 bits.
  */
-static inline void rapid_mum(uint64_t *A, uint64_t *B) RAPIDHASH_NOEXCEPT {
+static inline void rapid_mum(uint64_t* A, uint64_t* B) RAPIDHASH_NOEXCEPT
+{
 #if defined(__SIZEOF_INT128__)
-  __uint128_t r=*A; r*=*B; 
-  #ifdef RAPIDHASH_PROTECTED
-  *A^=(uint64_t)r; *B^=(uint64_t)(r>>64);
-  #else
-  *A=(uint64_t)r; *B=(uint64_t)(r>>64);
-  #endif
+    __uint128_t r = *A;
+    r *= *B;
+#ifdef RAPIDHASH_PROTECTED
+    *A ^= (uint64_t)r;
+    *B ^= (uint64_t)(r >> 64);
+#else
+    *A = (uint64_t)r;
+    *B = (uint64_t)(r >> 64);
+#endif
 #elif defined(_MSC_VER) && (defined(_WIN64) || defined(_M_HYBRID_CHPE_ARM64))
-  #if defined(_M_X64)
-    #ifdef RAPIDHASH_PROTECTED
-    uint64_t  a,  b;
-    a=_umul128(*A,*B,&b);
-    *A^=a;  *B^=b;
-    #else
-    *A=_umul128(*A,*B,B);
-    #endif
-  #else
-    #ifdef RAPIDHASH_PROTECTED
+#if defined(_M_X64)
+#ifdef RAPIDHASH_PROTECTED
+    uint64_t a, b;
+    a = _umul128(*A, *B, &b);
+    *A ^= a;
+    *B ^= b;
+#else
+    *A = _umul128(*A, *B, B);
+#endif
+#else
+#ifdef RAPIDHASH_PROTECTED
     uint64_t a, b;
     b = __umulh(*A, *B);
     a = *A * *B;
-    *A^=a;  *B^=b;
-    #else
+    *A ^= a;
+    *B ^= b;
+#else
     uint64_t c;
     c = __umulh(*A, *B);
     *A = *A * *B;
     *B = c;
-    #endif
-  #endif
+#endif
+#endif
 #else
-  uint64_t ha=*A>>32, hb=*B>>32, la=(uint32_t)*A, lb=(uint32_t)*B, hi, lo;
-  uint64_t rh=ha*hb, rm0=ha*lb, rm1=hb*la, rl=la*lb, t=rl+(rm0<<32), c=t<rl;
-  lo=t+(rm1<<32); c+=lo<t; hi=rh+(rm0>>32)+(rm1>>32)+c;
-  #ifdef RAPIDHASH_PROTECTED
-  *A^=lo;  *B^=hi;
-  #else
-  *A=lo;  *B=hi;
-  #endif
+    uint64_t ha = *A >> 32, hb = *B >> 32, la = (uint32_t)*A, lb = (uint32_t)*B, hi, lo;
+    uint64_t rh = ha * hb, rm0 = ha * lb, rm1 = hb * la, rl = la * lb, t = rl + (rm0 << 32), c = t < rl;
+    lo = t + (rm1 << 32);
+    c += lo < t;
+    hi = rh + (rm0 >> 32) + (rm1 >> 32) + c;
+#ifdef RAPIDHASH_PROTECTED
+    *A ^= lo;
+    *B ^= hi;
+#else
+    *A = lo;
+    *B = hi;
+#endif
 #endif
 }
 
@@ -181,28 +191,66 @@ static inline void rapid_mum(uint64_t *A, uint64_t *B) RAPIDHASH_NOEXCEPT {
  *  Calculates 128-bit C = A * B.
  *  Returns 64-bit xor between high and low 64 bits of C.
  */
-static inline uint64_t rapid_mix(uint64_t A, uint64_t B) RAPIDHASH_NOEXCEPT { rapid_mum(&A,&B); return A^B; }
+static inline uint64_t rapid_mix(uint64_t A, uint64_t B) RAPIDHASH_NOEXCEPT
+{
+    rapid_mum(&A, &B);
+    return A ^ B;
+}
 
 /*
  *  Read functions.
  */
 #ifdef RAPIDHASH_LITTLE_ENDIAN
-static inline uint64_t rapid_read64(const uint8_t *p) RAPIDHASH_NOEXCEPT { uint64_t v; memcpy(&v, p, 8); return v;}
-static inline uint64_t rapid_read32(const uint8_t *p) RAPIDHASH_NOEXCEPT { uint32_t v; memcpy(&v, p, 4); return v;}
-#elif defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
-static inline uint64_t rapid_read64(const uint8_t *p) { uint64_t v; memcpy(&v, p, 8); return __builtin_bswap64(v);}
-static inline uint64_t rapid_read32(const uint8_t *p) { uint32_t v; memcpy(&v, p, 4); return __builtin_bswap32(v);}
-#elif defined(_MSC_VER)
-static inline uint64_t rapid_read64(const uint8_t *p) { uint64_t v; memcpy(&v, p, 8); return _byteswap_uint64(v);}
-static inline uint64_t rapid_read32(const uint8_t *p) { uint32_t v; memcpy(&v, p, 4); return _byteswap_ulong(v);}
-#else
-static inline uint64_t rapid_read64(const uint8_t *p) {
-  uint64_t v; memcpy(&v, p, 8);
-  return (((v >> 56) & 0xff)| ((v >> 40) & 0xff00)| ((v >> 24) & 0xff0000)| ((v >>  8) & 0xff000000)| ((v <<  8) & 0xff00000000)| ((v << 24) & 0xff0000000000)| ((v << 40) & 0xff000000000000)| ((v << 56) & 0xff00000000000000));
+static inline uint64_t rapid_read64(const uint8_t* p) RAPIDHASH_NOEXCEPT
+{
+    uint64_t v;
+    memcpy(&v, p, 8);
+    return v;
 }
-static inline uint64_t rapid_read32(const uint8_t *p) {
-  uint32_t v; memcpy(&v, p, 4);
-  return (((v >> 24) & 0xff)| ((v >>  8) & 0xff00)| ((v <<  8) & 0xff0000)| ((v << 24) & 0xff000000));
+static inline uint64_t rapid_read32(const uint8_t* p) RAPIDHASH_NOEXCEPT
+{
+    uint32_t v;
+    memcpy(&v, p, 4);
+    return v;
+}
+#elif defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
+static inline uint64_t rapid_read64(const uint8_t* p)
+{
+    uint64_t v;
+    memcpy(&v, p, 8);
+    return __builtin_bswap64(v);
+}
+static inline uint64_t rapid_read32(const uint8_t* p)
+{
+    uint32_t v;
+    memcpy(&v, p, 4);
+    return __builtin_bswap32(v);
+}
+#elif defined(_MSC_VER)
+static inline uint64_t rapid_read64(const uint8_t* p)
+{
+    uint64_t v;
+    memcpy(&v, p, 8);
+    return _byteswap_uint64(v);
+}
+static inline uint64_t rapid_read32(const uint8_t* p)
+{
+    uint32_t v;
+    memcpy(&v, p, 4);
+    return _byteswap_ulong(v);
+}
+#else
+static inline uint64_t rapid_read64(const uint8_t* p)
+{
+    uint64_t v;
+    memcpy(&v, p, 8);
+    return (((v >> 56) & 0xff) | ((v >> 40) & 0xff00) | ((v >> 24) & 0xff0000) | ((v >> 8) & 0xff000000) | ((v << 8) & 0xff00000000) | ((v << 24) & 0xff0000000000) | ((v << 40) & 0xff000000000000) | ((v << 56) & 0xff00000000000000));
+}
+static inline uint64_t rapid_read32(const uint8_t* p)
+{
+    uint32_t v;
+    memcpy(&v, p, 4);
+    return (((v >> 24) & 0xff) | ((v >> 8) & 0xff00) | ((v << 8) & 0xff0000) | ((v << 24) & 0xff000000));
 }
 #endif
 
@@ -229,56 +277,79 @@ static inline uint64_t rapid_readSmall(const uint8_t* p, size_t k) RAPIDHASH_NOE
  *
  *  Returns a 64-bit hash.
  */
-static inline uint64_t rapidhash_internal(const void *key, size_t len, uint64_t seed, const uint64_t* secret) RAPIDHASH_NOEXCEPT {
-  const uint8_t *p=static_cast<const uint8_t *>(key); seed^=rapid_mix(seed^secret[0],secret[1])^len;  uint64_t  a,  b;
-  if(_likely_(len<=16)){
-    if(_likely_(len>=4)){ 
-      const uint8_t * plast = p + len - 4;
-      a = (rapid_read32(p) << 32) | rapid_read32(plast);
-      const uint64_t delta = ((len&24)>>(len>>3));
-      b = ((rapid_read32(p + delta) << 32) | rapid_read32(plast - delta)); }
-    else if(_likely_(len>0)){ a=rapid_readSmall(p,len); b=0;}
-    else a=b=0;
-  }
-  else{
-    size_t i=len; 
-    if(_unlikely_(i>48)){
-      uint64_t see1=seed, see2=seed;
+static inline uint64_t rapidhash_internal(const void* key, size_t len, uint64_t seed, const uint64_t* secret) RAPIDHASH_NOEXCEPT
+{
+    const uint8_t* p = static_cast<const uint8_t*>(key);
+    seed ^= rapid_mix(seed ^ secret[0], secret[1]) ^ len;
+    uint64_t a, b;
+    if (_likely_(len <= 16))
+    {
+        if (_likely_(len >= 4))
+        {
+            const uint8_t* plast = p + len - 4;
+            a = (rapid_read32(p) << 32) | rapid_read32(plast);
+            const uint64_t delta = ((len & 24) >> (len >> 3));
+            b = ((rapid_read32(p + delta) << 32) | rapid_read32(plast - delta));
+        }
+        else if (_likely_(len > 0))
+        {
+            a = rapid_readSmall(p, len);
+            b = 0;
+        }
+        else
+            a = b = 0;
+    }
+    else
+    {
+        size_t i = len;
+        if (_unlikely_(i > 48))
+        {
+            uint64_t see1 = seed, see2 = seed;
 #ifdef RAPIDHASH_UNROLLED
-      while(_likely_(i>=96)){
-        seed=rapid_mix(rapid_read64(p)^secret[0],rapid_read64(p+8)^seed);
-        see1=rapid_mix(rapid_read64(p+16)^secret[1],rapid_read64(p+24)^see1);
-        see2=rapid_mix(rapid_read64(p+32)^secret[2],rapid_read64(p+40)^see2);
-        seed=rapid_mix(rapid_read64(p+48)^secret[0],rapid_read64(p+56)^seed);
-        see1=rapid_mix(rapid_read64(p+64)^secret[1],rapid_read64(p+72)^see1);
-        see2=rapid_mix(rapid_read64(p+80)^secret[2],rapid_read64(p+88)^see2);
-        p+=96; i-=96;
-      }
-      if(_unlikely_(i>=48)){
-        seed=rapid_mix(rapid_read64(p)^secret[0],rapid_read64(p+8)^seed);
-        see1=rapid_mix(rapid_read64(p+16)^secret[1],rapid_read64(p+24)^see1);
-        see2=rapid_mix(rapid_read64(p+32)^secret[2],rapid_read64(p+40)^see2);
-        p+=48; i-=48;
-      }
+            while (_likely_(i >= 96))
+            {
+                seed = rapid_mix(rapid_read64(p) ^ secret[0], rapid_read64(p + 8) ^ seed);
+                see1 = rapid_mix(rapid_read64(p + 16) ^ secret[1], rapid_read64(p + 24) ^ see1);
+                see2 = rapid_mix(rapid_read64(p + 32) ^ secret[2], rapid_read64(p + 40) ^ see2);
+                seed = rapid_mix(rapid_read64(p + 48) ^ secret[0], rapid_read64(p + 56) ^ seed);
+                see1 = rapid_mix(rapid_read64(p + 64) ^ secret[1], rapid_read64(p + 72) ^ see1);
+                see2 = rapid_mix(rapid_read64(p + 80) ^ secret[2], rapid_read64(p + 88) ^ see2);
+                p += 96;
+                i -= 96;
+            }
+            if (_unlikely_(i >= 48))
+            {
+                seed = rapid_mix(rapid_read64(p) ^ secret[0], rapid_read64(p + 8) ^ seed);
+                see1 = rapid_mix(rapid_read64(p + 16) ^ secret[1], rapid_read64(p + 24) ^ see1);
+                see2 = rapid_mix(rapid_read64(p + 32) ^ secret[2], rapid_read64(p + 40) ^ see2);
+                p += 48;
+                i -= 48;
+            }
 #else
-      do {
-        seed=rapid_mix(rapid_read64(p)^secret[0],rapid_read64(p+8)^seed);
-        see1=rapid_mix(rapid_read64(p+16)^secret[1],rapid_read64(p+24)^see1);
-        see2=rapid_mix(rapid_read64(p+32)^secret[2],rapid_read64(p+40)^see2);
-        p+=48; i-=48;
-      } while (_likely_(i>=48));
+            do
+            {
+                seed = rapid_mix(rapid_read64(p) ^ secret[0], rapid_read64(p + 8) ^ seed);
+                see1 = rapid_mix(rapid_read64(p + 16) ^ secret[1], rapid_read64(p + 24) ^ see1);
+                see2 = rapid_mix(rapid_read64(p + 32) ^ secret[2], rapid_read64(p + 40) ^ see2);
+                p += 48;
+                i -= 48;
+            } while (_likely_(i >= 48));
 #endif
-      seed^=see1^see2;
+            seed ^= see1 ^ see2;
+        }
+        if (i > 16)
+        {
+            seed = rapid_mix(rapid_read64(p) ^ secret[2], rapid_read64(p + 8) ^ seed ^ secret[1]);
+            if (i > 32)
+                seed = rapid_mix(rapid_read64(p + 16) ^ secret[2], rapid_read64(p + 24) ^ seed);
+        }
+        a = rapid_read64(p + i - 16);
+        b = rapid_read64(p + i - 8);
     }
-    if(i>16){
-      seed=rapid_mix(rapid_read64(p)^secret[2],rapid_read64(p+8)^seed^secret[1]);
-      if(i>32)
-        seed=rapid_mix(rapid_read64(p+16)^secret[2],rapid_read64(p+24)^seed);
-    }
-    a=rapid_read64(p+i-16);  b=rapid_read64(p+i-8);
-  }
-  a^=secret[1]; b^=seed;  rapid_mum(&a,&b);
-  return  rapid_mix(a^secret[0]^len,b^secret[1]);
+    a ^= secret[1];
+    b ^= seed;
+    rapid_mum(&a, &b);
+    return rapid_mix(a ^ secret[0] ^ len, b ^ secret[1]);
 }
 
 /*
@@ -292,8 +363,9 @@ static inline uint64_t rapidhash_internal(const void *key, size_t len, uint64_t 
  *
  *  Returns a 64-bit hash.
  */
-static inline uint64_t rapidhash_withSeed(const void *key, size_t len, uint64_t seed) RAPIDHASH_NOEXCEPT {
-  return rapidhash_internal(key, len, seed, static_cast<const uint64_t *>(rapid_secret));
+static inline uint64_t rapidhash_withSeed(const void* key, size_t len, uint64_t seed) RAPIDHASH_NOEXCEPT
+{
+    return rapidhash_internal(key, len, seed, static_cast<const uint64_t*>(rapid_secret));
 }
 
 /*
@@ -306,6 +378,7 @@ static inline uint64_t rapidhash_withSeed(const void *key, size_t len, uint64_t 
  *
  *  Returns a 64-bit hash.
  */
-static inline uint64_t rapidhash(const void* key, size_t len) RAPIDHASH_NOEXCEPT {
-  return rapidhash_withSeed(key, len, RAPID_SEED);
+static inline uint64_t rapidhash(const void* key, size_t len) RAPIDHASH_NOEXCEPT
+{
+    return rapidhash_withSeed(key, len, RAPID_SEED);
 }
