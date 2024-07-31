microsoft/terminal

commit 5b44476048c3bf9d0838b468e4fc0bcade9596da

Replace IInputEvent with INPUT_RECORD (#15673)
`IInputEvent` makes adding Unicode support to `InputBuffer` more
difficult than necessary as the abstract class makes downcasting
as well as copying quite verbose. I found that using `INPUT_RECORD`s
directly leads to a significantly simplified implementation.

In addition, this commit fixes at least one bug: The previous approach
to detect the null key via `DoActiveModifierKeysMatch` didn't work.
As it compared the modifier keys as a bitset with `==` it failed to
match whenever the numpad key was set, which it usually is.

## Validation Steps Performed
* Unit and feature tests are ✅

src/interactivity/base/EventSynthesis.cpp
@@ -3,10 +3,6 @@
 
 #include "precomp.h"
 #include "../inc/EventSynthesis.hpp"
-#include "../../types/inc/convert.hpp"
-#include "../inc/VtApiRedirection.hpp"
-
-#pragma hdrstop
 
 // TODO: MSFT 14150722 - can these const values be generated at
 // runtime without breaking compatibility?
@@ -16,35 +12,29 @@ static constexpr WORD leftShiftScanCode = 0x2A;
 // Routine Description:
 // - naively determines the width of a UCS2 encoded wchar (with caveats noted above)
 #pragma warning(suppress : 4505) // this function will be deleted if numpad events are disabled
-static CodepointWidth GetQuickCharWidthLegacyForNumpadEventSynthesis(const wchar_t wch) noexcept
+static bool IsCharFullWidth(const wchar_t wch) noexcept
 {
-    if ((0x1100 <= wch && wch <= 0x115f) // From Unicode 9.0, Hangul Choseong is wide
-        || (0x2e80 <= wch && wch <= 0x303e) // From Unicode 9.0, this range is wide (assorted languages)
-        || (0x3041 <= wch && wch <= 0x3094) // Hiragana
-        || (0x30a1 <= wch && wch <= 0x30f6) // Katakana
-        || (0x3105 <= wch && wch <= 0x312c) // Bopomofo
-        || (0x3131 <= wch && wch <= 0x318e) // Hangul Elements
-        || (0x3190 <= wch && wch <= 0x3247) // From Unicode 9.0, this range is wide
-        || (0x3251 <= wch && wch <= 0x4dbf) // Unicode 9.0 CJK Unified Ideographs, Yi, Reserved, Han Ideograph (hexagrams from 4DC0..4DFF are ignored
-        || (0x4e00 <= wch && wch <= 0xa4c6) // Unicode 9.0 CJK Unified Ideographs, Yi, Reserved, Han Ideograph (hexagrams from 4DC0..4DFF are ignored
-        || (0xa960 <= wch && wch <= 0xa97c) // Wide Hangul Choseong
-        || (0xac00 <= wch && wch <= 0xd7a3) // Korean Hangul Syllables
-        || (0xf900 <= wch && wch <= 0xfaff) // From Unicode 9.0, this range is wide [CJK Compatibility Ideographs, Includes Han Compatibility Ideographs]
-        || (0xfe10 <= wch && wch <= 0xfe1f) // From Unicode 9.0, this range is wide [Presentation forms]
-        || (0xfe30 <= wch && wch <= 0xfe6b) // From Unicode 9.0, this range is wide [Presentation forms]
-        || (0xff01 <= wch && wch <= 0xff5e) // Fullwidth ASCII variants
-        || (0xffe0 <= wch && wch <= 0xffe6)) // Fullwidth symbol variants
-    {
-        return CodepointWidth::Wide;
-    }
-
-    return CodepointWidth::Narrow;
+    return (0x1100 <= wch && wch <= 0x115f) || // From Unicode 9.0, Hangul Choseong is wide
+           (0x2e80 <= wch && wch <= 0x303e) || // From Unicode 9.0, this range is wide (assorted languages)
+           (0x3041 <= wch && wch <= 0x3094) || // Hiragana
+           (0x30a1 <= wch && wch <= 0x30f6) || // Katakana
+           (0x3105 <= wch && wch <= 0x312c) || // Bopomofo
+           (0x3131 <= wch && wch <= 0x318e) || // Hangul Elements
+           (0x3190 <= wch && wch <= 0x3247) || // From Unicode 9.0, this range is wide
+           (0x3251 <= wch && wch <= 0x4dbf) || // Unicode 9.0 CJK Unified Ideographs, Yi, Reserved, Han Ideograph (hexagrams from 4DC0..4DFF are ignored
+           (0x4e00 <= wch && wch <= 0xa4c6) || // Unicode 9.0 CJK Unified Ideographs, Yi, Reserved, Han Ideograph (hexagrams from 4DC0..4DFF are ignored
+           (0xa960 <= wch && wch <= 0xa97c) || // Wide Hangul Choseong
+           (0xac00 <= wch && wch <= 0xd7a3) || // Korean Hangul Syllables
+           (0xf900 <= wch && wch <= 0xfaff) || // From Unicode 9.0, this range is wide [CJK Compatibility Ideographs, Includes Han Compatibility Ideographs]
+           (0xfe10 <= wch && wch <= 0xfe1f) || // From Unicode 9.0, this range is wide [Presentation forms]
+           (0xfe30 <= wch && wch <= 0xfe6b) || // From Unicode 9.0, this range is wide [Presentation forms]
+           (0xff01 <= wch && wch <= 0xff5e) || // Fullwidth ASCII variants
+           (0xffe0 <= wch && wch <= 0xffe6); // Fullwidth symbol variants
 }
 
-std::deque<std::unique_ptr<KeyEvent>> Microsoft::Console::Interactivity::CharToKeyEvents(const wchar_t wch,
-                                                                                         const unsigned int codepage)
+void Microsoft::Console::Interactivity::CharToKeyEvents(const wchar_t wch, const unsigned int codepage, InputEventQueue& keyEvents)
 {
-    const short invalidKey = -1;
+    static constexpr short invalidKey = -1;
     auto keyState = OneCoreSafeVkKeyScanW(wch);
 
     if (keyState == invalidKey)
@@ -57,18 +47,19 @@ std::deque<std::unique_ptr<KeyEvent>> Microsoft::Console::Interactivity::CharToK
             WORD CharType = 0;
             GetStringTypeW(CT_CTYPE3, &wch, 1, &CharType);
 
-            if (!(WI_IsFlagSet(CharType, C3_ALPHA) || GetQuickCharWidthLegacyForNumpadEventSynthesis(wch) == CodepointWidth::Wide))
+            if (WI_IsFlagClear(CharType, C3_ALPHA) && !IsCharFullWidth(wch))
             {
                 // It wasn't alphanumeric or determined to be wide by the old algorithm
                 // if VkKeyScanW fails (char is not in kbd layout), we must
                 // emulate the key being input through the numpad
-                return SynthesizeNumpadEvents(wch, codepage);
+                SynthesizeNumpadEvents(wch, codepage, keyEvents);
+                return;
             }
         }
         keyState = 0; // SynthesizeKeyboardEvents would rather get 0 than -1
     }
 
-    return SynthesizeKeyboardEvents(wch, keyState);
+    SynthesizeKeyboardEvents(wch, keyState, keyEvents);
 }
 
 // Routine Description:
@@ -80,80 +71,45 @@ std::deque<std::unique_ptr<KeyEvent>> Microsoft::Console::Interactivity::CharToK
 // - deque of KeyEvents that represent the wchar_t being typed
 // Note:
 // - will throw exception on error
-std::deque<std::unique_ptr<KeyEvent>> Microsoft::Console::Interactivity::SynthesizeKeyboardEvents(const wchar_t wch, const short keyState)
+void Microsoft::Console::Interactivity::SynthesizeKeyboardEvents(const wchar_t wch, const short keyState, InputEventQueue& keyEvents)
 {
-    const auto modifierState = HIBYTE(keyState);
-
-    auto altGrSet = false;
-    auto shiftSet = false;
-    std::deque<std::unique_ptr<KeyEvent>> keyEvents;
-
-    // add modifier key event if necessary
-    if (WI_AreAllFlagsSet(modifierState, VkKeyScanModState::CtrlAndAltPressed))
-    {
-        altGrSet = true;
-        keyEvents.push_back(std::make_unique<KeyEvent>(true,
-                                                       1ui16,
-                                                       static_cast<WORD>(VK_MENU),
-                                                       altScanCode,
-                                                       UNICODE_NULL,
-                                                       (ENHANCED_KEY | LEFT_CTRL_PRESSED | RIGHT_ALT_PRESSED)));
-    }
-    else if (WI_IsFlagSet(modifierState, VkKeyScanModState::ShiftPressed))
-    {
-        shiftSet = true;
-        keyEvents.push_back(std::make_unique<KeyEvent>(true,
-                                                       1ui16,
-                                                       static_cast<WORD>(VK_SHIFT),
-                                                       leftShiftScanCode,
-                                                       UNICODE_NULL,
-                                                       SHIFT_PRESSED));
-    }
-
     const auto vk = LOBYTE(keyState);
-    const auto virtualScanCode = gsl::narrow<WORD>(OneCoreSafeMapVirtualKeyW(vk, MAPVK_VK_TO_VSC));
-    KeyEvent keyEvent{ true, 1, LOBYTE(keyState), virtualScanCode, wch, 0 };
+    const auto sc = gsl::narrow<WORD>(OneCoreSafeMapVirtualKeyW(vk, MAPVK_VK_TO_VSC));
+    // The caller provides us with the result of VkKeyScanW() in keyState.
+    // The magic constants below are the expected (documented) return values from VkKeyScanW().
+    const auto modifierState = HIBYTE(keyState);
+    const auto shiftSet = WI_IsFlagSet(modifierState, 1);
+    const auto ctrlSet = WI_IsFlagSet(modifierState, 2);
+    const auto altSet = WI_IsFlagSet(modifierState, 4);
+    const auto altGrSet = WI_AreAllFlagsSet(modifierState, 4 | 2);
 
-    // add modifier flags if necessary
-    if (WI_IsFlagSet(modifierState, VkKeyScanModState::ShiftPressed))
-    {
-        keyEvent.ActivateModifierKey(ModifierKeyState::Shift);
-    }
-    if (WI_IsFlagSet(modifierState, VkKeyScanModState::CtrlPressed))
-    {
-        keyEvent.ActivateModifierKey(ModifierKeyState::LeftCtrl);
-    }
-    if (WI_AreAllFlagsSet(modifierState, VkKeyScanModState::CtrlAndAltPressed))
-    {
-        keyEvent.ActivateModifierKey(ModifierKeyState::RightAlt);
-    }
-
-    // add key event down and up
-    keyEvents.push_back(std::make_unique<KeyEvent>(keyEvent));
-    keyEvent.SetKeyDown(false);
-    keyEvents.push_back(std::make_unique<KeyEvent>(keyEvent));
-
-    // add modifier key up event
     if (altGrSet)
     {
-        keyEvents.push_back(std::make_unique<KeyEvent>(false,
-                                                       1ui16,
-                                                       static_cast<WORD>(VK_MENU),
-                                                       altScanCode,
-                                                       UNICODE_NULL,
-                                                       ENHANCED_KEY));
+        keyEvents.push_back(SynthesizeKeyEvent(true, 1, VK_MENU, altScanCode, 0, ENHANCED_KEY | LEFT_CTRL_PRESSED | RIGHT_ALT_PRESSED));
     }
     else if (shiftSet)
     {
-        keyEvents.push_back(std::make_unique<KeyEvent>(false,
-                                                       1ui16,
-                                                       static_cast<WORD>(VK_SHIFT),
-                                                       leftShiftScanCode,
-                                                       UNICODE_NULL,
-                                                       0));
+        keyEvents.push_back(SynthesizeKeyEvent(true, 1, VK_SHIFT, leftShiftScanCode, 0, SHIFT_PRESSED));
     }
 
-    return keyEvents;
+    auto keyEvent = SynthesizeKeyEvent(true, 1, vk, sc, wch, 0);
+    WI_SetFlagIf(keyEvent.Event.KeyEvent.dwControlKeyState, SHIFT_PRESSED, shiftSet);
+    WI_SetFlagIf(keyEvent.Event.KeyEvent.dwControlKeyState, LEFT_CTRL_PRESSED, ctrlSet);
+    WI_SetFlagIf(keyEvent.Event.KeyEvent.dwControlKeyState, RIGHT_ALT_PRESSED, altSet);
+
+    keyEvents.push_back(keyEvent);
+    keyEvent.Event.KeyEvent.bKeyDown = FALSE;
+    keyEvents.push_back(keyEvent);
+
+    // handle yucky alt-gr keys
+    if (altGrSet)
+    {
+        keyEvents.push_back(SynthesizeKeyEvent(false, 1, VK_MENU, altScanCode, 0, ENHANCED_KEY));
+    }
+    else if (shiftSet)
+    {
+        keyEvents.push_back(SynthesizeKeyEvent(false, 1, VK_SHIFT, leftShiftScanCode, 0, 0));
+    }
 }
 
 // Routine Description:
@@ -166,64 +122,35 @@ std::deque<std::unique_ptr<KeyEvent>> Microsoft::Console::Interactivity::Synthes
 // alt + numpad
 // Note:
 // - will throw exception on error
-std::deque<std::unique_ptr<KeyEvent>> Microsoft::Console::Interactivity::SynthesizeNumpadEvents(const wchar_t wch, const unsigned int codepage)
+void Microsoft::Console::Interactivity::SynthesizeNumpadEvents(const wchar_t wch, const unsigned int codepage, InputEventQueue& keyEvents)
 {
-    std::deque<std::unique_ptr<KeyEvent>> keyEvents;
+    char converted = 0;
+    const auto result = WideCharToMultiByte(codepage, 0, &wch, 1, &converted, 1, nullptr, nullptr);
 
-    //alt keydown
-    keyEvents.push_back(std::make_unique<KeyEvent>(true,
-                                                   1ui16,
-                                                   static_cast<WORD>(VK_MENU),
-                                                   altScanCode,
-                                                   UNICODE_NULL,
-                                                   LEFT_ALT_PRESSED));
-
-    std::wstring wstr{ wch };
-    const auto convertedChars = ConvertToA(codepage, wstr);
-    if (convertedChars.size() == 1)
+    if (result == 1)
     {
+        // alt keydown
+        keyEvents.push_back(SynthesizeKeyEvent(true, 1, VK_MENU, altScanCode, 0, LEFT_ALT_PRESSED));
+
         // It is OK if the char is "signed -1", we want to interpret that as "unsigned 255" for the
         // "integer to character" conversion below with ::to_string, thus the static_cast.
         // Prime example is nonbreaking space U+00A0 will convert to OEM by codepage 437 to 0xFF which is -1 signed.
         // But it is absolutely valid as 0xFF or 255 unsigned as the correct CP437 character.
         // We need to treat it as unsigned because we're going to pretend it was a keypad entry
         // and you don't enter negative numbers on the keypad.
-        const auto uch = static_cast<unsigned char>(convertedChars.at(0));
+        const auto charString = std::to_string(static_cast<unsigned char>(converted));
 
-        // unsigned char values are in the range [0, 255] so we need to be
-        // able to store up to 4 chars from the conversion (including the end of string char)
-        auto charString = std::to_string(uch);
-
-        for (auto& ch : std::string_view(charString))
+        for (const auto& ch : charString)
         {
-            if (ch == 0)
-            {
-                break;
-            }
-            const WORD virtualKey = ch - '0' + VK_NUMPAD0;
-            const auto virtualScanCode = gsl::narrow<WORD>(OneCoreSafeMapVirtualKeyW(virtualKey, MAPVK_VK_TO_VSC));
-
-            keyEvents.push_back(std::make_unique<KeyEvent>(true,
-                                                           1ui16,
-                                                           virtualKey,
-                                                           virtualScanCode,
-                                                           UNICODE_NULL,
-                                                           LEFT_ALT_PRESSED));
-            keyEvents.push_back(std::make_unique<KeyEvent>(false,
-                                                           1ui16,
-                                                           virtualKey,
-                                                           virtualScanCode,
-                                                           UNICODE_NULL,
-                                                           LEFT_ALT_PRESSED));
+            const WORD vk = ch - '0' + VK_NUMPAD0;
+            const auto sc = gsl::narrow<WORD>(OneCoreSafeMapVirtualKeyW(vk, MAPVK_VK_TO_VSC));
+            auto keyEvent = SynthesizeKeyEvent(true, 1, vk, sc, 0, LEFT_ALT_PRESSED);
+            keyEvents.push_back(keyEvent);
+            keyEvent.Event.KeyEvent.bKeyDown = FALSE;
+            keyEvents.push_back(keyEvent);
         }
-    }
 
-    // alt keyup
-    keyEvents.push_back(std::make_unique<KeyEvent>(false,
-                                                   1ui16,
-                                                   static_cast<WORD>(VK_MENU),
-                                                   altScanCode,
-                                                   wch,
-                                                   0));
-    return keyEvents;
+        // alt keyup
+        keyEvents.push_back(SynthesizeKeyEvent(false, 1, VK_MENU, altScanCode, wch, 0));
+    }
 }

src/cascadia/TerminalControl/ControlCore.cpp
@@ -511,7 +511,7 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         // modifier key. We'll wait for a real keystroke to dismiss the
         // GH #7395 - don't update selection when taking PrintScreen
         // selection.
-        return HasSelection() && !KeyEvent::IsModifierKey(vkey) && vkey != VK_SNAPSHOT;
+        return HasSelection() && ::Microsoft::Terminal::Core::Terminal::IsInputKey(vkey);
     }
 
     bool ControlCore::TryMarkModeKeybinding(const WORD vkey,

src/cascadia/TerminalCore/Terminal.cpp
@@ -661,7 +661,7 @@ bool Terminal::SendKeyEvent(const WORD vkey,
     // modifier key. We'll wait for a real keystroke to snap to the bottom.
     // GH#6481 - Additionally, make sure the key was actually pressed. This
     // check will make sure we behave the same as before GH#6309
-    if (!KeyEvent::IsModifierKey(vkey) && keyDown)
+    if (IsInputKey(vkey) && keyDown)
     {
         TrySnapOnInput();
     }
@@ -714,8 +714,8 @@ bool Terminal::SendKeyEvent(const WORD vkey,
         return false;
     }
 
-    const KeyEvent keyEv{ keyDown, 1, vkey, sc, ch, states.Value() };
-    return _handleTerminalInputResult(_terminalInput.HandleKey(&keyEv));
+    const auto keyEv = SynthesizeKeyEvent(keyDown, 1, vkey, sc, ch, states.Value());
+    return _handleTerminalInputResult(_terminalInput.HandleKey(keyEv));
 }
 
 // Method Description:
@@ -791,8 +791,8 @@ bool Terminal::SendCharEvent(const wchar_t ch, const WORD scanCode, const Contro
         MarkOutputStart();
     }
 
-    const KeyEvent keyDown{ true, 1, vkey, scanCode, ch, states.Value() };
-    return _handleTerminalInputResult(_terminalInput.HandleKey(&keyDown));
+    const auto keyDown = SynthesizeKeyEvent(true, 1, vkey, scanCode, ch, states.Value());
+    return _handleTerminalInputResult(_terminalInput.HandleKey(keyDown));
 }
 
 // Method Description:

src/cascadia/TerminalCore/Terminal.hpp
@@ -60,6 +60,22 @@ class Microsoft::Terminal::Core::Terminal final :
     using RenderSettings = Microsoft::Console::Render::RenderSettings;
 
 public:
+    static constexpr bool IsInputKey(WORD vkey)
+    {
+        return vkey != VK_CONTROL &&
+               vkey != VK_LCONTROL &&
+               vkey != VK_RCONTROL &&
+               vkey != VK_MENU &&
+               vkey != VK_LMENU &&
+               vkey != VK_RMENU &&
+               vkey != VK_SHIFT &&
+               vkey != VK_LSHIFT &&
+               vkey != VK_RSHIFT &&
+               vkey != VK_LWIN &&
+               vkey != VK_RWIN &&
+               vkey != VK_SNAPSHOT;
+    }
+
     Terminal();
 
     void Create(til::size viewportSize,

src/host/cmdline.h
@@ -87,4 +87,6 @@ void RedrawCommandLine(COOKED_READ_DATA& cookedReadData);
 bool IsWordDelim(const wchar_t wch);
 bool IsWordDelim(const std::wstring_view charData);
 
+bool IsValidStringBuffer(_In_ bool Unicode, _In_reads_bytes_(Size) PVOID Buffer, _In_ ULONG Size, _In_ ULONG Count, ...);
+
 void SetCurrentCommandLine(COOKED_READ_DATA& cookedReadData, _In_ CommandHistory::Index Index);

src/host/conimeinfo.cpp
@@ -462,18 +462,13 @@ void ConsoleImeInfo::_InsertConvertedString(const std::wstring_view text)
     }
 
     const auto dwControlKeyState = GetControlKeyState(0);
-    std::deque<std::unique_ptr<IInputEvent>> inEvents;
-    KeyEvent keyEvent{ TRUE, // keydown
-                       1, // repeatCount
-                       0, // virtualKeyCode
-                       0, // virtualScanCode
-                       0, // charData
-                       dwControlKeyState }; // activeModifierKeys
+    InputEventQueue inEvents;
+    auto keyEvent = SynthesizeKeyEvent(true, 1, 0, 0, 0, dwControlKeyState);
 
     for (const auto& ch : text)
     {
-        keyEvent.SetCharData(ch);
-        inEvents.push_back(std::make_unique<KeyEvent>(keyEvent));
+        keyEvent.Event.KeyEvent.uChar.UnicodeChar = ch;
+        inEvents.push_back(keyEvent);
     }
 
     gci.pInputBuffer->Write(inEvents);

src/host/directio.cpp
@@ -101,7 +101,7 @@ using Microsoft::Console::Interactivity::ServiceLocator;
 // Return Value:
 // - HRESULT indicating success or failure
 [[nodiscard]] static HRESULT _WriteConsoleInputWImplHelper(InputBuffer& context,
-                                                           std::deque<std::unique_ptr<IInputEvent>>& events,
+                                                           const std::span<const INPUT_RECORD>& events,
                                                            size_t& written,
                                                            const bool append) noexcept
 {
@@ -151,7 +151,7 @@ try
     auto Unlock = wil::scope_exit([&] { UnlockConsole(); });
 
     const auto& gci = ServiceLocator::LocateGlobals().getConsoleInformation();
-    til::small_vector<INPUT_RECORD, 16> events;
+    InputEventQueue events;
 
     auto it = buffer.begin();
     const auto end = buffer.end();
@@ -160,7 +160,7 @@ try
     // the next call to WriteConsoleInputAImpl to join it with the now available trailing DBCS.
     if (context.IsWritePartialByteSequenceAvailable())
     {
-        auto lead = context.FetchWritePartialByteSequence(false)->ToInputRecord();
+        auto lead = context.FetchWritePartialByteSequence();
         const auto& trail = *it;
 
         if (trail.EventType == KEY_EVENT)
@@ -200,7 +200,7 @@ try
             if (it == end)
             {
                 // Missing trailing DBCS -> Store the lead for the next call to WriteConsoleInputAImpl.
-                context.StoreWritePartialByteSequence(IInputEvent::Create(lead));
+                context.StoreWritePartialByteSequence(lead);
                 break;
             }
 
@@ -225,8 +225,7 @@ try
         }
     }
 
-    auto result = IInputEvent::Create(std::span{ events.data(), events.size() });
-    return _WriteConsoleInputWImplHelper(context, result, written, append);
+    return _WriteConsoleInputWImplHelper(context, events, written, append);
 }
 CATCH_RETURN();
 
@@ -252,9 +251,7 @@ CATCH_RETURN();
 
     try
     {
-        auto events = IInputEvent::Create(buffer);
-
-        return _WriteConsoleInputWImplHelper(context, events, written, append);
+        return _WriteConsoleInputWImplHelper(context, buffer, written, append);
     }
     CATCH_RETURN();
 }

src/host/ft_host/CJK_DbcsTests.cpp
@@ -1919,11 +1919,11 @@ void DbcsTests::TestMultibyteInputCoalescing()
 
     DWORD count;
     {
-        const auto record = KeyEvent{ true, 1, 123, 456, 0x82, 789 }.ToInputRecord();
+        const auto record = SynthesizeKeyEvent(true, 1, 123, 456, 0x82, 789);
         VERIFY_WIN32_BOOL_SUCCEEDED(WriteConsoleInputA(in, &record, 1, &count));
     }
     {
-        const auto record = KeyEvent{ true, 1, 234, 567, 0xA2, 890 }.ToInputRecord();
+        const auto record = SynthesizeKeyEvent(true, 1, 234, 567, 0xA2, 890);
         VERIFY_WIN32_BOOL_SUCCEEDED(WriteConsoleInputA(in, &record, 1, &count));
     }
 
@@ -1933,7 +1933,7 @@ void DbcsTests::TestMultibyteInputCoalescing()
     VERIFY_WIN32_BOOL_SUCCEEDED(ReadConsoleInputW(in, &actual[0], 2, &count));
     VERIFY_ARE_EQUAL(1u, count);
 
-    const auto expected = KeyEvent{ true, 1, 123, 456, L'い', 789 }.ToInputRecord();
+    const auto expected = SynthesizeKeyEvent(true, 1, 123, 456, L'い', 789);
     VERIFY_ARE_EQUAL(expected, actual[0]);
 }
 

commit 9bafa52c656573aff556aa099aba45c4ffcaa777

Fix alt-numpad events (#17635)
This fixes a regression caused by 5b44476 which accidentally moved
the two pushes into the if condition.

Closes MSFT:52463679

## Validation Steps Performed
* Enable `Feature_UseNumpadEventsForClipboardInput`
* `cmd`
* `chcp 54936`
* Paste narrow Unicode characters like ①
* It works ✅

src/interactivity/base/EventSynthesis.cpp
@@ -127,11 +127,11 @@ void Microsoft::Console::Interactivity::SynthesizeNumpadEvents(const wchar_t wch
     char converted = 0;
     const auto result = WideCharToMultiByte(codepage, 0, &wch, 1, &converted, 1, nullptr, nullptr);
 
+    // alt keydown
+    keyEvents.push_back(SynthesizeKeyEvent(true, 1, VK_MENU, altScanCode, 0, LEFT_ALT_PRESSED));
+
     if (result == 1)
     {
-        // alt keydown
-        keyEvents.push_back(SynthesizeKeyEvent(true, 1, VK_MENU, altScanCode, 0, LEFT_ALT_PRESSED));
-
         // It is OK if the char is "signed -1", we want to interpret that as "unsigned 255" for the
         // "integer to character" conversion below with ::to_string, thus the static_cast.
         // Prime example is nonbreaking space U+00A0 will convert to OEM by codepage 437 to 0xFF which is -1 signed.
@@ -149,8 +149,8 @@ void Microsoft::Console::Interactivity::SynthesizeNumpadEvents(const wchar_t wch
             keyEvent.Event.KeyEvent.bKeyDown = FALSE;
             keyEvents.push_back(keyEvent);
         }
-
-        // alt keyup
-        keyEvents.push_back(SynthesizeKeyEvent(false, 1, VK_MENU, altScanCode, wch, 0));
     }
+
+    // alt keyup
+    keyEvents.push_back(SynthesizeKeyEvent(false, 1, VK_MENU, altScanCode, wch, 0));
 }