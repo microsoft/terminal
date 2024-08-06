4040e4c654749aa291e4fa2a892de0c5188f0eae
Dustin L. Howett <duhowett@microsoft.com>
2024-08-06 16:50:48 -0500

    atlas,d2d: overdraw background bitmap by one cell on all sides (#17674)
    BackendD2D will now draw one extra cell on all sides when rendering the
    background, filled with the expected background color, starting at (-1,
    -1) to ensure that cell backgrounds do not bleed over the edges of the
    viewport where the is swapchain but no content.
    
    Fixes #17672
    
    (cherry picked from commit 9007fc2894f877175c63592ab1f7b3300a23abf8)
    Service-Card-Id: 93494324
    Service-Version: 1.21

--- src/renderer/atlas/BackendD2D.h
+++ src/renderer/atlas/BackendD2D.h
@@ -64,6 +64,7 @@ namespace Microsoft::Console::Render::Atlas
         til::generation_t _generation;
         til::generation_t _fontGeneration;
         til::generation_t _cursorGeneration;
+        til::generation_t _miscGeneration;
         u16x2 _viewportCellCount{};
 
 #if ATLAS_DEBUG_SHOW_DIRTY
