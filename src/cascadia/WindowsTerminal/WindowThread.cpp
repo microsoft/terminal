f8af9228db6437bbfebf39fdaf6b160ade39ac2c
Carlos Zamora <carlos.zamora@microsoft.com>
2024-08-07 11:47:38 -0700

    less tags

--- src/cascadia/WindowsTerminal/WindowThread.cpp
+++ src/cascadia/WindowsTerminal/WindowThread.cpp
@@ -198,9 +198,7 @@ int WindowThread::_messagePump()
             TraceLoggingWrite(
                 g_hWindowsTerminalProvider,
                 "InteractiveSession",
-                TraceLoggingDescription("Emitted the first time a key is pressed"),
                 TraceLoggingLevel(WINEVENT_LEVEL_VERBOSE),
-                TraceLoggingKeyword(TIL_KEYWORD_TRACE),
                 TelemetryPrivacyDataTag(PDT_ProductAndServiceUsage));
             _loggedInteraction = true;
         }
