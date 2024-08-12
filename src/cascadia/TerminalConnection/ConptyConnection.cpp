3096fe926dfbbc2192b01060fe0acada26b0d967
Leonard Hecker <lhecker@microsoft.com>
2024-08-12 17:19:23 +0200

    Align the OSS ConPTY API with Windows 11 24H2

--- src/cascadia/TerminalConnection/ConptyConnection.cpp
+++ src/cascadia/TerminalConnection/ConptyConnection.cpp
@@ -755,7 +755,7 @@ namespace winrt::Microsoft::Terminal::TerminalConnection::implementation
 
     void ConptyConnection::closePseudoConsoleAsync(HPCON hPC) noexcept
     {
-        ::ConptyClosePseudoConsoleTimeout(hPC, 0);
+        ::ConptyClosePseudoConsole(hPC);
     }
 
     HRESULT ConptyConnection::NewHandoff(HANDLE* in, HANDLE* out, HANDLE signal, HANDLE reference, HANDLE server, HANDLE client, const TERMINAL_STARTUP_INFO* startupInfo) noexcept
