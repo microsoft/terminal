4c018efd641dd71502e4019078bb6325c68b7f9a
Dustin L. Howett <duhowett@microsoft.com>
2024-08-16 17:50:19 -0500

    chore: Update to TAEF 10.93.240607003 (#16595)

--- tools/OpenConsole.psm1
+++ tools/OpenConsole.psm1
@@ -198,7 +198,7 @@ function Invoke-OpenConsoleTests()
         $OpenConsolePlatform = 'Win32'
     }
     $OpenConsolePath = "$root\bin\$OpenConsolePlatform\$Configuration\OpenConsole.exe"
-    $TaefExePath = "$root\packages\Microsoft.Taef.10.60.210621002\build\Binaries\$Platform\te.exe"
+    $TaefExePath = "$root\packages\Microsoft.Taef.10.93.240607003\build\Binaries\$Platform\te.exe"
     $BinDir = "$root\bin\$OpenConsolePlatform\$Configuration"
 
     [xml]$TestConfig = Get-Content "$root\tools\tests.xml"
