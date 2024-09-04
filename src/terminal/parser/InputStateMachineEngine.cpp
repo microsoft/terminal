be99f930942f48210d01b41a7577777b5519f372
Leonard Hecker <lhecker@microsoft.com>
2024-09-04 15:47:01 +0200

    Avoid dropping Esc characters from VT responses (#17833)
    `GetChar` checks if the vkey is VK_ESCAPE. `CharToKeyEvents` however
    tries really hard to figure out the vkeys of all characters.
    To avoid these issues all we need to do is to simply use the existing
    `WriteString` function we already use for all other VT responses.
    If it's good for conhost responses, it's good for ConPTY responses.
    
    Additionally, this removes another `IsVtInputEnabled` which was
    redundant with `WriteString` which worked exactly the same internally.
    
    Closes #17813
    Closes #17851
    Probably also related to #17823
    
    * Wrote a small app to send and receive a DA1 request. It works ✅
    * WSL already worked to begin with (and still works now) ✅
    * No double-encoding of mouse input events ✅
    
    (cherry picked from commit 7b50f12a780ec0f8a55e7f04ad12d65b451f7667)
    Service-Card-Id: PVTI_lADOAF3p4s4AmhmQzgSa4RU PVTI_lADOAF3p4s4AmhmQzgStRA0
    Service-Version: 1.22

--- src/terminal/parser/InputStateMachineEngine.cpp
+++ src/terminal/parser/InputStateMachineEngine.cpp
@@ -293,27 +293,10 @@ bool InputStateMachineEngine::ActionPrintString(const std::wstring_view string)
 // - true iff we successfully dispatched the sequence.
 bool InputStateMachineEngine::ActionPassThroughString(const std::wstring_view string)
 {
-    if (string.empty())
+    if (!string.empty())
     {
-        return true;
+        _pDispatch->WriteStringRaw(string);
     }
-
-    if (_pDispatch->IsVtInputEnabled())
-    {
-        // Synthesize string into key events that we'll write to the buffer
-        // similar to TerminalInput::_SendInputSequence
-        InputEventQueue inputEvents;
-        for (const auto& wch : string)
-        {
-            inputEvents.push_back(SynthesizeKeyEvent(true, 1, 0, 0, wch, 0));
-        }
-        _pDispatch->WriteInput(inputEvents);
-    }
-    else
-    {
-        _pDispatch->WriteString(string);
-    }
-
     return true;
 }
 
