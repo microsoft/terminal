2cef0c1dc60a7446ee74af92a3a6417b76b2cac6
Carlos Zamora <carlos.zamora@microsoft.com>
2024-09-20 11:10:27 -0700

    [broken] drag and drop don't work

--- src/cascadia/TerminalSettingsEditor/NewTabMenu.cpp
+++ src/cascadia/TerminalSettingsEditor/NewTabMenu.cpp
@@ -39,11 +39,60 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         _ViewModel = e.Parameter().as<Editor::NewTabMenuViewModel>();
     }
 
-	void NewTabMenu::FolderNameTextBox_TextChanged(const IInspectable& sender, const Controls::TextChangedEventArgs& /*e*/)
-	{
+    void NewTabMenu::FolderNameTextBox_TextChanged(const IInspectable& sender, const Controls::TextChangedEventArgs& /*e*/)
+    {
         const auto isTextEmpty = sender.as<Controls::TextBox>().Text().empty();
         AddFolderButton().IsEnabled(!isTextEmpty);
-	}
+    }
+
+    void NewTabMenu::TreeView_DragItemsStarting(const winrt::Microsoft::UI::Xaml::Controls::TreeView& /*sender*/, const winrt::Microsoft::UI::Xaml::Controls::TreeViewDragItemsStartingEventArgs& e)
+    {
+        _draggedEntry = e.Items().GetAt(0).as<Editor::NewTabMenuEntryViewModel>();
+    }
+
+    void NewTabMenu::TreeView_DragOver(const IInspectable& /*sender*/, const winrt::Windows::UI::Xaml::DragEventArgs& e)
+    {
+        e.AcceptedOperation(Windows::ApplicationModel::DataTransfer::DataPackageOperation::Move);
+        OutputDebugString(L"TreeView_DragOver\n");
+    }
+
+    void NewTabMenu::TreeView_Drop(const IInspectable& /*sender*/, const winrt::Windows::UI::Xaml::DragEventArgs& /*e*/)
+    {
+        OutputDebugString(L"TreeView_Drop\n");
+    }
+
+    void NewTabMenu::TreeView_DragItemsCompleted(const winrt::Microsoft::UI::Xaml::Controls::TreeView& /*sender*/, const winrt::Microsoft::UI::Xaml::Controls::TreeViewDragItemsCompletedEventArgs& /*e*/)
+    {
+        _draggedEntry = nullptr;
+    }
+
+    void NewTabMenu::TreeViewItem_DragOver(const IInspectable& /*sender*/, const DragEventArgs& e)
+    {
+        e.AcceptedOperation(Windows::ApplicationModel::DataTransfer::DataPackageOperation::Move);
+    }
+
+    void NewTabMenu::TreeViewItem_Drop(const IInspectable& sender, const DragEventArgs& /*e*/)
+    {
+        auto element = sender.as<FrameworkElement>();
+        auto entry = element.DataContext().as<Editor::NewTabMenuEntryViewModel>();
+        if (entry.Type() == NewTabMenuEntryType::Folder)
+        {
+            // add to the current folder
+            auto folderEntry = entry.as<Editor::FolderEntryViewModel>();
+            folderEntry.Entries().Append(_draggedEntry);
+        }
+        else
+        {
+            // create a parent folder and add both entries to it
+            // TODO CARLOS: localize
+            auto folderEntry = winrt::make<FolderEntryViewModel>(FolderEntry{ L"New Folder" });
+            folderEntry.Entries().Append(entry);
+            folderEntry.Entries().Append(_draggedEntry);
+
+            // TODO CARLOS: this is wrong, we should be placing the folder in the same place as before, but we're testing this stuff out
+            _ViewModel.Entries().Append(folderEntry);
+        }
+    }
 
     DataTemplate NewTabMenuEntryTemplateSelector::SelectTemplateCore(const IInspectable& item, const DependencyObject& /*container*/)
     {
