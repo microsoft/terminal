ef25c63e4ec0e4f51f5bfca3f5de0d47377a60fb
AgentK <dmitryk@3vium.com>
2024-10-30 12:34:20 +0200

    update win x64 release build script

--- tools/BuildWinx64Release.ps1
+++ tools/BuildWinx64Release.ps1
@@ -1,4 +1,4 @@
 Import-Module .\tools\OpenConsole.psm1
 Set-MsBuildDevEnvironment
-Invoke-OpenConsoleBuild /p:Configuration=Release /p:Platform=x64 /p:AdditionalOptions="/Zm2000"
-Invoke-OpenConsoleBuild /p:Configuration=Release /p:Platform=x64 /p:AdditionalOptions="/Zm2000" /t:Terminal\CascadiaPackage /m
+Invoke-OpenConsoleBuild /p:Configuration=Release /p:Platform=x64 /p:AdditionalOptions="/Zm1000" /maxcpucount:10
+Invoke-OpenConsoleBuild /p:Configuration=Release /p:Platform=x64 /p:AdditionalOptions="/Zm1000" /t:Terminal\CascadiaPackage /m
