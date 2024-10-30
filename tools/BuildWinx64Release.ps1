ab01742944fba3d7bd6ab88605829f9d0660b20e
AgentK <dmitryk@3vium.com>
2024-10-30 14:41:18 +0200

    removed newly added file

--- tools/BuildWinx64Release.ps1
+++
@@ -1,4 +0,0 @@
-Import-Module .\tools\OpenConsole.psm1
-Set-MsBuildDevEnvironment
-Invoke-OpenConsoleBuild /p:Configuration=Release /p:Platform=x64 /p:AdditionalOptions="/Zm1000" /maxcpucount:10
-Invoke-OpenConsoleBuild /p:Configuration=Release /p:Platform=x64 /p:AdditionalOptions="/Zm1000" /t:Terminal\CascadiaPackage /m
