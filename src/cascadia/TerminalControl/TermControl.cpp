microsoft/terminal

commit 8bbf00e0540ee67bbb4ba30cda66647a0bf8daa4

Remove Monarch/Peasant & Make UI single-threaded (#18215)
As before, a minor refactor:
* I started off by removing the Monarch/Peasant with the goal of moving
  it into and deduplicating its functionality with `WindowEmperor`.
* Since I needed a replacement for the Monarch (= ensures that there's
  a single instance), I wrote single-instance code with a NT mutex
  and by yeeting data across processes with `WM_COPYDATA`.
* This resulted in severe threading issues, because it now started up
  way faster. The more I tried to solve them the deeper I had to dig,
  because you can't just put a mutex around `CascadiaSettings`.
  I then tried to seeif WinUI can run multiple windows on a single
  thread and, as it turns out, it can.
  So, I removed the multi- from the window threading.
* At this point I had dig about 1 mile deep and brought no ladder.
  So, to finish it up, I had to clean up the entire eventing system
  around `WindowEmperor`, cleaned up all the coroutines,
  and cleaned up all the callbacks.

Closes #16183
Closes #16221
Closes #16487
Closes #16532
Closes #16733
Closes #16755
Closes #17015
Closes #17360
Closes #17420
Closes #17457
Closes #17799
Closes #17976
Closes #18057
Closes #18084
Closes #18169
Closes #18176
Closes #18191

## Validation Steps Performed
* It does not crash ✅
* New/close tab ✅
* New/close window ✅
* Move tabs between windows ✅
* Split tab into new window ✅
* Persist windows on exit / restore startup ✅

src/cascadia/TerminalControl/TermControl.cpp
@@ -5,6 +5,7 @@
 #include "TermControl.h"
 
 #include <LibraryResources.h>
+#include <inputpaneinterop.h>
 
 #include "TermControlAutomationPeer.h"
 #include "../../renderer/atlas/AtlasEngine.h"
@@ -46,13 +47,110 @@ constexpr std::wstring_view StateCollapsed{ L"Collapsed" };
 DEFINE_ENUM_FLAG_OPERATORS(winrt::Microsoft::Terminal::Control::CopyFormat);
 DEFINE_ENUM_FLAG_OPERATORS(winrt::Microsoft::Terminal::Control::MouseButtonState);
 
+// WinUI 3's UIElement.ProtectedCursor property allows someone to set the cursor on a per-element basis.
+// This would allow us to hide the cursor when the TermControl has input focus and someone starts typing.
+// Unfortunately, no equivalent exists for WinUI 2 so we fake it with the CoreWindow.
+// There are 3 downsides:
+// * SetPointerCapture() is global state and may interfere with other components.
+// * You can't start dragging the cursor (for text selection) while it's still hidden.
+// * The CoreWindow covers the union of all window rectangles, so the cursor is hidden even if it's outside
+//   the current foreground window, but still on top of another Terminal window in the background.
+static void hideCursorUntilMoved()
+{
+    static CoreCursor previousCursor{ nullptr };
+    static const auto shouldVanish = []() {
+        BOOL shouldVanish = TRUE;
+        SystemParametersInfoW(SPI_GETMOUSEVANISH, 0, &shouldVanish, 0);
+        if (!shouldVanish)
+        {
+            return false;
+        }
+
+        const auto window = CoreWindow::GetForCurrentThread();
+        static constexpr auto releaseCapture = [](CoreWindow window, PointerEventArgs) {
+            if (previousCursor)
+            {
+                window.ReleasePointerCapture();
+            }
+        };
+        static constexpr auto restoreCursor = [](CoreWindow window, PointerEventArgs) {
+            if (previousCursor)
+            {
+                window.PointerCursor(previousCursor);
+                previousCursor = nullptr;
+            }
+        };
+
+        winrt::Windows::Foundation::TypedEventHandler<CoreWindow, PointerEventArgs> releaseCaptureHandler{ releaseCapture };
+        std::ignore = window.PointerMoved(releaseCaptureHandler);
+        std::ignore = window.PointerPressed(releaseCaptureHandler);
+        std::ignore = window.PointerReleased(releaseCaptureHandler);
+        std::ignore = window.PointerWheelChanged(releaseCaptureHandler);
+        std::ignore = window.PointerCaptureLost(restoreCursor);
+        return true;
+    }();
+
+    if (shouldVanish && !previousCursor)
+    {
+        try
+        {
+            const auto window = CoreWindow::GetForCurrentThread();
+
+            previousCursor = window.PointerCursor();
+            if (!previousCursor)
+            {
+                return;
+            }
+
+            window.PointerCursor(nullptr);
+            window.SetPointerCapture();
+        }
+        catch (...)
+        {
+            // Swallow the 0x80070057 "Failed to get pointer information." exception that randomly occurs.
+            // Curiously, it doesn't happen during the PointerCursor() but during the SetPointerCapture() call (thanks, WinUI).
+        }
+    }
+}
+
+// InputPane::GetForCurrentView() does not reliably work for XAML islands,
+// as it assumes that there's a 1:1 relationship between windows and threads.
+//
+// During testing, I found that the input pane shows up when touching into the terminal even if
+// TryShow is never called. This surprised me, but I figured it's worth trying it without this.
+static void setInputPaneVisibility(HWND hwnd, bool visible)
+{
+    static const auto inputPaneInterop = []() {
+        return winrt::try_get_activation_factory<InputPane, IInputPaneInterop>();
+    }();
+
+    if (!inputPaneInterop)
+    {
+        return;
+    }
+
+    winrt::com_ptr<IInputPane2> inputPane;
+    if (FAILED(inputPaneInterop->GetForWindow(hwnd, winrt::guid_of<IInputPane2>(), inputPane.put_void())))
+    {
+        return;
+    }
+
+    bool result;
+    if (visible)
+    {
+        std::ignore = inputPane->TryShow(&result);
+    }
+    else
+    {
+        std::ignore = inputPane->TryHide(&result);
+    }
+}
+
 static Microsoft::Console::TSF::Handle& GetTSFHandle()
 {
-    // https://en.cppreference.com/w/cpp/language/storage_duration
-    // > Variables declared at block scope with the specifier static or thread_local
-    // > [...] are initialized the first time control passes through their declaration
-    // --> Lazy, per-(window-)thread initialization of the TSF handle
-    thread_local auto s_tsf = ::Microsoft::Console::TSF::Handle::Create();
+    // NOTE: If we ever go back to 1 thread per 1 window,
+    // you need to swap the `static` with a `thread_local`.
+    static auto s_tsf = ::Microsoft::Console::TSF::Handle::Create();
     return s_tsf;
 }
 
@@ -156,22 +254,10 @@ namespace winrt::Microsoft::Terminal::Control::implementation
 
     RECT TsfDataProvider::GetViewport()
     {
-        const auto scaleFactor = static_cast<float>(DisplayInformation::GetForCurrentView().RawPixelsPerViewPixel());
-        const auto globalOrigin = CoreWindow::GetForCurrentThread().Bounds();
-        const auto localOrigin = _termControl->TransformToVisual(nullptr).TransformPoint({});
-        const auto size = _termControl->ActualSize();
-
-        const auto left = globalOrigin.X + localOrigin.X;
-        const auto top = globalOrigin.Y + localOrigin.Y;
-        const auto right = left + size.x;
-        const auto bottom = top + size.y;
-
-        return {
-            lroundf(left * scaleFactor),
-            lroundf(top * scaleFactor),
-            lroundf(right * scaleFactor),
-            lroundf(bottom * scaleFactor),
-        };
+        const auto hwnd = reinterpret_cast<HWND>(_termControl->OwningHwnd());
+        RECT clientRect;
+        GetWindowRect(hwnd, &clientRect);
+        return clientRect;
     }
 
     RECT TsfDataProvider::GetCursorPosition()
@@ -182,16 +268,19 @@ namespace winrt::Microsoft::Terminal::Control::implementation
             return {};
         }
 
+        const auto hwnd = reinterpret_cast<HWND>(_termControl->OwningHwnd());
+        RECT clientRect;
+        GetWindowRect(hwnd, &clientRect);
+
         const auto scaleFactor = static_cast<float>(DisplayInformation::GetForCurrentView().RawPixelsPerViewPixel());
-        const auto globalOrigin = CoreWindow::GetForCurrentThread().Bounds();
         const auto localOrigin = _termControl->TransformToVisual(nullptr).TransformPoint({});
         const auto padding = _termControl->GetPadding();
         const auto cursorPosition = core->CursorPosition();
         const auto fontSize = core->FontSize();
 
         // fontSize is not in DIPs, so we need to first multiply by scaleFactor and then do the rest.
-        const auto left = (globalOrigin.X + localOrigin.X + static_cast<float>(padding.Left)) * scaleFactor + cursorPosition.X * fontSize.Width;
-        const auto top = (globalOrigin.Y + localOrigin.Y + static_cast<float>(padding.Top)) * scaleFactor + cursorPosition.Y * fontSize.Height;
+        const auto left = clientRect.left + (localOrigin.X + static_cast<float>(padding.Left)) * scaleFactor + cursorPosition.X * fontSize.Width;
+        const auto top = clientRect.top + (localOrigin.Y + static_cast<float>(padding.Top)) * scaleFactor + cursorPosition.Y * fontSize.Height;
         const auto right = left + fontSize.Width;
         const auto bottom = top + fontSize.Height;
 
@@ -768,40 +857,22 @@ namespace winrt::Microsoft::Terminal::Control::implementation
     // - Given Settings having been updated, applies the settings to the current terminal.
     // Return Value:
     // - <none>
-    safe_void_coroutine TermControl::UpdateControlSettings(IControlSettings settings,
-                                                           IControlAppearance unfocusedAppearance)
+    void TermControl::UpdateControlSettings(IControlSettings settings, IControlAppearance unfocusedAppearance)
     {
-        auto weakThis{ get_weak() };
+        _core.UpdateSettings(settings, unfocusedAppearance);
 
-        // Dispatch a call to the UI thread to apply the new settings to the
-        // terminal.
-        co_await wil::resume_foreground(Dispatcher());
+        _UpdateSettingsFromUIThread();
 
-        if (auto strongThis{ weakThis.get() })
-        {
-            _core.UpdateSettings(settings, unfocusedAppearance);
-
-            _UpdateSettingsFromUIThread();
-
-            _UpdateAppearanceFromUIThread(_focused ? _core.FocusedAppearance() : _core.UnfocusedAppearance());
-        }
+        _UpdateAppearanceFromUIThread(_focused ? _core.FocusedAppearance() : _core.UnfocusedAppearance());
     }
 
     // Method Description:
     // - Dispatches a call to the UI thread and updates the appearance
     // Arguments:
     // - newAppearance: the new appearance to set
-    safe_void_coroutine TermControl::UpdateAppearance(IControlAppearance newAppearance)
+    void TermControl::UpdateAppearance(IControlAppearance newAppearance)
     {
-        auto weakThis{ get_weak() };
-
-        // Dispatch a call to the UI thread
-        co_await wil::resume_foreground(Dispatcher());
-
-        if (auto strongThis{ weakThis.get() })
-        {
-            _UpdateAppearanceFromUIThread(newAppearance);
-        }
+        _UpdateAppearanceFromUIThread(newAppearance);
     }
 
     // Method Description:
@@ -1486,7 +1557,7 @@ namespace winrt::Microsoft::Terminal::Control::implementation
             return;
         }
 
-        HidePointerCursor.raise(*this, nullptr);
+        hideCursorUntilMoved();
 
         const auto ch = e.Character();
         const auto keyStatus = e.KeyStatus();
@@ -1895,8 +1966,6 @@ namespace winrt::Microsoft::Terminal::Control::implementation
                                        const winrt::Microsoft::Terminal::Core::ControlKeyStates modifiers,
                                        const bool keyDown)
     {
-        const auto window = CoreWindow::GetForCurrentThread();
-
         // If the terminal translated the key, mark the event as handled.
         // This will prevent the system from trying to get the character out
         // of it and sending us a CharacterReceived event.
@@ -1931,6 +2000,16 @@ namespace winrt::Microsoft::Terminal::Control::implementation
     void TermControl::_TappedHandler(const IInspectable& /*sender*/, const TappedRoutedEventArgs& e)
     {
         Focus(FocusState::Pointer);
+
+        if (e.PointerDeviceType() == Windows::Devices::Input::PointerDeviceType::Touch)
+        {
+            // Normally TSF would be responsible for showing the touch keyboard, but it's buggy for us:
+            // If you have focus on a TermControl and type on your physical keyboard then touching
+            // the TermControl will not show the touch keyboard ever again unless you focus another app.
+            // Why that happens is unclear, but it can be fixed by us showing it manually.
+            setInputPaneVisibility(reinterpret_cast<HWND>(OwningHwnd()), true);
+        }
+
         e.Handled(true);
     }
 
@@ -1954,12 +2033,6 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         const auto ptr = args.Pointer();
         const auto point = args.GetCurrentPoint(*this);
         const auto type = ptr.PointerDeviceType();
-
-        // We also TryShow in GotFocusHandler, but this call is specifically
-        // for the case where the Terminal is in focus but the user closed the
-        // on-screen keyboard. This lets the user simply tap on the terminal
-        // again to bring it up.
-        InputPane::GetForCurrentView().TryShow();
 
         if (!_focused)
         {
@@ -2182,10 +2255,8 @@ namespace winrt::Microsoft::Terminal::Control::implementation
     // - <unused>
     // Return Value:
     // - <none>
-    safe_void_coroutine TermControl::_coreTransparencyChanged(IInspectable /*sender*/,
-                                                              Control::TransparencyChangedEventArgs /*args*/)
+    void TermControl::_coreTransparencyChanged(IInspectable /*sender*/, Control::TransparencyChangedEventArgs /*args*/)
     {
-        co_await wil::resume_foreground(Dispatcher());
         try
         {
             _changeBackgroundOpacity();
@@ -2362,8 +2433,6 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         }
 
         _focused = true;
-
-        InputPane::GetForCurrentView().TryShow();
 
         // GH#5421: Enable the UiaEngine before checking for the SearchBox
         // That way, new selections are notified to automation clients.
@@ -3995,14 +4064,12 @@ namespace winrt::Microsoft::Terminal::Control::implementation
     void TermControl::_contextMenuHandler(IInspectable /*sender*/,
                                           Control::ContextMenuRequestedEventArgs args)
     {
-        // Position the menu where the pointer is. This was the best way I found how.
-        const auto absolutePointerPos = CoreWindow::GetForCurrentThread().PointerPosition();
-        const auto absoluteWindowOrigin = CoreWindow::GetForCurrentThread().Bounds();
-        // Get the offset (margin + tabs, etc..) of the control within the window
-        const auto controlOrigin = TransformToVisual(nullptr).TransformPoint({});
+        const auto inverseScale = 1.0f / static_cast<float>(XamlRoot().RasterizationScale());
+        const auto padding = GetPadding();
+        const auto pos = args.Position();
         _showContextMenuAt({
-            absolutePointerPos.X - absoluteWindowOrigin.X - controlOrigin.X,
-            absolutePointerPos.Y - absoluteWindowOrigin.Y - controlOrigin.Y,
+            pos.X * inverseScale + static_cast<float>(padding.Left),
+            pos.Y * inverseScale + static_cast<float>(padding.Top),
         });
     }
 

src/cascadia/WindowsTerminal/WindowEmperor.h
@@ -16,73 +16,77 @@ Abstract:
 --*/
 
 #pragma once
-#include "pch.h"
 
-#include "WindowThread.h"
+class AppHost;
 
-class WindowEmperor : public std::enable_shared_from_this<WindowEmperor>
+class WindowEmperor
 {
 public:
-    WindowEmperor() noexcept;
-    void WaitForWindows();
+    enum UserMessages : UINT
+    {
+        WM_CLOSE_TERMINAL_WINDOW = WM_USER,
+        WM_MESSAGE_BOX_CLOSED,
+        WM_IDENTIFY_ALL_WINDOWS,
+        WM_NOTIFY_FROM_NOTIFICATION_AREA,
+    };
 
+    HWND GetMainWindow() const noexcept;
+    AppHost* GetWindowById(uint64_t id) const noexcept;
+    AppHost* GetWindowByName(std::wstring_view name) const noexcept;
+    void CreateNewWindow(winrt::TerminalApp::WindowRequestedArgs args);
     void HandleCommandlineArgs(int nCmdShow);
 
 private:
-    void _createNewWindowThread(const winrt::Microsoft::Terminal::Remoting::WindowRequestedArgs& args);
-
-    [[nodiscard]] static LRESULT __stdcall _wndProc(HWND const window, UINT const message, WPARAM const wparam, LPARAM const lparam) noexcept;
-    LRESULT _messageHandler(UINT const message, WPARAM const wParam, LPARAM const lParam) noexcept;
-    wil::unique_hwnd _window;
-
-    winrt::TerminalApp::App _app;
-    winrt::Windows::System::DispatcherQueue _dispatcher{ nullptr };
-    winrt::Microsoft::Terminal::Remoting::WindowManager _manager;
-
-    til::shared_mutex<std::vector<std::shared_ptr<WindowThread>>> _windows;
-    std::atomic<uint32_t> _windowThreadInstances;
-
-    til::shared_mutex<std::vector<std::shared_ptr<WindowThread>>> _oldThreads;
-
-    winrt::event_token _WindowCreatedToken;
-    winrt::event_token _WindowClosedToken;
-
-    std::vector<winrt::Microsoft::Terminal::Settings::Model::GlobalSummonArgs> _hotkeys;
-
-    std::unique_ptr<NotificationIcon> _notificationIcon;
-
-    bool _requiresPersistenceCleanupOnExit = false;
-    bool _quitting{ false };
-
-    void _windowStartedHandlerPostXAML(const std::shared_ptr<WindowThread>& sender);
-    void _removeWindow(uint64_t senderID);
-    void _decrementWindowCount();
-
-    void _becomeMonarch();
-    void _numberOfWindowsChanged(const winrt::Windows::Foundation::IInspectable&, const winrt::Windows::Foundation::IInspectable&);
-
-    safe_void_coroutine _windowIsQuakeWindowChanged(winrt::Windows::Foundation::IInspectable sender, winrt::Windows::Foundation::IInspectable args);
-    safe_void_coroutine _windowRequestUpdateSettings();
-
-    void _createMessageWindow();
-
-    void _hotkeyPressed(const long hotkeyIndex);
-    bool _registerHotKey(const int index, const winrt::Microsoft::Terminal::Control::KeyChord& hotkey) noexcept;
-    void _unregisterHotKey(const int index) noexcept;
-    safe_void_coroutine _setupGlobalHotkeys();
-
-    safe_void_coroutine _close();
-    void _finalizeSessionPersistence() const;
-
-    void _createNotificationIcon();
-    void _destroyNotificationIcon();
-    void _checkWindowsForNotificationIcon();
-    void _showNotificationIconRequested();
-    void _hideNotificationIconRequested();
-
-    struct Revokers
+    struct SummonWindowSelectionArgs
     {
-        winrt::Microsoft::Terminal::Remoting::WindowManager::WindowCreated_revoker WindowCreated;
-        winrt::Microsoft::Terminal::Remoting::WindowManager::WindowClosed_revoker WindowClosed;
-    } _revokers{};
+        uint64_t WindowID = 0;
+        std::wstring_view WindowName;
+        bool OnCurrentDesktop = false;
+        winrt::TerminalApp::SummonWindowBehavior SummonBehavior;
+    };
+
+    [[nodiscard]] static LRESULT __stdcall _wndProc(HWND window, UINT message, WPARAM wparam, LPARAM lparam) noexcept;
+
+    AppHost* _mostRecentWindow() const noexcept;
+    bool _summonWindow(const SummonWindowSelectionArgs& args) const;
+    void _summonAllWindows() const;
+    void _dispatchCommandline(winrt::TerminalApp::CommandlineArgs args);
+    safe_void_coroutine _dispatchCommandlineCurrentDesktop(winrt::TerminalApp::CommandlineArgs args);
+    LRESULT _messageHandler(HWND window, UINT message, WPARAM wParam, LPARAM lParam) noexcept;
+    void _createMessageWindow(const wchar_t* className);
+    void _postQuitMessageIfNeeded() const;
+    safe_void_coroutine _showMessageBox(winrt::hstring message, bool error);
+    void _notificationAreaMenuRequested(WPARAM wParam);
+    void _notificationAreaMenuClicked(WPARAM wParam, LPARAM lParam) const;
+    void _hotkeyPressed(long hotkeyIndex);
+    void _registerHotKey(int index, const winrt::Microsoft::Terminal::Control::KeyChord& hotkey) noexcept;
+    void _unregisterHotKey(int index) noexcept;
+    void _setupGlobalHotkeys();
+    void _finalizeSessionPersistence() const;
+    void _checkWindowsForNotificationIcon();
+
+    wil::unique_hwnd _window;
+    winrt::TerminalApp::App _app{ nullptr };
+    std::vector<std::shared_ptr<::AppHost>> _windows;
+    std::vector<winrt::Microsoft::Terminal::Settings::Model::GlobalSummonArgs> _hotkeys;
+    NOTIFYICONDATA _notificationIcon{};
+    UINT WM_TASKBARCREATED = 0;
+    HMENU _currentWindowMenu = nullptr;
+    bool _notificationIconShown = false;
+    bool _forcePersistence = false;
+    bool _needsPersistenceCleanup = false;
+    std::optional<bool> _currentSystemThemeIsDark;
+    int32_t _messageBoxCount = 0;
+
+#ifdef NDEBUG
+    static constexpr void _assertIsMainThread() noexcept
+    {
+    }
+#else
+    void _assertIsMainThread() const noexcept
+    {
+        assert(_mainThreadId == GetCurrentThreadId());
+    }
+    DWORD _mainThreadId = GetCurrentThreadId();
+#endif
 };

commit 6ac55f679204f679412c42cd3c6ea25d4d4024ab

Fix multiple regressions since #18215

src/cascadia/TerminalControl/TermControl.cpp
@@ -57,7 +57,7 @@ DEFINE_ENUM_FLAG_OPERATORS(winrt::Microsoft::Terminal::Control::MouseButtonState
 //   the current foreground window, but still on top of another Terminal window in the background.
 static void hideCursorUntilMoved()
 {
-    static CoreCursor previousCursor{ nullptr };
+    static bool cursorIsHidden;
     static const auto shouldVanish = []() {
         BOOL shouldVanish = TRUE;
         SystemParametersInfoW(SPI_GETMOUSEVANISH, 0, &shouldVanish, 0);
@@ -68,16 +68,16 @@ static void hideCursorUntilMoved()
 
         const auto window = CoreWindow::GetForCurrentThread();
         static constexpr auto releaseCapture = [](CoreWindow window, PointerEventArgs) {
-            if (previousCursor)
+            if (cursorIsHidden)
             {
                 window.ReleasePointerCapture();
             }
         };
         static constexpr auto restoreCursor = [](CoreWindow window, PointerEventArgs) {
-            if (previousCursor)
+            if (cursorIsHidden)
             {
-                window.PointerCursor(previousCursor);
-                previousCursor = nullptr;
+                cursorIsHidden = false;
+                window.PointerCursor(CoreCursor{ CoreCursorType::Arrow, 0 });
             }
         };
 
@@ -90,20 +90,14 @@ static void hideCursorUntilMoved()
         return true;
     }();
 
-    if (shouldVanish && !previousCursor)
+    if (shouldVanish && !cursorIsHidden)
     {
         try
         {
             const auto window = CoreWindow::GetForCurrentThread();
-
-            previousCursor = window.PointerCursor();
-            if (!previousCursor)
-            {
-                return;
-            }
-
             window.PointerCursor(nullptr);
             window.SetPointerCapture();
+            cursorIsHidden = true;
         }
         catch (...)
         {

src/cascadia/WindowsTerminal/AppHost.cpp
@@ -815,7 +815,7 @@ void AppHost::_WindowActivated(bool activated)
 {
     _windowLogic.WindowActivated(activated);
 
-    if (activated && _isWindowInitialized != WindowInitializedState::NotInitialized)
+    if (activated)
     {
         QueryPerformanceCounter(&_lastActivatedTime);
         _virtualDesktopId = {};

src/cascadia/WindowsTerminal/WindowEmperor.cpp
@@ -413,13 +413,8 @@ void WindowEmperor::HandleCommandlineArgs(int nCmdShow)
                 // It almost seems like there's a pattern here...
                 (msg.wParam == VK_SPACE && msg.message == WM_SYSKEYDOWN))
             {
-                if (const auto w = _mostRecentWindow())
-                {
-                    const auto vkey = gsl::narrow_cast<uint32_t>(msg.wParam);
-                    const auto scanCode = gsl::narrow_cast<uint8_t>(msg.lParam >> 16);
-                    w->OnDirectKeyEvent(vkey, scanCode, keyDown);
-                    continue;
-                }
+                _dispatchSpecialKey(msg);
+                continue;
             }
         }
 
@@ -440,6 +435,40 @@ void WindowEmperor::HandleCommandlineArgs(int nCmdShow)
     // std::exit(), etc., cannot be used here, because those use ExitProcess for unpackaged applications.
     TerminateProcess(GetCurrentProcess(), gsl::narrow_cast<UINT>(0));
     __assume(false);
+}
+
+void WindowEmperor::_dispatchSpecialKey(MSG& msg) const
+{
+    const auto hwnd = msg.hwnd;
+    AppHost* window = nullptr;
+
+    // Just in case someone has targed a specific HWND,
+    // we'll try to dispatch it to the corresponding class.
+    // Usually this will not find anything because under WinUI the hidden CoreInput
+    // window is responsible for all input handling (for whatever reason).
+    for (const auto& h : _windows)
+    {
+        const auto w = h->GetWindow();
+        if (w && w->GetHandle() == hwnd)
+        {
+            window = h.get();
+            break;
+        }
+    }
+
+    if (!window)
+    {
+        window = _mostRecentWindow();
+        if (!window)
+        {
+            return;
+        }
+    }
+
+    const auto vkey = gsl::narrow_cast<uint32_t>(msg.wParam);
+    const auto scanCode = gsl::narrow_cast<uint8_t>(msg.lParam >> 16);
+    const bool keyDown = msg.message & 1;
+    window->OnDirectKeyEvent(vkey, scanCode, keyDown);
 }
 
 void WindowEmperor::_dispatchCommandline(winrt::TerminalApp::CommandlineArgs args)
@@ -826,14 +855,10 @@ LRESULT WindowEmperor::_messageHandler(HWND window, UINT const message, WPARAM c
         case WM_QUERYENDSESSION:
             // For WM_QUERYENDSESSION and WM_ENDSESSION, refer to:
             // https://docs.microsoft.com/en-us/windows/win32/rstmgr/guidelines-for-applications
-            if (lParam == ENDSESSION_CLOSEAPP)
-            {
-                // ENDSESSION_CLOSEAPP: The application is using a file that must be replaced,
-                // the system is being serviced, or system resources are exhausted.
-                RegisterApplicationRestart(nullptr, RESTART_NO_CRASH | RESTART_NO_HANG);
-                return TRUE;
-            }
-            return FALSE;
+            // ENDSESSION_CLOSEAPP: The application is using a file that must be replaced,
+            // the system is being serviced, or system resources are exhausted.
+            RegisterApplicationRestart(nullptr, RESTART_NO_CRASH | RESTART_NO_HANG);
+            return TRUE;
         case WM_ENDSESSION:
             _forcePersistence = true;
             PostQuitMessage(0);

src/cascadia/WindowsTerminal/WindowEmperor.h
@@ -50,6 +50,7 @@ private:
     AppHost* _mostRecentWindow() const noexcept;
     bool _summonWindow(const SummonWindowSelectionArgs& args) const;
     void _summonAllWindows() const;
+    void _dispatchSpecialKey(MSG& msg) const;
     void _dispatchCommandline(winrt::TerminalApp::CommandlineArgs args);
     safe_void_coroutine _dispatchCommandlineCurrentDesktop(winrt::TerminalApp::CommandlineArgs args);
     LRESULT _messageHandler(HWND window, UINT message, WPARAM wParam, LPARAM lParam) noexcept;