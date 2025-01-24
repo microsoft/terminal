08f9afe31547c109536eb260171fe3b83835d1ff
Dustin L. Howett <duhowett@microsoft.com>
2025-01-24 17:17:41 -0600

    Unify padding parsers, make SUI operate on struct `Thickness` (#18300)
    The code in #17909 was not completely right for padding values with
    fewer than four components, and it was doing some fragile string math
    (that is: if you wanted to change the third element in the padding it
    would parse out the whole thing, edit the third value, and then format
    it again).
    
    This pull request moves the control's padding parser into cppwinrt_utils
    (for lack of a better place) and makes the settings UI use it to parse
    the padding out into a `Thickness` as early as possible. Then, the
    controls operate directly on the Thickness' members rather than parsing
    the padding string again.
    
    To handle two-way serialization properly, we also required a function
    that converts a thickness back into a reduced string representation
    (i.e. when all four values are N, it will return "N").
    
    As a bonus, this pull request also:
    - removes another use of `std::getline`
    - fixes an issue where resetting the padding would change it
    (infinitesimally) and cause it to be set again
    - adds a readout of the current padding value in the expander itself
    - removes `MaxValueFromPaddingString`, which was apparently unused

--- src/cascadia/TerminalSettingsEditor/ProfileViewModel.h
+++ src/cascadia/TerminalSettingsEditor/ProfileViewModel.h
@@ -177,6 +177,8 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         Editor::AppearanceViewModel _defaultAppearanceViewModel;
         Windows::UI::Core::CoreDispatcher _dispatcher;
 
+        winrt::Windows::UI::Xaml::Thickness _parsedPadding;
+
         void _InitializeCurrentBellSounds();
         void _PrepareModelForBellSoundModification();
         void _MarkDuplicateBellSoundDirectories();
@@ -187,17 +189,6 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
 
         Model::CascadiaSettings _appSettings;
         Editor::AppearanceViewModel _unfocusedAppearanceViewModel;
-
-        enum class PaddingDirection
-        {
-            Left = 0,
-            Top = 1,
-            Right = 2,
-            Bottom = 3
-        };
-
-        winrt::hstring _GetNewPadding(PaddingDirection paddingDirection, double newPaddingValue) const;
-        double _GetPaddingValue(PaddingDirection paddingDirection) const;
         void _UpdateBuiltInIcons();
         void _DeduceCurrentIconType();
         void _DeduceCurrentBuiltInIcon();
