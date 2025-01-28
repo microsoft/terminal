c60a006e778e014fe8a3ae8b0bcf59886f2f939c
Dustin L. Howett <duhowett@microsoft.com>
2025-01-28 15:33:50 -0600

    build: change how we find  the latest VC tools version (#18468)
    Apparently, we were using the package containing the CRT _source code_
    to determine the version of the tools.
    
    Also apparently, VS does not guarantee that that package has the same
    version as the tools package.
    
    We should use the version of the tools package instead.
    
    (cherry picked from commit 8e4da6e938430e11535e444032cfa2c32a6ab1a2)
    Service-Card-Id: PVTI_lADOAF3p4s4AmhmszgWyLAM
    Service-Version: 1.21

--- build/scripts/Set-LatestVCToolsVersion.ps1
+++ build/scripts/Set-LatestVCToolsVersion.ps1
@@ -1,6 +1,6 @@
 $VSInstances = ([xml](& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -include packages -format xml))
 $VSPackages = $VSInstances.instances.instance.packages.package
-$LatestVCPackage = ($VSInstances.instances.instance.packages.package | ? { $_.id -eq "Microsoft.VisualCpp.CRT.Source" })
+$LatestVCPackage = ($VSInstances.instances.instance.packages.package | ? { $_.id -eq "Microsoft.VisualCpp.Tools.Core" })
 $LatestVCToolsVersion = $LatestVCPackage.version;
 
 Write-Output "Latest VCToolsVersion: $LatestVCToolsVersion"
