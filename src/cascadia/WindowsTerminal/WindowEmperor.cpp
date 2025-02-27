microsoft/terminal

commit e1be2f4c73b8a8d55e07a9499a72d7b943ac3fe7

Fix persistence of the last closed window (#18623)
Does what it says on the tin.

Closes #18525

## Validation Steps Performed
* Enable persistence
* Close the last window
* Persisted ✅

src/cascadia/WindowsTerminal/WindowEmperor.cpp
@@ -354,7 +354,10 @@ void WindowEmperor::HandleCommandlineArgs(int nCmdShow)
         }
 
         // If we created no windows, e.g. because the args are "/?" we can just exit now.
-        _postQuitMessageIfNeeded();
+        if (_windows.empty())
+        {
+            _postQuitMessageIfNeeded();
+        }
     }
 
     // ALWAYS change the _real_ CWD of the Terminal to system32,
@@ -735,9 +738,30 @@ void WindowEmperor::_createMessageWindow(const wchar_t* className)
     StringCchCopy(_notificationIcon.szTip, ARRAYSIZE(_notificationIcon.szTip), appNameLoc.c_str());
 }
 
+// Counterpart to _postQuitMessageIfNeeded:
+// If it returns true, don't close that last window, if any.
+// This ensures we persist the last window.
+bool WindowEmperor::_shouldSkipClosingWindows() const
+{
+    const auto globalSettings = _app.Logic().Settings().GlobalSettings();
+    const size_t windowLimit = globalSettings.ShouldUsePersistedLayout() ? 1 : 0;
+    return _windows.size() <= windowLimit;
+}
+
+// Posts a WM_QUIT as soon as we have no reason to exist anymore.
+// That basically means no windows [^1] and no message boxes.
+//
+// [^1] Unless:
+// * We've been asked to persist the last remaining window
+//   in which case we exit with 1 remaining window.
+// * We're allowed to be headless
+//   in which case we never exit.
 void WindowEmperor::_postQuitMessageIfNeeded() const
 {
-    if (_messageBoxCount <= 0 && _windows.empty() && !_app.Logic().Settings().GlobalSettings().AllowHeadless())
+    const auto globalSettings = _app.Logic().Settings().GlobalSettings();
+    const size_t windowLimit = globalSettings.ShouldUsePersistedLayout() ? 1 : 0;
+
+    if (_messageBoxCount <= 0 && _windows.size() <= windowLimit && !globalSettings.AllowHeadless())
     {
         PostQuitMessage(0);
     }
@@ -771,17 +795,20 @@ LRESULT WindowEmperor::_messageHandler(HWND window, UINT const message, WPARAM c
         {
         case WM_CLOSE_TERMINAL_WINDOW:
         {
-            const auto host = reinterpret_cast<AppHost*>(lParam);
-            auto it = _windows.begin();
-            const auto end = _windows.end();
-
-            for (; it != end; ++it)
+            if (!_shouldSkipClosingWindows())
             {
-                if (host == it->get())
+                const auto host = reinterpret_cast<AppHost*>(lParam);
+                auto it = _windows.begin();
+                const auto end = _windows.end();
+
+                for (; it != end; ++it)
                 {
-                    host->Close();
-                    _windows.erase(it);
-                    break;
+                    if (host == it->get())
+                    {
+                        host->Close();
+                        _windows.erase(it);
+                        break;
+                    }
                 }
             }
 

src/cascadia/WindowsTerminal/WindowEmperor.h
@@ -55,6 +55,7 @@ private:
     safe_void_coroutine _dispatchCommandlineCurrentDesktop(winrt::TerminalApp::CommandlineArgs args);
     LRESULT _messageHandler(HWND window, UINT message, WPARAM wParam, LPARAM lParam) noexcept;
     void _createMessageWindow(const wchar_t* className);
+    bool _shouldSkipClosingWindows() const;
     void _postQuitMessageIfNeeded() const;
     safe_void_coroutine _showMessageBox(winrt::hstring message, bool error);
     void _notificationAreaMenuRequested(WPARAM wParam);

commit 455dc443e7a03cf578d11911449348fab8a955f2

Fix bugs introduced in #18623

src/cascadia/WindowsTerminal/WindowEmperor.cpp
@@ -246,6 +246,8 @@ void WindowEmperor::CreateNewWindow(winrt::TerminalApp::WindowRequestedArgs args
 
     auto host = std::make_shared<AppHost>(this, _app.Logic(), std::move(args));
     host->Initialize();
+
+    _windowCount += 1;
     _windows.emplace_back(std::move(host));
 }
 
@@ -354,10 +356,7 @@ void WindowEmperor::HandleCommandlineArgs(int nCmdShow)
         }
 
         // If we created no windows, e.g. because the args are "/?" we can just exit now.
-        if (_windows.empty())
-        {
-            _postQuitMessageIfNeeded();
-        }
+        _postQuitMessageIfNeeded();
     }
 
     // ALWAYS change the _real_ CWD of the Terminal to system32,
@@ -738,30 +737,14 @@ void WindowEmperor::_createMessageWindow(const wchar_t* className)
     StringCchCopy(_notificationIcon.szTip, ARRAYSIZE(_notificationIcon.szTip), appNameLoc.c_str());
 }
 
-// Counterpart to _postQuitMessageIfNeeded:
-// If it returns true, don't close that last window, if any.
-// This ensures we persist the last window.
-bool WindowEmperor::_shouldSkipClosingWindows() const
-{
-    const auto globalSettings = _app.Logic().Settings().GlobalSettings();
-    const size_t windowLimit = globalSettings.ShouldUsePersistedLayout() ? 1 : 0;
-    return _windows.size() <= windowLimit;
-}
-
 // Posts a WM_QUIT as soon as we have no reason to exist anymore.
-// That basically means no windows [^1] and no message boxes.
-//
-// [^1] Unless:
-// * We've been asked to persist the last remaining window
-//   in which case we exit with 1 remaining window.
-// * We're allowed to be headless
-//   in which case we never exit.
+// That basically means no windows and no message boxes.
 void WindowEmperor::_postQuitMessageIfNeeded() const
 {
-    const auto globalSettings = _app.Logic().Settings().GlobalSettings();
-    const size_t windowLimit = globalSettings.ShouldUsePersistedLayout() ? 1 : 0;
-
-    if (_messageBoxCount <= 0 && _windows.size() <= windowLimit && !globalSettings.AllowHeadless())
+    if (
+        _messageBoxCount <= 0 &&
+        _windowCount <= 0 &&
+        !_app.Logic().Settings().GlobalSettings().AllowHeadless())
     {
         PostQuitMessage(0);
     }
@@ -795,8 +778,20 @@ LRESULT WindowEmperor::_messageHandler(HWND window, UINT const message, WPARAM c
         {
         case WM_CLOSE_TERMINAL_WINDOW:
         {
-            if (!_shouldSkipClosingWindows())
+            const auto globalSettings = _app.Logic().Settings().GlobalSettings();
+            // Keep the last window in the array so that we can persist it on exit.
+            // We check for AllowHeadless(), as that being true prevents us from ever quitting in the first place.
+            // (= If we avoided closing the last window you wouldn't be able to reach a headless state.)
+            const auto shouldKeepWindow =
+                _windows.size() == 1 &&
+                globalSettings.ShouldUsePersistedLayout() &&
+                !globalSettings.AllowHeadless();
+
+            if (!shouldKeepWindow)
             {
+                // Did the window counter get out of sync? It shouldn't.
+                assert(_windowCount == gsl::narrow_cast<int32_t>(_windows.size()));
+
                 const auto host = reinterpret_cast<AppHost*>(lParam);
                 auto it = _windows.begin();
                 const auto end = _windows.end();
@@ -812,6 +807,8 @@ LRESULT WindowEmperor::_messageHandler(HWND window, UINT const message, WPARAM c
                 }
             }
 
+            // Counterpart specific to CreateNewWindow().
+            _windowCount -= 1;
             _postQuitMessageIfNeeded();
             return 0;
         }

src/cascadia/WindowsTerminal/WindowEmperor.h
@@ -78,6 +78,7 @@ private:
     bool _forcePersistence = false;
     bool _needsPersistenceCleanup = false;
     std::optional<bool> _currentSystemThemeIsDark;
+    int32_t _windowCount = 0;
     int32_t _messageBoxCount = 0;
 
 #ifdef NDEBUG