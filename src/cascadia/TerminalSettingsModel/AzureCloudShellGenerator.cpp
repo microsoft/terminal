9842bdc71497dd5334fe71a0b911f45ea8a04bab
Carlos Zamora <carlos.zamora@microsoft.com>
2025-02-27 17:49:02 -0800

    Introduce PowerShell installer stub

--- src/cascadia/TerminalSettingsModel/AzureCloudShellGenerator.cpp
+++ src/cascadia/TerminalSettingsModel/AzureCloudShellGenerator.cpp
@@ -31,7 +31,7 @@ std::wstring_view AzureCloudShellGenerator::GetDisplayName() const noexcept
 // - <none>
 // Return Value:
 // - a vector with the Azure Cloud Shell connection profile, if available.
-void AzureCloudShellGenerator::GenerateProfiles(std::vector<winrt::com_ptr<implementation::Profile>>& profiles) const
+void AzureCloudShellGenerator::GenerateProfiles(std::vector<winrt::com_ptr<implementation::Profile>>& profiles)
 {
     if (AzureConnection::IsAzureConnectionAvailable())
     {
