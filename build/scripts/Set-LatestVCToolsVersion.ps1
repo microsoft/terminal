3591d513237de4903efde95c6b893ca6783c8be4
Dustin L. Howett <dustin@howett.net>
2025-06-02 12:48:10 -0500

    ok sure comment it

--- build/scripts/Set-LatestVCToolsVersion.ps1
+++ build/scripts/Set-LatestVCToolsVersion.ps1
@@ -6,6 +6,10 @@ $LatestVCToolsVersion = $LatestVCPackage.version;
 $VSRoot = (& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property 'resolvedInstallationPath')
 $VCToolsRoot = Join-Path $VSRoot "VC\Tools\MSVC"
 
+# We have observed a few instances where the VC tools package version actually
+# differs from the version on the files themselves. We might as well check
+# whether the version we just found _actually exists_ before we use it.
+# We'll use whichever highest version exists.
 $PackageVCToolPath = Join-Path $VCToolsRoot $LatestVCToolsVersion
 If ($Null -Eq (Get-Item $PackageVCToolPath -ErrorAction:Ignore)) {
     $VCToolsVersions = Get-ChildItem $VCToolsRoot | ForEach-Object {
