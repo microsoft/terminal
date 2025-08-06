microsoft/terminal

commit c0f9a198c62d23dfcd9912c8f2506f411ed84bf6

Rewrite media resource handling (relative path icons, web URLs) (#19143)
This pull request broadly rewrites how we handle all media resources in
the Terminal settings model.

## What is a media resource?

A media resource is any JSON property that refers to a file on disk,
including:

- `icon` on profile
- `backgroundImage` on profile (appearance)
- `pixelShaderPath` and `pixelShaderImagePath` on profile (appearance)
- `icon` on command and the new tab menu entries

The last two bullet points were newly discovered during the course of
this work.

## Description of Changes

In every place the settings model used to store a string for a media
path, it now stores an `IMediaResource`.

A media resource must be _resolved_ before it's used. When resolved, it
can report whether it is `Ok` (found, valid) and what the final
normalized path was.

This allows the settings model to apply some new behaviors.

One of those new behaviors is resolving media paths _relative to the
JSON file that referred to them._ This means fragments and user settings
can now contain _local_ images, pixel shaders and more and refer to them
by filename.

Relative path support requires us to track the path from which every
media resource "container" was read[^2]. For "big" objects like Profile,
we track it directly in the object and for each layer. This means that
fragments **updating** a profile pass their relative base path into the
mix. For some of the entries such as those in `newTabMenu`, we just wing
it (#19191). For everything that is recursively owned by a parent that
has a path (say each Command inside an ActionMap), we pass it in from
the parent during media resolution.

During resolution, we now track _exactly which layer_ an icon,
background image, or pixel shader path came from and read the "base
path" from only that layer. The base path is not inherited.

Another new behavior is in the handling of web and other URLs.

src/cascadia/TerminalSettingsEditor/ProfileViewModel.cpp
@@ -11,6 +11,7 @@
 #include "../WinRTUtils/inc/Utils.h"
 #include "../../renderer/base/FontCache.h"
 #include "SegoeFluentIconList.h"
+#include "../../types/inc/utils.hpp"
 
 using namespace winrt::Windows::UI::Text;
 using namespace winrt::Windows::UI::Xaml;
@@ -94,6 +95,8 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
                 // _DeduceCurrentIconType() ends with a "CurrentIconType" notification
                 //  so we don't need to call _UpdateIconPreview() here
                 _DeduceCurrentIconType();
+                // The icon changed; let's re-evaluate it with its new context.
+                _appSettings.ResolveMediaResources();
             }
             else if (viewModelProperty == L"CurrentIconType")
             {
@@ -109,11 +112,11 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
             }
             else if (viewModelProperty == L"CurrentBuiltInIcon")
             {
-                Icon(unbox_value<hstring>(_CurrentBuiltInIcon.as<Editor::EnumEntry>().EnumValue()));
+                IconPath(unbox_value<hstring>(_CurrentBuiltInIcon.as<Editor::EnumEntry>().EnumValue()));
             }
             else if (viewModelProperty == L"CurrentEmojiIcon")
             {
-                Icon(CurrentEmojiIcon());
+                IconPath(CurrentEmojiIcon());
             }
             else if (viewModelProperty == L"CurrentBellSounds")
             {
@@ -182,17 +185,17 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
 
     void ProfileViewModel::_DeduceCurrentIconType()
     {
-        const auto& profileIcon = _profile.Icon();
+        const auto profileIcon = IconPath();
         if (profileIcon == HideIconValue)
         {
             _currentIconType = _IconTypes.GetAt(0);
         }
-        else if (L"\uE700" <= profileIcon && profileIcon <= L"\uF8B3")
+        else if (profileIcon.size() == 1 && (L'\uE700' <= til::at(profileIcon, 0) && til::at(profileIcon, 0) <= L'\uF8B3'))
         {
             _currentIconType = _IconTypes.GetAt(1);
             _DeduceCurrentBuiltInIcon();
         }
-        else if (profileIcon.size() <= 2)
+        else if (::Microsoft::Console::Utils::IsLikelyToBeEmojiOrSymbolIcon(profileIcon))
         {
             // We already did a range check for MDL2 Assets in the previous one,
             // so if we're out of that range but still short, assume we're an emoji
@@ -211,7 +214,7 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         {
             _UpdateBuiltInIcons();
         }
-        const auto& profileIcon = Icon();
+        const auto profileIcon = IconPath();
         for (uint32_t i = 0; i < _BuiltInIcons.Size(); i++)
         {
             const auto& builtIn = _BuiltInIcons.GetAt(i);
@@ -282,6 +285,9 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
     }
     Model::TerminalSettings ProfileViewModel::TermSettings() const
     {
+        // This may look pricey, but it only resolves resources that have not been visited
+        // and the preview update is debounced.
+        _appSettings.ResolveMediaResources();
         return Model::TerminalSettings::CreateForPreview(_appSettings, _profile);
     }
 
@@ -539,7 +545,7 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         {
             return RS_(L"Profile_IconTypeNone");
         }
-        return Icon();
+        return IconPath(); // For display as a string
     }
 
     Windows::UI::Xaml::Controls::IconElement ProfileViewModel::IconPreview() const
@@ -561,7 +567,7 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
                 // Stash the current value of Icon. If the user
                 // switches out of then back to IconType::Image, we want
                 // the path that we display in the text box to remain unchanged.
-                _lastIconPath = Icon();
+                _lastIconPath = IconPath();
             }
 
             // Set the member here instead of after setting Icon() below!
@@ -576,7 +582,7 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
             {
             case IconType::None:
             {
-                _profile.Icon(HideIconValue);
+                IconPath(winrt::hstring{ HideIconValue });
                 break;
             }
             case IconType::Image:
@@ -585,7 +591,7 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
                 {
                     // Conversely, if we switch to Image,
                     // retrieve that saved value and apply it
-                    _profile.Icon(_lastIconPath);
+                    IconPath(_lastIconPath);
                 }
                 break;
             }
@@ -593,7 +599,7 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
             {
                 if (_CurrentBuiltInIcon)
                 {
-                    _profile.Icon(unbox_value<hstring>(_CurrentBuiltInIcon.as<Editor::EnumEntry>().EnumValue()));
+                    IconPath(unbox_value<hstring>(_CurrentBuiltInIcon.as<Editor::EnumEntry>().EnumValue()));
                 }
                 break;
             }

src/cascadia/TerminalSettingsEditor/ProfileViewModel.h
@@ -81,7 +81,7 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
 
         winrt::hstring EvaluatedIcon() const
         {
-            return _profile.EvaluatedIcon();
+            return _profile.Icon().Resolved();
         }
         Windows::Foundation::IInspectable CurrentIconType() const noexcept
         {
@@ -94,6 +94,12 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         bool UsingBuiltInIcon() const;
         bool UsingEmojiIcon() const;
         bool UsingImageIcon() const;
+        winrt::hstring IconPath() const { return _profile.Icon().Path(); }
+        void IconPath(const winrt::hstring& path)
+        {
+            Icon(Model::MediaResourceHelper::FromString(path));
+            _NotifyChanges(L"Icon");
+        }
 
         // starting directory
         hstring CurrentStartingDirectoryPreview() const;

commit 6c2f38c73263b0afa10ceb1618908789097a4241

Profiles Editor: make sure we notify the IconPath property (#19223)
There were instances where changing the icon or resetting it did not
result in the text box changing.

Regressed in #19143

src/cascadia/TerminalSettingsEditor/ProfileViewModel.cpp
@@ -108,6 +108,7 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
                                L"UsingImageIcon",
                                L"LocalizedIcon",
                                L"IconPreview",
+                               L"IconPath",
                                L"EvaluatedIcon");
             }
             else if (viewModelProperty == L"CurrentBuiltInIcon")

src/cascadia/TerminalSettingsEditor/ProfileViewModel.h
@@ -98,7 +98,7 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         void IconPath(const winrt::hstring& path)
         {
             Icon(Model::MediaResourceHelper::FromString(path));
-            _NotifyChanges(L"Icon");
+            _NotifyChanges(L"Icon", L"IconPath");
         }
 
         // starting directory