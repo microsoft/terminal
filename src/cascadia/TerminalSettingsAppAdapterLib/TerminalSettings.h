6efcf47956861757def5a697456f9f51b0a3e397
Dustin L. Howett <duhowett@microsoft.com>
2025-08-20 00:08:27 -0500

    TSAAL: build using ControlProperties

--- src/cascadia/TerminalSettingsAppAdapterLib/TerminalSettings.h
+++ src/cascadia/TerminalSettingsAppAdapterLib/TerminalSettings.h
@@ -15,6 +15,7 @@ Author(s):
 #pragma once
 
 #include <../TerminalSettingsModel/IInheritable.h>
+#include <../inc/ControlProperties.h>
 #include <DefaultSettings.h>
 #include <conattrs.hpp>
 
@@ -72,37 +73,8 @@ namespace winrt::Microsoft::Terminal::Settings
 
         void ApplyColorScheme(const Model::ColorScheme& scheme);
 
-        // --------------------------- Core Settings ---------------------------
-        //  All of these settings are defined in ICoreSettings.
-
         void GetColorTable(winrt::com_array<Microsoft::Terminal::Core::Color>& table) noexcept;
         void SetColorTable(std::array<Microsoft::Terminal::Core::Color, 16> colors);
-
-        SIMPLE_INHERITABLE_SETTING(til::color, DefaultForeground, DEFAULT_FOREGROUND);
-        SIMPLE_INHERITABLE_SETTING(til::color, DefaultBackground, DEFAULT_BACKGROUND);
-        SIMPLE_INHERITABLE_SETTING(til::color, SelectionBackground, DEFAULT_FOREGROUND);
-        SIMPLE_INHERITABLE_SETTING(int32_t, HistorySize, DEFAULT_HISTORY_SIZE);
-        SIMPLE_INHERITABLE_SETTING(int32_t, InitialRows, 30);
-        SIMPLE_INHERITABLE_SETTING(int32_t, InitialCols, 80);
-
-        SIMPLE_INHERITABLE_SETTING(bool, SnapOnInput, true);
-        SIMPLE_INHERITABLE_SETTING(bool, AltGrAliasing, true);
-        SIMPLE_INHERITABLE_SETTING(hstring, AnswerbackMessage);
-        SIMPLE_INHERITABLE_SETTING(til::color, CursorColor, DEFAULT_CURSOR_COLOR);
-        SIMPLE_INHERITABLE_SETTING(Microsoft::Terminal::Core::CursorStyle, CursorShape, Core::CursorStyle::Vintage);
-        SIMPLE_INHERITABLE_SETTING(uint32_t, CursorHeight, DEFAULT_CURSOR_HEIGHT);
-        SIMPLE_INHERITABLE_SETTING(hstring, WordDelimiters, DEFAULT_WORD_DELIMITERS);
-        SIMPLE_INHERITABLE_SETTING(bool, CopyOnSelect, false);
-        SIMPLE_INHERITABLE_SETTING(Microsoft::Terminal::Control::CopyFormat, CopyFormatting, 0);
-        SIMPLE_INHERITABLE_SETTING(bool, FocusFollowMouse, false);
-        SIMPLE_INHERITABLE_SETTING(bool, ScrollToZoom, true);
-        SIMPLE_INHERITABLE_SETTING(bool, ScrollToChangeOpacity, true);
-        SIMPLE_INHERITABLE_SETTING(bool, AllowVtChecksumReport, false);
-        SIMPLE_INHERITABLE_SETTING(bool, TrimBlockSelection, true);
-        SIMPLE_INHERITABLE_SETTING(bool, DetectURLs, true);
-        SIMPLE_INHERITABLE_SETTING(bool, AllowVtClipboardWrite, true);
-
-        SIMPLE_INHERITABLE_SETTING(Windows::Foundation::IReference<Microsoft::Terminal::Core::Color>, TabColor, nullptr);
 
         // When set, StartingTabColor allows to create a terminal with a "sticky" tab color.
         // This color is prioritized above the TabColor (that is usually initialized based on profile settings).
@@ -112,73 +84,20 @@ namespace winrt::Microsoft::Terminal::Settings
         // TODO: to ensure that this property is not populated during settings reload,
         // we should consider moving this property to a separate interface,
         // passed to the terminal only upon creation.
-        SIMPLE_INHERITABLE_SETTING(Windows::Foundation::IReference<Microsoft::Terminal::Core::Color>, StartingTabColor, nullptr);
 
-        SIMPLE_INHERITABLE_SETTING(bool, IntenseIsBold);
-        SIMPLE_INHERITABLE_SETTING(bool, IntenseIsBright);
-
-        SIMPLE_INHERITABLE_SETTING(Microsoft::Terminal::Core::AdjustTextMode, AdjustIndistinguishableColors, Core::AdjustTextMode::Never);
-        SIMPLE_INHERITABLE_SETTING(bool, RainbowSuggestions, false);
-
-        // ------------------------ End of Core Settings -----------------------
-
-        SIMPLE_INHERITABLE_SETTING(guid, SessionId);
-        SIMPLE_INHERITABLE_SETTING(bool, EnableUnfocusedAcrylic, false);
-        SIMPLE_INHERITABLE_SETTING(bool, UseAcrylic, false);
+        CORE_APPEARANCE_SETTINGS(SIMPLE_INHERITABLE_SETTING);
+        CONTROL_APPEARANCE_SETTINGS(SIMPLE_INHERITABLE_SETTING);
+        CORE_SETTINGS(SIMPLE_INHERITABLE_SETTING);
+        CONTROL_SETTINGS(SIMPLE_INHERITABLE_SETTING);
+#if 0
         SIMPLE_INHERITABLE_SETTING(float, Opacity, UseAcrylic() ? 0.5f : 1.0f);
-        SIMPLE_INHERITABLE_SETTING(hstring, Padding, DEFAULT_PADDING);
         SIMPLE_INHERITABLE_SETTING(hstring, FontFace, DEFAULT_FONT_FACE);
-        SIMPLE_INHERITABLE_SETTING(float, FontSize, DEFAULT_FONT_SIZE);
+#endif
 
-        SIMPLE_INHERITABLE_SETTING(winrt::Windows::UI::Text::FontWeight, FontWeight);
-        SIMPLE_INHERITABLE_SETTING(IFontAxesMap, FontAxes);
-        SIMPLE_INHERITABLE_SETTING(IFontFeatureMap, FontFeatures);
-        SIMPLE_INHERITABLE_SETTING(bool, EnableBuiltinGlyphs, true);
-        SIMPLE_INHERITABLE_SETTING(bool, EnableColorGlyphs, true);
-        SIMPLE_INHERITABLE_SETTING(hstring, CellWidth);
-        SIMPLE_INHERITABLE_SETTING(hstring, CellHeight);
-
-        SIMPLE_INHERITABLE_SETTING(hstring, BackgroundImage);
-        SIMPLE_INHERITABLE_SETTING(float, BackgroundImageOpacity, 1.0f);
-
-        SIMPLE_INHERITABLE_SETTING(winrt::Windows::UI::Xaml::Media::Stretch, BackgroundImageStretchMode, winrt::Windows::UI::Xaml::Media::Stretch::UniformToFill);
-        SIMPLE_INHERITABLE_SETTING(winrt::Windows::UI::Xaml::HorizontalAlignment, BackgroundImageHorizontalAlignment, winrt::Windows::UI::Xaml::HorizontalAlignment::Center);
-        SIMPLE_INHERITABLE_SETTING(winrt::Windows::UI::Xaml::VerticalAlignment, BackgroundImageVerticalAlignment, winrt::Windows::UI::Xaml::VerticalAlignment::Center);
-
-        SIMPLE_INHERITABLE_SETTING(Microsoft::Terminal::Control::IKeyBindings, KeyBindings, nullptr);
-
-        SIMPLE_INHERITABLE_SETTING(hstring, Commandline);
-        SIMPLE_INHERITABLE_SETTING(hstring, StartingDirectory);
-        SIMPLE_INHERITABLE_SETTING(hstring, StartingTitle);
-        SIMPLE_INHERITABLE_SETTING(bool, SuppressApplicationTitle);
-        SIMPLE_INHERITABLE_SETTING(IEnvironmentVariableMap, EnvironmentVariables);
-
-        SIMPLE_INHERITABLE_SETTING(Microsoft::Terminal::Control::ScrollbarState, ScrollState, Microsoft::Terminal::Control::ScrollbarState::Visible);
-
-        SIMPLE_INHERITABLE_SETTING(Microsoft::Terminal::Control::TextAntialiasingMode, AntialiasingMode, Microsoft::Terminal::Control::TextAntialiasingMode::Grayscale);
-
-        SIMPLE_INHERITABLE_SETTING(bool, RetroTerminalEffect, false);
-        SIMPLE_INHERITABLE_SETTING(Microsoft::Terminal::Control::GraphicsAPI, GraphicsAPI);
-        SIMPLE_INHERITABLE_SETTING(bool, DisablePartialInvalidation, false);
-        SIMPLE_INHERITABLE_SETTING(bool, SoftwareRendering, false);
-        SIMPLE_INHERITABLE_SETTING(Microsoft::Terminal::Control::TextMeasurement, TextMeasurement);
-        SIMPLE_INHERITABLE_SETTING(Microsoft::Terminal::Control::DefaultInputScope, DefaultInputScope);
-        SIMPLE_INHERITABLE_SETTING(bool, UseBackgroundImageForWindow, false);
-        SIMPLE_INHERITABLE_SETTING(bool, ForceVTInput, false);
-
-        SIMPLE_INHERITABLE_SETTING(hstring, PixelShaderPath);
-        SIMPLE_INHERITABLE_SETTING(hstring, PixelShaderImagePath);
-
+        // Settings which do not pertain to the control (why are they here?)
         SIMPLE_INHERITABLE_SETTING(bool, Elevate, false);
-
-        SIMPLE_INHERITABLE_SETTING(bool, AutoMarkPrompts, false);
-        SIMPLE_INHERITABLE_SETTING(bool, ShowMarks, false);
-        SIMPLE_INHERITABLE_SETTING(bool, RightClickContextMenu, false);
-        SIMPLE_INHERITABLE_SETTING(bool, RepositionCursorWithMouse, false);
-
+        SIMPLE_INHERITABLE_SETTING(IEnvironmentVariableMap, EnvironmentVariables);
         SIMPLE_INHERITABLE_SETTING(bool, ReloadEnvironmentVariables, true);
-
-        SIMPLE_INHERITABLE_SETTING(Microsoft::Terminal::Control::PathTranslationStyle, PathTranslationStyle, Microsoft::Terminal::Control::PathTranslationStyle::None);
 
     private:
         std::optional<std::array<Microsoft::Terminal::Core::Color, COLOR_TABLE_SIZE>> _ColorTable;
