98d1433e6c73490de5213667b68683d927c92278
Dustin L. Howett <duhowett@microsoft.com>
2025-12-03 23:20:16 -0600

    WpfTest: Add a more complicated test string

--- src/cascadia/WpfTerminalTestNetCore/MainWindow.xaml.cs
+++ src/cascadia/WpfTerminalTestNetCore/MainWindow.xaml.cs
@@ -11,6 +11,29 @@ namespace WpfTerminalTestNetCore
 {
     public class EchoConnection : Microsoft.Terminal.Wpf.ITerminalConnection
     {
+        const string vtClear = "\x1b[H\x1b[2J\x1b[3J";
+        const string ST = "\x1b\\";
+        const string CSI = "\x1b[";
+        const string OSC = "\x1b]";
+        const string funPreamble =
+            CSI + "38;5;118mT" + CSI + "0m" + CSI + "38;5;154mh" + CSI + "0m" + CSI + "38;5;154ma" + CSI + "0m" + CSI + "38;5;154mn" + CSI + "0m" + CSI + "38;5;148mk" + CSI + "0m" + CSI + "38;5;184ms" + CSI + "0m" + CSI + "38;5;184m " + CSI + "0m" + CSI + "38;5;184mf" + CSI + "0m" + CSI + "38;5;178mo" + CSI + "0m" + CSI + "38;5;214mr" + CSI + "0m" + CSI + "38;5;214m " + CSI + "0m" + CSI + "38;5;214mv" + CSI + "0m" + CSI + "38;5;208mi" + CSI + "0m" + CSI + "38;5;208ms" + CSI + "0m" + CSI + "38;5;208mi" + CSI + "0m" + CSI + "38;5;203mt" + CSI + "0m" + CSI + "38;5;203mi" + CSI + "0m" + CSI + "38;5;203mn" + CSI + "0m" + CSI + "38;5;203mg" + CSI + "0m" + CSI + "38;5;198m " + CSI + "0m" + CSI + "38;5;198mm" + CSI + "0m" + CSI + "38;5;198my" + CSI + "0m" + CSI + "38;5;199m " + CSI + "0m" + CSI + "38;5;199mw" + CSI + "0m" + CSI + "38;5;199me" + CSI + "0m" + CSI + "38;5;163mb" + CSI + "0m" + CSI + "38;5;164ms" + CSI + "0m" + CSI + "38;5;164mi" + CSI + "0m" + CSI + "38;5;164mt" + CSI + "0m" + CSI + "38;5;128me" + CSI + "0m" + CSI + "38;5;129m!" + CSI + "0m\r\n" +
+            "\r\n" +
+            "This is my cool website about terminals\r\n" +
+            "Here is a list of cool terminals\r\n" +
+            "* Windows Terminal (https://github.com/microsoft/terminal)\r\n" +
+            "\r\n" +
+            CSI + "5m[UNDER CONSTRUCTION]" + CSI + "m\r\n" +
+            "\r\n" +
+            "You are visitor number " + CSI + "53;4;92m00002" + CSI + "m to my website.\r\n" +
+            "\r\n" +
+            CSI + "38;5;244m" + CSI + "53m This is part of the Cool Terminals webring! " + CSI + "55m\r\n" +
+            "\r\n" +
+            " Visit other sites in the web ring...        \r\n" +
+            "\r\n" +
+            "       " + CSI + "94m" + OSC + "8;id=prev;https://terminal.site/?previous=1" + ST + "Previous" + OSC + "8;;" + ST + "" + CSI + "m     " + CSI + "94m" + OSC + "8;id=rand;https://terminal.site/?random=1" + ST + "Random" + OSC + "8;;" + ST + "" + CSI + "m     " + CSI + "94m" + OSC + "8;id=next;https://terminal.site/?next=1" + ST + "Next" + OSC + "8;;" + ST + "" + CSI + "m \r\n" +
+            "       " + CSI + "94m" + OSC + "8;id=prev;https://terminal.site/?previous=1" + ST + "  Site  " + OSC + "8;;" + ST + "" + CSI + "m                " + CSI + "94m" + OSC + "8;id=next;https://terminal.site/?next=1" + ST + "Site" + OSC + "8;;" + ST + "" + CSI + "m \r\n" +
+            CSI + "4;38;5;244m                                             " + CSI + "m\r\n";
+
         public event EventHandler<TerminalOutputEventArgs> TerminalOutput;
 
         public void Resize(uint rows, uint columns)
@@ -18,10 +41,16 @@ namespace WpfTerminalTestNetCore
             return;
         }
 
+        private void _preamble()
+        {
+            _print(vtClear);
+            _print(funPreamble);
+            _print("^A: prt esc ^B: sgrmouse ^C: win32im ^D: again!\r\n");
+            return;
+        }
         public void Start()
         {
-            TerminalOutput.Invoke(this, new TerminalOutputEventArgs("ECHO CONNECTION\r\n^A: toggle printable ESC\r\n^B: toggle SGR mouse mode\r\n^C: toggle win32 input mode\r\n\r\n"));
-            return;
+            _preamble();
         }
 
         private bool _escapeMode;
@@ -62,16 +91,25 @@ namespace WpfTerminalTestNetCore
                     TerminalOutput.Invoke(this, new TerminalOutputEventArgs($"Printable ESC mode: {_escapeMode}\r\n"));
                 }
             }
+            else if (data[0] == '\x04')
+            {
+                _preamble();
+            }
             else
             {
                 // Echo back to the terminal, but make backspace/newline work properly.
                 var str = data.Replace("\r", "\r\n").Replace("\x7f", "\x08 \x08");
-                if (_escapeMode)
-                {
-                    str = str.Replace("\x1b", "\u241b");
-                }
-                TerminalOutput.Invoke(this, new TerminalOutputEventArgs(str));
+                _print(str);
             }
+        }
+
+        private void _print(string str)
+        {
+            if (_escapeMode)
+            {
+                str = str.Replace("\x1b", "\u241b");
+            }
+            TerminalOutput.Invoke(this, new TerminalOutputEventArgs(str));
         }
 
         public void Close()
