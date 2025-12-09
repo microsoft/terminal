8b8f1be674889894c882e96e88857997511e0eb0
Dustin L. Howett <dustin@howett.net>
2025-12-09 15:44:01 -0600

    Put Atlas Engine Errors behind a feautre flag

--- src/renderer/atlas/AtlasEngine.r.cpp
+++ src/renderer/atlas/AtlasEngine.r.cpp
@@ -61,18 +61,19 @@ catch (const wil::ResultException& exception)
         return E_PENDING;
     }
 
-#ifndef NDEBUG
-    // We may fail to present repeatedly, e.g. if there's a short-term device failure.
-    // We should not bombard the consumer with repeated warning callbacks (where they may present a dialog to the user).
-    if (_p.warningCallback)
+    if constexpr (Feature_AtlasEngineLoudErrors::IsEnabled())
     {
-        try
+        // We may fail to present repeatedly, e.g. if there's a short-term device failure.
+        // We should not bombard the consumer with repeated warning callbacks (where they may present a dialog to the user).
+        if (_p.warningCallback)
         {
-            _p.warningCallback(hr, {});
+            try
+            {
+                _p.warningCallback(hr, {});
+            }
+            CATCH_LOG()
         }
-        CATCH_LOG()
     }
-#endif
 
     _b.reset();
     return hr;
