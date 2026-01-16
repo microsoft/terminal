5df57cd7c04980b694369c923d077963fca622a1
Dustin L. Howett <dustin@howett.net>
2026-01-16 14:01:09 -0600

    Protect more places against not having a XamlRoot
    We're seeing some crashes where we call GetFocusedElement(null).

--- src/cascadia/TerminalApp/TerminalWindow.cpp
+++ src/cascadia/TerminalApp/TerminalWindow.cpp
@@ -894,6 +894,11 @@ namespace winrt::TerminalApp::implementation
         {
             // Manually bubble the OnDirectKeyEvent event up through the focus tree.
             auto xamlRoot{ _root->XamlRoot() };
+            if (!xamlRoot)
+            {
+                return;
+            }
+
             auto focusedObject{ Windows::UI::Xaml::Input::FocusManager::GetFocusedElement(xamlRoot) };
             do
             {
