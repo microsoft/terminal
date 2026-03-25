f2bc2f0466dba6dd0cca9fd371e1593d3ef37cc5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-03-25 19:20:14 -0400

    spelling: the...that the
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/cascadia/TerminalApp/Pane.cpp
+++ src/cascadia/TerminalApp/Pane.cpp
@@ -2264,7 +2264,7 @@ SplitState Pane::_convertAutomaticOrDirectionalSplitState(const SplitDirection& 
 //   creates a new Pane to host the control, registers event handlers.
 // Arguments:
 // - splitType: what type of split we should create.
-// - splitSize: what fraction of the pane the new pane should get
+// - splitSize: the fraction of the pane that the new pane should get
 // - newPane: the pane to add as a child
 // Return Value:
 // - The two newly created Panes, with the original pane as the first pane.
