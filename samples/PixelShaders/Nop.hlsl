f33de21c78bc0c44c02defed64a180415c42091c
Ajay Singh Adhikari <137770330+AjaySinghAdhikari@users.noreply.github.com>
2026-06-05 23:46:57 +0530

    Enhance comments in Nop.hlsl pixel shader
    Updated comments for clarity and added explanations for parameters.

--- samples/PixelShaders/Nop.hlsl
+++ samples/PixelShaders/Nop.hlsl
@@ -1,17 +1,30 @@
 // Does nothing, serves as an example of a minimal pixel shader
+
+// The terminal graphics as a texture
 Texture2D shaderTexture;
 SamplerState samplerState;
 
+// Terminal settings such as the resolution of the texture
 cbuffer PixelShaderSettings {
+  // The number of seconds since the pixel shader was enabled
   float  Time;
+  // UI Scale
   float  Scale;
+  // Resolution of the shaderTexture
   float2 Resolution;
+  // Background color as rgba
   float4 Background;
 };
 
+// A pixel shader is a program that given a texture coordinate (tex) produces a color.
+// tex is an x,y tuple that ranges from 0,0 (top left) to 1,1 (bottom right).
+// Just ignore the pos parameter.
 float4 main(float4 pos : SV_POSITION, float2 tex : TEXCOORD) : SV_TARGET
 {
+    // Read the color value at the current texture coordinate (tex)
+    //  float4 is tuple of 4 floats, rgba
     float4 color = shaderTexture.Sample(samplerState, tex);
 
+    // Return the color unchanged
     return color;
 }
