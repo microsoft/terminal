e0a6bcf69c99d350937099b5e36ab8918d3439b2
Vadim <dev.kuzia15@gmail.com>
2026-08-07 23:16:48 +0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- src/cascadia/TerminalApp/AppCommandlineArgs.cpp
+++ src/cascadia/TerminalApp/AppCommandlineArgs.cpp
@@ -9,6 +9,53 @@
 using namespace winrt::Microsoft::Terminal::Settings::Model;
 using namespace TerminalApp;
 
+namespace
+{
+    std::string quoteCommandlineArg(const std::string& arg)
+    {
+        if (arg.empty())
+        {
+            return "\"\"";
+        }
+
+        if (arg.find_first_of(" \t\"") == std::string::npos)
+        {
+            return arg;
+        }
+
+        std::string result;
+        result.push_back('"');
+
+        size_t backslashes = 0;
+
+        for (const char ch : arg)
+        {
+            if (ch == '\\')
+            {
+                ++backslashes;
+                continue;
+            }
+
+            if (ch == '"')
+            {
+                result.append(backslashes * 2 + 1, '\\');
+                result.push_back('"');
+                backslashes = 0;
+                continue;
+            }
+
+            result.append(backslashes, '\\');
+            backslashes = 0;
+            result.push_back(ch);
+        }
+
+        result.append(backslashes * 2, '\\');
+        result.push_back('"');
+
+        return result;
+    }
+}
+
 // Either a ; at the start of a line, or a ; preceded by any non-\ char.
 const std::wregex AppCommandlineArgs::_commandDelimiterRegex{ LR"(^;|[^\\];)" };
 
@@ -572,14 +619,7 @@ void AppCommandlineArgs::_buildSaveSnippetParser()
                         cmdlineBuffer << ' ';
                     }
 
-                    if (arg.find(" ") != std::string::npos)
-                    {
-                        cmdlineBuffer << '"' << arg << '"';
-                    }
-                    else
-                    {
-                        cmdlineBuffer << arg;
-                    }
+                    cmdlineBuffer << quoteCommandlineArg(arg);
                 }
 
                 args.Commandline(winrt::to_hstring(cmdlineBuffer.str()));
@@ -680,14 +720,7 @@ NewTerminalArgs AppCommandlineArgs::_getNewTerminalArgs(AppCommandlineArgs::NewT
                 cmdlineBuffer << ' ';
             }
 
-            if (arg.find(" ") != std::string::npos)
-            {
-                cmdlineBuffer << '"' << arg << '"';
-            }
-            else
-            {
-                cmdlineBuffer << arg;
-            }
+            cmdlineBuffer << quoteCommandlineArg(arg);
         }
 
         args.Commandline(winrt::to_hstring(cmdlineBuffer.str()));
