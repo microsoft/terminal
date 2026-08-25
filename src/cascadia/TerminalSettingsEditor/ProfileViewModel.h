11e222c3b120b4a2969f3082831cb24fd1aeb516
Carlos Zamora <carlos.zamora@microsoft.com>
2026-08-25 12:46:33 -0700

    Introduce Profiles page to SUI (#20199)
    ## Summary of the Pull Request
    Updates the settings UI to introduce a new Profiles page. This page
    includes:
    - a "Profile Defaults" navigator
    - a "Color Schemes" navigator (removed top-level item in favor of this)
    - an "add profile" split button (removed AddProfile page in favor of
    this)
    - a navigator for each profile
      - tooltips were added for the "hidden"/"orphaned" info badge
    
    ## Validation Steps Performed
    Scenarios:
    - [x] settings search
       - [X] defaults
       - [X] color schemes
       - [X] setting in defaults
    - [X] breadcrumbs
    - [X] navigate to individual profiles (via search and not)
    - [X] update settings for a profile
    - [X] accessibility testing
    
    ## PR Checklist
    Related to #17000
    Stacked on #20232

--- src/cascadia/TerminalSettingsEditor/ProfileViewModel.h
+++ src/cascadia/TerminalSettingsEditor/ProfileViewModel.h
@@ -104,6 +104,8 @@ namespace winrt::Microsoft::Terminal::Settings::Editor::implementation
         bool RepositionCursorWithMouseAvailable() const noexcept;
 
         bool Orphaned() const;
+        hstring AccessibleStateDescription() const;
+        bool ShowHiddenBadge() const;
         hstring TabTitlePreview() const;
         hstring AnswerbackMessagePreview() const;
         hstring AnswerbackMessageAccessibleName() const;
